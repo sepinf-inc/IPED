e8f9ad30f2726c70591b522912a8aff3c8dbb2c4
Nassif <nassif.lfcn@dpf.gov.br>
2015-02-06 12:12:13 -0300

    javadoc

--- src/main/java/dpf/sp/gpinf/indexer/process/task/AbstractTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/AbstractTask.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 import dpf.sp.gpinf.indexer.process.Worker;
 
-/*
+/**
  * Classe que representa uma tarefa de procesamento (assinatura, hash, carving, indexação, etc).
  * Cada Worker cria, inicializa e usa suas instâncias das tarefas, assim normalmente há várias
  * instâncias de uma mesma tarefa.
@@ -18,25 +18,25 @@ import dpf.sp.gpinf.indexer.process.Worker;
  */
 public abstract class AbstractTask {
 	
-	/*
+	/**
 	 * Worker que executará esta tarefa.
 	 */
 	protected Worker worker;
 	
-	/*
+	/**
 	 * Construtor padrão
 	 */
 	public AbstractTask(){
 	}
 	
-	/*
-	 * Constrt
+	/**
+	 * Construtor
 	 */
 	public AbstractTask(Worker worker){
 		this.worker = worker;
 	}
 	
-	/*
+	/**
 	 * Método de inicialização da tarefa. Chamado em cada instância da tarefa pelo
 	 * Worker no qual ela está instalada.
 	 * @param confProps Parâmetros obtidos do arquivo de configuração principal
@@ -44,13 +44,13 @@ public abstract class AbstractTask {
 	 */
 	abstract public void init(final Properties confProps, File confDir) throws Exception;
 	
-	/*
+	/**
 	 * Método que realiza o processamento do item pela tarefa.
 	 * @param evidence Item a ser processado.
 	 */
 	abstract public void process(EvidenceFile evidence) throws Exception;
 	
-	/*
+	/**
 	 * Método chamado ao final do processamento em cada tarefa instanciada.
 	 * Pode conter código de finalização da tarefa e liberação de recursos.
 	 */
