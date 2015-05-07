2d35119e1469f4f01f3b71a5f8c3fd539c790087
Nassif <nassif.lfcn@dpf.gov.br>
2015-05-07 10:24:22 -0300

    Melhoria no TikaHtmlViewer para aproveitar o mediaType já conhecido do arquivo na detecção do tipo.

--- src/main/java/dpf/sp/gpinf/indexer/search/viewer/TikaHtmlViewer.java
+++ src/main/java/dpf/sp/gpinf/indexer/search/viewer/TikaHtmlViewer.java
@@ -14,13 +14,13 @@ import org.apache.tika.parser.Parser;
 import dpf.sp.gpinf.indexer.parsers.jdbc.ToXMLContentHandler;
 import dpf.sp.gpinf.indexer.util.IOUtil;
 
-/*
+/**
  * Visualizador para versão Html dos arquivos gerados pelos parsers do Tika.
  * Somente os tipos cujo Html seja satisfatório devem ser configurados.
  */
 public class TikaHtmlViewer extends HtmlViewer{
 	
-	Parser parser = new AutoDetectParser();
+	private Parser parser = new AutoDetectParser();
 
 	@Override
 	public String getName() {
@@ -31,28 +31,33 @@ public class TikaHtmlViewer extends HtmlViewer{
 	public boolean isSupportedType(String contentType) {
 		return 	contentType.equals("application/x-msaccess") 
 				|| contentType.equals("application/x-sqlite3")
-				|| contentType.equals("application/sqlite-skype");  
+				|| contentType.equals("application/sqlite-skype");
 	}
 	
-
 	@Override
-	public void loadFile(File file, Set<String> highlightTerms) {
+	public void loadFile(File file, String contentType, Set<String> highlightTerms) {
 		
 		if(file != null)
-			file = getHtmlVersion(file);
+			file = getHtmlVersion(file, contentType);
 		
 		super.loadFile(file, highlightTerms);
 	}
-
-	private File getHtmlVersion(File file) {
+	/*
+	 * TODO: Gerar preview html em outra thread, senão pode travar a interface ao trocar de abas no
+	 * 		 visualizador caso a geração do preview seja lenta.
+	 */
+	private File getHtmlVersion(File file, String contentType) {
 
 		File outFile = null;
 		TikaInputStream tis = null;
 		BufferedOutputStream outStream = null;
 		try {
 			Metadata metadata = new Metadata();
+			metadata.set(Metadata.CONTENT_TYPE, contentType);
 			tis = TikaInputStream.get(file);
 			ParseContext context = new ParseContext();
+			//Habilita parsing de subitens embutidos, o que ficaria ruim no preview de certos arquivos
+			//Ex: Como renderizar no preview html um PDF embutido num banco de dados?
 			//context.set(Parser.class, parser);
 			outFile = File.createTempFile("tmp", ".html");
 			outFile.deleteOnExit();
