5487503b8cf9ef53fcaf6a535d31d552d55406c1
Nassif <nassif.lfcn@dpf.gov.br>
2015-10-16 10:39:31 -0300

    imprime erro ao exportar dicionário

--- src/main/java/dpf/sp/gpinf/indexer/search/ExportIndexedTerms.java
+++ src/main/java/dpf/sp/gpinf/indexer/search/ExportIndexedTerms.java
@@ -10,7 +10,6 @@ import java.io.OutputStreamWriter;
 import javax.swing.JFileChooser;
 
 import org.apache.lucene.index.AtomicReader;
-import org.apache.lucene.index.AtomicReaderContext;
 import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.SlowCompositeReaderWrapper;
@@ -34,80 +33,85 @@ public class ExportIndexedTerms extends CancelableWorker<Boolean, Integer> imple
 	}
 	
 	public void export() {
-		try {
-			JFileChooser fileChooser = new JFileChooser();
-			fileChooser.setFileFilter(null);
-			fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-			if (fileChooser.showSaveDialog(App.get()) == JFileChooser.APPROVE_OPTION) {
-				file = fileChooser.getSelectedFile();
-				progressMonitor = new ProgressDialog(App.get(), this, true);
-				this.execute();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(null);
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		if (fileChooser.showSaveDialog(App.get()) == JFileChooser.APPROVE_OPTION) {
+			file = fileChooser.getSelectedFile();
+			progressMonitor = new ProgressDialog(App.get(), this, true);
+			this.execute();
 		}
 	}
 
 	@Override
-	protected Boolean doInBackground() throws Exception {
-
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
-
-		String[] field = {IndexItem.NAME, IndexItem.CONTENT};
-		long progress = 0;
+	protected Boolean doInBackground() {
 		
-		//Exibe progresso, porém exporta termos por subíndice, logo exporta mtos termos duplicados
-		/*for(AtomicReaderContext atomicContext: reader.leaves()){
-			AtomicReader atomicReader = atomicContext.reader();
-			Fields fields = atomicReader.fields();
-			for (String f : field) {
-	            Terms terms = fields.terms(f);
-	            total += (int)terms.size();
-			}
-		}
-		this.firePropertyChange("total", 0, total);
+		try{
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
 
-		for(AtomicReaderContext atomicContext: reader.leaves()){
-			AtomicReader atomicReader = atomicContext.reader();
-			Fields fields = atomicReader.fields();
+			String[] field = {IndexItem.NAME, IndexItem.CONTENT};
+			long progress = 0;
 			
+			//Exibe progresso, porém exporta termos por subíndice, logo exporta mtos termos duplicados
+			/*for(AtomicReaderContext atomicContext: reader.leaves()){
+				AtomicReader atomicReader = atomicContext.reader();
+				Fields fields = atomicReader.fields();
+				for (String f : field) {
+		            Terms terms = fields.terms(f);
+		            total += (int)terms.size();
+				}
+			}
+			this.firePropertyChange("total", 0, total);
+
+			for(AtomicReaderContext atomicContext: reader.leaves()){
+				AtomicReader atomicReader = atomicContext.reader();
+				Fields fields = atomicReader.fields();
+				
+				for (String f : field) {
+		            Terms terms = fields.terms(f);
+		            TermsEnum termsEnum = terms.iterator(null);	            
+		            while (termsEnum.next() != null) {
+		            	writer.write(termsEnum.term().utf8ToString());
+		            	writer.write("\r\n");
+		            	if(progress++ % (total / 100) == 0)
+		            		this.firePropertyChange("progress", 0, progress);
+		            }
+		            if (this.isCancelled())
+						break;
+		        }
+				if (this.isCancelled())
+					break;
+			}*/
+			
+			//Não exibe progresso pois SlowCompositeReaderWrapper não fornece o número total de termos
+			AtomicReader atomicReader = SlowCompositeReaderWrapper.wrap(reader);
+			Fields fields = atomicReader.fields();
 			for (String f : field) {
 	            Terms terms = fields.terms(f);
 	            TermsEnum termsEnum = terms.iterator(null);	            
+	            
 	            while (termsEnum.next() != null) {
 	            	writer.write(termsEnum.term().utf8ToString());
 	            	writer.write("\r\n");
-	            	if(progress++ % (total / 100) == 0)
+	            	if(++progress % (1000000) == 0){
 	            		this.firePropertyChange("progress", 0, progress);
+	            		if (this.isCancelled())
+	        				break;
+	            	}
 	            }
+	            
 	            if (this.isCancelled())
 					break;
-	        }
-			if (this.isCancelled())
-				break;
-		}*/
+	        }	
+			
+			writer.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
-		//Não exibe progresso pois SlowCompositeReaderWrapper não fornece o número total de termos
-		AtomicReader atomicReader = SlowCompositeReaderWrapper.wrap(reader);
-		Fields fields = atomicReader.fields();
-		for (String f : field) {
-            Terms terms = fields.terms(f);
-            TermsEnum termsEnum = terms.iterator(null);	            
-            while (termsEnum.next() != null) {
-            	writer.write(termsEnum.term().utf8ToString());
-            	writer.write("\r\n");
-            	if(++progress % (1000000) == 0){
-            		this.firePropertyChange("progress", 0, progress);
-            		if (this.isCancelled())
-        				break;
-            	}
-            }
-            if (this.isCancelled())
-				break;
-        }
-		this.firePropertyChange("close", 0, progress);		
+		this.firePropertyChange("close", 0, 1);	
 		
-		writer.close();
 		return null;
 	}
 
