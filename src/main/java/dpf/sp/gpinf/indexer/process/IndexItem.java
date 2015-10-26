5d7f1aa085f8ac1cffdeb951d128f40dd2bd5da3
Nassif <nassif.lfcn@dpf.gov.br>
2015-10-26 20:33:45 -0200

    corrige visualização de imagens originais (e nao thumbs) em relatórios

--- src/main/java/dpf/sp/gpinf/indexer/process/IndexItem.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/IndexItem.java
@@ -305,12 +305,6 @@ public class IndexItem {
 				
 				if(!value.isEmpty()){
 					File viewFile = Util.findFileFromHash(new File(outputBase, "view"), value);
-					if(viewFile == null){
-						File thumb = Util.getFileFromHash(new File(outputBase.getParent(),
-								HTMLReportTask.reportSubFolderName + "/" + HTMLReportTask.thumbsFolderName), value, "jpg");
-						if(thumb.exists())
-							viewFile = thumb;
-					}
 					if(viewFile != null)
 						evidence.setViewFile(viewFile);
 				}
