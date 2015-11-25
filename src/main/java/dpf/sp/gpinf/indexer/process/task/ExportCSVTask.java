397d7d83dfd349257803250ee01fb0411b107e93
Nassif <nassif.lfcn@dpf.gov.br>
2015-11-25 12:55:52 -0300

    adiciona coluna Recuperado(Carved) no CSV de propriedades

--- src/main/java/dpf/sp/gpinf/indexer/process/task/ExportCSVTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/ExportCSVTask.java
@@ -112,6 +112,9 @@ public class ExportCSVTask extends AbstractTask{
 		value = Boolean.toString(evidence.isDeleted());
 		list.append("\"" + value.replace("\"", "\"\"") + "\";");
 		
+		value = Boolean.toString(evidence.isCarved());
+        list.append("\"" + value.replace("\"", "\"\"") + "\";");
+		
 		Date date = evidence.getAccessDate();
 		if (date == null)
 			value = "";
@@ -153,7 +156,7 @@ public class ExportCSVTask extends AbstractTask{
 	private static synchronized void flush(StringBuilder list, File output) throws IOException {
 		OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(output, true), "UTF-8");
 		if (!headerWritten) {
-			writer.write("\"Nome\";\"Atalho\";\"Tamanho\";\"Ext\";\"Marcador\";\"Categoria\";\"Hash\";\"Deletado\";\"Acesso\";\"Modificação\";\"Criação\";\"Caminho\";\r\n");
+			writer.write("\"Nome\";\"Atalho\";\"Tamanho\";\"Ext\";\"Marcador\";\"Categoria\";\"Hash\";\"Deletado\";\"Recuperado\";\"Acesso\";\"Modificação\";\"Criação\";\"Caminho\";\r\n");
 			headerWritten = true;
 		}
 		writer.write(list.toString());
