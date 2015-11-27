54cefeb513b2c3e60153cac60c9b93ae7111bd58
Nassif <nassif.lfcn@dpf.gov.br>
2015-11-27 17:05:25 -0300

    atualiza notas

--- src/main/java/dpf/sp/gpinf/indexer/Versao.java
+++ src/main/java/dpf/sp/gpinf/indexer/Versao.java
@@ -26,7 +26,7 @@ import org.apache.lucene.util.Version;
 public class Versao {
 	
 	public static Version current = Version.LUCENE_4_9;
-	public static String APP_VERSION = "3.7.2";
+	public static String APP_VERSION = "3.7.3";
 	public static String APP_NAME = "Indexador e Processador de Evidências Digitais " + APP_VERSION;
 	public static String APP_EXT = "IPED";
 }
