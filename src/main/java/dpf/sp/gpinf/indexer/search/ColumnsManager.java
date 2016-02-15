f6ab22efe872929cc276abc497a96fbe28be3896
Nassif <nassif.lfcn@dpf.gov.br>
2016-02-15 13:54:28 -0200

    Divide propriedades/colunas em 3 grupos

--- src/main/java/dpf/sp/gpinf/indexer/search/ColumnsManager.java
+++ src/main/java/dpf/sp/gpinf/indexer/search/ColumnsManager.java
@@ -2,6 +2,8 @@ package dpf.sp.gpinf.indexer.search;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,20 +16,32 @@ import java.util.LinkedHashSet;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 import javax.swing.RowSorter.SortKey;
 import javax.swing.SortOrder;
 import javax.swing.event.TableModelEvent;
 import javax.swing.table.TableColumn;
 
+import org.apache.tika.metadata.IPTC;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
+
+import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
+import dpf.sp.gpinf.indexer.parsers.OCRParser;
 import dpf.sp.gpinf.indexer.process.IndexItem;
+import dpf.sp.gpinf.indexer.process.task.ImageThumbTask;
+import dpf.sp.gpinf.indexer.process.task.KFFTask;
+import dpf.sp.gpinf.indexer.process.task.ParsingTask;
 
 public class ColumnsManager implements ActionListener{
 	
@@ -45,11 +59,39 @@ public class ColumnsManager implements ActionListener{
 			IndexItem.PATH
 		};
 	
+	private String[] extraFields = 
+	    {
+	        IndexItem.CARVED,
+	        IndexItem.CONTENTTYPE,
+	        IndexItem.DUPLICATE,
+	        IndexItem.EXPORT,
+	        IndexItem.HASCHILD,
+	        IndexItem.ID,
+	        IndexItem.ISDIR,
+	        IndexItem.ISROOT,
+	        IndexItem.PARENTID,
+	        IndexItem.PARENTIDs,
+	        IndexItem.SLEUTHID,
+	        IndexItem.SUBITEM,
+	        IndexItem.TIMEOUT,
+	        KFFTask.KFF_STATUS,
+	        ParsingTask.ENCRYPTED,
+	        ImageThumbTask.HAS_THUMB,
+	        OCRParser.OCR_CHAR_COUNT,
+	        IndexerDefaultParser.PARSER_EXCEPTION
+	    };
+	
+	String[] indexFields;
+	
+	private String[] colGroups = {"Básicas", "Estendidas", "Metadados"};
+	
 	private static ColumnsManager instance = new ColumnsManager();
 	
 	private ArrayList<String> loadedFields = new ArrayList<String>();
 	
 	private JDialog dialog = new JDialog();
+	private JPanel listPanel = new JPanel();
+	private JComboBox<String> combo;
 	
 	public static ColumnsManager getInstance(){
 		return instance;
@@ -71,15 +113,22 @@ public class ColumnsManager implements ActionListener{
 		dialog.setAlwaysOnTop(true);
 		
 		JLabel label = new JLabel("Exibir colunas:");
-		label.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+		label.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
 		
-		JPanel listPanel = new JPanel();
 		listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
 		listPanel.setBackground(Color.WHITE);
 		
+		combo = new JComboBox<String>(colGroups);
+		
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+		topPanel.add(label);
+		topPanel.add(combo);
+		combo.addActionListener(this);
+		
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		panel.add(label, BorderLayout.NORTH);
+		panel.add(topPanel, BorderLayout.NORTH);
 		
 		JScrollPane scrollList = new JScrollPane(listPanel);
 		panel.add(scrollList, BorderLayout.CENTER);
@@ -87,48 +136,66 @@ public class ColumnsManager implements ActionListener{
 		for(String col : defaultFields)
 			loadedFields.add(col);
 		
-		String[] indexFields = LoadIndexFields.addExtraFields(App.get().reader, new String[0]);
+		indexFields = LoadIndexFields.addExtraFields(App.get().reader, new String[0]);
+		
+		Arrays.sort(defaultFields, Collator.getInstance());
+		Arrays.sort(extraFields, Collator.getInstance());
 		Arrays.sort(indexFields, Collator.getInstance());
 		
-		int i = 0;
-		for(String f : indexFields){
-			if(f.startsWith("Unknown"))
-				continue;
-			JCheckBox check = new JCheckBox();
-			check.setText(f);
-			if(loadedFields.contains(f))
-				check.setSelected(true);
-			check.addActionListener(this);
-			listPanel.add(check);
-			i++;
-		}
-		System.out.println(i);
+		updateList();
 		
 		dialog.getContentPane().add(panel);
 		dialog.setLocationRelativeTo(App.get());
 		
 	}
+	
+	private void updateList(){
+	    listPanel.removeAll();
+	    String[] fields = indexFields;
+	    if(combo.getSelectedIndex() == 0)
+	    	fields = defaultFields;
+	    if(combo.getSelectedIndex() == 1)
+	    	fields = extraFields;
+	    
+        for(String f : fields){
+            if(combo.getSelectedIndex() == 2 && (Arrays.asList(defaultFields).contains(f) || Arrays.asList(extraFields).contains(f)))
+                continue;
+            JCheckBox check = new JCheckBox();
+            check.setText(f);
+            if(loadedFields.contains(f))
+                check.setSelected(true);
+            check.addActionListener(this);
+            listPanel.add(check);
+        }
+        dialog.revalidate();
+        dialog.repaint();
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+	    
+	    if(e.getSource().equals(combo)){
+	        updateList();
+	    }else{
+	        JCheckBox source = (JCheckBox)e.getSource();
+	        int modelIdx = loadedFields.indexOf(source.getText());
+	        if(source.isSelected()){
+	            if(modelIdx == -1){
+	                loadedFields.add(source.getText());
+	                App.get().resultsModel.updateCols();
+	                modelIdx = ResultTableModel.fixedCols.length + loadedFields.size() - 1;
+	                ((ResultTableRowSorter)App.get().resultsTable.getRowSorter()).initComparator(modelIdx);
+	            }else
+	                modelIdx += ResultTableModel.fixedCols.length;
+	            
+	            App.get().resultsTable.addColumn(new TableColumn(modelIdx));
+	        }else{
+	            modelIdx += ResultTableModel.fixedCols.length;
+	            int viewIdx = App.get().resultsTable.convertColumnIndexToView(modelIdx);
+	            App.get().resultsTable.removeColumn(App.get().resultsTable.getColumnModel().getColumn(viewIdx));
+	        }
+	    }
 		
-		JCheckBox source = (JCheckBox)e.getSource();
-		int modelIdx = loadedFields.indexOf(source.getText());
-		if(source.isSelected()){
-			if(modelIdx == -1){
-				loadedFields.add(source.getText());
-				App.get().resultsModel.updateCols();
-				modelIdx = ResultTableModel.fixedCols.length + loadedFields.size() - 1;
-				((ResultTableRowSorter)App.get().resultsTable.getRowSorter()).initComparator(modelIdx);
-			}else
-				modelIdx += ResultTableModel.fixedCols.length;
-			
-			App.get().resultsTable.addColumn(new TableColumn(modelIdx));
-		}else{
-			modelIdx += ResultTableModel.fixedCols.length;
-			int viewIdx = App.get().resultsTable.convertColumnIndexToView(modelIdx);
-			App.get().resultsTable.removeColumn(App.get().resultsTable.getColumnModel().getColumn(viewIdx));
-		}
 	}
 
 }
