046da0907cc2fc7ae8477ac6179c53d6959ebe7e
Nassif <nassif.lfcn@dpf.gov.br>
2016-02-19 09:27:59 -0200

    carrega hashes md5, sha1 e edonkey da base do LED para destaque de registros p2p gera preview html do histórico do shareAza para destaque de hits

--- src/main/java/dpf/sp/gpinf/indexer/process/task/LedKFFTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/LedKFFTask.java
@@ -1,18 +1,19 @@
 package dpf.sp.gpinf.indexer.process.task;
 
 import gpinf.dev.data.EvidenceFile;
-import gpinf.dev.data.FileGroup;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Properties;
 
 import dpf.sp.gpinf.indexer.IndexFiles;
+import dpf.sp.gpinf.indexer.parsers.util.LedHashes;
 import dpf.sp.gpinf.indexer.process.Worker;
-import dpf.sp.gpinf.indexer.process.task.HashTask.HashValue;
+import dpf.sp.gpinf.indexer.util.HashValue;
 import dpf.sp.gpinf.indexer.util.Log;
 
 /**
@@ -26,8 +27,9 @@ import dpf.sp.gpinf.indexer.util.Log;
 public class LedKFFTask extends AbstractTask {
 
     private static Object lock = new Object();
-    private static HashValue[] hashArray;
+    private static HashMap<String, HashValue[]> hashArrays;
     private static final String taskName = "Consulta Base de Hashes do LED";
+    private static String[] ledHashOrder = {"md5", null, "edonkey", "sha-1"};
 
     public LedKFFTask(Worker worker) {
         super(worker);
@@ -37,7 +39,7 @@ public class LedKFFTask extends AbstractTask {
     public void init(Properties confParams, File confDir) throws Exception {
 
         synchronized (lock) {
-            if (hashArray != null)
+            if (hashArrays != null)
             	return;
 
             //this.caseData.addBookmark(new FileGroup(ledCategory, "", ""));
@@ -45,54 +47,70 @@ public class LedKFFTask extends AbstractTask {
             String ledWkffPath = confParams.getProperty("ledWkffPath");
             if (ledWkffPath == null)
             	return;
+            
             File wkffDir = new File(ledWkffPath);
             if (!wkffDir.exists())
             	throw new Exception("Caminho para base de hashes do LED inválido!");
 
-            int column = -1;
             hash = hash.toLowerCase();
-            if (hash.contains("md5"))
-            	column = 0;
-            else if (hash.contains("sha-1"))
-            	column = 3;
-            else
+            if (!hash.contains("md5") && !hash.contains("sha-1"))
             	throw new Exception("Habilite o hash md5 ou sha-1 para consultar a base do LED!");
 
             IndexFiles.getInstance().firePropertyChange("mensagem", "", "Carregando base de hashes do LED...");
+            
+            hashArrays = new HashMap<String, HashValue[]>();
 
-            ArrayList<HashValue> hashList = new ArrayList<HashValue>();
+            HashMap<String, ArrayList<HashValue>> hashList = new HashMap<String, ArrayList<HashValue>>();
+            for(int col = 0; col < ledHashOrder.length; col++)
+            	if(ledHashOrder[col] != null)
+            		hashList.put(ledHashOrder[col], new ArrayList<HashValue>());
+            
             for (File wkffFile : wkffDir.listFiles()) {
                 BufferedReader reader = new BufferedReader(new FileReader(wkffFile));
                 String line = reader.readLine();
                 while ((line = reader.readLine()) != null) {
                     String[] hashes = line.split(" \\*");
-                    hashList.add(new HashValue(hashes[column].trim()));
+                    for(int col = 0; col < ledHashOrder.length; col++)
+                    	if(ledHashOrder[col] != null)
+                    		hashList.get(ledHashOrder[col]).add(new HashValue(hashes[col].trim()));
                 }
                 reader.close();
             }
-            hashArray = hashList.toArray(new HashValue[0]);
-            hashList = null;
-            Arrays.sort(hashArray);
-            Log.info(taskName, "Hashes carregados: " + hashArray.length);
+        	for(int col = 0; col < ledHashOrder.length; col++)
+            	if(ledHashOrder[col] != null){
+            		hashArrays.put(ledHashOrder[col], hashList.get(ledHashOrder[col]).toArray(new HashValue[0]));
+            		hashList.remove(ledHashOrder[col]);
+            		Arrays.sort(hashArrays.get(ledHashOrder[col]));
+            	}
+            
+            Log.info(taskName, "Hashes carregados: " + hashArrays.get(ledHashOrder[0]).length);
+            
+            LedHashes.hashMap = hashArrays;
         }
 
     }
 
     @Override
     public void finish() throws Exception {
-        hashArray = null;
+        hashArrays = null;
 
     }
 
     @Override
     protected void process(EvidenceFile evidence) throws Exception {
 
-    	HashValue hash = evidence.getHashValue();
-        if (hash != null && hashArray != null) {
-            if (Arrays.binarySearch(hashArray, hash) >= 0)
-            	//evidence.addCategory(ledCategory);
-            	evidence.setExtraAttribute(KFFTask.KFF_STATUS, "pedo");
-        }
+        if (hashArrays == null)
+        	return;
+        	
+        for(int col = 0; col < ledHashOrder.length; col++)
+        	if(ledHashOrder[col] != null){
+        		String hash = (String)evidence.getExtraAttribute(ledHashOrder[col]);
+            	if(hash != null){
+            		if(Arrays.binarySearch(hashArrays.get(ledHashOrder[col]), new HashValue(hash)) >= 0)
+            			evidence.setExtraAttribute(KFFTask.KFF_STATUS, "pedo");
+            		break;
+            	}   	
+        	}
 
     }
 
