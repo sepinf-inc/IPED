dc400b23e20f2b74b82f35e484fb8dfe9f36b314
Nassif <nassif.lfcn@dpf.gov.br>
2016-03-23 10:03:12 -0300

    proteção contra itens sem pais

--- src/main/java/dpf/sp/gpinf/indexer/process/task/IndexTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/IndexTask.java
@@ -332,8 +332,9 @@ public class IndexTask extends AbstractTask{
             boolean[] doNotDelete = new boolean[stats.getLastId() + 1];
             for (int docID : result.docs) {
                 String parentIds = App.get().reader.document(docID).get(IndexItem.PARENTIDs);
-                for(String parentId : parentIds.split(" "))
-                	doNotDelete[Integer.parseInt(parentId)] = true;
+                if(!parentIds.trim().isEmpty())
+	                for(String parentId : parentIds.trim().split(" "))
+	                	doNotDelete[Integer.parseInt(parentId)] = true;
             }
             
             BooleanQuery query;
