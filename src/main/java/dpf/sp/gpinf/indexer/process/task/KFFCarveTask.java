bb866d40abbd2360956d958592be1fd1b542ee07
Wladimir <wladimir.wlcl>
2016-08-05 09:57:25 -0300

    Evitar tarefas (KFFCarve e KnownMetCarve) de rodar quando é Report.

--- src/main/java/dpf/sp/gpinf/indexer/process/task/KFFCarveTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/KFFCarveTask.java
@@ -124,7 +124,7 @@ public class KFFCarveTask extends BaseCarveTask {
 
   protected void process(EvidenceFile evidence) throws Exception {
     //Verifica se está desabilitado e se o tipo de arquivo é tratado
-    if (!taskEnabled || !isAcceptedType(evidence.getMediaType())) return;
+    if (!taskEnabled || caseData.containsReport() || !isAcceptedType(evidence.getMediaType())) return;
 
     byte[] buf512 = new byte[512];
     byte[] buf64K = new byte[65536 - buf512.length];
