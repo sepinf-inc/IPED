ee088044e5229a3ae9a736797575570c82fbdf27
lfcnassif <lfcnassif@gmail.com>
2017-06-29 11:18:50 -0300

    checagem por java 1.8 ou superior

--- src/main/java/dpf/sp/gpinf/indexer/desktop/AppMain.java
+++ src/main/java/dpf/sp/gpinf/indexer/desktop/AppMain.java
@@ -3,19 +3,44 @@ package dpf.sp.gpinf.indexer.desktop;
 import java.io.File;
 import java.net.URL;
 
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 import dpf.sp.gpinf.indexer.LogConfiguration;
 import dpf.sp.gpinf.indexer.process.Manager;
 
 public class AppMain {
 	
 	private static final String appLogFileName = "IPED-SearchApp.log";
+	private static final String MIN_JAVA_VER = "1.8";
 	
 	boolean isMultiCase = false;
 	boolean nolog = false;
 	File casesPathFile = null;
 
 	public static void main(String[] args) {
+	    
+	    checkJavaVersion();
 		new AppMain().start(args);
+	}
+	
+	private static void checkJavaVersion(){
+	    try {
+            SwingUtilities.invokeAndWait(new Runnable(){
+                  @Override
+                  public void run(){
+                      String javaVersion = System.getProperty("java.version");
+                      if(javaVersion.compareTo(MIN_JAVA_VER) < 0){
+                          JOptionPane.showMessageDialog(App.get(), 
+                              "É necessário atualizar o Java para a versão " + MIN_JAVA_VER + " ou superior!", 
+                              "Erro na inicialização", JOptionPane.ERROR_MESSAGE);
+                          System.exit(1);
+                      }
+                  }
+              });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 	
 	private void start(String[] args) {	
