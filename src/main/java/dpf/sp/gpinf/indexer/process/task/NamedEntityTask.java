65dcddde3c12311a2650f4d5ac249aa40458e265
lfcnassif <lfcnassif@gmail.com>
2017-07-21 18:40:19 -0300

    usa modelo NER em inglês, altera tipos, corrige NPE, filtra metadados

--- src/main/java/dpf/sp/gpinf/indexer/process/task/NamedEntityTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/NamedEntityTask.java
@@ -12,6 +12,7 @@ import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.ner.NamedEntityParser;
 import org.apache.tika.parser.ner.corenlp.CoreNLPNERecogniser;
 
+import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
 import dpf.sp.gpinf.indexer.parsers.util.IgnoreContentHandler;
 import dpf.sp.gpinf.indexer.process.Worker;
 import gpinf.dev.data.EvidenceFile;
@@ -32,8 +33,8 @@ public class NamedEntityTask extends AbstractTask {
             return;
         
         String nerImpl = "org.apache.tika.parser.ner.corenlp.CoreNLPNERecogniser";
-        String modelPath = "edu/stanford/nlp/models/ner/spanish.ancora.distsim.s512.crf.ser.gz";
-        //String modelPath = "edu/stanford/nlp/models/ner/english.all.3class.caseless.distsim.crf.ser.gz";
+        //String modelPath = "edu/stanford/nlp/models/ner/spanish.ancora.distsim.s512.crf.ser.gz";
+        String modelPath = "edu/stanford/nlp/models/ner/english.all.3class.caseless.distsim.crf.ser.gz";
         
         System.setProperty(NamedEntityParser.SYS_PROP_NER_IMPL, nerImpl);
         System.setProperty(CoreNLPNERecogniser.MODEL_PROP_NAME, modelPath);
@@ -51,24 +52,30 @@ public class NamedEntityTask extends AbstractTask {
     @Override
     protected void process(EvidenceFile evidence) throws Exception {
         
+        String text = evidence.getParsedTextCache();
         String mime = evidence.getMediaType().toString();
-        if(mime.startsWith("image") || mime.startsWith("audio") || mime.startsWith("video")
-                || MediaType.OCTET_STREAM.toString().equals(mime))
+        String categories = evidence.getCategories(); 
+        
+        if(text == null || mime.startsWith("audio") || mime.startsWith("video")
+                || categories.contains("Programas e Bibliotecas") || categories.contains("Outros Arquivos") || categories.contains("Discos Virtuais") 
+                || MediaType.OCTET_STREAM.toString().equals(mime) || CarveTask.UNALLOCATED_MIMETYPE.toString().equals(mime))
             return;
         
-        String text = evidence.getParsedTextCache();
-        
-        InputStream is = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+        int i = text.lastIndexOf(IndexerDefaultParser.METADATA_HEADER);
+        if(i != -1)
+            text = text.substring(0, i);
         
         Metadata metadata = evidence.getMetadata();
         String originalContentType = metadata.get(Metadata.CONTENT_TYPE);
         metadata.set(Metadata.CONTENT_TYPE, MediaType.TEXT_PLAIN.toString());
         
-        nep.parse(is, new IgnoreContentHandler(), metadata, new ParseContext());
-        
-        metadata.set(Metadata.CONTENT_TYPE, originalContentType);
-        
-        is.close();
+        try(InputStream is = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8))){
+            
+            nep.parse(is, new IgnoreContentHandler(), metadata, new ParseContext());
+            
+        }finally{
+            metadata.set(Metadata.CONTENT_TYPE, originalContentType);
+        }
         
     }
 
