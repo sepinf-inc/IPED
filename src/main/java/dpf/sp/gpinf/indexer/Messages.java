471c317079206eaa6d2b9c74c9f2c44a41737e2e
Nassif <nassif.lfcn@dpf.gov.br>
2018-01-16 17:24:18 -0200

    move bundles para pacote default

--- src/main/java/dpf/sp/gpinf/indexer/Messages.java
+++ src/main/java/dpf/sp/gpinf/indexer/Messages.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "dpf.sp.gpinf.indexer.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
--- src/main/java/dpf/sp/gpinf/indexer/desktop/Messages.java
+++ src/main/java/dpf/sp/gpinf/indexer/desktop/Messages.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Messages {
 	
-	private static final String BUNDLE_NAME = "dpf.sp.gpinf.indexer.desktop.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "messages-desktop"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
