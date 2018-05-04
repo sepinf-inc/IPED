915e5b7e3fa5cad73ac9f0e9ad9e71d63a244623
Nassif <nassif.lfcn@dpf.gov.br>
2018-05-04 14:03:47 -0300

    permite criar e instalar novas tarefas via scripts

--- src/main/java/dpf/sp/gpinf/indexer/process/task/TaskInstaller.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/TaskInstaller.java
@@ -3,10 +3,10 @@ package dpf.sp.gpinf.indexer.process.task;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 import dpf.sp.gpinf.indexer.process.Worker;
+import dpf.sp.gpinf.indexer.util.IPEDException;
 
 /**
  * Instancia e instala as tarefas de processamento em um Worker. A ordem de execução das tarefas
@@ -18,9 +18,13 @@ import dpf.sp.gpinf.indexer.process.Worker;
 public class TaskInstaller {
     
   private static final String TASKS_CONFIG_XML = "conf/TaskInstaller.xml";
+  private static final String SCRIPT_BASE = "conf/scripts";
+  
+  private File scriptDir;
 
   public void installProcessingTasks(Worker worker) throws Exception {
       
+    scriptDir = new File(worker.output, SCRIPT_BASE);
     List<AbstractTask> tasks = worker.tasks;
     File taskConfig = new File(worker.output, TASKS_CONFIG_XML);
     
@@ -31,19 +35,32 @@ public class TaskInstaller {
     }
   }
   
-  private List<AbstractTask> loadTasks(File file, List<AbstractTask> tasks) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
-      List<String> lines = Files.readAllLines(file.toPath());
-      List<String> names = new ArrayList<>();
-      String prefix = "task class=\"";
-      for(String line : lines) {
-          int i = line.indexOf(prefix);
-          if(i > -1)
-              names.add(line.substring(i + prefix.length(), line.indexOf("\"></task>")));
-      }
-      for(String className : names)
-          tasks.add((AbstractTask)Class.forName(className).newInstance());
+  private List<AbstractTask> loadTasks(File file, List<AbstractTask> tasks) throws InstantiationException, IllegalAccessException, IOException, ClassNotFoundException{
       
+      String classPrefix = "task class=\"";
+      String scriptPrefix = "task script=\"";
+      List<String> lines = Files.readAllLines(file.toPath());
+      for(String line : lines) {
+          int i = line.indexOf(classPrefix);
+          if(i > -1) {
+              String className = line.substring(i + classPrefix.length(), line.indexOf("\"></task>"));
+              tasks.add((AbstractTask)Class.forName(className).newInstance());
+          }
+          i = line.indexOf(scriptPrefix);
+          if(i > -1) {
+              String scriptName = line.substring(i + scriptPrefix.length(), line.indexOf("\"></task>"));
+              tasks.add(getScriptTask(scriptName));
+          }
+      }
       return tasks;
+  }
+  
+  private ScriptTask getScriptTask(String name){
+      File script = new File(scriptDir, name);
+      if(!script.exists())
+          throw new IPEDException("Script File not found: " + script.getAbsolutePath());
+      
+      return new ScriptTask(script);
   }
 
 }
