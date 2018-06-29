03f7e7e1629ef26591b89cc88860e85f95501241
Nassif <nassif.lfcn@dpf.gov.br>
2018-06-29 12:19:39 -0300

    possibilita ignorar parsing de itens via script

--- src/main/java/dpf/sp/gpinf/indexer/process/task/ParsingTask.java
+++ src/main/java/dpf/sp/gpinf/indexer/process/task/ParsingTask.java
@@ -253,7 +253,7 @@ public class ParsingTask extends AbstractTask implements EmbeddedDocumentExtract
     }
     long start = System.nanoTime()/1000;
     
-    if (!evidence.isTimedOut() && ((evidence.getLength() == null || 
+    if (evidence.getParsedTextCache() == null && !evidence.isTimedOut() && ((evidence.getLength() == null || 
     		evidence.getLength() < Configuration.minItemSizeToFragment) ||
     		isSpecificParser(parser) )) {
         try{
