9b67320ba283aa6b5bb71054d0a6cd5b3438202e
Patrick Dalla Bernardina <patrick.pdb@dpf.gov.br>
2018-08-20 17:28:54 -0400

    Move a classe de configuração do projeto IPED-CARVERS-API para o IPED

---
+++ src/main/java/dpf/sp/gpinf/carver/CarverConfigurationFactory.java
@@ -0,0 +1,32 @@
+package dpf.sp.gpinf.carver;
+
+import java.io.File;
+import java.util.Properties;
+
+import dpf.sp.gpinf.carver.api.CarverConfiguration;
+import dpf.sp.gpinf.carver.api.CarverConfigurationException;
+
+
+public class CarverConfigurationFactory {
+    static CarverConfiguration carverConfig = null;
+    static String CARVE_CONFIG_XML = "CarverConfig.xml"; //$NON-NLS-1$
+
+    static public CarverConfiguration getCarverConfiguration(File confDir) throws CarverConfigurationException {
+        try {
+            if (carverConfig == null) {
+                Class<?> classe = Thread.currentThread().getContextClassLoader().loadClass("dpf.sp.gpinf.carving.XMLCarverConfiguration");
+                carverConfig = (CarverConfiguration) classe.newInstance();
+                File confFile = new File(confDir, CARVE_CONFIG_XML);
+                Properties props = new Properties();
+                props.setProperty("XML_CONFIG_FILE", confFile.getAbsolutePath());
+                props.setProperty("XML_CONFIG_DIR", confDir.getAbsolutePath());
+                carverConfig.init(props);
+            }
+            return carverConfig;
+
+        } catch (Exception e) {
+            throw new CarverConfigurationException(e);
+        }
+    }
+
+}
