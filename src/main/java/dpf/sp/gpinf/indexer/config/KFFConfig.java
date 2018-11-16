05afd73cecb8b5c0c998449f2bfc3fad6a991637
Nassif <nassif.lfcn@dpf.gov.br>
2018-11-16 13:30:20 -0200

    remove código não utilizado

--- src/main/java/dpf/sp/gpinf/indexer/config/KFFConfig.java
+++ src/main/java/dpf/sp/gpinf/indexer/config/KFFConfig.java
@@ -5,14 +5,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 import java.nio.file.DirectoryStream.Filter;
 
-import org.apache.commons.logging.LogFactory;
-import org.apache.commons.logging.impl.NoOpLog;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import dpf.sp.gpinf.indexer.Configuration;
-import dpf.sp.gpinf.indexer.util.CustomLoader.CustomURLClassLoader;
-
 public class KFFConfig extends AbstractPropertiesConfigurable{
 	public static final String CONFIG_FILE = "LocalConfig.txt"; //$NON-NLS-1$
 	public static final String IPEDCONFIGFILE = "IPEDConfig.txt"; //$NON-NLS-1$
@@ -30,20 +22,6 @@ public class KFFConfig extends AbstractPropertiesConfigurable{
 	@Override
 	public Filter<Path> getResourceLookupFilter() {
 		return filter;
-	}
-
-	Logger getLogger(Path resource) {
-	    // DataSource.testConnection(configPathStr);
-	    LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", NoOpLog.class.getName()); //$NON-NLS-1$
-
-	    Logger LOGGER = null;
-	    if(Configuration.class.getClassLoader().getClass().getName()
-	            .equals(CustomURLClassLoader.class.getName()))
-	        LOGGER = LoggerFactory.getLogger(Configuration.class);
-
-	    if(LOGGER != null) LOGGER.info("Loading configuration from " + resource.toAbsolutePath()); //$NON-NLS-1$
-
-	    return LOGGER;
 	}
 	
 	public void processConfig(Path resource) throws IOException {
