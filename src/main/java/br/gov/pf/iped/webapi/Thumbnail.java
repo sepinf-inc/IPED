b6999fd112e90c9eaf1bc558936f4d3fae969af4
Nassif <nassif.lfcn@dpf.gov.br>
2019-03-08 17:30:26 -0300

    ws to get thumbnails

---
+++ src/main/java/br/gov/pf/iped/webapi/Thumbnail.java
@@ -0,0 +1,40 @@
+package br.gov.pf.iped.webapi;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.apache.commons.io.IOUtils;
+import org.sleuthkit.datamodel.TskCoreException;
+
+import dpf.sp.gpinf.indexer.search.IPEDSource;
+import gpinf.dev.data.EvidenceFile;
+
+@Path("sources/{sourceID}/docs/{id}/thumb")
+public class Thumbnail {
+
+    @GET
+    @Produces("image/jpg")
+    public StreamingOutput content(
+            @PathParam("sourceID") int sourceID,
+            @PathParam("id") int id)
+                    throws TskCoreException, IOException, URISyntaxException{
+
+        IPEDSource source = Sources.get(sourceID);
+        EvidenceFile item = source.getItemByID(id);
+        final byte[] thumb = item.getThumb();
+        return new StreamingOutput() {
+            @Override
+            public void write(OutputStream arg0) throws IOException, WebApplicationException {
+                IOUtils.copy(new ByteArrayInputStream(thumb), arg0);
+            }};
+    }
+}
