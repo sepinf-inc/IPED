9994425cde1e04a42ddd8b81a7ff75f652816964
Nassif <nassif.lfcn@dpf.gov.br>
2019-03-08 17:03:32 -0300

    web service to get categories (still without hierarchy)

---
+++ src/main/java/br/gov/pf/iped/webapi/Categories.java
@@ -0,0 +1,28 @@
+package br.gov.pf.iped.webapi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+@Path("categories")
+public class Categories {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String doSearch() throws Exception{
+        
+        JSONArray data = new JSONArray();
+        for (String category : Sources.multiSource.getCategories()) {
+            data.add(category);
+        }
+        
+        JSONObject json = new JSONObject();
+        json.put("data", data);
+
+        return json.toString();
+    }   
+}
