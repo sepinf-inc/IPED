042aa086dc616bc95289e65e0f4c0593728c8ff1
Nassif <nassif.lfcn@dpf.gov.br>
2019-03-11 11:46:15 -0300

    configure to extract same text as desktopApp (html, containers, ocr...)

--- src/main/java/br/gov/pf/iped/webapi/Text.java
+++ src/main/java/br/gov/pf/iped/webapi/Text.java
@@ -1,5 +1,6 @@
 package br.gov.pf.iped.webapi;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -11,8 +12,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.StreamingOutput;
 
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
-import org.apache.lucene.document.Document;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
@@ -20,35 +19,31 @@ import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.ToTextContentHandler;
 import org.xml.sax.ContentHandler;
 
+import dpf.sp.gpinf.indexer.Configuration;
 import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
-import dpf.sp.gpinf.indexer.parsers.util.ItemInfo;
-import dpf.sp.gpinf.indexer.process.IndexItem;
+import dpf.sp.gpinf.indexer.parsers.util.OCROutputFolder;
+import dpf.sp.gpinf.indexer.process.task.ParsingTask;
 import dpf.sp.gpinf.indexer.search.IPEDSource;
-import dpf.sp.gpinf.indexer.util.ItemInfoFactory;
-import dpf.sp.gpinf.indexer.util.StreamSource;
 import gpinf.dev.data.EvidenceFile;
 
 @Path("/sources/{sourceID}/docs/{id}/text")
 public class Text {
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN+"; charset=UTF-8")
 	public static StreamingOutput content(
 			@PathParam("sourceID") int sourceID, 
-			@PathParam("id") int id) throws IOException{
+			@PathParam("id") int id) throws Exception{
     	
     	IPEDSource source = Sources.get(sourceID);
-		int luceneID = source.getLuceneId(id);
-		Document doc = source.getReader().document(luceneID);
-    	final String contentType = doc.getField(IndexItem.CONTENTTYPE).stringValue();
     	final EvidenceFile item = source.getItemByID(id);
-		final ParseContext context = Text.getTikaContext(item);
+    	final IndexerDefaultParser parser = new IndexerDefaultParser();
+		final ParseContext context = getTikaContext(item, parser, source.getModuleDir());
     	final Metadata metadata = new Metadata();
-		metadata.set(IndexerDefaultParser.INDEXER_CONTENT_TYPE, contentType);
-		metadata.set(Metadata.RESOURCE_NAME_KEY, item.getName());
+        
+    	ParsingTask.fillMetadata(item, metadata);
+        parser.setPrintMetadata(false);
 		
-		final IndexerDefaultParser parser = new IndexerDefaultParser();
-		parser.setPrintMetadata(false);
-
     	return new StreamingOutput() {
             @Override
 			public void write(OutputStream arg0) throws IOException, WebApplicationException {
@@ -62,20 +57,13 @@ public class Text {
 		};
     }
     
-    private static Parser autoParser = new IndexerDefaultParser();
-    
-	private static ParseContext getTikaContext(EvidenceFile item) throws IOException{
-		ParseContext context = new ParseContext();
-		context.set(Parser.class, autoParser);
-		context.set(ItemInfo.class, ItemInfoFactory.getItemInfo(item));
-		
-		// Tratamento p/ acentos de subitens de ZIP
-		ArchiveStreamFactory factory = new ArchiveStreamFactory();
-		factory.setEntryEncoding("Cp850");
-		context.set(ArchiveStreamFactory.class, factory);
-		
-		context.set(StreamSource.class, (StreamSource)item);
-		return context;
-	}
+    public static ParseContext getTikaContext(EvidenceFile item, Parser parser, File moduleDir) throws Exception {
+        ParsingTask expander = new ParsingTask(item, (IndexerDefaultParser) parser);
+        expander.init(Configuration.properties, new File(Configuration.configPath, "conf")); //$NON-NLS-1$
+        ParseContext context = expander.getTikaContext(); 
+        expander.setExtractEmbedded(false);
+        context.set(OCROutputFolder.class, new OCROutputFolder(moduleDir));
+        return context;
+      }
 
 }
