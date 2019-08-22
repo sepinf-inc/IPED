e05cc0f5d03b512cfb08e6896017f7efb862b372
Nassif <nassif.lfcn@dpf.gov.br>
2019-08-22 14:29:04 -0300

    optimizations

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/regex/RegexTask.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/regex/RegexTask.java
@@ -49,6 +49,8 @@ public class RegexTask extends AbstractTask {
     private static boolean formatRegexMatches = false;
 
     private boolean enabled = true;
+    
+    private char[] cbuf = new char[1000000];
 
     private static RegexValidator regexValidator;
 
@@ -211,11 +213,10 @@ public class RegexTask extends AbstractTask {
         }
         processRegex(evidence, new StringReader(evidence.getName()));
     }
-
+    
     @SuppressWarnings("unchecked")
     private void processRegex(IItem evidence, Reader reader) throws IOException {
 
-        char[] cbuf = new char[1000000];
         int k = 0;
         while (k != -1) {
             int off = 0;
