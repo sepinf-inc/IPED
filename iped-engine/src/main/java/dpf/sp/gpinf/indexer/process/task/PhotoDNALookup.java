6523256f98375fdc713e8efb6599b2bf85c88d14
Nassif <nassif.lfcn@dpf.gov.br>
2019-09-11 13:40:49 -0300

    PhotoDNALookupTask, slow exaustive search for now

---
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/PhotoDNALookup.java
@@ -0,0 +1,93 @@
+package dpf.sp.gpinf.indexer.process.task;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Properties;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import iped3.IItem;
+
+public class PhotoDNALookup extends AbstractTask{
+    
+    private static final String photoDNAFilePath = "photoDNAFilePath";
+    
+    public static final String PHOTO_DNA_KFF_HIT = "photoDnaKffHit";
+    
+    public static final String PHOTO_DNA_KFF_DIST = "photoDnaKffDistance";
+    
+    public static final String PHOTO_DNA_KFF_HASH = "photoDnaKffHash";
+    
+    public static int MAX_DISTANCE = 50000;
+    
+    private static ArrayList<byte[]> photoDNAHashSet = new ArrayList<>();
+    
+    private static Comparator<byte[]> photoDNAComparator;
+
+    @Override
+    public void init(Properties confParams, File confDir) throws Exception {
+        loadDatabase(confParams);
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return !photoDNAHashSet.isEmpty();
+    }
+    
+    private static synchronized void loadDatabase(Properties confParams) throws IOException, DecoderException {
+        try {
+            photoDNAComparator = new br.dpf.sepinf.photodna.PhotoDNAComparator();
+        }catch(NoClassDefFoundError e) {
+            return;
+        }
+        
+        if(photoDNAHashSet.isEmpty()) {
+            String path = confParams.getProperty(photoDNAFilePath);
+            if(path != null && !path.trim().isEmpty()) {
+                try (BufferedReader bf = new BufferedReader(new FileReader(path.trim()))){
+                    String line = null;
+                    while((line = bf.readLine()) != null) {
+                        String hash = line.split(",")[1];
+                        photoDNAHashSet.add(getBytes(hash));
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void finish() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void process(IItem evidence) throws Exception {
+        
+        String hashStr = (String)evidence.getExtraAttribute(PhotoDNATask.PHOTO_DNA);
+        if(hashStr == null || photoDNAHashSet.isEmpty())
+            return;
+        
+        byte[] photodna = getBytes(hashStr);
+        for(byte[] hash : photoDNAHashSet) {
+            int dist = photoDNAComparator.compare(hash, photodna); 
+            if(dist <= MAX_DISTANCE) {
+                evidence.setExtraAttribute(PHOTO_DNA_KFF_HIT, "true");
+                evidence.setExtraAttribute(PHOTO_DNA_KFF_DIST, dist);
+                evidence.setExtraAttribute(PHOTO_DNA_KFF_HASH, new String(Hex.encodeHex(hash, false)));
+                break;
+            }
+        }
+                
+    }
+    
+    private static byte[] getBytes(String hash) throws DecoderException {
+        return Hex.decodeHex(hash.toCharArray());
+    }
+
+}
