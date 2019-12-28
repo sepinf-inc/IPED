sepinf-inc/IPED

commit 8f384fa353bd184087f34fc8dceae2a626212c53

removes photoDNA plugin compile dependency

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/PhotoDNALookup.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 import com.eatthepath.jvptree.DistanceFunction;
 import com.eatthepath.jvptree.VPTree;
 
-import br.dpf.sepinf.photodna.PhotoDNATransforms;
+import dpf.sp.gpinf.indexer.util.PhotoDNATransforms;
 import iped3.IItem;
 
 public class PhotoDNALookup extends AbstractTask{
@@ -40,12 +40,15 @@ public class PhotoDNALookup extends AbstractTask{
     
     private static boolean taskEnabled = true;
     
+    private PhotoDNATransforms transforms;
+    
     @Override
     public void init(Properties confParams, File confDir) throws Exception {
         
         if(taskEnabled && vptree.isEmpty()) {
             try {
-                PhotoDNATransforms.class.getName();
+                Class<?> c = Class.forName("br.dpf.sepinf.photodna.PhotoDNATransforms");
+                transforms = (PhotoDNATransforms)c.newInstance();
             }catch(NoClassDefFoundError e) {
                 taskEnabled = false;
                 return;
@@ -119,7 +122,7 @@ public class PhotoDNALookup extends AbstractTask{
         boolean flip = false;
         while(rot == 0 || (rotateAndFlip && rot < 4)) {
             int degree = 90 * rot++;
-            byte[] photodnaRot = PhotoDNATransforms.rotate(photodna, degree, flip);
+            byte[] photodnaRot = transforms.rotate(photodna, degree, flip);
             
             List<byte[]> neighbors = vptree.getAllWithinDistance(photodnaRot, MAX_DISTANCE);
             

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/PhotoDNATask.java
@@ -15,7 +15,7 @@ import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import br.dpf.sepinf.photodna.PhotoDNA;
+import dpf.sp.gpinf.indexer.util.PhotoDNA;
 import dpf.sp.gpinf.indexer.util.UTF8Properties;
 import iped3.IItem;
 
@@ -86,7 +86,8 @@ public class PhotoDNATask extends AbstractTask{
             PhotoDNALookup.rotateAndFlip = Boolean.valueOf(value.trim());
         
         try {
-            photodna = new PhotoDNA();
+            Class<?> c = Class.forName("br.dpf.sepinf.photodna.PhotoDNA");
+            photodna = (PhotoDNA)c.newInstance();
             
         }catch(NoClassDefFoundError e) {
             enabled = false;

iped-engine/pom.xml
@@ -184,12 +184,6 @@
   			<version>2.28</version>
   			<scope>runtime</scope>
 		</dependency>
-		<dependency>
-			<groupId>microsoft</groupId>
-  			<artifactId>photodna</artifactId>
-  			<version>1.0.0-SNAPSHOT</version>
-  			<scope>provided</scope>
-  		</dependency>
     </dependencies>
     
 </project>

iped-utils/src/main/java/dpf/sp/gpinf/indexer/util/PhotoDNA.java
@@ -1,0 +1,16 @@
+package dpf.sp.gpinf.indexer.util;
+
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+public interface PhotoDNA {
+    
+    public abstract int distance(byte[] hash1, byte[] hash2);
+    
+    public abstract byte[] computePhotoDNA(InputStream is) throws Exception;
+    
+    public abstract byte[] computePhotoDNA(BufferedImage img) throws Exception;
+    
+    public abstract void reset();
+
+}

iped-utils/src/main/java/dpf/sp/gpinf/indexer/util/PhotoDNATransforms.java
@@ -1,0 +1,7 @@
+package dpf.sp.gpinf.indexer.util;
+
+public interface PhotoDNATransforms {
+    
+    public abstract byte[] rotate(byte[] hash, int degree, boolean flip);
+
+}

commit cf52d973d003cf3c860ed739ccee90f790e83af6

fix ClassNotFoundException caused by commit 8f384fa

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/PhotoDNALookup.java
@@ -49,7 +49,7 @@ public class PhotoDNALookup extends AbstractTask{
             try {
                 Class<?> c = Class.forName("br.dpf.sepinf.photodna.PhotoDNATransforms");
                 transforms = (PhotoDNATransforms)c.newInstance();
-            }catch(NoClassDefFoundError e) {
+            }catch(ClassNotFoundException e) {
                 taskEnabled = false;
                 return;
             }

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/PhotoDNATask.java
@@ -89,7 +89,7 @@ public class PhotoDNATask extends AbstractTask{
             Class<?> c = Class.forName("br.dpf.sepinf.photodna.PhotoDNA");
             photodna = (PhotoDNA)c.newInstance();
             
-        }catch(NoClassDefFoundError e) {
+        }catch(ClassNotFoundException e) {
             enabled = false;
             if(!warned.getAndSet(true))
                 LOGGER.error("Optional photoDNA lib not loaded. If you have rights to use it, you should put it into plugin/optional_jars folder.");