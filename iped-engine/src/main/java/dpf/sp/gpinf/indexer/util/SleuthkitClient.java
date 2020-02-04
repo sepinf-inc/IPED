sepinf-inc/IPED

commit 5b6368d0455372bf6a0aef14d9706c69982c9518

fix open streams control

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/SleuthkitClient.java
@@ -269,7 +269,7 @@ public class SleuthkitClient {
         return stream;
     }
 
-    synchronized void removeStream(long streamID) {
+    public void removeStream(long streamID) {
         currentStreams.remove(streamID);
         if(currentStreams.size() == 0)
             clientsDeque.addFirst(this);

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/SleuthkitClientInputStream.java
@@ -247,6 +247,8 @@ public class SleuthkitClientInputStream extends SeekableInputStream {
     @Override
     public void close() throws IOException {
         
+        client.removeStream(streamId);
+        
         synchronized (client) {
             if(!closed && !client.serverError) {
                 mbb.putInt(1, sleuthId);
@@ -255,10 +257,9 @@ public class SleuthkitClientInputStream extends SeekableInputStream {
                 notifyServer();
                 waitServerResponse();
             }
-            empty = true;
-            client.removeStream(streamId);
-            closed = true;
         }
+        empty = true;
+        closed = true;
 
     }
 

commit c7bd9c7fb125c3e15fd10d01af9e752b53336be2

fix server restarting before closing all open streams(caused by 5b6368d)

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/SleuthkitClient.java
@@ -11,7 +11,6 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -66,7 +65,7 @@ public class SleuthkitClient implements Comparable<SleuthkitClient>{
     volatile boolean serverError = false;
     
     private int openedStreams = 0;
-    private Set<Long> currentStreams = Collections.synchronizedSet(new HashSet<>());
+    private Set<Long> currentStreams = new HashSet<>();
     private int priority = 0;
     private volatile long requestTime = 0;
     
@@ -273,7 +272,7 @@ public class SleuthkitClient implements Comparable<SleuthkitClient>{
         return stream;
     }
 
-    public void removeStream(long streamID) {
+    public synchronized void removeStream(long streamID) {
         boolean removed = currentStreams.remove(streamID);
         if(removed) {
             synchronized(lock) {

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/SleuthkitClientInputStream.java
@@ -251,8 +251,6 @@ public class SleuthkitClientInputStream extends SeekableInputStream {
     @Override
     public void close() throws IOException {
         
-        client.removeStream(streamId);
-        
         synchronized (client) {
             if(!closed && !client.serverError) {
                 mbb.putInt(1, sleuthId);
@@ -262,6 +260,7 @@ public class SleuthkitClientInputStream extends SeekableInputStream {
                 waitServerResponse();
             }
         }
+        client.removeStream(streamId);
         empty = true;
         closed = true;
 