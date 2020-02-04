sepinf-inc/IPED

commit b5a8b7424689a43bba9df70856b1448574ba6159

fix -ocr option when used with bookmark names in report creation

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ParsingTask.java
@@ -25,6 +25,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -60,7 +61,6 @@ import dpf.sp.gpinf.indexer.config.ConfigurationManager;
 import dpf.sp.gpinf.indexer.io.ParsingReader;
 import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
 import dpf.sp.gpinf.indexer.parsers.OCRParser;
-import dpf.sp.gpinf.indexer.parsers.OutlookPSTParser;
 import dpf.sp.gpinf.indexer.parsers.external.ExternalParser;
 import dpf.sp.gpinf.indexer.parsers.util.EmbeddedItem;
 import dpf.sp.gpinf.indexer.parsers.util.EmbeddedParent;
@@ -221,7 +221,7 @@ public class ParsingTask extends AbstractTask implements EmbeddedDocumentExtract
         reader.close();
     }
 
-    private static boolean isToBeExpanded(HashSet<String> categories) {
+    private static boolean isToBeExpanded(Collection<String> categories) {
 
         if (!expandContainers) {
             return false;

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/ItemInfoFactory.java
@@ -6,7 +6,7 @@ import iped3.IItem;
 public class ItemInfoFactory {
 
     public static ItemInfo getItemInfo(IItem evidence) {
-        ItemInfo info = new ItemInfo(evidence.getId(), evidence.getHash(), evidence.getCategorySet(),
+        ItemInfo info = new ItemInfo(evidence.getId(), evidence.getHash(), evidence.getLabels(), evidence.getCategorySet(),
                 evidence.getPath(), evidence.isCarved());
         // info.setEvidence(evidence);
         return info;

iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/OCRParser.java
@@ -186,10 +186,15 @@ public class OCRParser extends AbstractParser {
         if (bookmarksToOCR.size() == 0)
             return true;
 
-        for (String bookmarkToOCR : bookmarksToOCR)
+        for (String group : bookmarksToOCR) {
             for (String bookmark : ocrContext.getBookmarks())
-                if (bookmarkToOCR.equalsIgnoreCase(bookmark))
+                if (group.equalsIgnoreCase(bookmark))
                     return true;
+            for (String category : ocrContext.getCategories())
+                if (group.equalsIgnoreCase(category))
+                    return true;
+        }
+            
         return false;
     }
 

iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/util/ItemInfo.java
@@ -19,6 +19,7 @@
 package dpf.sp.gpinf.indexer.parsers.util;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 
 /**
@@ -36,14 +37,16 @@ public class ItemInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private int child = -1, id;
-    private HashSet<String> bookmarks;
+    private Collection<String> bookmarks;
+    private Collection<String> categories;
     private String path, hash;
     private boolean carved = false;
 
-    public ItemInfo(int id, String hash, HashSet<String> bookmarks, String path, boolean carved) {
+    public ItemInfo(int id, String hash, Collection<String> bookmarks, Collection<String> categories, String path, boolean carved) {
         this.id = id;
         this.hash = hash;
         this.bookmarks = bookmarks;
+        this.categories = categories;
         this.path = path;
         this.setCarved(carved);
     }
@@ -76,8 +79,12 @@ public class ItemInfo implements Serializable {
         this.bookmarks = bookmarks;
     }
 
-    public HashSet<String> getBookmarks() {
+    public Collection<String> getBookmarks() {
         return bookmarks;
+    }
+    
+    public Collection<String> getCategories() {
+        return categories;
     }
 
     public boolean isCarved() {

commit 49e73e7dd1b66ea8e5558ecc2d5a4c76429cf1f3

fix containers not being expanded (caused by commit b5a8b74)

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ParsingTask.java
@@ -177,7 +177,7 @@ public class ParsingTask extends AbstractTask implements EmbeddedDocumentExtract
         if (output != null && worker != null)
             context.set(IItemSearcher.class, new ItemSearcher(output.getParentFile(), worker.writer));
 
-        extractEmbedded = isToBeExpanded(itemInfo.getBookmarks());
+        extractEmbedded = isToBeExpanded(itemInfo.getCategories());
         if (extractEmbedded) {
             context.set(EmbeddedDocumentExtractor.class, this);
         } else