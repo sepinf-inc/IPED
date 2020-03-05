7bd688b15c327f4b0c358475b5cada78ec5f1c71
Luis Filipe <lfcnassif@gmail.com>
2020-03-05 16:45:23 -0300

    keep instance of imageMagick converter and internal thread pool

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/HTMLReportTask.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/HTMLReportTask.java
@@ -229,6 +229,8 @@ public class HTMLReportTask extends AbstractTask {
      * geração de miniaturas.
      */
     private static final Set<String> currentFiles = new HashSet<String>();
+    
+    private static GraphicsMagicConverter graphicsMagicConverter = new GraphicsMagicConverter();
 
     /**
      * Armazena modelo de formatação no nome/mat/classe do(s) perito(s).
@@ -381,7 +383,7 @@ public class HTMLReportTask extends AbstractTask {
      */
     @Override
     public void finish() throws Exception {
-
+    	
         if (taskEnabled && caseData.containsReport() && info != null) {
 
             String reportRoot = Messages.getString("HTMLReportTask.ReportFileName"); //$NON-NLS-1$
@@ -449,6 +451,8 @@ public class HTMLReportTask extends AbstractTask {
 
             t = (System.currentTimeMillis() - t + 500) / 1000;
             Log.info(taskName, "Report creation time (seconds): " + t); //$NON-NLS-1$
+            
+            graphicsMagicConverter.close();
         }
     }
 
@@ -950,7 +954,7 @@ public class HTMLReportTask extends AbstractTask {
                 if (img == null) {
                     stream = evidence.getBufferedStream();
                     try {
-                        img = new GraphicsMagicConverter().getImage(stream, thumbSize * sampleFactor);
+                        img = graphicsMagicConverter.getImage(stream, thumbSize * sampleFactor);
                     } finally {
                         IOUtil.closeQuietly(stream);
                     }
