sepinf-inc/IPED

commit 60bce4c7fd657cf12573651274364e6b343bbbfc

show error msg instead of aborting when optional databases are not found

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/KFFTask.java
@@ -20,9 +20,7 @@ import org.mapdb.Serializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dpf.sp.gpinf.indexer.CmdLineArgs;
 import dpf.sp.gpinf.indexer.Configuration;
-import dpf.sp.gpinf.indexer.process.Worker;
 import dpf.sp.gpinf.indexer.process.task.HashTask.HashValue;
 import dpf.sp.gpinf.indexer.util.IPEDException;
 import iped3.IItem;
@@ -61,7 +59,7 @@ public class KFFTask extends AbstractTask {
     private static Map<Integer, String[]> products;
     private static Set<String> alertProducts;
     private static DB db;
-    private static boolean taskEnabled = false;
+    private static Boolean taskEnabled;
 
     private boolean excludeKffIgnorable = true;
     private boolean md5 = true;
@@ -74,7 +72,7 @@ public class KFFTask extends AbstractTask {
     public void init(Properties confParams, File confDir) throws Exception {
 
         String hashes = confParams.getProperty("hash"); //$NON-NLS-1$
-        if (hashes == null) {
+        if (hashes == null || (taskEnabled != null && !taskEnabled)) {
             return;
         }
         if (hashes.contains("md5")) { //$NON-NLS-1$
@@ -90,8 +88,12 @@ public class KFFTask extends AbstractTask {
         excludeKffIgnorable = Boolean.valueOf(confParams.getProperty("excludeKffIgnorable").trim()); //$NON-NLS-1$
 
         String kffDbPath = confParams.getProperty("kffDb"); //$NON-NLS-1$
-        if (taskEnabled && kffDbPath == null)
-            throw new IPEDException("Configure hash database path on " + Configuration.LOCAL_CONFIG); //$NON-NLS-1$
+        if (taskEnabled && kffDbPath == null) {
+            String msg = "Configure hash database path on " + Configuration.LOCAL_CONFIG; //$NON-NLS-1$
+            LOGGER.error(msg);
+            taskEnabled = false;
+            return;
+        }
 
         // backwards compatibility
         if (enableParam == null && kffDbPath != null)
@@ -106,15 +108,9 @@ public class KFFTask extends AbstractTask {
             File kffDb = new File(kffDbPath.trim());
             if (!kffDb.exists()) {
                 String msg = "Invalid hash database path on " + kffDb.getAbsolutePath(); //$NON-NLS-1$
-                CmdLineArgs args = (CmdLineArgs) caseData.getCaseObject(CmdLineArgs.class.getName());
-                for (File source : args.getDatasources()) {
-                    if (source.getName().endsWith(".iped")) {
-                        LOGGER.warn(msg);
-                        taskEnabled = false;
-                        return;
-                    }
-                }
-                throw new IPEDException(msg);
+                LOGGER.error(msg);
+                taskEnabled = false;
+                return;
             }
 
             try {

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/LedKFFTask.java
@@ -19,15 +19,13 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import dpf.sp.gpinf.indexer.CmdLineArgs;
 import dpf.sp.gpinf.indexer.Configuration;
 import dpf.sp.gpinf.indexer.parsers.util.LedHashes;
-import dpf.sp.gpinf.indexer.process.Worker;
 import dpf.sp.gpinf.indexer.util.HashValue;
 import dpf.sp.gpinf.indexer.util.IPEDException;
 import dpf.sp.gpinf.indexer.util.Log;
-import iped3.IItem;
 import iped3.IHashValue;
+import iped3.IItem;
 
 /**
  * Tarefa de consulta a base de hashes do LED. Pode ser removida no futuro e ser
@@ -51,7 +49,7 @@ public class LedKFFTask extends AbstractTask {
     private static final int idxLength = 5;
     private static final int idxName = 6;
     private static final String ENABLE_PARAM = "enableLedWkff"; //$NON-NLS-1$
-    private static boolean taskEnabled = false;
+    private static Boolean taskEnabled;
 
     public static void setEnabled(boolean enabled) {
         taskEnabled = enabled;
@@ -61,7 +59,7 @@ public class LedKFFTask extends AbstractTask {
     public void init(Properties confParams, File confDir) throws Exception {
 
         synchronized (lock) {
-            if (hashArrays != null) {
+            if (hashArrays != null || (taskEnabled != null && !taskEnabled)) {
                 return;
             }
 
@@ -71,8 +69,12 @@ public class LedKFFTask extends AbstractTask {
 
             String hash = confParams.getProperty("hash"); //$NON-NLS-1$
             String ledWkffPath = confParams.getProperty("ledWkffPath"); //$NON-NLS-1$
-            if (taskEnabled && ledWkffPath == null)
-                throw new IPEDException("Configure LED database path on " + Configuration.LOCAL_CONFIG); //$NON-NLS-1$
+            if (taskEnabled && ledWkffPath == null) {
+                String msg = "Configure LED database path on " + Configuration.LOCAL_CONFIG;
+                logger.error(msg);
+                taskEnabled = false;
+                return;
+            }
 
             // backwards compatibility
             if (enabled == null && ledWkffPath != null)
@@ -84,15 +86,9 @@ public class LedKFFTask extends AbstractTask {
             File wkffDir = new File(ledWkffPath.trim());
             if (!wkffDir.exists()) {
                 String msg = "Invalid LED database path: " + wkffDir.getAbsolutePath(); //$NON-NLS-1$
-                CmdLineArgs args = (CmdLineArgs) caseData.getCaseObject(CmdLineArgs.class.getName());
-                for (File source : args.getDatasources()) {
-                    if (source.getName().endsWith(".iped")) {
-                        logger.warn(msg);
-                        taskEnabled = false;
-                        return;
-                    }
-                }
-                throw new IPEDException(msg);
+                logger.error(msg);
+                taskEnabled = false;
+                return;
             }
 
             if (hash == null || hash.trim().isEmpty())

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/PhotoDNALookup.java
@@ -16,8 +16,6 @@ import com.eatthepath.jvptree.DistanceFunction;
 import com.eatthepath.jvptree.VPTree;
 
 import br.dpf.sepinf.photodna.PhotoDNATransforms;
-import dpf.sp.gpinf.indexer.CmdLineArgs;
-import dpf.sp.gpinf.indexer.util.IPEDException;
 import iped3.IItem;
 
 public class PhotoDNALookup extends AbstractTask{
@@ -57,15 +55,9 @@ public class PhotoDNALookup extends AbstractTask{
                 File photoDnaHashSet = new File(path.trim());
                 if (!photoDnaHashSet.exists()) {
                     String msg = "Invalid hash database path on " + photoDnaHashSet.getAbsolutePath(); //$NON-NLS-1$
-                    CmdLineArgs args = (CmdLineArgs) caseData.getCaseObject(CmdLineArgs.class.getName());
-                    for (File source : args.getDatasources()) {
-                        if (source.getName().endsWith(".iped")) {
-                            LOGGER.warn(msg);
-                            taskEnabled = false;
-                            return;
-                        }
-                    }
-                    throw new IPEDException(msg);
+                    LOGGER.error(msg);
+                    taskEnabled = false;
+                    return;
                 }
                 try (BufferedReader bf = new BufferedReader(new FileReader(photoDnaHashSet))){
                     String line = null;

commit 43370333c8c34f56c9e91a4f934a23642295e375

fix NPE with fastmode profile (caused by 60bce4c)

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/KFFTask.java
@@ -59,7 +59,7 @@ public class KFFTask extends AbstractTask {
     private static Map<Integer, String[]> products;
     private static Set<String> alertProducts;
     private static DB db;
-    private static Boolean taskEnabled;
+    private static boolean taskEnabled = true;
 
     private boolean excludeKffIgnorable = true;
     private boolean md5 = true;
@@ -78,7 +78,7 @@ public class KFFTask extends AbstractTask {
     public void init(Properties confParams, File confDir) throws Exception {
 
         String hashes = confParams.getProperty("hash"); //$NON-NLS-1$
-        if (hashes == null || (taskEnabled != null && !taskEnabled)) {
+        if (hashes == null || !taskEnabled) {
             return;
         }
         if (hashes.contains("md5")) { //$NON-NLS-1$