0a8a3d37827202495d83cb79472f553a23f887aa
Matheus <Matheus@Matheus-PC>
2020-03-17 22:30:42 -0300

    Extract Timeline registries as subitems

---
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/winx/parsers/TimelineEntry.java
@@ -0,0 +1,297 @@
+package dpf.inc.sepinf.winx.parsers;
+
+/**
+ * Windows 10 Timeline Bean
+ * @author Matheus Bichara de Assumpção <bda.matheus@gmail.com>
+ */
+
+public class TimelineEntry {
+
+	private String etag; 
+	private String order; 
+	private String application; 
+	private String displayName; 
+	private String fileOpened; 
+	private String fullPath; 
+	private String content_url; 
+	private String info; 
+	private String appActivityId; 
+	private String payload_Timezone; 
+	private String activity_type; 
+	private String group; 
+	private String synced; 
+	private String platform; 
+	private String tileStatus; 
+	private String wasRemoved; 
+	private String uploadQueue; 
+	private String isLocalOnly; 
+	private String app_Uri; 
+	private String priority; 
+	private String activeDuration; 
+	private String calculatedDuration; 
+	private String startTime; 
+	private String lastModified; 
+	private String lastModifiedOnClient; 
+	private String endTime; 
+	private String createdInCloud; 
+	private String expiresIndays; 
+	private String expirationOnPackageID; 
+	private String expiration; 
+	private String tag; 
+	private String matchID; 
+	private String deviceID; 
+	private String packageIdHash; 
+	private String id; 
+	private String clipboardTextBase64; 
+	private String groupAppActivityId; 
+	private String enterpriseId; 
+	private String parentActivityId; 
+	private String originalPayload;
+	
+	
+	
+	public String getEtag() {
+		return etag;
+	}
+	public String getOrder() {
+		return order;
+	}
+	public String getApplication() {
+		return application;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public String getFileOpened() {
+		return fileOpened;
+	}
+	public String getFullPath() {
+		return fullPath;
+	}
+	public String getContent_url() {
+		return content_url;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public String getAppActivityId() {
+		return appActivityId;
+	}
+	public String getPayload_Timezone() {
+		return payload_Timezone;
+	}
+	public String getActivity_type() {
+		return activity_type;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public String getSynced() {
+		return synced;
+	}
+	public String getPlatform() {
+		return platform;
+	}
+	public String getTileStatus() {
+		return tileStatus;
+	}
+	public String getWasRemoved() {
+		return wasRemoved;
+	}
+	public String getUploadQueue() {
+		return uploadQueue;
+	}
+	public String getIsLocalOnly() {
+		return isLocalOnly;
+	}
+	public String getApp_Uri() {
+		return app_Uri;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public String getActiveDuration() {
+		return activeDuration;
+	}
+	public String getCalculatedDuration() {
+		return calculatedDuration;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public String getLastModified() {
+		return lastModified;
+	}
+	public String getLastModifiedOnClient() {
+		return lastModifiedOnClient;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public String getCreatedInCloud() {
+		return createdInCloud;
+	}
+	public String getExpiresIndays() {
+		return expiresIndays;
+	}
+	public String getExpirationOnPackageID() {
+		return expirationOnPackageID;
+	}
+	public String getExpiration() {
+		return expiration;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public String getMatchID() {
+		return matchID;
+	}
+	public String getDeviceID() {
+		return deviceID;
+	}
+	public String getPackageIdHash() {
+		return packageIdHash;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getClipboardTextBase64() {
+		return clipboardTextBase64;
+	}
+	public String getGroupAppActivityId() {
+		return groupAppActivityId;
+	}
+	public String getEnterpriseId() {
+		return enterpriseId;
+	}
+	public String getParentActivityId() {
+		return parentActivityId;
+	}
+	public String getOriginalPayload() {
+		return originalPayload;
+	}
+	public void setEtag(String etag) {
+		this.etag = etag;
+	}
+	public void setOrder(String order) {
+		this.order = order;
+	}
+	public void setApplication(String application) {
+		this.application = application;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public void setFileOpened(String fileOpened) {
+		this.fileOpened = fileOpened;
+	}
+	public void setFullPath(String fullPath) {
+		this.fullPath = fullPath;
+	}
+	public void setContent_url(String content_url) {
+		this.content_url = content_url;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public void setAppActivityId(String appActivityId) {
+		this.appActivityId = appActivityId;
+	}
+	public void setPayload_Timezone(String payload_Timezone) {
+		this.payload_Timezone = payload_Timezone;
+	}
+	public void setActivity_type(String activity_type) {
+		this.activity_type = activity_type;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	public void setSynced(String synced) {
+		this.synced = synced;
+	}
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+	public void setTileStatus(String tileStatus) {
+		this.tileStatus = tileStatus;
+	}
+	public void setWasRemoved(String wasRemoved) {
+		this.wasRemoved = wasRemoved;
+	}
+	public void setUploadQueue(String uploadQueue) {
+		this.uploadQueue = uploadQueue;
+	}
+	public void setIsLocalOnly(String isLocalOnly) {
+		this.isLocalOnly = isLocalOnly;
+	}
+	public void setApp_Uri(String app_Uri) {
+		this.app_Uri = app_Uri;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public void setActiveDuration(String activeDuration) {
+		this.activeDuration = activeDuration;
+	}
+	public void setCalculatedDuration(String calculatedDuration) {
+		this.calculatedDuration = calculatedDuration;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public void setLastModified(String lastModified) {
+		this.lastModified = lastModified;
+	}
+	public void setLastModifiedOnClient(String lastModifiedOnClient) {
+		this.lastModifiedOnClient = lastModifiedOnClient;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public void setCreatedInCloud(String createdInCloud) {
+		this.createdInCloud = createdInCloud;
+	}
+	public void setExpiresIndays(String expiresIndays) {
+		this.expiresIndays = expiresIndays;
+	}
+	public void setExpirationOnPackageID(String expirationOnPackageID) {
+		this.expirationOnPackageID = expirationOnPackageID;
+	}
+	public void setExpiration(String expiration) {
+		this.expiration = expiration;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public void setMatchID(String matchID) {
+		this.matchID = matchID;
+	}
+	public void setDeviceID(String deviceID) {
+		this.deviceID = deviceID;
+	}
+	public void setPackageIdHash(String packageIdHash) {
+		this.packageIdHash = packageIdHash;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setClipboardTextBase64(String clipboardTextBase64) {
+		this.clipboardTextBase64 = clipboardTextBase64;
+	}
+	public void setGroupAppActivityId(String groupAppActivityId) {
+		this.groupAppActivityId = groupAppActivityId;
+	}
+	public void setEnterpriseId(String enterpriseId) {
+		this.enterpriseId = enterpriseId;
+	}
+	public void setParentActivityId(String parentActivityId) {
+		this.parentActivityId = parentActivityId;
+	}
+	public void setOriginalPayload(String originalPayload) {
+		this.originalPayload = originalPayload;
+	}
+	
+	
+}
+
+
