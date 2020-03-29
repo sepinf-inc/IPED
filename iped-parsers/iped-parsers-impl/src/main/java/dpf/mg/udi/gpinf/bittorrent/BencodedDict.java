ff10b5da121bd5fa7e3b3712734f31179dfa30fa
Fabio Melo Pfeifer <pfeifer.fmp@dpf.gov.br>
2020-03-29 16:36:00 -0300

    Add reading of encoded byte buffers from bencoded data.

--- iped-parsers/iped-parsers-impl/src/main/java/dpf/mg/udi/gpinf/bittorrent/BencodedDict.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/mg/udi/gpinf/bittorrent/BencodedDict.java
@@ -2,6 +2,8 @@ package dpf.mg.udi.gpinf.bittorrent;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -22,7 +24,7 @@ public class BencodedDict {
     private DateFormat df;
 
     public BencodedDict(InputStream is, DateFormat df) throws IOException {
-        dict = new BencodeInputStream(is).readDictionary();
+        dict = new BencodeInputStream(is, StandardCharsets.UTF_8, true).readDictionary();
         this.df = df;
     }
 
@@ -34,7 +36,7 @@ public class BencodedDict {
     public boolean containsKey(String key) {
         return dict.containsKey(key);
     }
-    
+
     public Set<String> keySet() {
         return dict.keySet();
     }
@@ -66,10 +68,11 @@ public class BencodedDict {
 
     public String getString(String key) {
         Object obj = getObject(key);
-        if (obj == null || !(obj instanceof String)) {
+        if (obj == null || !(obj instanceof ByteBuffer)) {
             return ""; //$NON-NLS-1$
         }
-        return (String) obj;
+        ByteBuffer buffer = (ByteBuffer) obj;
+        return new String(buffer.array(), StandardCharsets.UTF_8);
     }
 
     public BencodedDict getDict(String key) {
@@ -78,36 +81,41 @@ public class BencodedDict {
             return null;
         }
 
-        @SuppressWarnings("unchecked")
+        @SuppressWarnings("unchecked") //$NON-NLS-1$
         Map<String, Object> childDict = (Map<String, Object>) obj;
         return new BencodedDict(childDict, df);
     }
-    
+
     public List<Object> getList(String key) {
         Object obj = getObject(key);
         if (obj == null || !(obj instanceof List)) {
             return null;
         }
-        
-        @SuppressWarnings("unchecked")
+
+        @SuppressWarnings("unchecked") //$NON-NLS-1$
         List<Object> theList = (List<Object>) obj;
         List<Object> resp = new ArrayList<>(theList.size());
-        
-        for (Object elem: theList) {
+
+        for (Object elem : theList) {
             if (elem instanceof Map) {
-                @SuppressWarnings("unchecked")
+                @SuppressWarnings("unchecked") //$NON-NLS-1$
                 Map<String, Object> childDict = (Map<String, Object>) elem;
                 resp.add(new BencodedDict(childDict, df));
+            } else if (elem instanceof ByteBuffer) {
+                ByteBuffer buff = (ByteBuffer) elem;
+                resp.add(new String(buff.array(), StandardCharsets.UTF_8));
             } else {
                 resp.add(elem);
             }
         }
-        
+
         return resp;
     }
-    
+
     /**
-     * Returns a list of all String objects in list. Ignore elements that are not Strings.
+     * Returns a list of all String objects in list. Ignore elements that are not
+     * Strings.
+     * 
      * @param key
      * @return
      */
@@ -115,12 +123,61 @@ public class BencodedDict {
         List<String> resp = new ArrayList<>();
         List<Object> listOfObj = getList(key);
         if (listOfObj != null) {
-            for (Object obj: listOfObj) {
-                if (obj instanceof String) {
+            for (Object obj : listOfObj) {
+                if (obj instanceof ByteBuffer) {
+                    ByteBuffer buff = (ByteBuffer) obj;
+                    resp.add(new String(buff.array(), StandardCharsets.UTF_8));
+                } else if (obj instanceof String) {
                     resp.add((String) obj);
                 }
             }
         }
         return resp;
+    }
+
+    public String getHexEncodedBytes(String key) {
+        String resp = ""; //$NON-NLS-1$
+
+        if (containsKey(key)) {
+            Object objBuffer = getObject(key);
+            if (objBuffer != null && objBuffer instanceof ByteBuffer) {
+                ByteBuffer buffer = (ByteBuffer) objBuffer;
+                resp = new String(encodeHex(buffer.array()));
+            }
+        }
+
+        return resp;
+    }
+    
+    /**
+     * Used to build output as Hex
+     */
+    private static final char[] DIGITS_LOWER =
+        {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    
+    /**
+     * Copied from apache commons codec binary.
+     * 
+     * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+     * The returned array will be double the length of the passed array, as it takes two characters to represent any
+     * given byte.
+     *
+     * @param data
+     *            a byte[] to convert to Hex characters
+     * @param toDigits
+     *            the output alphabet (must contain at least 16 chars)
+     * @return A char[] containing the appropriate characters from the alphabet
+     *         For best results, this should be either upper- or lower-case hex.
+     * @since 1.4
+     */
+    private static char[] encodeHex(final byte[] data) {
+        final int l = data.length;
+        final char[] out = new char[l << 1];
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS_LOWER[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS_LOWER[0x0F & data[i]];
+        }
+        return out;
     }
 }
