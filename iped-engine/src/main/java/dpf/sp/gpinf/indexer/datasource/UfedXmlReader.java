sepinf-inc/IPED

commit ea96c7f10d3a2c36374e91cec65e99e986ce4e00

fix date parse exceptions
Conflicts:
	iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/ufed/UFEDChatParser.java

iped-utils/src/main/java/dpf/sp/gpinf/indexer/util/DateUtil.java
@@ -46,6 +46,7 @@ public class DateUtil {
         dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", UTC));   // UTC/Zulu
         dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", null));    // With timezone
         dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss", null));     // Without timezone
+        dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", null));
         // yyyy-mm-dd hh...
         dateFormats.add(createDateFormat("yyyy-MM-dd' 'HH:mm:ss'Z'", UTC));   // UTC/Zulu
         dateFormats.add(createDateFormat("yyyy-MM-dd' 'HH:mm:ssZ", null));    // With timezone

iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/ufed/UFEDChatParser.java
@@ -26,6 +26,7 @@ import org.xml.sax.SAXException;
 
 import dpf.mg.udi.gpinf.whatsappextractor.Message;
 import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
+import dpf.sp.gpinf.indexer.util.DateUtil;
 import iped3.io.IItemBase;
 import iped3.search.IItemSearcher;
 import iped3.util.BasicProps;
@@ -72,7 +73,6 @@ public class UFEDChatParser extends AbstractParser {
             String query = BasicProps.PARENTID + ":" + chat.getId(); //$NON-NLS-1$
             List<IItemBase> msgs = searcher.search(query);
 
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX"); //$NON-NLS-1$
             List<Message> messages = new ArrayList<>();
 
             for (IItemBase msg : msgs) {
@@ -81,13 +81,10 @@ public class UFEDChatParser extends AbstractParser {
                 m.setData(msg.getMetadata().get(ExtraProperties.MESSAGE_BODY));
                 m.setFromMe(Boolean.valueOf(msg.getMetadata().get(META_FROM_OWNER)));
                 String str = msg.getMetadata().get(ExtraProperties.MESSAGE_DATE);
-                if (str != null)
-                    try {
-                        Date date = df.parse(str);
-                        m.setTimeStamp(date);
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
+                if (str != null) {
+                    Date date = DateUtil.tryToParseDate(str);
+                    m.setTimeStamp(date);
+                }
                 if (!m.isFromMe()) {
                     m.setRemoteResource(msg.getMetadata().get(org.apache.tika.metadata.Message.MESSAGE_FROM));
                     m.setLocalResource(msg.getMetadata().get(org.apache.tika.metadata.Message.MESSAGE_TO));

commit 98a517969ceb6b254e67d202eb6a4193943a08ed

fix timezone issue caused by ea96c7f, throw date parse exceptions

iped-engine/src/main/java/dpf/sp/gpinf/indexer/datasource/UfedXmlReader.java
@@ -39,7 +39,6 @@ import dpf.sp.gpinf.indexer.Messages;
 import dpf.sp.gpinf.indexer.config.ConfigurationManager;
 import dpf.sp.gpinf.indexer.config.UFEDReaderConfig;
 import dpf.sp.gpinf.indexer.parsers.ufed.UFEDChatParser;
-import dpf.sp.gpinf.indexer.process.IndexItem;
 import dpf.sp.gpinf.indexer.util.IOUtil;
 import dpf.sp.gpinf.indexer.util.MetadataInputStreamFactory;
 import dpf.sp.gpinf.indexer.util.SimpleHTMLEncoder;
@@ -279,6 +278,7 @@ public class UfedXmlReader extends DataSourceReader {
         String df2Pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS"; //$NON-NLS-1$
         DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"); //$NON-NLS-1$
         DateFormat df2 = new SimpleDateFormat(df2Pattern);
+        DateFormat out = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
         ArrayList<XmlNode> nodeSeq = new ArrayList<>();
         ArrayList<Item> itemSeq = new ArrayList<>();
@@ -341,6 +341,7 @@ public class UfedXmlReader extends DataSourceReader {
             // TODO remover timezone da exibição? obter da linha de comando?
             df2.setTimeZone(TimeZone.getTimeZone("GMT")); //$NON-NLS-1$
             df1.setTimeZone(TimeZone.getTimeZone("GMT")); //$NON-NLS-1$
+            out.setTimeZone(TimeZone.getTimeZone("GMT")); //$NON-NLS-1$
         }
 
         @Override
@@ -594,14 +595,14 @@ public class UfedXmlReader extends DataSourceReader {
                         DateFormat df = df1;
                         if (df2Pattern.length() - 2 == value.length())
                             df = df2;
-                        if (type.equals("TimeStamp")) //$NON-NLS-1$
+                        if (type.equals("TimeStamp") && !value.isEmpty()) //$NON-NLS-1$
                             try {
-                                item.getMetadata().add(meta, df.format(df.parse(value)));
+                                item.getMetadata().add(meta, out.format(df.parse(value)));
                             } catch (ParseException e) {
-                                e.printStackTrace();
+                                throw new SAXException(e);
                             }
-                        else
-                            if(item != null) item.getMetadata().add(meta, value);
+                        else if(item != null && !value.isEmpty())
+                            item.getMetadata().add(meta, value);
                     }
                 }
             } else if (qName.equals("targetid") && parentNode.element.equals("jumptargets")) { //$NON-NLS-1$ //$NON-NLS-2$
@@ -637,7 +638,6 @@ public class UfedXmlReader extends DataSourceReader {
                     String source = item.getMetadata().get(ExtraProperties.UFED_META_PREFIX + "Source"); //$NON-NLS-1$
                     if ("whatsapp".equalsIgnoreCase(source)) //$NON-NLS-1$
                         item.setMediaType(UFEDChatParser.UFED_CHAT_WA_MIME);
-                    item.setExtraAttribute(IndexItem.TREENODE, "true"); //$NON-NLS-1$
                 }
                 if ("InstantMessage".equals(type) || "Email".equals(type)) { //$NON-NLS-1$ //$NON-NLS-2$
                     String date = item.getMetadata().get(ExtraProperties.UFED_META_PREFIX + "TimeStamp"); //$NON-NLS-1$

iped-utils/src/main/java/dpf/sp/gpinf/indexer/util/DateUtil.java
@@ -45,8 +45,8 @@ public class DateUtil {
         // yyyy-mm-ddThh...
         dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", UTC));   // UTC/Zulu
         dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", null));    // With timezone
+        dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", null));// With timezone
         dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss", null));     // Without timezone
-        dateFormats.add(createDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", null));
         // yyyy-mm-dd hh...
         dateFormats.add(createDateFormat("yyyy-MM-dd' 'HH:mm:ss'Z'", UTC));   // UTC/Zulu
         dateFormats.add(createDateFormat("yyyy-MM-dd' 'HH:mm:ssZ", null));    // With timezone