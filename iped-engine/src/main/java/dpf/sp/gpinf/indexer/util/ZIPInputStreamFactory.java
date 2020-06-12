e0b3af5990502ad602a4f2008ff00238e3c7759c
Luis Nassif <lfcnassif@gmail.com>
2020-06-12 14:04:46 -0300

    first version of cache for ufdr items after uncompression

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/ZIPInputStreamFactory.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/ZIPInputStreamFactory.java
@@ -7,6 +7,10 @@ import java.nio.channels.ClosedChannelException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
 import org.apache.commons.io.IOUtils;
@@ -18,6 +22,22 @@ import net.lingala.zip4j.model.FileHeader;
 public class ZIPInputStreamFactory extends SeekableInputStreamFactory implements Closeable {
 
     private static int MAX_MEM_BYTES = 1 << 24;
+    
+    private static int MAX_CACHE = 1 << 28;
+    
+    private static int cacheSize = 0;
+    
+    //TODO cache must be per evience/ufdr to not mix content
+    private static Map<String, byte[]> cache = new LinkedHashMap<String, byte[]>(128, 0.75f, true) {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<String, byte[]> eldest) {
+            //return this.size() > MAX_CACHE;
+            return false;
+        }
+    };
 
     private ZipFile4j zip;
 
@@ -35,6 +55,11 @@ public class ZIPInputStreamFactory extends SeekableInputStreamFactory implements
     public SeekableInputStream getSeekableInputStream(String path) throws IOException {
         Path tmp = null;
         byte[] bytes = null;
+        synchronized(cache) {
+            bytes = cache.get(path);
+        }
+        if(bytes != null) return new SeekableFileInputStream(new SeekableInMemoryByteChannel(bytes));
+        
         FileHeader zae;
         try {
             if (zip == null)
@@ -49,6 +74,18 @@ public class ZIPInputStreamFactory extends SeekableInputStreamFactory implements
         try (InputStream is = zip.getInputStream(zae)) {
             if (zae.getUncompressedSize() <= MAX_MEM_BYTES) {
                 bytes = IOUtils.toByteArray(is);
+                synchronized(cache) {
+                    cache.put(path, bytes);
+                    cacheSize += bytes.length;
+                    if(cacheSize > MAX_CACHE) {
+                        Iterator<byte[]> i = cache.values().iterator();
+                        while(cacheSize > MAX_CACHE && i.hasNext()){
+                            byte[] eldest = i.next();
+                            i.remove();
+                            cacheSize -= eldest.length;
+                        }
+                    }
+                }
             } else {
                 tmp = Files.createTempFile("zip-stream", null);
                 Files.copy(is, tmp, StandardCopyOption.REPLACE_EXISTING);
