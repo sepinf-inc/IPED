sepinf-inc/IPED

commit 8aa9d97d60838414ddddde9e27f5585d437b6156

Closes #141: simple button to clear all filters

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/AppListener.java
@@ -33,9 +33,11 @@ import javax.swing.JOptionPane;
 
 import org.apache.lucene.search.Query;
 
-public class AppListener implements ActionListener, MouseListener {
-
-    volatile boolean clearSearchBox = false;
+public class AppListener implements ActionListener, MouseListener, ClearFilterListener {
+    
+    private String texto = ""; //$NON-NLS-1$
+    private boolean clearAllFilters = false;
+    private boolean clearSearchBox = false;
 
     public void updateFileListing() {
         updateFileListing(null);
@@ -73,23 +75,6 @@ public class AppListener implements ActionListener, MouseListener {
         App.get().parentItemModel.results = new LuceneSearchResult(0);
         App.get().parentItemModel.fireTableDataChanged();
 
-        String texto = ""; //$NON-NLS-1$
-        if (App.get().termo.getSelectedItem() != null) {
-            texto = App.get().termo.getSelectedItem().toString();
-            if (texto.equals(MarcadoresController.HISTORY_DIV) || texto.equals(App.SEARCH_TOOL_TIP)) {
-                texto = ""; //$NON-NLS-1$
-                clearSearchBox = true;
-                App.get().termo.setSelectedItem(""); //$NON-NLS-1$
-            }
-            texto = texto.trim();
-            MarcadoresController.get().addToRecentSearches(texto);
-        }
-
-        if (!texto.isEmpty())
-            App.get().termo.setBorder(BorderFactory.createLineBorder(App.get().alertColor, 2, true));
-        else
-            App.get().termo.setBorder(null);
-
         try {
             PesquisarIndice task;
             if (query == null)
@@ -107,9 +92,10 @@ public class AppListener implements ActionListener, MouseListener {
 
     @Override
     public void actionPerformed(ActionEvent evt) {
+        
+        boolean updateFileList = false;
 
-        if (!clearSearchBox && (evt.getActionCommand().equals("comboBoxChanged")) && //$NON-NLS-1$
-                !App.get().filterManager.isUpdatingFilter() && !MarcadoresController.get().updatingHistory) {
+        if (evt.getSource() == App.get().filtro && !App.get().filterManager.isUpdatingFilter() ) {
 
             int filterIndex = App.get().filtro.getSelectedIndex();
             if (filterIndex == 0 || filterIndex == -1) {
@@ -118,16 +104,39 @@ public class AppListener implements ActionListener, MouseListener {
                 App.get().filtro.setBackground(App.get().alertColor);
             }
 
-            updateFileListing();
+            updateFileList = true;
+        }
+        
+        if(evt.getSource() == App.get().termo && !clearSearchBox && !MarcadoresController.get().isUpdatingHistory()) {
+            if (App.get().termo.getSelectedItem() != null) {
+                texto = App.get().termo.getSelectedItem().toString();
+                if (texto.equals(MarcadoresController.HISTORY_DIV) || texto.equals(App.SEARCH_TOOL_TIP)) {
+                    texto = ""; //$NON-NLS-1$
+                    clearSearchBox = true;
+                    App.get().termo.setSelectedItem(""); //$NON-NLS-1$
+                }
+                texto = texto.trim();
+                MarcadoresController.get().addToRecentSearches(texto);
+            }
 
+            if (!texto.isEmpty())
+                App.get().termo.setBorder(BorderFactory.createLineBorder(App.get().alertColor, 2, true));
+            else
+                App.get().termo.setBorder(null);
+            
+            updateFileList = true;
         }
 
         if (evt.getSource() == App.get().filterDuplicates) {
-            if (App.get().filterDuplicates.getForeground() == App.get().alertColor)
+            if (!App.get().filterDuplicates.isSelected())
                 App.get().filterDuplicates.setForeground(App.get().topPanel.getBackground());
             else
                 App.get().filterDuplicates.setForeground(App.get().alertColor);
 
+            updateFileList = true;
+        }
+        
+        if(!clearAllFilters && updateFileList) {
             updateFileListing();
         }
 
@@ -201,6 +210,16 @@ public class AppListener implements ActionListener, MouseListener {
     public void mouseReleased(MouseEvent arg0) {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public void clearFilter() {
+        clearAllFilters = true;
+        App.get().filtro.setSelectedIndex(0);
+        App.get().termo.setSelectedItem(""); //$NON-NLS-1$
+        if(App.get().filterDuplicates.isSelected())
+            App.get().filterDuplicates.doClick();
+        clearAllFilters = false;
     }
 
 }

iped-app/src/main/java/br/gov/pf/labld/graph/desktop/AppGraphAnalytics.java
@@ -60,10 +60,11 @@ import br.gov.pf.labld.graph.desktop.renderers.PeopleNodeRenderer;
 import br.gov.pf.labld.graph.desktop.renderers.PersonNodeRenderer;
 import br.gov.pf.labld.graph.desktop.renderers.PhoneNodeRenderer;
 import dpf.sp.gpinf.indexer.desktop.App;
+import dpf.sp.gpinf.indexer.desktop.ClearFilterListener;
 import dpf.sp.gpinf.indexer.desktop.Messages;
 import dpf.sp.gpinf.indexer.search.ItemId;
 
-public class AppGraphAnalytics extends JPanel {
+public class AppGraphAnalytics extends JPanel implements ClearFilterListener {
 
   static Logger LOGGER = LoggerFactory.getLogger(AppGraphAnalytics.class);
 
@@ -253,12 +254,18 @@ public class AppGraphAnalytics extends JPanel {
       if(evt.isControlDown() || evt.isShiftDown())
           return;
       graphPane.deselectAll();
-      FilterSelectedEdges.getInstance().clearSelection();
+      FilterSelectedEdges.getInstance().clearSelection(true);
       if (SwingUtilities.isRightMouseButton(evt)) {
         stagePopup.show(graphPane, evt.getX(), evt.getY());
       }
     }
 
+  }
+  
+  @Override
+  public void clearFilter() {
+      graphPane.deselectAll();
+      FilterSelectedEdges.getInstance().clearSelection(false);
   }
   
   private class AppEdgeListener implements EdgeListener {

iped-app/src/main/java/br/gov/pf/labld/graph/desktop/FilterSelectedEdges.java
@@ -113,10 +113,14 @@ public class FilterSelectedEdges {
         App.get().getAppListener().updateFileListing();
     }
     
-    public void clearSelection() {
+    public void clearSelection(boolean updateResults) {
         if(selectedEdges.size() > 0) {
             selectedEdges.clear();
-            updateResults();
+            if(updateResults) {
+                updateResults();
+            }else {
+                App.get().setGraphDefaultColor(true);
+            }
         }
     }
     
@@ -139,6 +143,5 @@ public class FilterSelectedEdges {
         }
         return result;
     }
-    
 
 }

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/App.java
@@ -100,9 +100,6 @@ import bibliothek.gui.dock.common.action.CButton;
 import bibliothek.gui.dock.common.action.CCheckBox;
 import bibliothek.gui.dock.common.event.CDockableLocationEvent;
 import bibliothek.gui.dock.common.event.CDockableLocationListener;
-import bibliothek.gui.dock.common.event.CDockableStateListener;
-import bibliothek.gui.dock.common.intern.CDockable;
-import bibliothek.gui.dock.common.mode.ExtendedMode;
 import bibliothek.gui.dock.common.theme.ThemeMap;
 import bibliothek.gui.dock.station.stack.tab.layouting.TabPlacement;
 import br.gov.pf.labld.graph.desktop.AppGraphAnalytics;
@@ -124,8 +121,8 @@ import dpf.sp.gpinf.indexer.ui.hitsViewer.HitsTableModel;
 import dpf.sp.gpinf.indexer.util.IconUtil;
 import iped3.IIPEDSource;
 import iped3.desktop.CancelableWorker;
-import iped3.desktop.IColumnsManager;
 import iped3.desktop.GUIProvider;
+import iped3.desktop.IColumnsManager;
 import iped3.desktop.ProgressDialog;
 import iped3.desktop.ResultSetViewer;
 import iped3.desktop.ResultSetViewerConfiguration;
@@ -190,7 +187,7 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
     private JScrollPane hitsScroll, subItemScroll, parentItemScroll, duplicatesScroll;
     JScrollPane viewerScroll, resultsScroll, galleryScroll;
     JPanel topPanel;
-    JPanel multiFilterAlert;
+    ClearFilterButton clearAllFilters;
     boolean disposicaoVertical = false;
 
     public ResultTableModel resultsModel;
@@ -425,19 +422,12 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.LINE_AXIS));
         topPanel.setAlignmentX(LEFT_ALIGNMENT);
 
-        JLabel alertLabel = new JLabel(Messages.getString("App.FilterWarn")); //$NON-NLS-1$
-        alertLabel.setForeground(Color.WHITE);
-        multiFilterAlert = new JPanel();
-        multiFilterAlert.add(alertLabel);
-        multiFilterAlert.setBackground(alertColor);
-        multiFilterAlert.setMaximumSize(new Dimension(100, 100));
-        // multiFilterAlert.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1,
-        // true));
-        multiFilterAlert.setVisible(false);
+        clearAllFilters = new ClearFilterButton();
+        clearAllFilters.setMaximumSize(new Dimension(100, 100));
 
         topPanel.add(filtro);
         topPanel.add(filterDuplicates);
-        topPanel.add(multiFilterAlert);
+        topPanel.add(clearAllFilters);
         topPanel.add(new JLabel(tab + Messages.getString("App.SearchLabel"))); //$NON-NLS-1$
         topPanel.add(termo);
         topPanel.add(opcoes);
@@ -674,6 +664,13 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         resultsTable.getSelectionModel().addListSelectionListener(new ResultTableListener());
         resultsTable.addMouseListener(new ResultTableListener());
         resultsTable.addKeyListener(new ResultTableListener());
+        
+        clearAllFilters.addClearListener(categoryListener);
+        clearAllFilters.addClearListener(bookmarksListener);
+        clearAllFilters.addClearListener(treeListener);
+        clearAllFilters.addClearListener(metadataPanel);
+        clearAllFilters.addClearListener(appletListener);
+        clearAllFilters.addClearListener(appGraphAnalytics);
 
         hitsTable.getSelectionModel().addListSelectionListener(new HitsTableListener(TextViewer.font));
         subItemTable.addMouseListener(subItemModel);
@@ -736,9 +733,7 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         tableTabDock = createDockable("tabletab", Messages.getString("App.Table"), resultsScroll); //$NON-NLS-1$ //$NON-NLS-2$
         galleryTabDock = createDockable("galleryscroll", Messages.getString("App.Gallery"), galleryScroll); //$NON-NLS-1$ //$NON-NLS-2$
 
-        if (appGraphAnalytics != null) {
-            graphDock = createDockable("graphtab", Messages.getString("App.Links"), appGraphAnalytics);
-        }
+        graphDock = createDockable("graphtab", Messages.getString("App.Links"), appGraphAnalytics);
 
         CButton butToggleVideoFramesMode = new CButton(Messages.getString("Gallery.ToggleVideoFrames"), IconUtil.getIcon("video", resPath));
         galleryTabDock.addAction(butToggleVideoFramesMode);

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/BookmarksTreeListener.java
@@ -12,11 +12,12 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreePath;
 
-public class BookmarksTreeListener implements TreeSelectionListener, TreeExpansionListener {
+public class BookmarksTreeListener implements TreeSelectionListener, TreeExpansionListener, ClearFilterListener {
 
     public HashSet<String> selection = new HashSet<String>();
     private volatile boolean updatingSelection = false;
     private long collapsed = 0;
+    private boolean clearing = false;
 
     @Override
     public void valueChanged(TreeSelectionEvent evt) {
@@ -40,7 +41,7 @@ public class BookmarksTreeListener implements TreeSelectionListener, TreeExpansi
             }
         }
 
-        App.get().appletListener.updateFileListing();
+        if(!clearing) App.get().appletListener.updateFileListing();
 
         if (selection.contains(BookmarksTreeModel.ROOT) || selection.isEmpty()) {
             App.get().setBookmarksDefaultColor(true);
@@ -102,6 +103,13 @@ public class BookmarksTreeListener implements TreeSelectionListener, TreeExpansi
     public void treeCollapsed(TreeExpansionEvent event) {
         collapsed = System.currentTimeMillis();
 
+    }
+
+    @Override
+    public void clearFilter() {
+        clearing = true;
+        App.get().bookmarksTree.clearSelection();
+        clearing = false;
     }
 
 }

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/CategoryTreeListener.java
@@ -17,12 +17,13 @@ import dpf.sp.gpinf.indexer.analysis.FastASCIIFoldingFilter;
 import dpf.sp.gpinf.indexer.desktop.CategoryTreeModel.Category;
 import dpf.sp.gpinf.indexer.process.IndexItem;
 
-public class CategoryTreeListener implements TreeSelectionListener, TreeExpansionListener {
+public class CategoryTreeListener implements TreeSelectionListener, TreeExpansionListener, ClearFilterListener {
 
     BooleanQuery query;
     private HashSet<TreePath> selection = new HashSet<TreePath>();
     private TreePath root;
     private long collapsed = 0;
+    private boolean clearing = false;
 
     @Override
     public void valueChanged(TreeSelectionEvent evt) {
@@ -59,7 +60,7 @@ public class CategoryTreeListener implements TreeSelectionListener, TreeExpansio
             }
         }
 
-        App.get().appletListener.updateFileListing();
+        if(!clearing) App.get().appletListener.updateFileListing();
 
     }
 
@@ -86,6 +87,13 @@ public class CategoryTreeListener implements TreeSelectionListener, TreeExpansio
     public void treeCollapsed(TreeExpansionEvent event) {
         collapsed = System.currentTimeMillis();
 
+    }
+
+    @Override
+    public void clearFilter() {
+        clearing = true;
+        App.get().categoryTree.clearSelection();
+        clearing = false;
     }
 
 }

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ClearFilterButton.java
@@ -1,0 +1,65 @@
+package dpf.sp.gpinf.indexer.desktop;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+
+public class ClearFilterButton extends JButton{
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    private String oneFilter = Messages.getString("App.ClearFilter");
+    private String manyFilters = Messages.getString("App.ClearFilters");
+    
+    private Color alertBackground = Color.RED;
+    private Color alertForeground = Color.WHITE;
+    private Color defaultBackground = this.getBackground();
+    private Color defaultForeground = this.getForeground();
+    
+    private List<ClearFilterListener> listeners = new ArrayList<>();
+    
+    public ClearFilterButton() {
+        super();
+        this.addActionListener(new ClearAction());   
+    }
+    
+    public void addClearListener(ClearFilterListener listener) {
+        this.listeners.add(listener);
+    }
+    
+    public void setNumberOfFilters(int numFilters) {
+        if(numFilters == 0) {
+            this.setVisible(false);
+        }else if (numFilters == 1) {
+            this.setText(oneFilter);
+            this.setBackground(defaultBackground);
+            this.setForeground(defaultForeground);
+            this.setVisible(true);
+        }else {
+            this.setText(manyFilters);
+            this.setBackground(alertBackground);
+            this.setForeground(alertForeground);
+            this.setVisible(true);
+        }
+    }
+    
+    private class ClearAction implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            for(ClearFilterListener l : listeners) {
+                l.clearFilter();
+            }
+            App.get().appletListener.updateFileListing();
+        }
+        
+    }
+
+}

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ClearFilterListener.java
@@ -1,0 +1,7 @@
+package dpf.sp.gpinf.indexer.desktop;
+
+public interface ClearFilterListener {
+    
+    void clearFilter();
+
+}

commit f71d71e06e5277f857bd7496b182e8df47343c77

fix searching twice when typing keywords caused by 8aa9d97

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/AppListener.java
@@ -107,7 +107,7 @@ public class AppListener implements ActionListener, MouseListener, ClearFilterLi
             updateFileList = true;
         }
         
-        if(evt.getSource() == App.get().termo && !clearSearchBox && !MarcadoresController.get().isUpdatingHistory()) {
+        if(evt.getSource() == App.get().termo && !clearSearchBox && evt.getActionCommand().equals("comboBoxChanged") && !MarcadoresController.get().isUpdatingHistory()) {
             if (App.get().termo.getSelectedItem() != null) {
                 texto = App.get().termo.getSelectedItem().toString();
                 if (texto.equals(MarcadoresController.HISTORY_DIV) || texto.equals(App.SEARCH_TOOL_TIP)) {