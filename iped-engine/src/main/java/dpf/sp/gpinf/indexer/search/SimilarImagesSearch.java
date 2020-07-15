3824c5f5adee00379b59121328238b60fd3c7cfc
Luis Nassif <lfcnassif@gmail.com>
2020-07-15 13:44:54 -0300

    minor optimization to similar image search

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/search/SimilarImagesSearch.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/search/SimilarImagesSearch.java
@@ -25,13 +25,15 @@ public class SimilarImagesSearch {
             return null;
         }
 
-        BooleanQuery.Builder similarImagesQuery = new BooleanQuery.Builder();
+        int[] lower = new int[4];
+        int[] upper = new int[4];
         for (int i = 0; i < 4; i++) {
             int refVal = similarityFeatures[i];
-            similarImagesQuery.add(
-                    IntPoint.newRangeQuery(BasicProps.SIMILARITY_FEATURES + i, refVal - range, refVal + range),
-                    Occur.MUST);
+            lower[i] = refVal - range;
+            upper[i] = refVal + range;
         }
+        BooleanQuery.Builder similarImagesQuery = new BooleanQuery.Builder();
+        similarImagesQuery.add(IntPoint.newRangeQuery(BasicProps.SIMILARITY_FEATURES, lower, upper), Occur.MUST);
 
         return similarImagesQuery.build();
     }
