77f4fab50a03bd6a823e2d14305d7a1a8a4ded9b
PCF Hauck <joao.jvsh@dpf.gov.br>
2020-09-07 10:35:38 -0300

    include License header in all files.

--- iped-parsers/iped-parsers-impl/src/main/java/dpf/ap/gpinf/telegramextractor/PostBoxCoding.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/ap/gpinf/telegramextractor/PostBoxCoding.java
@@ -1,7 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2020-2020, João Vitor de Sá Hauck
+ * 
+ * This file is part of Indexador e Processador de Evidencias Digitais (IPED).
+ *
+ * IPED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IPED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IPED.  If not, see <http://www.gnu.org/licenses/>.
  */
 package dpf.ap.gpinf.telegramextractor;
 
@@ -477,15 +490,7 @@ public class PostBoxCoding {
                 media.readMedia(m);
             }
             
-
-
-
-           
-
-            
             if(m!=null){
-
-            	
                 m.setTimeStamp(Date.from(Instant.ofEpochSecond(pk.readInt32(12,false))));
                 m.setData(txt);
                 m.setId(stableId);
--- iped-parsers/iped-parsers-impl/src/main/java/dpf/ap/gpinf/telegramextractor/ReportGenerator.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/ap/gpinf/telegramextractor/ReportGenerator.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2020-2020, João Vitor de Sá Hauck
+ * 
+ * This file is part of Indexador e Processador de Evidencias Digitais (IPED).
+ *
+ * IPED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IPED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IPED.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dpf.ap.gpinf.telegramextractor;
 
 import java.io.ByteArrayOutputStream;
