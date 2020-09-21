26f45347fe48dc4537bac47ec1120b7a79062ff2
Nassif <Nassif@DESKTOP-GVTQV0G>
2020-09-21 11:04:51 -0300

    handle ProjectVic "Series" property

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ProjectVICHashLookup.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ProjectVICHashLookup.java
@@ -1,15 +1,21 @@
 package dpf.sp.gpinf.indexer.process.task;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Reader;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Properties;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,17 +42,19 @@ public class ProjectVICHashLookup extends AbstractTask {
 
     private static boolean isMd5Enabled;
 
-    private static File vicJsonFile;
+    private static VicSet vicSet = new VicSet();
 
     private static Boolean enabled;
-
-    private static byte[] compressedHashArray;
-
-    private static Set<String> seriesNames = new HashSet<>();
 
     private class VicEntry extends HashValue {
 
         private static final long serialVersionUID = 1L;
+
+        private byte category;
+        private boolean victimIdentified;
+        private boolean ofenderIdentified;
+        private boolean isDistributed;
+        private int seriesId;
 
         private VicEntry() {
             super();
@@ -56,16 +64,19 @@ public class ProjectVICHashLookup extends AbstractTask {
             super(bytes);
         }
 
-        private byte category;
-        private boolean victimIdentified;
-        private boolean ofenderIdentified;
-        private boolean isDistributed;
+    }
 
+    private static class VicSet implements Serializable {
+
+        private static final long serialVersionUID = 9091381179250834449L;
+
+        private HashMap<Integer, String> seriesMap = new HashMap<>();
+        private byte[] compressedHashArray;
     }
 
     @Override
     public void finish() throws Exception {
-        compressedHashArray = null;
+        vicSet = null;
     }
 
     @Override
@@ -82,9 +93,10 @@ public class ProjectVICHashLookup extends AbstractTask {
         if (!enabled)
             return;
 
-        if (compressedHashArray != null)
+        if (vicSet.compressedHashArray != null)
             return;
 
+        File vicJsonFile;
         String path = confParams.getProperty(JSON_PATH_KEY);
         if (path != null && !path.trim().isEmpty()) {
             vicJsonFile = new File(path.trim());
@@ -114,7 +126,10 @@ public class ProjectVICHashLookup extends AbstractTask {
         File cacheFile = new File(CACHE_PATH + cacheSuffix);
         if (cacheFile.exists() && cacheFile.lastModified() == jsonDate) {
             logger.info("Loading ProjectVic cache from " + new Date(jsonDate) + ": " + cacheFile.getAbsolutePath());
-            compressedHashArray = Files.readAllBytes(cacheFile.toPath());
+            try (BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(cacheFile.toPath()));
+                    ObjectInputStream ois = new ObjectInputStream(bis)) {
+                vicSet = (VicSet) ois.readObject();
+            }
             return;
         } else {
             Files.deleteIfExists(cacheFile.toPath());
@@ -124,6 +139,8 @@ public class ProjectVICHashLookup extends AbstractTask {
         logger.info("Loading ProjectVic json " + vicJsonFile.getAbsolutePath());
 
         int nullHashes = 0;
+
+        HashMap<String, Integer> seriesMap = new HashMap<>();
 
         JsonFactory jfactory = new JsonFactory();
         try (Reader reader = Files.newBufferedReader(vicJsonFile.toPath())) {
@@ -139,6 +156,7 @@ public class ProjectVICHashLookup extends AbstractTask {
                 if (jp.nextFieldName().equals("value")) {
                     int arrayDepth = 0;
                     VicEntry ve = null;
+                    int seriesNum = 1;
                     do {
                         JsonToken token = jp.nextToken();
                         if (token == JsonToken.START_ARRAY)
@@ -161,10 +179,20 @@ public class ProjectVICHashLookup extends AbstractTask {
                             } else if ("IsDistributed".equals(jp.currentName())) {
                                 ve.isDistributed = Boolean.valueOf(jp.nextTextValue());
                             } else if ("Series".equals(jp.currentName())) {
-                                // TODO handle series
-                                // seriesNames.add(jp.nextTextValue());
+                                String seriesName = jp.nextTextValue();
+                                Integer id = seriesMap.get(seriesName);
+                                if (id == null) {
+                                    id = seriesNum++;
+                                    if (id >= 1 << 16)
+                                        throw new IPEDException(
+                                                "This ProjectVic json has more than max supported number of series. Please report to iped dev team.");
+                                    seriesMap.put(seriesName, id);
+                                }
+                                ve.seriesId = id;
                             } else if ("IsPrecategorized".equals(jp.currentName())) {
                                 // ignore
+                            } else if ("Tags".equals(jp.currentName())) {
+                                // TODO
                             } else if (token == JsonToken.END_OBJECT) {
                                 if (ve.getBytes() != null)
                                     vicHashList.add(ve);
@@ -184,8 +212,12 @@ public class ProjectVICHashLookup extends AbstractTask {
             jp.close();
         }
 
+        for (Entry<String, Integer> e : seriesMap.entrySet()) {
+            vicSet.seriesMap.put(e.getValue(), e.getKey());
+        }
+
         logger.info("Number of ProjectVic hashes loaded: " + vicHashList.size());
-        // logger.info("Number of ProjectVic series loaded: " + seriesNames.size());
+        logger.debug("Number of ProjectVic series loaded: " + vicSet.seriesMap.size());
         logger.info("Number of ProjectVic null " + hash + " values: " + nullHashes);
 
         VicEntry[] hashArray = vicHashList.toArray(new VicEntry[vicHashList.size()]);
@@ -196,10 +228,10 @@ public class ProjectVICHashLookup extends AbstractTask {
 
         logger.info("Compressing ProjectVic hashes.");
         int hashSize = hashArray[0].getBytes().length;
-        int recordSize = hashSize + 2;
-        compressedHashArray = new byte[hashArray.length * recordSize];
+        int recordSize = getRecordSize(hashSize);
+        vicSet.compressedHashArray = new byte[hashArray.length * recordSize];
         for (int i = 0; i < hashArray.length; i++) {
-            System.arraycopy(hashArray[i].getBytes(), 0, compressedHashArray, i * recordSize, hashSize);
+            System.arraycopy(hashArray[i].getBytes(), 0, vicSet.compressedHashArray, i * recordSize, hashSize);
             byte flags = 0;
             if (hashArray[i].victimIdentified)
                 flags |= 1;
@@ -207,23 +239,38 @@ public class ProjectVICHashLookup extends AbstractTask {
                 flags |= 1 << 1;
             if (hashArray[i].isDistributed)
                 flags |= 1 << 2;
-            compressedHashArray[(i + 1) * recordSize - 2] = hashArray[i].category;
-            compressedHashArray[(i + 1) * recordSize - 1] = flags;
+            vicSet.compressedHashArray[(i + 1) * recordSize - 4] = hashArray[i].category;
+            vicSet.compressedHashArray[(i + 1) * recordSize - 3] = (byte) (hashArray[i].seriesId >> 8);
+            vicSet.compressedHashArray[(i + 1) * recordSize - 2] = (byte) (hashArray[i].seriesId & 0xFF);
+            vicSet.compressedHashArray[(i + 1) * recordSize - 1] = flags;
         }
 
         cacheFile.getParentFile().mkdirs();
-        Files.write(cacheFile.toPath(), compressedHashArray);
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(cacheFile));
+                ObjectOutputStream oos = new ObjectOutputStream(bos)){
+            oos.writeObject(vicSet);
+        }
         cacheFile.setLastModified(jsonDate);
 
     }
 
+    /**
+     * Record format: hash | category (1 byte) | series (2 bytes) | flags (1 byte)
+     * 
+     * @param hashSize
+     * @return size of vic record
+     */
+    private int getRecordSize(int hashLen) {
+        return hashLen + 4;
+    }
+
     private int binarySearch(byte[] array, byte[] value) {
         int hashSize = value.length;
-        int recordSize = hashSize + 2;
+        int recordSize = getRecordSize(hashSize);
         int start = 0, end = (array.length / recordSize) - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
-            int comp = compare(array, mid * (hashSize + 2), value);
+            int comp = compare(array, mid * recordSize, value);
             if (comp < 0) {
                 start = mid + 1;
             } else if (comp > 0) {
@@ -256,13 +303,17 @@ public class ProjectVICHashLookup extends AbstractTask {
         else
             hash = new HashValue((String) item.getExtraAttribute(HashTask.HASH.SHA1.toString())).getBytes();
 
-        int pos = binarySearch(compressedHashArray, hash);
+        int pos = binarySearch(vicSet.compressedHashArray, hash);
         if (pos >= 0) {
-            VicEntry ve = getEntry(compressedHashArray, pos, hash.length);
+            VicEntry ve = getEntry(vicSet.compressedHashArray, pos, hash.length);
             item.setExtraAttribute("projectvic_category", ve.category);
             item.setExtraAttribute("projectvic_victimIdentified", ve.victimIdentified);
             item.setExtraAttribute("projectvic_ofenderIdentified", ve.ofenderIdentified);
-            item.setExtraAttribute("projectvic_IsDistributed", ve.isDistributed);
+            item.setExtraAttribute("projectvic_isDistributed", ve.isDistributed);
+            String series = vicSet.seriesMap.get(ve.seriesId);
+            if (series != null) {
+                item.setExtraAttribute("projectvic_Series", series);
+            }
         }
 
     }
@@ -270,7 +321,8 @@ public class ProjectVICHashLookup extends AbstractTask {
     private VicEntry getEntry(byte[] array, int index, int hashSize) {
         VicEntry e = new VicEntry();
         e.category = array[index + hashSize];
-        byte flags = array[index + hashSize + 1];
+        e.seriesId = (array[index + hashSize + 1] & (int) 0xFF) << 8 | (array[index + hashSize + 2] & (int) 0xFF);
+        byte flags = array[index + hashSize + 3];
         e.victimIdentified = (flags & 1) != 0;
         e.ofenderIdentified = (flags & 2) != 0;
         e.isDistributed = (flags & 4) != 0;
