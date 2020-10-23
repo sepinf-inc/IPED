35968d58cbad9549e380c0568c1d3eecd021dbac
Luis Nassif <lfcnassif@gmail.com>
2020-10-23 15:19:40 -0300

    fix #301: use different 7zip temp dir for each external parsing process

--- iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/SevenZipParser.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/SevenZipParser.java
@@ -9,22 +9,10 @@ import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeMap;
-
-import net.sf.sevenzipjbinding.ExtractAskMode;
-import net.sf.sevenzipjbinding.ExtractOperationResult;
-import net.sf.sevenzipjbinding.IArchiveExtractCallback;
-import net.sf.sevenzipjbinding.ISequentialOutStream;
-import net.sf.sevenzipjbinding.IInArchive;
-import net.sf.sevenzipjbinding.SevenZip;
-import net.sf.sevenzipjbinding.SevenZipException;
-import net.sf.sevenzipjbinding.SevenZipNativeInitializationException;
-import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream;
-import net.sf.sevenzipjbinding.simple.ISimpleInArchive;
-import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.tika.exception.EncryptedDocumentException;
@@ -48,6 +36,16 @@ import dpf.sp.gpinf.indexer.parsers.util.RawISOConverter;
 import dpf.sp.gpinf.indexer.parsers.util.Util;
 import dpf.sp.gpinf.indexer.util.IOUtil;
 import iped3.util.ExtraProperties;
+import net.sf.sevenzipjbinding.ExtractAskMode;
+import net.sf.sevenzipjbinding.ExtractOperationResult;
+import net.sf.sevenzipjbinding.IArchiveExtractCallback;
+import net.sf.sevenzipjbinding.IInArchive;
+import net.sf.sevenzipjbinding.ISequentialOutStream;
+import net.sf.sevenzipjbinding.SevenZip;
+import net.sf.sevenzipjbinding.SevenZipException;
+import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream;
+import net.sf.sevenzipjbinding.simple.ISimpleInArchive;
+import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem;
 
 public class SevenZipParser extends AbstractParser {
 
@@ -61,16 +59,21 @@ public class SevenZipParser extends AbstractParser {
 
     private static final String ISO9660 = "x-iso9660-image"; //$NON-NLS-1$
     private static final String UDF = "x-udf-image"; //$NON-NLS-1$
+    private static final String RAR = "x-rar-compressed"; //$NON-NLS-1$
 
     private static synchronized Set<MediaType> getTypes() {
 
         HashSet<MediaType> supportedTypes = new HashSet<MediaType>();
 
         try {
-            SevenZip.initSevenZipFromPlatformJAR();
+            File javaTmp = new File(System.getProperty("java.io.tmpdir"));
+            File tmpDir = new File(javaTmp, "7zip-" + new Random().nextLong());
+            Files.createDirectories(tmpDir.toPath());
+            // use a different tmp dir for each process, see
+            SevenZip.initSevenZipFromPlatformJAR(tmpDir);
             supportedTypes.add(MediaType.application(ISO9660));
             supportedTypes.add(MediaType.application(UDF));
-            supportedTypes.add(MediaType.application("x-rar-compressed")); // The newer  7zipbinding is capable of interpret RAR5 format and others previous rar formats
+            supportedTypes.add(MediaType.application(RAR));
 
             /*
              * 7zipJBinding-4.65 does not work with 7z files created by 7z-16.04 or higher.
@@ -84,8 +87,8 @@ public class SevenZipParser extends AbstractParser {
             // supportedTypes.add(MediaType.application("x-vhd"));
             // supportedTypes.add(MediaType.application("x-wim-image"));
 
-        } catch (SevenZipNativeInitializationException e) {
-            LOGGER.error("Error starting 7zipJBinding: {}", e.getMessage()); //$NON-NLS-1$
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         return supportedTypes;
