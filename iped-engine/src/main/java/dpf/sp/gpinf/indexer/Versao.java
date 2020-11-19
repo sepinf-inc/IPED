2db41dcc8970cdf62cbbefae701bc4578bf52b05
Luis Nassif <lfcnassif@gmail.com>
2020-11-19 19:31:26 -0300

    closes #240: block appending to case with different major version

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/Versao.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/Versao.java
@@ -23,7 +23,8 @@ package dpf.sp.gpinf.indexer;
  */
 public class Versao {
 
-    public static String APP_VERSION = "3.19-snapshot"; //$NON-NLS-1$
-    public static String APP_NAME = "Indexador e Processador de Evidências Digitais " + APP_VERSION; //$NON-NLS-1$
+    public static String APP_VERSION = "3.19.0-snapshot"; //$NON-NLS-1$
+    public static String APP_NAME_PREFIX = "Indexador e Processador de Evidências Digitais"; //$NON-NLS-1$
+    public static String APP_NAME = APP_NAME_PREFIX + " " + APP_VERSION; //$NON-NLS-1$
     public static String APP_EXT = "IPED"; //$NON-NLS-1$
 }
