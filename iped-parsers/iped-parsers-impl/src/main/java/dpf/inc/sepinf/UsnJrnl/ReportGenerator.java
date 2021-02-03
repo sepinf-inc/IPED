501751d4ba41534466898e057ed1cd9806a96375
Hauck <joaoh14@gmail.com>
2021-02-03 09:37:18 -0300

    Changing code style.

--- iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/UsnJrnl/ReportGenerator.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/UsnJrnl/ReportGenerator.java
@@ -3,14 +3,13 @@ package dpf.inc.sepinf.UsnJrnl;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class ReportGenerator {
-	
-	private static final String NEW_ROW = "<TR><TD>"; //$NON-NLS-1$
+
+    private static final String NEW_ROW = "<TR><TD>"; //$NON-NLS-1$
     private static final String CLOSE_ROW = "</TD></TR>"; //$NON-NLS-1$
     private static final String NEW_COL = "</TD><TD>"; //$NON-NLS-1$
     private final SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss XXX"); //$NON-NLS-1$
@@ -24,53 +23,49 @@ public class ReportGenerator {
         out.println("<BODY>"); //$NON-NLS-1$
         out.println("<h2>Usn Journal</h2>"); //$NON-NLS-1$
     }
-    
-    
+
     public void endDocument(PrintWriter out) {
         out.println("</BODY></HTML>"); //$NON-NLS-1$
     }
-    
-    
-    public byte[] createHTMLReport(ArrayList<UsnJrnlEntry> entries) {
-    	 ByteArrayOutputStream bout = new ByteArrayOutputStream();
-         PrintWriter out = new PrintWriter(new OutputStreamWriter(bout,StandardCharsets.UTF_8)); //$NON-NLS-1$
-         
-         startDocument(out);
-         
-         out.print("<table border=1>");
-         
-         out.print(NEW_ROW+"FileName");
-         out.print(NEW_COL+"USN");
-         out.print(NEW_COL+"TimeStamp");
-         out.print(NEW_COL+"Reasons");
-         out.print(NEW_COL+"MTF Ref.");
-         out.print(NEW_COL+"MTF parent Ref");
-         out.print(NEW_COL+"File attr");
-         out.print(NEW_COL+"Source Info");
-         out.print(NEW_COL+"Security Id");
-         out.print(CLOSE_ROW);
-         for(UsnJrnlEntry u:entries) {
-        	 out.print(NEW_ROW+u.getFileName());
-             out.print(NEW_COL+u.getUSN());
-             out.print(NEW_COL+timeFormat.format(u.getFileTime()));
-             out.print(NEW_COL+u.getReasons());
-             out.print(NEW_COL+u.getMftRef());
-             out.print(NEW_COL+u.getParentMftRef());
-             out.print(NEW_COL+u.getHumanAttributes());
-             out.print(NEW_COL+u.getSourceInformation());
-             out.print(NEW_COL+u.getSecurityId());
-             out.print(CLOSE_ROW);
-         }        
-         
-         out.print("</table>");
-         
-         
-         endDocument(out);
-                  
-         out.flush();
-         return bout.toByteArray();
-    	
-    }
 
+    public byte[] createHTMLReport(ArrayList<UsnJrnlEntry> entries) {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(bout, StandardCharsets.UTF_8)); // $NON-NLS-1$
+
+        startDocument(out);
+
+        out.print("<table border=1>");
+
+        out.print(NEW_ROW + "FileName");
+        out.print(NEW_COL + "USN");
+        out.print(NEW_COL + "TimeStamp");
+        out.print(NEW_COL + "Reasons");
+        out.print(NEW_COL + "MTF Ref.");
+        out.print(NEW_COL + "MTF parent Ref");
+        out.print(NEW_COL + "File attr");
+        out.print(NEW_COL + "Source Info");
+        out.print(NEW_COL + "Security Id");
+        out.print(CLOSE_ROW);
+        for (UsnJrnlEntry u : entries) {
+            out.print(NEW_ROW + u.getFileName());
+            out.print(NEW_COL + u.getUSN());
+            out.print(NEW_COL + timeFormat.format(u.getFileTime()));
+            out.print(NEW_COL + u.getReasons());
+            out.print(NEW_COL + u.getMftRef());
+            out.print(NEW_COL + u.getParentMftRef());
+            out.print(NEW_COL + u.getHumanAttributes());
+            out.print(NEW_COL + u.getSourceInformation());
+            out.print(NEW_COL + u.getSecurityId());
+            out.print(CLOSE_ROW);
+        }
+
+        out.print("</table>");
+
+        endDocument(out);
+
+        out.flush();
+        return bout.toByteArray();
+
+    }
 
 }
--- iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/UsnJrnl/UsnJrnlEntry.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/UsnJrnl/UsnJrnlEntry.java
@@ -1,211 +1,237 @@
 package dpf.inc.sepinf.UsnJrnl;
 
-import java.nio.file.attribute.FileTime;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
 
 public class UsnJrnlEntry {
-	
-	private int tam;
-	private int majorVersion;
-	private int minorVersion;
-	private long mftRef;
-	private long parentMftRef;
-	private long USN;
-	private Date fileTime;
-	private int reasonFlag;
-	private int sourceInformation;
-	private int securityId;
-	private int fileAttributes;
-	private int sizeofFileName;
-	private int offsetFilename;
-	private String fileName;
-	
-	private HashMap<Integer, String> reasonFlags=new HashMap<>();
-	private HashMap<Integer, String> fileAttributesToString=new HashMap<>();
-	
-	public UsnJrnlEntry() {
-		initHashMaps();
-	}
-	
-	private void initHashMaps() {
-		reasonFlags.put(0x01, "The file was overwritten");
-		reasonFlags.put(0x02, "The file or directory was added to");
-		reasonFlags.put(0x04, "The file or directory was truncated");
-		reasonFlags.put(0x10, "The named data streams for a file is overwritten");
-		reasonFlags.put(0x20, "A named data streams for the file were added");
-		reasonFlags.put(0x40, "A named data streams for the file was truncated");
-		reasonFlags.put(0x100, "The file or directory was created for the first time");
-		reasonFlags.put(0x200, "The file or directory was deleted");
-		reasonFlags.put(0x400, "The file's or directory's extended attributes were changed");
-		reasonFlags.put(0x800, "The access rights to the file or directory was changed");
-		reasonFlags.put(0x1000, "The file or directory was renamed(previous name)");
-		reasonFlags.put(0x2000, "The file or directory was renamed(new name)");
-		reasonFlags.put(0x4000, "A user changed the FILE_ATTRIBUTE_NOT_CONTENT_INDEXED attribute");
-		reasonFlags.put(0x8000, "A user has either changed one or more file or directory attributes or one or more time stamps");
-		reasonFlags.put(0x10000, "A hard link was added to or removed from the file or directory");
-		reasonFlags.put(0x20000, "The compression state of the file or directory was changed from or to compressed.");
-		reasonFlags.put(0x40000, "The file or directory was encrypted or decrypted");
-		reasonFlags.put(0x80000, "The object identifier of the file or directory was changed");
-		reasonFlags.put(0x100000, "The reparse point contained in the file or directory was changed, or a reparse point was added to or deleted from the file ordirectory");
-		reasonFlags.put(0x200000, "A named stream has been added to or removed from the file, or a named stream has been renamed.");
-		reasonFlags.put(0x80000000, "The file or directory was closed");
-		
-		
-		fileAttributesToString.put(0x01, "READONLY");
-		fileAttributesToString.put(0x02, "HIDDEN");
-		fileAttributesToString.put(0x04, "SYSTEM");
-		fileAttributesToString.put(0x10, "DIRECTORY_ID");
-		fileAttributesToString.put(0x20, "FILE/DIRECTORY");
-		fileAttributesToString.put(0x40, "RESERVED");
-		fileAttributesToString.put(0x80, "A file that does not have other attributes set");
-		fileAttributesToString.put(0x100, "TEMPORARY");
-		fileAttributesToString.put(0x200, "SPARSE");
-		fileAttributesToString.put(0x400, "SYMBOLIC");
-		fileAttributesToString.put(0x800, "COMPRESSED");
-		fileAttributesToString.put(0x1000, "OFFILINE_STORAGE");
-		fileAttributesToString.put(0x2000, "NOT_INDEXED");
-		fileAttributesToString.put(0x4000, "ENCRYPTED");
-		fileAttributesToString.put(0x8000, "INTEGRITY");
-		fileAttributesToString.put(0x10000, "RESERVED");
-		fileAttributesToString.put(0x20000, "The user data stream not to be read by the background data integrity scanner");
-		
-	}
-	
-	
-	public String getReasons() {
-		
-		StringBuilder sb=new StringBuilder();
-		
-		for(int k :reasonFlags.keySet()) {
-			if( (k & reasonFlag)!=0) {
-				if(sb.length()>0) {
-					sb.append(", ");
-				}
-				sb.append(reasonFlags.get(k));
-			}
-		}
-		
-		return sb.toString();
-		
-	}
-public String getHumanAttributes() {
-		
-		StringBuilder sb=new StringBuilder();
-		
-		for(int k :fileAttributesToString.keySet()) {
-			if( (k & this.fileAttributes)!=0) {
-				if(sb.length()>0) {
-					sb.append(", ");
-				}
-				sb.append(fileAttributesToString.get(k));
-			}
-		}
-		
-		return sb.toString();
-		
-	}
-	
-	public int getMajorVersion() {
-		return majorVersion;
-	}
-	public void setMajorVersion(int majorVersion) {
-		this.majorVersion = majorVersion;
-	}
-	public int getMinorVersion() {
-		return minorVersion;
-	}
-	public void setMinorVersion(int minorVersion) {
-		this.minorVersion = minorVersion;
-	}
-	public long getMftRef() {
-		return mftRef;
-	}
-	public void setMftRef(long mftRef) {
-		this.mftRef = mftRef;
-	}
-	public long getParentMftRef() {
-		return parentMftRef;
-	}
-	public void setParentMftRef(long parentMftRef) {
-		this.parentMftRef = parentMftRef;
-	}
-	public long getUSN() {
-		return USN;
-	}
-	public void setUSN(long uSN) {
-		USN = uSN;
-	}
-	public Date getFileTime() {
-		return fileTime;
-	}
-	public void setFileTime(Date fileTime) {
-		this.fileTime = fileTime;
-	}
-	
-	
-	/** Difference between Filetime epoch and Unix epoch (in ms). */
-	private static final long FILETIME_EPOCH_DIFF = 11644473600000L;
 
-	/** One millisecond expressed in units of 100s of nanoseconds. */
-	private static final long FILETIME_ONE_MILLISECOND = 10 * 1000;
+    private int tam;
+    private int majorVersion;
+    private int minorVersion;
+    private long mftRef;
+    private long parentMftRef;
+    private long USN;
+    private Date fileTime;
+    private int reasonFlag;
+    private int sourceInformation;
+    private int securityId;
+    private int fileAttributes;
+    private int sizeofFileName;
+    private int offsetFilename;
+    private String fileName;
 
-	public static long filetimeToMillis(final long filetime) {
-	    return (filetime / FILETIME_ONE_MILLISECOND) - FILETIME_EPOCH_DIFF;
-	}
-	
-	
-	public void setFileTime(long fileTime) {
-		this.fileTime = new Date(filetimeToMillis(fileTime));
-	}
-	public int getReasonFlag() {
-		return reasonFlag;
-	}
-	public void setReasonFlag(int reasonFlag) {
-		this.reasonFlag = reasonFlag;
-	}
-	public int getSourceInformation() {
-		return sourceInformation;
-	}
-	public void setSourceInformation(int sourceInformation) {
-		this.sourceInformation = sourceInformation;
-	}
-	public int getSecurityId() {
-		return securityId;
-	}
-	public void setSecurityId(int securityId) {
-		this.securityId = securityId;
-	}
-	public int getFileAttributes() {
-		return fileAttributes;
-	}
-	public void setFileAttributes(int fileAttributes) {
-		this.fileAttributes = fileAttributes;
-	}
-	public int getSizeofFileName() {
-		return sizeofFileName;
-	}
-	public void setSizeofFileName(int sizeofFileName) {
-		this.sizeofFileName = sizeofFileName;
-	}
-	public int getOffsetFilename() {
-		return offsetFilename;
-	}
-	public void setOffsetFilename(int offsetFilename) {
-		this.offsetFilename = offsetFilename;
-	}
-	public String getFileName() {
-		return fileName;
-	}
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-	public int getTam() {
-		return tam;
-	}
-	public void setTam(int tam) {
-		this.tam = tam;
-	}
+    private HashMap<Integer, String> reasonFlags = new HashMap<>();
+    private HashMap<Integer, String> fileAttributesToString = new HashMap<>();
+
+    public UsnJrnlEntry() {
+        initHashMaps();
+    }
+
+    private void initHashMaps() {
+        reasonFlags.put(0x01, "The file was overwritten");
+        reasonFlags.put(0x02, "The file or directory was added to");
+        reasonFlags.put(0x04, "The file or directory was truncated");
+        reasonFlags.put(0x10, "The named data streams for a file is overwritten");
+        reasonFlags.put(0x20, "A named data streams for the file were added");
+        reasonFlags.put(0x40, "A named data streams for the file was truncated");
+        reasonFlags.put(0x100, "The file or directory was created for the first time");
+        reasonFlags.put(0x200, "The file or directory was deleted");
+        reasonFlags.put(0x400, "The file's or directory's extended attributes were changed");
+        reasonFlags.put(0x800, "The access rights to the file or directory was changed");
+        reasonFlags.put(0x1000, "The file or directory was renamed(previous name)");
+        reasonFlags.put(0x2000, "The file or directory was renamed(new name)");
+        reasonFlags.put(0x4000, "A user changed the FILE_ATTRIBUTE_NOT_CONTENT_INDEXED attribute");
+        reasonFlags.put(0x8000,
+                "A user has either changed one or more file or directory attributes or one or more time stamps");
+        reasonFlags.put(0x10000, "A hard link was added to or removed from the file or directory");
+        reasonFlags.put(0x20000, "The compression state of the file or directory was changed from or to compressed.");
+        reasonFlags.put(0x40000, "The file or directory was encrypted or decrypted");
+        reasonFlags.put(0x80000, "The object identifier of the file or directory was changed");
+        reasonFlags.put(0x100000,
+                "The reparse point contained in the file or directory was changed, or a reparse point was added to or deleted from the file ordirectory");
+        reasonFlags.put(0x200000,
+                "A named stream has been added to or removed from the file, or a named stream has been renamed.");
+        reasonFlags.put(0x80000000, "The file or directory was closed");
+
+        fileAttributesToString.put(0x01, "READONLY");
+        fileAttributesToString.put(0x02, "HIDDEN");
+        fileAttributesToString.put(0x04, "SYSTEM");
+        fileAttributesToString.put(0x10, "DIRECTORY_ID");
+        fileAttributesToString.put(0x20, "FILE/DIRECTORY");
+        fileAttributesToString.put(0x40, "RESERVED");
+        fileAttributesToString.put(0x80, "A file that does not have other attributes set");
+        fileAttributesToString.put(0x100, "TEMPORARY");
+        fileAttributesToString.put(0x200, "SPARSE");
+        fileAttributesToString.put(0x400, "SYMBOLIC");
+        fileAttributesToString.put(0x800, "COMPRESSED");
+        fileAttributesToString.put(0x1000, "OFFILINE_STORAGE");
+        fileAttributesToString.put(0x2000, "NOT_INDEXED");
+        fileAttributesToString.put(0x4000, "ENCRYPTED");
+        fileAttributesToString.put(0x8000, "INTEGRITY");
+        fileAttributesToString.put(0x10000, "RESERVED");
+        fileAttributesToString.put(0x20000,
+                "The user data stream not to be read by the background data integrity scanner");
+
+    }
+
+    public String getReasons() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int k : reasonFlags.keySet()) {
+            if ((k & reasonFlag) != 0) {
+                if (sb.length() > 0) {
+                    sb.append(", ");
+                }
+                sb.append(reasonFlags.get(k));
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public String getHumanAttributes() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int k : fileAttributesToString.keySet()) {
+            if ((k & this.fileAttributes) != 0) {
+                if (sb.length() > 0) {
+                    sb.append(", ");
+                }
+                sb.append(fileAttributesToString.get(k));
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public int getMajorVersion() {
+        return majorVersion;
+    }
+
+    public void setMajorVersion(int majorVersion) {
+        this.majorVersion = majorVersion;
+    }
+
+    public int getMinorVersion() {
+        return minorVersion;
+    }
+
+    public void setMinorVersion(int minorVersion) {
+        this.minorVersion = minorVersion;
+    }
+
+    public long getMftRef() {
+        return mftRef;
+    }
+
+    public void setMftRef(long mftRef) {
+        this.mftRef = mftRef;
+    }
+
+    public long getParentMftRef() {
+        return parentMftRef;
+    }
+
+    public void setParentMftRef(long parentMftRef) {
+        this.parentMftRef = parentMftRef;
+    }
+
+    public long getUSN() {
+        return USN;
+    }
+
+    public void setUSN(long uSN) {
+        USN = uSN;
+    }
+
+    public Date getFileTime() {
+        return fileTime;
+    }
+
+    public void setFileTime(Date fileTime) {
+        this.fileTime = fileTime;
+    }
+
+    /** Difference between Filetime epoch and Unix epoch (in ms). */
+    private static final long FILETIME_EPOCH_DIFF = 11644473600000L;
+
+    /** One millisecond expressed in units of 100s of nanoseconds. */
+    private static final long FILETIME_ONE_MILLISECOND = 10 * 1000;
+
+    public static long filetimeToMillis(final long filetime) {
+        return (filetime / FILETIME_ONE_MILLISECOND) - FILETIME_EPOCH_DIFF;
+    }
+
+    public void setFileTime(long fileTime) {
+        this.fileTime = new Date(filetimeToMillis(fileTime));
+    }
+
+    public int getReasonFlag() {
+        return reasonFlag;
+    }
+
+    public void setReasonFlag(int reasonFlag) {
+        this.reasonFlag = reasonFlag;
+    }
+
+    public int getSourceInformation() {
+        return sourceInformation;
+    }
+
+    public void setSourceInformation(int sourceInformation) {
+        this.sourceInformation = sourceInformation;
+    }
+
+    public int getSecurityId() {
+        return securityId;
+    }
+
+    public void setSecurityId(int securityId) {
+        this.securityId = securityId;
+    }
+
+    public int getFileAttributes() {
+        return fileAttributes;
+    }
+
+    public void setFileAttributes(int fileAttributes) {
+        this.fileAttributes = fileAttributes;
+    }
+
+    public int getSizeofFileName() {
+        return sizeofFileName;
+    }
+
+    public void setSizeofFileName(int sizeofFileName) {
+        this.sizeofFileName = sizeofFileName;
+    }
+
+    public int getOffsetFilename() {
+        return offsetFilename;
+    }
+
+    public void setOffsetFilename(int offsetFilename) {
+        this.offsetFilename = offsetFilename;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getTam() {
+        return tam;
+    }
+
+    public void setTam(int tam) {
+        this.tam = tam;
+    }
 
 }
