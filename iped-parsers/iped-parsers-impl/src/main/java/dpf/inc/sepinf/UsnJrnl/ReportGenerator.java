e00607d556444c19068dddfe218bb8d92c1fdcc3
Hauck <joaoh14@gmail.com>
2021-02-03 11:32:22 -0300

    Better table style

--- iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/UsnJrnl/ReportGenerator.java
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/inc/sepinf/UsnJrnl/ReportGenerator.java
@@ -9,16 +9,29 @@ import java.util.ArrayList;
 
 public class ReportGenerator {
 
-    private static final String NEW_ROW = "<TR><TD>"; //$NON-NLS-1$
-    private static final String CLOSE_ROW = "</TD></TR>"; //$NON-NLS-1$
-    private static final String NEW_COL = "</TD><TD>"; //$NON-NLS-1$
+    private static final String NEW_ROW = "<TR>"; //$NON-NLS-1$
+    private static final String CLOSE_ROW = "</TR>"; //$NON-NLS-1$
+
     private final SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss XXX"); //$NON-NLS-1$
+
+    private static final String newCol(String value, String tag) {
+
+        return "<" + tag + ">" + value + "</" + tag + ">";
+    }
+
+    private static final String newCol(String value) {
+        return newCol(value, "TD");
+    }
 
     public void startDocument(PrintWriter out) {
         out.println("<!DOCTYPE html>"); //$NON-NLS-1$
         out.println("<HTML>"); //$NON-NLS-1$
         out.println("<HEAD>"); //$NON-NLS-1$
         out.print("<meta charset=\"UTF-8\">"); //$NON-NLS-1$
+        out.println("<style>\r\n" + "table {\r\n" + "  border-collapse: collapse;\r\n" + "  width: 100%;\r\n"
+                + "  border: 1px solid #ddd;\r\n" + "}\r\n" + "\r\n" + "th, td {\r\n" + "  text-align: left;\r\n"
+                + "  padding: 16px;\r\n" + "}\r\n" + "\r\n" + "tr:nth-child(even) {\r\n"
+                + "  background-color: #f2f2f2;\r\n" + "}\r\n" + "</style>");
         out.println("</HEAD>"); //$NON-NLS-1$
         out.println("<BODY>"); //$NON-NLS-1$
         out.println("<h2>Usn Journal</h2>"); //$NON-NLS-1$
@@ -36,26 +49,28 @@ public class ReportGenerator {
 
         out.print("<table border=1>");
 
-        out.print(NEW_ROW + "FileName");
-        out.print(NEW_COL + "USN");
-        out.print(NEW_COL + "TimeStamp");
-        out.print(NEW_COL + "Reasons");
-        out.print(NEW_COL + "MTF Ref.");
-        out.print(NEW_COL + "MTF parent Ref");
-        out.print(NEW_COL + "File attr");
-        out.print(NEW_COL + "Source Info");
-        out.print(NEW_COL + "Security Id");
+        out.print(NEW_ROW);
+        out.print(newCol("FileName", "TH"));
+        out.print(newCol("USN", "TH"));
+        out.print(newCol("TimeStamp", "TH"));
+        out.print(newCol("Reasons", "TH"));
+        out.print(newCol("MTF Ref.", "TH"));
+        out.print(newCol("MTF parent Ref", "TH"));
+        out.print(newCol("File attr", "TH"));
+        out.print(newCol("Source Info", "TH"));
+        out.print(newCol("Security Id", "TH"));
         out.print(CLOSE_ROW);
         for (UsnJrnlEntry u : entries) {
-            out.print(NEW_ROW + u.getFileName());
-            out.print(NEW_COL + u.getUSN());
-            out.print(NEW_COL + timeFormat.format(u.getFileTime()));
-            out.print(NEW_COL + u.getReasons());
-            out.print(NEW_COL + u.getMftRef());
-            out.print(NEW_COL + u.getParentMftRef());
-            out.print(NEW_COL + u.getHumanAttributes());
-            out.print(NEW_COL + u.getSourceInformation());
-            out.print(NEW_COL + u.getSecurityId());
+            out.print(NEW_ROW);
+            out.print(newCol(u.getFileName()));
+            out.print(newCol(u.getUSN() + ""));
+            out.print(newCol(timeFormat.format(u.getFileTime())));
+            out.print(newCol(u.getReasons()));
+            out.print(newCol(u.getMftRef() + ""));
+            out.print(newCol(u.getParentMftRef() + ""));
+            out.print(newCol(u.getHumanAttributes()));
+            out.print(newCol(u.getSourceInformation() + ""));
+            out.print(newCol(u.getSecurityId() + ""));
             out.print(CLOSE_ROW);
         }
 
