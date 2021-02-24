6edcd67a8878bf2aa6e81f101a6b5fff25bb0fc0
Luis Nassif <lfcnassif@gmail.com>
2021-02-24 15:01:50 -0300

    fixes #374: create log and multi graphs in local temp folder

--- iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/App.java
+++ iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/App.java
@@ -273,6 +273,10 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         return new MenuClass();
     }
 
+    public LogConfiguration getLogConfiguration() {
+        return this.logConfiguration;
+    }
+
     public void init(LogConfiguration logConfiguration, boolean isMultiCase, File casesPathFile,
             Manager processingManager) {
 
