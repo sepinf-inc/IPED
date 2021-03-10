a872b310b123c20c20ed82dfed197ed1eefddd8d
Luis Nassif <lfcnassif@gmail.com>
2021-03-10 14:56:04 -0300

    search cases using multiple threads

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/IPEDCrawler.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/IPEDCrawler.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import dpf.sp.gpinf.indexer.search.IPEDSearcher;
 import dpf.sp.gpinf.indexer.search.IPEDSource;
@@ -15,57 +18,66 @@ public class IPEDCrawler {
 
     public static void main(String[] args) {
 
-        // String query = "name:(\"cloud_graph.db\" \"snapshot.db\") OR
-        // nome:(\"cloud_graph.db\" \"snapshot.db\")";
-        // String query = "name:(\"cache4.db\" \"db_sqlite\") OR nome:(\"cache4.db\"
-        // \"db_sqlite\")";
-        String query = "duplicate:false AND tipo:(jpg gif bmp png tif emf wmf heic webp pdf doc docx rtf ppt pptx xls xlsx htm html xhtml xml eml log csv db sqlite mp3 aac opus mp4 wmv avi 3gp mpg mov txt)";
+        String query = "name:(\"logs.db\" \"contacts.db\" \"contacts2.db\") OR nome:(\"logs.db\" \"contacts.db\" \"contacts2.db\")";
 
-        File folderToScan = new File("Z:\\SINQ\\TrisInIdem");
-        File exportFolder = new File("Z:\\POC-DATASHARE");
+        File folderToScan = new File("Z:\\SINQ");
+        File exportFolder = new File("F:\\teste-files\\android-calllogs");
         exportFolder.mkdirs();
 
         List<File> cases = searchCasesinFolder(folderToScan);
         System.out.println("Cases found: " + cases.size());
-        int exported = 0;
-        File parentDir = null;
+        AtomicInteger exported = new AtomicInteger(), caseNum = new AtomicInteger();
+        Executor executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        if (cases.isEmpty()) {
+            System.out.println("No cases found!!!");
+            System.exit(0);
+        }
+        // initialize sleuthkit using just this thread
+        try (IPEDSource ipedCase = new IPEDSource(cases.get(0))) {
+        }
         for (File file : cases) {
-            System.out.println("Searching in case " + file.getAbsolutePath());
-            try (IPEDSource ipedCase = new IPEDSource(file)) {
-                IPEDSearcher searcher = new IPEDSearcher(ipedCase, query);
-                List<Integer> itemIds = searcher.search().getIds();
-                System.out.println("Found " + itemIds.size() + " files.");
-                if (!itemIds.isEmpty())
-                    System.out.println("Exporting...");
-                for (Integer id : itemIds) {
-                    IItem item = ipedCase.getItemByID(id);
-                    if (exported % 1000 == 0) {
-                        parentDir = new File(exportFolder, Integer.toString(exported / 1000));
-                        parentDir.mkdirs();
-                    }
-                    File target = new File(parentDir, Util.getValidFilename(Util.getNameWithTrueExt(item)));
-                    String ext = "";
-                    int idx, suffix = 0;
-                    if ((idx = target.getName().lastIndexOf('.')) > -1) {
-                        ext = target.getName().substring(idx);
-                    } else {
-                        idx = target.getName().length();
-                    }
-                    while (target.exists()) {
-                        target = new File(parentDir, target.getName().substring(0, idx) + (++suffix) + ext);
-                    }
-                    try (InputStream in = item.getBufferedStream()) {
-                        Files.copy(in, target.toPath());
-                        exported++;
-                    } catch (Exception e0) {
-                        e0.printStackTrace();
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Searching in case " + file.getAbsolutePath());
+                    try (IPEDSource ipedCase = new IPEDSource(file)) {
+                        IPEDSearcher searcher = new IPEDSearcher(ipedCase, query);
+                        List<Integer> itemIds = searcher.search().getIds();
+                        System.out.println("Found " + itemIds.size() + " files.");
+                        if (!itemIds.isEmpty()) {
+                            System.out.println("Exporting...");
+                            caseNum.getAndIncrement();
+                        }
+                        for (Integer id : itemIds) {
+                            IItem item = ipedCase.getItemByID(id);
+                            File parentDir = new File(exportFolder, "case_" + caseNum);
+                            parentDir.mkdirs();
+                            File target = new File(parentDir, Util.getValidFilename(Util.getNameWithTrueExt(item)));
+                            String ext = "";
+                            int idx, suffix = 0;
+                            if ((idx = target.getName().lastIndexOf('.')) > -1) {
+                                ext = target.getName().substring(idx);
+                            } else {
+                                idx = target.getName().length();
+                            }
+                            while (target.exists()) {
+                                target = new File(parentDir, target.getName().substring(0, idx) + (++suffix) + ext);
+                            }
+                            try (InputStream in = item.getBufferedStream()) {
+                                Files.copy(in, target.toPath());
+                                exported.getAndIncrement();
+                            } catch (Exception e0) {
+                                e0.printStackTrace();
+                            }
+                        }
+                        System.out.println("Exported " + exported + " files.");
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-                System.out.println("Exported " + exported + " files.");
+            });
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
 
     }
