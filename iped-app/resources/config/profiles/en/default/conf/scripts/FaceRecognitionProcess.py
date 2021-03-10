bdb9e29ee515d1cb3702ed2d95e1b4882dad95c4
Luis Nassif <lfcnassif@gmail.com>
2021-03-10 15:22:44 -0300

    refactor to implement class, separate process code, processes queue

---
+++ iped-app/resources/config/profiles/en/default/conf/scripts/FaceRecognitionProcess.py
@@ -0,0 +1,65 @@
+﻿'''
+# External process used by FaceRecognitionTask.py to do the hard work to bypass python GIL and allow multiprocess parallelization.
+'''
+
+import face_recognition as fr
+import cv2
+
+terminate = 'terminate_process'
+imgError = "image_error"
+ping = "ping"
+
+# Image rotation, when necessary
+def rotateImg(img, tiff_orient):
+    if tiff_orient != 1 and tiff_orient != 2:
+        if tiff_orient == 8 or tiff_orient == 5:
+            img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        elif tiff_orient == 3 or tiff_orient == 4:
+            img = cv2.rotate(img, cv2.ROTATE_180)
+        elif tiff_orient == 6 or tiff_orient == 7:
+            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)        
+    return img
+
+'''
+Main function of external process which will detect and encode faces.
+It is executed out of process to workaroung python GIL bottleneck.
+Multiprocessing module does not work with jep-3.9.1.
+'''
+def main():
+    while True:
+        line = input()
+        if line == terminate:
+            break
+        if line == ping:
+            print(ping, flush=True)
+            continue
+        
+        tiff_orient = input()        
+        try:
+            img = fr.load_image_file(line)
+        except:
+            print(imgError, flush=True)
+            continue
+        
+        img = rotateImg(img, int(tiff_orient))
+                
+        face_locations = fr.face_locations(img, model="hog")
+        
+        num_faces = len(face_locations)
+        print(str(num_faces), flush=True)
+        if num_faces == 0:
+            continue
+        
+        for i in range(num_faces):
+            print(str(face_locations[i]), flush=True)
+        
+        face_encodings = fr.face_encodings(img, face_locations)
+        
+        for i in range(num_faces):
+            for j in range(128):
+                print(str(face_encodings[i][j]), flush=True)
+    return
+    
+if __name__ == "__main__":
+     main()
+    
--- iped-app/resources/config/profiles/en/default/conf/scripts/FaceRecognitionTask.py
+++ iped-app/resources/config/profiles/en/default/conf/scripts/FaceRecognitionTask.py
@@ -7,221 +7,203 @@
 
 import face_recognition as fr
 import os
-import face_recognition.api as face_recognition
 import cv2
 import time
 import subprocess
 import numpy as np
+import threading, queue
+import FaceRecognitionProcess as fp
 
-enabled = False
+'''
+Maximum number of face recognition processes to run simultaneously. You can set if you are having GPU memory problems.
+'''
+maxProcesses = None
+
+
+terminate = fp.terminate
+imgError = fp.imgError
+ping = fp.ping
+
+processQueue = None
 cache = None
+timeLock = threading.Lock()
 detectTime = 0
 featureTime = 0
-configDir = None
-proc = None
-terminate = 'terminate_process'
-imgError = "image_error"
-ping = "ping"
 
-'''
-Main function of external process which will detect and encode faces.
-It is executed out of process to workaroung python GIL bottleneck.
-Multiprocessing module does not work with jep-3.9.1.
-'''
-def main():
-    while True:
-        line = input()
-        if line == terminate:
-            break
-        if line == ping:
-            print(ping, flush=True)
-            continue
-        
-        tiff_orient = input()        
-        try:
-            img = fr.load_image_file(line)
-        except:
-            print(imgError, flush=True)
-            continue
-        
-        img = rotateImg(img, int(tiff_orient))
-                
-        face_locations = fr.face_locations(img)
-        
-        num_faces = len(face_locations)
-        print(str(num_faces), flush=True)
-        if num_faces == 0:
-            continue
-        
-        for i in range(num_faces):
-            print(str(face_locations[i]), flush=True)
-        
-        face_encodings = fr.face_encodings(img, face_locations)
-        
-        for i in range(num_faces):
-            for j in range(128):
-                print(str(face_encodings[i][j]), flush=True)
-    return
-
-def isEnabled():
-    return enabled
-
-# This method is executed before starting the processing of items.
-def init(confProps, configFolder):
-    global enabled, configDir, cache
-    enabled = confProps.getProperty('enableFaceRecognition').lower() == 'true'
-    if not enabled:
-        return
-    configDir = configFolder.getAbsolutePath()
-    cache = caseData.getCaseObject('faces_cache')
-    if cache is None:
-        from java.util.concurrent import ConcurrentHashMap
-        cache = ConcurrentHashMap()
-        caseData.putCaseObject('faces_cache', cache)
-    return
+def createProcessQueue(configDir):
+    global processQueue, maxProcesses
+    if processQueue is None:
+        if maxProcesses is None:
+            maxProcesses = numThreads
+        processQueue = queue.Queue(maxProcesses)
+        for i in range(maxProcesses):
+            proc = createExternalProcess(configDir)
+            processQueue.put(proc)
 
 # Start external process, check if it is alive and ping to test communication
-def checkExternalProcess():
-    global proc
+def createExternalProcess(configDir):
+    proc = None
     for i in range(3):
         if proc is None or proc.poll() is not None:
-            proc = subprocess.Popen(['python', os.path.join(configDir, 'scripts', 'FaceRecognitionTask.py')], stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
+            proc = subprocess.Popen(['python', os.path.join(configDir, 'scripts', 'FaceRecognitionProcess.py')], stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
         
-        print(ping, file=proc.stdin, flush=True)
-        line = proc.stdout.readline().strip()
-        if line == ping:
-            return True
+        if pingExternalProcess(proc):
+            return proc
         else:
             proc.kill()
             proc = None
             
+    return None
+
+def pingExternalProcess(proc):
+    try:
+        print(ping, file=proc.stdin, flush=True)
+        line = proc.stdout.readline().strip()
+        if line == ping:
+            return True
+    except:
+        return False
     return False
 
-# It is executed after processing all items in case.
-def finish():
-    #terminate subprocess
-    if proc is not None:
-        try:
-            print(terminate, file=proc.stdin, flush=True)
-            proc.wait(2)
-        except:
-            proc.kill()
+class FaceRecognitionTask:
+
+    enabled = False
+    configDir = None
     
-    times = caseData.getCaseObject('face_recog_times')
-    if times is None:
-        times = [0, 0]
-    times[0] += detectTime / numThreads
-    times[1] += featureTime / numThreads
-    caseData.putCaseObject('face_recog_times', times)
+    def isEnabled(self):
+        return self.enabled
     
-    if worker.id == numThreads - 1:
-        logger.info('Time(s) to detect faces: ' + str(times[0]))
-        logger.info('Time(s) to get face features: ' + str(times[1]))
-
-# Needed because tuples cause ClassNotFoundException on java side later
-def convertTuplesToList(tuples):
-    result = []
-    for i in tuples:
-        result.append(list(i))
-    return result
-
-# Image rotation, when necessary
-def rotateImg(img, tiff_orient):
-    if tiff_orient != 1 and tiff_orient != 2:
-        if tiff_orient == 8 or tiff_orient == 5:
-            img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        elif tiff_orient == 3 or tiff_orient == 4:
-            img = cv2.rotate(img, cv2.ROTATE_180)
-        elif tiff_orient == 6 or tiff_orient == 7:
-            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)        
-    return img
-
-def cacheResults(item, locations, encodings):
-    cache.put(item.getHash() + '_locations', locations)
-    cache.put(item.getHash() + '_encodings', encodings)
-
-# This function is executed on all case items
-def process(item):
-
-    # Only image type items are processed
-    if item.getHash() is None or not item.getMediaType().toString().startswith('image'):
-        return
-    
-    #reuse cached results
-    face_locations = cache.get(item.getHash() + '_locations')
-    face_encodings = cache.get(item.getHash() + '_encodings')
-    if face_locations is not None and face_encodings is not None:
-        if len(face_locations) > 0 and len(face_encodings) > 0:
-            item.setExtraAttribute("face_locations", face_locations)
-            item.setExtraAttribute("face_encodings", face_encodings)
+    # This method is executed before starting the processing of items.
+    def init(self, confProps, configFolder):
+        self.enabled = confProps.getProperty('enableFaceRecognition').lower() == 'true'
+        if not self.enabled:
             return
-
-    # Load absolute path
-    img_path = item.getTempFile().getAbsolutePath()
-
-    try:
-        # Get tiff:Orientation attribute
-        tiff_orient = int(item.getMetadata().get("image:tiff:Orientation"))
-    except:
-        # If item has no tiff:Orientation attribute
-        tiff_orient = 1
-    
-    
-    if checkExternalProcess() == False:
-        logger.error("Error pinging external face recognition process")
+        self.configDir = configFolder.getAbsolutePath()
+        global cache
+        if cache is None:
+            from java.util.concurrent import ConcurrentHashMap
+            cache = ConcurrentHashMap()
+        createProcessQueue(self.configDir)
         return
-    
-    print(img_path, file=proc.stdin, flush=True)
-    print(str(tiff_orient), file=proc.stdin, flush=True)
             
-    t1 = time.time()
-    #face_locations = fr.face_locations(img)
-    
-    line = proc.stdout.readline().strip()
-    if line == imgError:
-        logger.info("Error loading image {} ({} bytes)", item.getPath(), item.getLength())
-        cacheResults(item, [], [])
-        return
+    # It is executed after processing all items in case.
+    def finish(self):
+        #terminate subprocess
+        if not processQueue.empty():
+            proc = processQueue.get(block=True)
+            if proc.poll() is None:
+                try:
+                    print(terminate, file=proc.stdin, flush=True)
+                    proc.wait(2)
+                except:
+                    proc.kill()
         
-    num_faces = int(line)
-    if num_faces == 0:
-        cacheResults(item, [], [])
-        return
+        with timeLock:
+            global detectTime, featureTime
+            if detectTime + featureTime > 0:
+                logger.info('Time(s) to detect faces: ' + str(detectTime / maxProcesses))
+                logger.info('Time(s) to get face features: ' + str(featureTime / maxProcesses))
+                detectTime = 0
+                featureTime = 0
     
-    face_locations = []
-    for i in range(num_faces):
-        line = proc.stdout.readline()
-        face_locations.append(eval(line))
-    #print('locations ' + str(face_locations))
+    # Needed because tuples cause ClassNotFoundException on java side later
+    def convertTuplesToList(self, tuples):
+        result = []
+        for i in tuples:
+            result.append(list(i))
+        return result
     
-    t2 = time.time()
-    #face_encodings = fr.face_encodings(img, face_locations)
+    def cacheResults(self, item, locations, encodings):
+        cache.put(item.getHash() + '_locations', locations)
+        cache.put(item.getHash() + '_encodings', encodings)
     
-    face_encodings = []
-    for i in range(num_faces):
-        list = []
-        for j in range(128):
-            line = proc.stdout.readline()
-            list.append(float(line))
-        np_array = np.array(list)
-        face_encodings.append(np_array)
+    # This function is executed on all case items
+    def process(self, item):
     
-    #print('encodings ' + str(face_encodings))
+        # Only image type items are processed
+        if item.getHash() is None or not item.getMediaType().toString().startswith('image'):
+            return
+        
+        #reuse cached results
+        face_locations = cache.get(item.getHash() + '_locations')
+        face_encodings = cache.get(item.getHash() + '_encodings')
+        if face_locations is not None and face_encodings is not None:
+            if len(face_locations) > 0 and len(face_encodings) > 0:
+                item.setExtraAttribute("face_locations", face_locations)
+                item.setExtraAttribute("face_encodings", face_encodings)
+                return
     
-    t3 = time.time()
+        # Load absolute path
+        img_path = item.getTempFile().getAbsolutePath()
     
-    global detectTime, featureTime
-    detectTime += t2 - t1
-    featureTime += t3 - t2
-    
-    face_locations = convertTuplesToList(face_locations)
-    
-    item.setExtraAttribute("face_locations", face_locations)
-    item.setExtraAttribute("face_encodings", face_encodings)
-    
-    cacheResults(item, face_locations, face_encodings)
-    
-    
-if __name__ == "__main__":
-    main()
-    
+        try:
+            # Get tiff:Orientation attribute
+            tiff_orient = int(item.getMetadata().get("image:tiff:Orientation"))
+        except:
+            # If item has no tiff:Orientation attribute
+            tiff_orient = 1
+        
+        
+        #if getExternalProcess() == False:
+        #    logger.error("Error pinging external face recognition process")
+        #    return
+            
+        try:
+            proc = processQueue.get(block=True)
+            if not pingExternalProcess(proc):
+                proc.kill()
+                proc = createExternalProcess(self.configDir)
+            
+            print(img_path, file=proc.stdin, flush=True)
+            print(str(tiff_orient), file=proc.stdin, flush=True)
+                    
+            t1 = time.time()
+            #face_locations = fr.face_locations(img)
+            
+            line = proc.stdout.readline().strip()
+            if line == imgError:
+                logger.info("Error loading image {} ({} bytes)", item.getPath(), item.getLength())
+                self.cacheResults(item, [], [])
+                return
+                
+            num_faces = int(line)
+            if num_faces == 0:
+                self.cacheResults(item, [], [])
+                return
+            
+            face_locations = []
+            for i in range(num_faces):
+                line = proc.stdout.readline()
+                face_locations.append(eval(line))
+            #print('locations ' + str(face_locations))
+            
+            t2 = time.time()
+            #face_encodings = fr.face_encodings(img, face_locations)
+            
+            face_encodings = []
+            for i in range(num_faces):
+                list = []
+                for j in range(128):
+                    line = proc.stdout.readline()
+                    list.append(float(line))
+                np_array = np.array(list)
+                face_encodings.append(np_array)
+            
+            #print('encodings ' + str(face_encodings))
+            
+            t3 = time.time()
+        
+        finally:
+            processQueue.put(proc, block=True)
+            
+        global detectTime, featureTime
+        with timeLock:
+            detectTime += t2 - t1
+            featureTime += t3 - t2
+        
+        face_locations = self.convertTuplesToList(face_locations)
+        
+        item.setExtraAttribute("face_locations", face_locations)
+        item.setExtraAttribute("face_encodings", face_encodings)
+        
+        self.cacheResults(item, face_locations, face_encodings)
