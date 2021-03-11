732297fc1c336ef32a72a992253a9d9b40a1819d
Luis Nassif <lfcnassif@gmail.com>
2021-03-11 16:16:35 -0300

    closes #388: change python task impls and example to implement classes

--- iped-app/resources/config/profiles/en/default/conf/scripts/NSFWNudityDetectTask.py
+++ iped-app/resources/config/profiles/en/default/conf/scripts/NSFWNudityDetectTask.py
@@ -21,241 +21,248 @@ import time
 import sys
 from java.lang import System
 
-itemList = []
-imageList = []
 targetSize = (224, 224)
 videoFramesTime = 0
 arrayConvTime = 0
 predictTime = 0
 loadImgTime = 0
-queued = False
 enabled = False
 semaphore = None
 
-def isEnabled():
-	return enabled
-	
-def processQueueEnd():
-	return True
-
 def loadModel():
-	model = caseData.getCaseObject('nsfw_model')
-	if model is None:
-		file = System.getProperty('iped.root') + '/models/nsfw-keras-1.0.0.h5'
-		from keras.models import load_model
-		model = load_model(file)
-		x = np.zeros((1, 224, 224, 3))
-		#compile predict function to be used by multiple threads
-		model.predict(x)
-		caseData.putCaseObject('nsfw_model', model)
-		logger.info('Loaded NSFW model ' + file)
-		from java.util.concurrent import ConcurrentHashMap
-		cache = ConcurrentHashMap()
-		caseData.putCaseObject('nsfw_score_cache', cache)
-	
-	return model
+    model = caseData.getCaseObject('nsfw_model')
+    if model is None:
+        file = System.getProperty('iped.root') + '/models/nsfw-keras-1.0.0.h5'
+        from keras.models import load_model
+        model = load_model(file)
+        x = np.zeros((1, 224, 224, 3))
+        #compile predict function to be used by multiple threads
+        model.predict(x)
+        caseData.putCaseObject('nsfw_model', model)
+        logger.info('Loaded NSFW model ' + file)
+        from java.util.concurrent import ConcurrentHashMap
+        cache = ConcurrentHashMap()
+        caseData.putCaseObject('nsfw_score_cache', cache)
+    
+    return model
 
 def createSemaphore():
-	if maxThreads is None:
-		return
-	global semaphore
-	semaphore = caseData.getCaseObject('nsfw_semaphore')
-	if(semaphore is None):
-		from java.util.concurrent import Semaphore
-		semaphore = Semaphore(maxThreads)
-		caseData.putCaseObject('nsfw_semaphore', semaphore)
-	return semaphore
-
-def init(confProps, configFolder):
-	global enabled
-	enabled = confProps.getProperty('enableYahooNSFWDetection').lower() == 'true'
-	if enabled:
-		loadModel()
-	createSemaphore()
-	return
-
-def finish():
-	num_finishes = caseData.getCaseObject('num_finishes')
-	if num_finishes is None:
-		num_finishes = 0;
-	num_finishes += 1
-	caseData.putCaseObject('num_finishes', num_finishes)
-	
-	times = caseData.getCaseObject('nsfw_times')
-	if times is None:
-		times = [0, 0, 0, 0]
-		
-	times[0] += videoFramesTime / numThreads
-	times[1] += arrayConvTime / numThreads
-	times[2] += predictTime / numThreads
-	times[3] += loadImgTime / numThreads
-	caseData.putCaseObject('nsfw_times', times)
-	
-	if num_finishes == numThreads:
-		logger.info('Time(s) to get video frames: ' + str(times[0]))
-		logger.info('Time(s) to convert java arrays: ' + str(times[1]))
-		logger.info('Time(s) to NSFW prediction: ' + str(times[2]))
-		logger.info('Time(s) to load images: ' + str(times[3]))
-
-def isSupportedVideo(item):
-	return item.getMediaType() is not None and item.getMediaType().toString().startswith('video') and item.getViewFile() is not None
-	
+    if maxThreads is None:
+        return
+    global semaphore
+    semaphore = caseData.getCaseObject('nsfw_semaphore')
+    if(semaphore is None):
+        from java.util.concurrent import Semaphore
+        semaphore = Semaphore(maxThreads)
+        caseData.putCaseObject('nsfw_semaphore', semaphore)
+    return semaphore
+   
 def isImage(item):
-	return item.getMediaType() is not None and item.getMediaType().toString().startswith('image')
-
+    return item.getMediaType() is not None and item.getMediaType().toString().startswith('image')
+       
+def isSupportedVideo(item):
+    return item.getMediaType() is not None and item.getMediaType().toString().startswith('video') and item.getViewFile() is not None
+    
 def supported(item):
-	return item.getLength() > 0 and (isImage(item) or isSupportedVideo(item))
+    return item.getLength() > 0 and (isImage(item) or isSupportedVideo(item))
 
 def convertJavaByteArray(byteArray):
-	global arrayConvTime
-	t = time.time()
-	result =  bytes(b % 256 for b in byteArray)
-	arrayConvTime += time.time() - t
-	return result
-	
+    global arrayConvTime
+    t = time.time()
+    result =  bytes(b % 256 for b in byteArray)
+    arrayConvTime += time.time() - t
+    return result
+    
 def loadRawImage(input):
-	global loadImgTime 
-	t = time.time()
-	img = PilImage.open(io.BytesIO(input))
-	img = img.convert('RGB')
-	img = img.resize(targetSize, PilImage.NEAREST)
-	loadImgTime += time.time() - t
-	return img
-	
-def sendToNextTask(item):
-	
-	if not item.isQueueEnd() and not queued:
-		javaTask.sendToNextTaskSuper(item)
-		return
-	
-	if isToProcessBatch(item):
-		for i in itemList:
-			javaTask.sendToNextTaskSuper(i)
-		
-		itemList.clear()
-		imageList.clear()
-		
-	if item.isQueueEnd():
-		javaTask.sendToNextTaskSuper(item)
-		
+    global loadImgTime 
+    t = time.time()
+    img = PilImage.open(io.BytesIO(input))
+    img = img.convert('RGB')
+    img = img.resize(targetSize, PilImage.NEAREST)
+    loadImgTime += time.time() - t
+    return img
 
-def isToProcessBatch(item):
-	size = len(itemList)
-	return size >= batchSize or (size > 0 and item.isQueueEnd())
+'''
+Main class
+'''
+class NSFWNudityDetectTask:
+    
+    itemList = []
+    imageList = []
+    queued = False
 
-def process(item):
-	
-	global queued
-	queued = False
-
-	if not item.isQueueEnd() and not supported(item):
-		return
-		
-	try:
-		if item.getHash() is not None:
-			cache = caseData.getCaseObject('nsfw_score_cache')
-			score = cache.get(item.getHash())
-			if score is not None:
-				item.setExtraAttribute('nsfw_nudity_score', score)
-				return
-		
-		#print('Processing ' + item.getPath())
-		img = None
-		
-		if isSupportedVideo(item):
-			processVideoFrames(item)
-			return
-			
-		from keras.preprocessing import image
-			
-		if isImage(item) and not useImageThumbs and item.getTempFile() is not None:
-			img_path = item.getTempFile().getAbsolutePath()
-			img = image.load_img(img_path, target_size=targetSize)
-			
-		if isImage(item) and useImageThumbs and item.getExtraAttribute('hasThumb'):
-			input = convertJavaByteArray(item.getThumb())
-			img = loadRawImage(input)
-			
-		if not item.isQueueEnd():
-			if img is None:	
-				item.setExtraAttribute('nsfw_error', 1)
-				return
-			
-			x = image.img_to_array(img)
-			imageList.append(x)
-			itemList.append(item)
-			queued = True
-		
-	except Exception as e:
-		item.setExtraAttribute('nsfw_error', 2)
-		raise e
-		
-	if isToProcessBatch(item):
-		processImages(imageList, itemList)
-
-
+    def isEnabled(self):
+        return enabled
+        
+    def processQueueEnd(self):
+        return True
+    
+    def init(self, confProps, configFolder):
+        global enabled
+        enabled = confProps.getProperty('enableYahooNSFWDetection').lower() == 'true'
+        if enabled:
+            loadModel()
+        createSemaphore()
+        return
+    
+    def finish(self):
+        num_finishes = caseData.getCaseObject('num_finishes')
+        if num_finishes is None:
+            num_finishes = 0;
+        num_finishes += 1
+        caseData.putCaseObject('num_finishes', num_finishes)
+        
+        times = caseData.getCaseObject('nsfw_times')
+        if times is None:
+            times = [0, 0, 0, 0]
+            
+        times[0] += videoFramesTime / numThreads
+        times[1] += arrayConvTime / numThreads
+        times[2] += predictTime / numThreads
+        times[3] += loadImgTime / numThreads
+        caseData.putCaseObject('nsfw_times', times)
+        
+        if num_finishes == numThreads:
+            logger.info('Time(s) to get video frames: ' + str(times[0]))
+            logger.info('Time(s) to convert java arrays: ' + str(times[1]))
+            logger.info('Time(s) to NSFW prediction: ' + str(times[2]))
+            logger.info('Time(s) to load images: ' + str(times[3]))
+    
+    
+    def sendToNextTask(self, item):
+        
+        if not item.isQueueEnd() and not self.queued:
+            javaTask.sendToNextTaskSuper(item)
+            return
+        
+        if self.isToProcessBatch(item):
+            for i in self.itemList:
+                javaTask.sendToNextTaskSuper(i)
+            
+            self.itemList.clear()
+            self.imageList.clear()
+            
+        if item.isQueueEnd():
+            javaTask.sendToNextTaskSuper(item)
+    
+    
+    def isToProcessBatch(self, item):
+        size = len(self.itemList)
+        return size >= batchSize or (size > 0 and item.isQueueEnd())
+    
+    
+    def process(self, item):
+        
+        self.queued = False
+    
+        if not item.isQueueEnd() and not supported(item):
+            return
+            
+        try:
+            if item.getHash() is not None:
+                cache = caseData.getCaseObject('nsfw_score_cache')
+                score = cache.get(item.getHash())
+                if score is not None:
+                    item.setExtraAttribute('nsfw_nudity_score', score)
+                    return
+            
+            #print('Processing ' + item.getPath())
+            img = None
+            
+            if isSupportedVideo(item):
+                processVideoFrames(item)
+                return
+                
+            from keras.preprocessing import image
+                
+            if isImage(item) and not useImageThumbs and item.getTempFile() is not None:
+                img_path = item.getTempFile().getAbsolutePath()
+                img = image.load_img(img_path, target_size=targetSize)
+                
+            if isImage(item) and useImageThumbs and item.getExtraAttribute('hasThumb'):
+                input = convertJavaByteArray(item.getThumb())
+                img = loadRawImage(input)
+                
+            if not item.isQueueEnd():
+                if img is None:    
+                    item.setExtraAttribute('nsfw_error', 1)
+                    return
+                
+                x = image.img_to_array(img)
+                self.imageList.append(x)
+                self.itemList.append(item)
+                self.queued = True
+            
+        except Exception as e:
+            item.setExtraAttribute('nsfw_error', 2)
+            raise e
+            
+        if self.isToProcessBatch(item):
+            processImages(self.imageList, self.itemList)
+    
+    
 def processVideoFrames(item):
-	global videoFramesTime
-	t = time.time()
-	frames = ImageUtil.getBmpFrames(item.getViewFile())
-	videoFramesTime += time.time() - t 
-	list = []
-	scores = []
-	numFrames = frames.size()
-	for i in range(numFrames):
-		input = convertJavaByteArray(frames.get(i))
-		img = loadRawImage(input)
-		from keras.preprocessing import image
-		x = image.img_to_array(img)
-		list.append(x)
-		if batchSize == 1 or (i > 0 and i % batchSize == 0) or i == (numFrames - 1):
-			preds = makePrediction(list)
-			for i in range(len(list)):
-				scores.append(preds[i][1])
-			list.clear()
-	
-	finalScore = videoScore(scores)
-	item.setExtraAttribute('nsfw_nudity_score', finalScore)
-	cache = caseData.getCaseObject('nsfw_score_cache')
-	cache.put(item.getHash(), finalScore)
-	
+    global videoFramesTime
+    t = time.time()
+    frames = ImageUtil.getBmpFrames(item.getViewFile())
+    videoFramesTime += time.time() - t 
+    list = []
+    scores = []
+    numFrames = frames.size()
+    for i in range(numFrames):
+        input = convertJavaByteArray(frames.get(i))
+        img = loadRawImage(input)
+        from keras.preprocessing import image
+        x = image.img_to_array(img)
+        list.append(x)
+        if batchSize == 1 or (i > 0 and i % batchSize == 0) or i == (numFrames - 1):
+            preds = makePrediction(list)
+            for i in range(len(list)):
+                scores.append(preds[i][1])
+            list.clear()
+    
+    finalScore = videoScore(scores)
+    item.setExtraAttribute('nsfw_nudity_score', finalScore)
+    cache = caseData.getCaseObject('nsfw_score_cache')
+    cache.put(item.getHash(), finalScore)
+    
 def videoScore(scores):
-	scores.sort(reverse=True)
-	weight = 1
-	mult = 0.7
-	div = 0
-	sum = 0
-	for v in scores:
-		div += weight
-		sum += v * weight
-		weight *= mult
-	if div > 0:
-		sum /= div
-	return sum * 100
+    scores.sort(reverse=True)
+    weight = 1
+    mult = 0.7
+    div = 0
+    sum = 0
+    for v in scores:
+        div += weight
+        sum += v * weight
+        weight *= mult
+    if div > 0:
+        sum /= div
+    return sum * 100
 
 def processImages(imageList, itemList):
-	logger.debug('Processing batch of ' + str(len(imageList)) + " images.")
-	preds = makePrediction(imageList)
-	cache = caseData.getCaseObject('nsfw_score_cache')
-	for i in range(len(itemList)):
-		score = preds[i][1] * 100
-		itemList[i].setExtraAttribute('nsfw_nudity_score', score)
-		cache.put(itemList[i].getHash(), score)
+    logger.debug('Processing batch of ' + str(len(imageList)) + " images.")
+    preds = makePrediction(imageList)
+    cache = caseData.getCaseObject('nsfw_score_cache')
+    for i in range(len(itemList)):
+        score = preds[i][1] * 100
+        itemList[i].setExtraAttribute('nsfw_nudity_score', score)
+        cache.put(itemList[i].getHash(), score)
 
 def makePrediction(list):
-	global predictTime 
-	t = time.time()
-	x = np.stack(list, axis=0)
-	from keras.applications.imagenet_utils import preprocess_input
-	x = preprocess_input(x)
-	model = loadModel()
-	try:
-		if semaphore is not None:
-			semaphore.acquire()
+    global predictTime 
+    t = time.time()
+    x = np.stack(list, axis=0)
+    from keras.applications.imagenet_utils import preprocess_input
+    x = preprocess_input(x)
+    model = loadModel()
+    try:
+        if semaphore is not None:
+            semaphore.acquire()
 
-		preds = model.predict(x)
-		predictTime += time.time() - t
-		return preds
-	finally:
-		if semaphore is not None:
-			semaphore.release()
+        preds = model.predict(x)
+        predictTime += time.time() - t
+        return preds
+    finally:
+        if semaphore is not None:
+            semaphore.release()
--- iped-app/resources/config/profiles/en/default/conf/scripts/PythonScriptTask.py
+++ iped-app/resources/config/profiles/en/default/conf/scripts/PythonScriptTask.py
@@ -1,76 +1,81 @@
 ﻿# Python task script example. It must be installed in TaskInstaller.xml to be executed.
-# At least, getName() and process(item) functions should be implemented.
-# The script could access any item properties, extracted text, thumbs and raw content.
-# The script can ignore items, put them in bookmarks or set extra item properties after external processing.
-# You need to install jep (pip install jep==3.8.2) and include jep.dll(.so) in PATH or LD_LIBRARY_PATH (Linux)
-
-# Returns the script task name.
-def getName():
-	return ("PythonScriptTask")
+# The script could access any item properties, extracted text, thumbs and raw bytes.
+# The script can set extra item properties/columns in process() method, ignore items or create bookmarks in finish().
+# You need to install jep (pip install jep) and include jep.dll(.so) in PATH or LD_LIBRARY_PATH (Linux)
+# see https://github.com/sepinf-inc/IPED/wiki/User-Manual#python-modules
 
 
-# Do some task initialization, like reading options, custom config file or model.
-# It is executed when application starts.
-# @Params
-# confProps: java Properties object with main processing options (IPEDConfig.txt)
-# configFolder: java File object pointing 'conf' folder in profile folder, where a custom script config file can be created.
-def init(confProps, configFolder):
-	#print("init")
-	return
+# The main class name must be equal to the script file name without .py extension
+# One instance of this class is created by each processing thread and each thread calls the implemented methods of its own object.
+class PythonScriptTask:
 
+    # Returns if this task is enabled or not. This could access options read by init() method.
+    def isEnabled(self):
+        return True
 
-# Finish task, maybe cleaning resources. It is executed after processing all items in case.
-# Objects "ipedCase" and "searcher" are shared, so case can be queried for items and bookmarks can be created, for example.
-# TODO: document methods of those objects.
-def finish():
-	
-	query = "tipo:doc"
-	
-	#set query into searcher
-	searcher.setQuery(query)
-	
-	#search in case and return item ids
-	ids = searcher.search().getIds()
-	
-	#create new bookmark and get its id
-	labelId = ipedCase.getMarcadores().newLabel("DOC files")
-	
-	#set bookmark comment
-	ipedCase.getMarcadores().setLabelComment(labelId, "Documents of DOC file format")
-	
-	#add item ids to created bookmark
-	ipedCase.getMarcadores().addLabel(ids, labelId)
-	
-	#save changes
-	ipedCase.getMarcadores().saveState()
-
-
-# Process an Item object. This function is executed on all case items.
-# It can access any method of Item class:
-#
-#  Some Getters:
-#  String:  getName(), getExt(), getTypeExt(), getPath(), getHash(), getMediaType().toString(), getCategories() (categories separated by | )
-#  Date:    getModDate(), getCreationDate(), getAccessDate() (podem ser nulos)
-#  Boolean: isDeleted(), isDuplicate(), isDir(), isRoot(), isCarved(), isSubItem(), isTimedOut(), hasChildren()
-#  Long:    getLength()
-#  Metadata getMetadata()
-#  Object:  getExtraAttribute(String key) (returns an extra attribute)
-#  String:  getParsedTextCache() (returns item extracted text, if this task is placed after ParsingTask)
-#  File:    getTempFile() (returns a temp file with item content)
-#  BufferedInputStream: getBufferedStream() (returns an InputStream with item content)
-#
-#  Some Setters: 
-#           setToIgnore(boolean) (ignores the item and excludes it from processing and case)
-#           setAddToCase(boolean) (inserts or not item in case, after being processed: default true)
-#           addCategory(String), removeCategory(String), setMediaTypeStr(String)
-# 		 	 setExtraAttribute(key, value), setParsedTextCache(String)
-#
-def process(item):
-	
-	# Ignore item example
-	if(".dll" in item.getExt().lower()):
-		item.setToIgnore(true)
-	
-	# Create extra attribute/column example
-	if(".com" in item.getParsedTextCache().lower()):
-		item.setExtraAttribute("containsDotCom", "true")
+    # Do some task initialization, like reading options, custom config files or model.
+    # It is executed when application starts by each processing thread on its class instance.
+    # @Params
+    # confProps: java Properties object with main processing options (IPEDConfig.txt)
+    # configFolder: java File object pointing 'conf' folder in profile folder, where a custom script config file can be created.
+    def init(self, confProps, configFolder):
+        #print("init")
+        return
+    
+    
+    # Finish method run after processing all items in case, e.g. to clean resources.
+    # It is executed by each processing thread on its class instance.
+    # Objects "ipedCase" and "searcher" are provided, so case can be queried for items and bookmarks can be created, for example.
+    # TODO: document methods of those objects.
+    def finish(self):
+        
+        query = "tipo:doc"
+        
+        #set query into searcher
+        searcher.setQuery(query)
+        
+        #search in case and return item ids
+        ids = searcher.search().getIds()
+        
+        #create new bookmark and get its id
+        labelId = ipedCase.getMarcadores().newLabel("DOC files")
+        
+        #set bookmark comment
+        ipedCase.getMarcadores().setLabelComment(labelId, "Documents of DOC file format")
+        
+        #add item ids to created bookmark
+        ipedCase.getMarcadores().addLabel(ids, labelId)
+        
+        #save changes
+        ipedCase.getMarcadores().saveState()
+    
+    
+    # Process an Item object. This method is executed on all case items.
+    # It can access any method of Item class and store results as a new extra attribute.
+    #
+    #  Some Getters:
+    #  String:  getName(), getExt(), getTypeExt(), getPath(), getHash(), getMediaType().toString(), getCategories() (categories separated by | )
+    #  Date:    getModDate(), getCreationDate(), getAccessDate() (podem ser nulos)
+    #  Boolean: isDeleted(), isDuplicate(), isDir(), isRoot(), isCarved(), isSubItem(), isTimedOut(), hasChildren()
+    #  Long:    getLength()
+    #  Metadata getMetadata()
+    #  Object:  getExtraAttribute(String key) (returns an extra attribute)
+    #  String:  getParsedTextCache() (returns item extracted text, if this task is placed after ParsingTask)
+    #  File:    getTempFile() (returns a temp file with item content)
+    #  BufferedInputStream: getBufferedStream() (returns an InputStream with item content)
+    #
+    #  Some Setters: 
+    #           setToIgnore(boolean) (ignores the item and excludes it from processing and case)
+    #           setAddToCase(boolean) (inserts or not item in case, after being processed: default true)
+    #           addCategory(String), removeCategory(String), setMediaTypeStr(String)
+    #              setExtraAttribute(key, value), setParsedTextCache(String)
+    #
+    def process(self, item):
+        
+        # Ignore item example
+        if item.getExt() is not None and ".dll" in item.getExt().lower():
+            item.setToIgnore(True)
+        
+        # Create extra attribute/column example
+        if item.getParsedTextCache() is not None and ".com" in item.getParsedTextCache().lower():
+            item.setExtraAttribute("containsDotCom", True)
