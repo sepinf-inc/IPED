be6b1b6e2039d953d6774715ad10867b5448c2ea
Luis Nassif <lfcnassif@gmail.com>
2021-03-24 20:27:40 -0300

    update version

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/Versao.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/Versao.java
@@ -23,7 +23,7 @@ package dpf.sp.gpinf.indexer;
  */
 public class Versao {
 
-    public static String APP_VERSION = "3.18.4"; //$NON-NLS-1$
+    public static String APP_VERSION = "3.18.5"; //$NON-NLS-1$
     public static String APP_NAME_PREFIX = "Indexador e Processador de Evidências Digitais"; //$NON-NLS-1$
     public static String APP_NAME = APP_NAME_PREFIX + " " + APP_VERSION; //$NON-NLS-1$
     public static String APP_EXT = "IPED"; //$NON-NLS-1$
