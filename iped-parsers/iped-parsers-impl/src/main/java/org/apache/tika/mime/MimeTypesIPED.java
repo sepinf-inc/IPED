611c3bd499baa780ab635b69588901b493deee85
Matheus Bichara <bda.matheus@gmail.com>
2021-04-16 11:44:40 -0400

    Adds to CustomSignatures.xml the mimes that should use name based detection

--- iped-parsers/iped-parsers-impl/src/main/java/org/apache/tika/mime/MimeTypesIPED.java
+++ iped-parsers/iped-parsers-impl/src/main/java/org/apache/tika/mime/MimeTypesIPED.java
@@ -1,15 +1,23 @@
 package org.apache.tika.mime;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.tika.config.TikaConfig;
 import org.apache.tika.detect.Detector;
+import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.utils.XMLReaderUtils;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 /**
  * Custom MimeTypes
@@ -18,13 +26,69 @@ public class MimeTypesIPED implements Detector {
     private static final long serialVersionUID = 1L;
 
     private static MimeTypes mimeTypes;
+    private static HashSet<MimeType> nameDetectionMimeSet = new HashSet<MimeType>();
 
     static {
+    	//Get Default MimeTypes
     	TikaConfig tikaConfig = TikaConfig.getDefaultConfig();
     	mimeTypes = tikaConfig.getMimeRepository();
+    	
+    	//Get the list of mimes from CustomSignatures.xml that should always use name based detection
+    	String customMimesPath = System.getProperty(MimeTypesFactory.CUSTOM_MIMES_SYS_PROP);
+        File externalFile = new File(customMimesPath);        
+		try {
+			URL externalURL = externalFile.toURI().toURL();
+			InputStream stream = externalURL.openStream();
+			Element element = XMLReaderUtils.buildDOM(stream).getDocumentElement();
+			getNameDetectionMimeSet(element);
+		} catch (IOException | TikaException | SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
     }
     
-    /**
+    private static void getNameDetectionMimeSet(Element element) {
+    	  	
+		Element elMimesNameDetection = getChild(element, "mimes-name-detection");
+    	if(elMimesNameDetection == null)
+    		return;
+    	
+    	int size = elMimesNameDetection.getChildNodes().getLength();
+    	for(int i = 0; i < size; i++) {
+    		
+    		Node nodeMime = elMimesNameDetection.getChildNodes().item(i);
+    		if(nodeMime.getNodeType() != Node.ELEMENT_NODE) {
+    			continue;
+    		}
+    		
+    		MimeType mimeType = null;
+    		try {	
+        		String mimeString = ((Element)nodeMime).getAttribute("type");
+    			mimeType = mimeTypes.forName(mimeString);
+    			if(mimeType != null) {
+    				nameDetectionMimeSet.add(mimeType);
+    			}
+			} catch (MimeTypeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+	}
+    
+    private static Element getChild(Element element, String name) {
+        Node child = element.getFirstChild();
+        while (child != null) {
+            if (child.getNodeType() == Node.ELEMENT_NODE
+                    && name.equals(child.getNodeName())) {
+                return (Element) child;
+            }
+            child = child.getNextSibling();
+        }
+        return null;
+    }
+
+	/**
      * Use the MimeType hint to try to clarify or specialise the current
      *  possible types list.
      * If the hint is a specialised form, use that instead
@@ -103,12 +167,14 @@ public class MimeTypesIPED implements Detector {
                 	
                 	List<Magic> magics = extensionHint.getMagics();
                 	boolean noMagicDefinedForHint = (magics == null) || magics.isEmpty();
+                	
+                	boolean runNameDetection = nameDetectionMimeSet.contains(extensionHint);
                 	  	
                 	//Name based detection is used in three situations:
                 	//1 - If the mime hint has no magic associated with it. 
                 	//2 - If there was a signature match(try to specialize).
-                	//3 - If the mime hint is defined to always run name detection(TO DO).
-                	if (noMagicDefinedForHint || signatureDetected) {
+                	//3 - If the mime hint is defined to always run name detection.
+                	if (noMagicDefinedForHint || signatureDetected || runNameDetection) {
                 		// If we have some types based on mime magic, try to specialise
                         //  and/or select the type based on that
                         // Otherwise, use the type identified from the name
