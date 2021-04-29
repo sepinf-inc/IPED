98711b38be149283d0f9fdec5c7b26077256812f
guilh <guilh@g_3lb>
2021-04-29 14:17:24 -0300

    Created RawStringParserTest for utf8 and iso8859-1.
    I have removed the software and system files for now. When I find
    smaller samples I'll put it back again and uncomment the tests.
    
    I'll work on the PackageParser now.

---
+++ iped-parsers/iped-parsers-impl/src/test/java/dpf/sp/gpinf/indexer/parsers/RawStringParserTest.java
@@ -0,0 +1,73 @@
+package dpf.sp.gpinf.indexer.parsers;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.junit.Test;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import junit.framework.TestCase;
+
+public class RawStringParserTest extends TestCase{
+    
+    private static InputStream getStream(String name) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+    } 
+    
+    //Note: A lot of IPED parsers uses RawStringParser. It seems to be working fine. Just created this one
+    //So we can test utf8 and iso8859-1 encoding properly. 
+    
+    @Test
+    public void testRawStringUTF8() throws IOException, SAXException, TikaException{
+
+        RawStringParser parser = new RawStringParser();
+        Metadata metadata = new Metadata();
+        ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
+        InputStream stream = getStream("test-files/test_utf8");
+        parser.getSupportedTypes(context);
+        parser.parse(stream, handler, metadata, context);
+        String hts = handler.toString();
+        assertTrue(hts.contains("issO é OUTR4 stRin8888 codificada em UTF8"));
+        assertTrue(hts.contains("Essa stRin8888G esta´ sendÖOO utilizada n0 P4RSER"));
+        assertTrue(hts.contains("do 1P3D para R4W STR1N85...!!111"));
+    }
+    
+    @Test
+    public void testRawStringISO88591() throws IOException, SAXException, TikaException{
+
+        RawStringParser parser = new RawStringParser();
+        Metadata metadata = new Metadata();
+        ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
+        InputStream stream = getStream("test-files/test_iso8859-1");
+        parser.getSupportedTypes(context);
+        parser.parse(stream, handler, metadata, context);
+        String hts = handler.toString();
+        assertTrue(hts.contains("ssO é ÜM4 stRin8888 codificada em ISO8859-1."));
+        assertTrue(hts.contains("Essa stRinÝGG888 esta\n sendo utilizada n0 P4RsÈR"));
+        assertTrue(hts.contains("do 1P3D para R4W STR1N85...!!111"));
+    }
+    
+    @Test
+    public void testRawStringISOUTF() throws IOException, SAXException, TikaException{
+
+        RawStringParser parser = new RawStringParser();
+        Metadata metadata = new Metadata();
+        ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
+        InputStream stream = getStream("test-files/test_isoutf");
+        parser.getSupportedTypes(context);
+        parser.parse(stream, handler, metadata, context);
+        String hts = handler.toString();
+        assertTrue(hts.contains("issO é ÜM4 MISTUR4 DE stRin888855s codificada em ISO8859-1 e em UTF8."));
+        assertTrue(hts.contains("Essa stRin8888 sendÖOO utilizada n0 sendo utilizada n0 P4RSER"));
+        assertTrue(hts.contains("do 1P3D R4W STR1N85..!!111"));
+    }
+
+}
--- iped-parsers/iped-parsers-impl/src/test/java/dpf/sp/gpinf/indexer/parsers/RegistryParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/dpf/sp/gpinf/indexer/parsers/RegistryParserTest.java
@@ -20,24 +20,26 @@ public class RegistryParserTest extends TestCase{
     private static InputStream getStream(String name) {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
     } 
-    @Test
-    public void testRegistryParserSOFTWARE() throws IOException, SAXException, TikaException{
-
-        RegistryParser parser = new RegistryParser();
-        Metadata metadata = new Metadata();
-        ContentHandler handler = new BodyContentHandler(28000000);
-        ParseContext context = new ParseContext();
-        InputStream stream = getStream("test-files/SOFTWARE");
-        metadata.set(Metadata.RESOURCE_NAME_KEY, "software");
-        parser.getSupportedTypes(context);
-        parser.parse(stream, handler, metadata, context);
+    
+//  Commenting this test until i find a smaller SOFTWARE file.     
+//    @Test
+//    public void testRegistryParserSOFTWARE() throws IOException, SAXException, TikaException{
+//
+//        RegistryParser parser = new RegistryParser();
+//        Metadata metadata = new Metadata();
+//        ContentHandler handler = new BodyContentHandler(28000000);
+//        ParseContext context = new ParseContext();
+//        InputStream stream = getStream("test-files/SOFTWARE");
+//        metadata.set(Metadata.RESOURCE_NAME_KEY, "software");
+//        parser.getSupportedTypes(context);
+//        parser.parse(stream, handler, metadata, context);
 //        String hts = handler.toString();
 //        assertTrue(hts.contains("*.wpc.azureedge.net")); 
 //        assertTrue(hts.contains("California1")); 
 //        assertTrue(hts.contains("App.AppXyk8e485yjpp3qmb8j9cw21eqy1snr07m.mca")); 
 //        assertTrue(hts.contains("microsoftvideo"));
-         
-    }
+//         
+//    }
     
     @Test
     public void testRegistryParserSAM() throws IOException, SAXException, TikaException{
@@ -77,22 +79,23 @@ public class RegistryParserTest extends TestCase{
 
     }
     
-    @Test
-    public void testRegistryParserSYSTEM() throws IOException, SAXException, TikaException{
-
-        RegistryParser parser = new RegistryParser();
-        Metadata metadata = new Metadata();
-        ContentHandler handler = new BodyContentHandler(6000000);
-        ParseContext context = new ParseContext();
-        InputStream stream = getStream("test-files/SYSTEM");
-        metadata.set(Metadata.RESOURCE_NAME_KEY, "system");
-        parser.getSupportedTypes(context);
-        parser.parse(stream, handler, metadata, context);
+//  Also commenting this test until i find some smaller SYSTEM file.     
+//    @Test
+//    public void testRegistryParserSYSTEM() throws IOException, SAXException, TikaException{
+//
+//        RegistryParser parser = new RegistryParser();
+//        Metadata metadata = new Metadata();
+//        ContentHandler handler = new BodyContentHandler(6000000);
+//        ParseContext context = new ParseContext();
+//        InputStream stream = getStream("test-files/SYSTEM");
+//        metadata.set(Metadata.RESOURCE_NAME_KEY, "system");
+//        parser.getSupportedTypes(context);
+//        parser.parse(stream, handler, metadata, context);
 //        String hts = handler.toString();
 //        assertTrue(hts.contains("C:\\Program Files\\Avast Software\\Avast\\defs\\21012716\\engsup.exe")); 
 //        assertTrue(hts.contains("RC:\\Windows\\system32\\MusNotificationUx.exe")); 
 //        assertTrue(hts.contains("C:\\Users\\guilh\\AppData\\Local\\NVIDIA\\NvBackend\\DAO\\29549383\\0.dat")); 
 //        assertTrue(hts.contains("C:\\Program Files\\NVIDIA Corporation\\NVIDIA GeForce Experience\\NVIDIA GeForce Experience.exe")); 
-    }
+//    }
 
 }
