488946561f47181acad16195c3939e21ab06deed
guilh <guilh@g_3lb>
2021-05-03 15:21:02 -0300

    Merging files as binary streams using python snippets

--- iped-parsers/iped-parsers-impl/src/test/java/dpf/sp/gpinf/indexer/parsers/RawStringParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/dpf/sp/gpinf/indexer/parsers/RawStringParserTest.java
@@ -71,29 +71,29 @@ public class RawStringParserTest extends TestCase{
     }
     
     @Test
-    public void testRawStringISOUTF8() throws IOException, SAXException, TikaException{
+    public void testRawStringUTF8ISO() throws IOException, SAXException, TikaException{
 
         RawStringParser parser = new RawStringParser();
         Metadata metadata = new Metadata();
         ContentHandler handler = new BodyContentHandler();
         ParseContext context = new ParseContext();
-        InputStream stream = getStream("test-files/test_isoutf8");
+        InputStream stream = getStream("test-files/test_utf8iso88591");
         parser.getSupportedTypes(context);
         parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
-        assertTrue(hts.contains("issO é ÜM4 MISTUR4 DE stRin888855s codificada em ISO8859-1 e em UTF8."));
-        assertTrue(hts.contains("Essa stRin8888 sendÖOO utilizada n0 sendo utilizada n0 P4RSER"));
-        assertTrue(hts.contains("do 1P3D R4W STR1N85..!!111"));
+        assertTrue(hts.contains("issO é OUTR4 stRin8888 codificada em UTF8."));
+        assertTrue(hts.contains("Essa part3 está em UTF8.issO é ÜM4 stRin8888 codificada em ISO8859-1."));
+        assertTrue(hts.contains("Essa p4rte está em ISO8859-1"));
     }
     
     @Test
-    public void testRawStringISOUTF16() throws IOException, SAXException, TikaException{
+    public void testRawStringUTF16ISO() throws IOException, SAXException, TikaException{
 
         RawStringParser parser = new RawStringParser();
         Metadata metadata = new Metadata();
         ContentHandler handler = new BodyContentHandler();
         ParseContext context = new ParseContext();
-        InputStream stream = getStream("test-files/test_isoutf16");
+        InputStream stream = getStream("test-files/test_utf16iso88591");
         parser.getSupportedTypes(context);
         parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
@@ -112,26 +112,62 @@ public class RawStringParserTest extends TestCase{
         parser.getSupportedTypes(context);
         parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
-        assertTrue(hts.contains("Essa parte está em UTF8."));
+        assertTrue(hts.contains("Essa part3 está em UTF8.i\nssO"));
         assertTrue(hts.contains("Essa pa5te está em UTF16.")); 
         
     }
     
     @Test
-    public void testRawStringISOUTF8UTF16() throws IOException, SAXException, TikaException{
+    public void testRawStringUTF16ISOUTF8() throws IOException, SAXException, TikaException{
 
         RawStringParser parser = new RawStringParser();
         Metadata metadata = new Metadata();
         ContentHandler handler = new BodyContentHandler();
         ParseContext context = new ParseContext();
-        InputStream stream = getStream("test-files/test_isoutf8utf16");
+        InputStream stream = getStream("test-files/test_utf16iso88591utf8");
         parser.getSupportedTypes(context);
         parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
-        assertTrue(hts.contains("Essa p4rte está em ISO8859-1"));
-        assertTrue(hts.contains("Essa parte está em UTF8."));
+        assertTrue(hts.contains("Essa p4rte está em ISO8859-1issO"));
+        assertTrue(hts.contains("Essa part3 está em UTF8."));
         assertTrue(hts.contains("Essa pa5te está em UTF16."));
     }
    
+    
 
 }
+
+/*
+# Python program for writing with particular encode
+data = """sample text"""
+
+# Change as needed (iso8859-1, utf8, utf16)
+data.encode('iso8859-1')
+
+# Writing file.
+with open ('test_iso8859-1', 'w') as fp:
+    fp.write(data)
+
+
+# Python program for reading and merging streams
+
+data = data2 = b''
+out_data = b''
+
+# Reading data from file1
+# Change as needed
+with open('test_utf16iso88591', 'rb') as fp:
+    data = fp.read()
+    out_data += data
+
+# Reading data from file2
+# Change as needed
+with open('test_utf8', 'rb') as fp:
+    data2 = fp.read()
+    out_data += data2
+    
+# Writing file
+# Change as needed    
+with open ('test_utf16iso88591utf8', 'wb') as fp:
+    fp.write(out_data)
+*/
