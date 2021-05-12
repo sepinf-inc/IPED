eca4ddc7f155aaecce430d8ba5067f5c62605a8d
Fabio Melo Pfeifer <fmpfeifer@gmail.com>
2021-05-12 15:54:46 -0300

    Extract Bech32 validator to new class and make it generic

---
+++ iped-engine/src/main/java/br/gov/pf/iped/regex/Bech32AddressValidator.java
@@ -0,0 +1,52 @@
+package br.gov.pf.iped.regex;
+
+public class Bech32AddressValidator {
+
+    private int[] charset_rev;
+    private int[] generator;
+
+    public Bech32AddressValidator(int[] charset_rev, int[] generator) {
+        this.charset_rev = charset_rev;
+        this.generator = generator;
+    }
+
+    private int bech32Polymod(int[] values) {
+        int chk = 1;
+        int top = 0;
+
+        for (int value : values) {
+            top = chk >>> 25;
+            chk = (chk & 0x1ffffff) << 5 ^ value;
+            for (int i = 0; i < 5; i++) {
+                if (((top >>> i) & 1) != 0) {
+                    chk ^= generator[i];
+                }
+            }
+        }
+
+        return chk;
+    }
+
+    private int[] bech32ExpandData(String valueString) {
+        char[] value = valueString.toCharArray();
+        int hrpSize = valueString.lastIndexOf('1');
+        int[] data = new int[value.length + hrpSize];
+
+        data[hrpSize] = 0;
+        for (int i = 0; i < hrpSize; i++) {
+            data[i] = ((int) value[i]) >> 5;
+            data[i + hrpSize + 1] = ((int) value[i]) & 31;
+        }
+        
+        for (int i = 0; i < value.length - hrpSize - 1; i++) {
+            data[i + (hrpSize*2 + 1)] = charset_rev[(int) value[i + hrpSize + 1]];
+        }
+
+        return data;
+    }
+
+    public boolean bech32VerifyChecksum(String value) {
+        return bech32Polymod(bech32ExpandData(value)) == 1;
+    }
+
+}
--- iped-engine/src/main/java/br/gov/pf/iped/regex/BitcoinAddressValidatorService.java
+++ iped-engine/src/main/java/br/gov/pf/iped/regex/BitcoinAddressValidatorService.java
@@ -9,10 +9,10 @@ import dpf.sp.gpinf.indexer.process.task.regex.BasicAbstractRegexValidatorServic
 public class BitcoinAddressValidatorService extends BasicAbstractRegexValidatorService {
 
     private static final AltcoinBase58CheckValidator validator;
-    
+
     static {
         validator = new AltcoinBase58CheckValidator();
-        validator.setVersionForPrefix("1", (byte) 0 );
+        validator.setVersionForPrefix("1", (byte) 0);
         validator.setVersionForPrefix("3", (byte) 5);
         validator.setVersionForPrefix("5", (byte) 128);
         validator.setVersionForPrefix("K", (byte) 128);
@@ -28,6 +28,9 @@ public class BitcoinAddressValidatorService extends BasicAbstractRegexValidatorS
             25, 9, 8, 23, -1, 18, 22, 31, 27, 19, -1, 1, 0, 3, 16, 11, 28, 12, 14, 6, 4, 2, -1, -1, -1, -1, -1 };
 
     private static int[] BECH32_GENERATOR = { 0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3 };
+
+    private static Bech32AddressValidator bech32validator = new Bech32AddressValidator(BECH32_CHARSET_REV,
+            BECH32_GENERATOR);
 
     @Override
     public void init(File confDir) {
@@ -48,52 +51,10 @@ public class BitcoinAddressValidatorService extends BasicAbstractRegexValidatorS
 
     public boolean validateBitcoinAddress(String addr) {
         if (addr.startsWith("bc1")) {
-            return bech32VerifyChecksum(addr);
+            return bech32validator.bech32VerifyChecksum(addr);
         }
-        
+
         return validator.validate(addr);
-    }
-
-    /*
-     * Validacao de enderecos bitcoin que começam com "bc1"
-     */
-
-    private static int bech32Polymod(int[] values) {
-        int chk = 1;
-        int top = 0;
-
-        for (int value : values) {
-            top = chk >>> 25;
-            chk = (chk & 0x1ffffff) << 5 ^ value;
-            for (int i = 0; i < 5; i++) {
-                if (((top >>> i) & 1) != 0) {
-                    chk ^= BECH32_GENERATOR[i];
-                }
-            }
-        }
-
-        return chk;
-    }
-
-    private static int[] bech32ExpandData(String valueString) {
-        char[] value = valueString.toCharArray();
-        int[] data = new int[value.length + 2];
-
-        data[0] = 3;
-        data[1] = 3;
-        data[2] = 0;
-        data[3] = 2;
-        data[4] = 3;
-
-        for (int i = 0; i < value.length - 3; i++) {
-            data[i + 5] = BECH32_CHARSET_REV[(int) value[i + 3]];
-        }
-
-        return data;
-    }
-
-    private static boolean bech32VerifyChecksum(String value) {
-        return bech32Polymod(bech32ExpandData(value)) == 1;
     }
 
 }
