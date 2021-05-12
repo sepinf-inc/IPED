6c15712da8b87da51ae02e707e8acf5df64beb9b
Fabio Melo Pfeifer <fmpfeifer@gmail.com>
2021-05-12 12:17:16 -0300

    Create DASH address validator and enable it

---
+++ iped-engine/src/main/java/br/gov/pf/iped/regex/DashAddressValidatorService.java
@@ -0,0 +1,33 @@
+package br.gov.pf.iped.regex;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import dpf.sp.gpinf.indexer.process.task.regex.BasicAbstractRegexValidatorService;
+
+public class DashAddressValidatorService extends BasicAbstractRegexValidatorService {
+
+    private static final AltcoinBase58CheckValidator validator;
+    
+    static {
+        validator = new AltcoinBase58CheckValidator();
+        validator.setVersionForPrefix("7", 16);
+        validator.setVersionForPrefix("X", 76);
+    }
+
+    @Override
+    public void init(File confDir) {
+        // Nothing to do.
+    }
+
+    @Override
+    public List<String> getRegexNames() {
+        return Arrays.asList("CRIPTOCOIN_DASH");
+    }
+
+    @Override
+    protected boolean validate(String hit) {
+        return validator.validate(hit);
+    }
+}
