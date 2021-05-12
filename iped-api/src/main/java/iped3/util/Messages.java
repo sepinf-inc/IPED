f4e00ee887eb55fe31b26fbed93bc6e4ee73d227
Guilherme Andreúce <guilhermeandreuce@gmail.com>
2021-05-12 16:30:31 -0300

    Updated messages.java from main branch

--- iped-api/src/main/java/iped3/util/Messages.java
+++ iped-api/src/main/java/iped3/util/Messages.java
@@ -14,9 +14,9 @@ public abstract class Messages {
     public static final String LOCALE_SYS_PROP = "iped-locale";
     public static final String BUNDLES_FOLDER = "localization";
 //<<<<<<< ParsersTests
-    public static final String BUNDLES_FOLDER_PREFIX = "iped-app/resources/config/";
+//    public static final String BUNDLES_FOLDER_PREFIX = "iped-app/resources/config/";
 //=======
-//    public static final String BUNDLES_FOLDER_PREFIX = "iped-app/resources/";
+    public static final String BUNDLES_FOLDER_PREFIX = "iped-app/resources/";
 //>>>>>>> master
 
     private static final String BUNDLE_NAME = "iped-basicprops"; //$NON-NLS-1$
