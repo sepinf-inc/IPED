78a85f5d6a9cb189933f19277ef449ac330376f3
tc-wleite <wladimir.leite@gmail.com>
2021-05-31 14:56:10 -0300

    Limit thumbnail dimensions. Closes #578.

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ImageThumbTask.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ImageThumbTask.java
@@ -275,11 +275,7 @@ public class ImageThumbTask extends ThumbTask {
         long[] performanceStats = new long[numStats];
         try {
             BufferedImage img = null;
-            Dimension dimension = null;
-            try (BufferedInputStream stream = evidence.getBufferedStream()) {
-                dimension = ImageUtil.getImageFileDimension(stream);
-            }
-            if (extractThumb && isJpeg(evidence)) { // $NON-NLS-1$
+            if (extractThumb && isJpeg(evidence)) {
                 long t = System.currentTimeMillis();
                 try (BufferedInputStream stream = evidence.getBufferedStream()) {
                     img = ImageUtil.getThumb(stream);
@@ -306,7 +302,6 @@ public class ImageThumbTask extends ThumbTask {
                             true);
                     if (img != null)
                         evidence.setExtraAttribute("externalThumb", "true"); //$NON-NLS-1$ //$NON-NLS-2$
-                    dimension = null;
                 } catch (TimeoutException e) {
                     stats.incTimeouts();
                     evidence.setExtraAttribute(THUMB_TIMEOUT, "true"); //$NON-NLS-1$
@@ -317,8 +312,7 @@ public class ImageThumbTask extends ThumbTask {
             }
 
             if (img != null) {
-                if (dimension != null && (dimension.width > thumbSize || dimension.height > thumbSize)
-                        && Math.max(img.getWidth(), img.getHeight()) != thumbSize) {
+                if (img.getWidth() > thumbSize || img.getHeight() > thumbSize) {
                     long t = System.currentTimeMillis();
                     img = ImageUtil.resizeImage(img, thumbSize, thumbSize);
                     performanceStats[12]++;
