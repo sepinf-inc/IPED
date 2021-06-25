6da06570b56d76c01ac87965cbfeee21785a363d
streeg <140141961@aluno.unb.br>
2021-06-25 15:24:54 -0300

    CertificateParser does not need tracker as it only fills metadata once.

--- iped-parsers/iped-parsers-impl/src/test/java/dpf/mt/gpinf/security/parsers/CertificateParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/dpf/mt/gpinf/security/parsers/CertificateParserTest.java
@@ -6,15 +6,20 @@ import java.io.InputStream;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.BodyContentHandler;
 import org.junit.Test;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
-import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
+import com.google.common.net.HttpHeaders;
 
-public class CertificateParserTest extends AbstractPkgTest{
+import dpf.sp.gpinf.indexer.parsers.IndexerDefaultParser;
+import junit.framework.TestCase;
+
+public class CertificateParserTest extends TestCase{
 
 
     private static InputStream getStream(String name) {
@@ -39,11 +44,11 @@ public class CertificateParserTest extends AbstractPkgTest{
         metadata.add(IndexerDefaultParser.INDEXER_CONTENT_TYPE,
                 MediaType.application("pkix-cert").toString());
         ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
         InputStream stream = getStream("test-files/test_serverCert.der");
-        parser.getSupportedTypes(certificateContext);
-        parser.parse(stream, handler, metadata, certificateContext);
+        parser.getSupportedTypes(context);
+        parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
-        String mts = metadata.toString();
         assertTrue(hts.contains("Propriedade"));
         assertTrue(hts.contains("Valor"));
         assertTrue(hts.contains("Subject"));
@@ -65,16 +70,14 @@ public class CertificateParserTest extends AbstractPkgTest{
         assertTrue(hts.contains("Alternative Names:"));
         assertTrue(hts.contains("This certificate has no alternative names."));
         
-        //assertEquals("subject=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", certificatetracker.subject.get(0));
-        assertTrue(mts.contains("certificate:subject=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("certificate:notafter=2021-07-01"));
-        assertTrue(mts.contains("dc:title=Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("Indexer-Content-Type=application/pkix-cert"));
-        assertTrue(mts.contains("certificate:subjectIsCertAuthority=true"));
-        assertTrue(mts.contains("certificate:issuer=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("title=Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("certificate:notbefore=2021-06-01"));
-        assertTrue(mts.contains("Content-Type=text/plain"));
+        assertEquals("1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(CertificateParser.SUBJECT));
+        assertEquals("2021-07-01", metadata.get(CertificateParser.NOTAFTER).substring(0,10));
+        assertEquals("Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(TikaCoreProperties.TITLE));
+        assertEquals("application/pkix-cert", metadata.get(IndexerDefaultParser.INDEXER_CONTENT_TYPE));
+        assertEquals("true", metadata.get(CertificateParser.ISSUBJECTAUTHORITY));
+        assertEquals("1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(CertificateParser.ISSUER));
+        assertEquals("2021-06-01", metadata.get(CertificateParser.NOTBEFORE).substring(0,10));
+        assertEquals("text/plain", metadata.get(HttpHeaders.CONTENT_TYPE));
         
     }
     
@@ -86,11 +89,11 @@ public class CertificateParserTest extends AbstractPkgTest{
         metadata.add(IndexerDefaultParser.INDEXER_CONTENT_TYPE,
                 MediaType.application("x-pem-file").toString());
         ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
         InputStream stream = getStream("test-files/test_serverPEM.pem");
-        parser.getSupportedTypes(certificateContext);
-        parser.parse(stream, handler, metadata, certificateContext);
+        parser.getSupportedTypes(context);
+        parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
-        String mts = metadata.toString();
         
         assertTrue(hts.contains("Propriedade"));
         assertTrue(hts.contains("Valor"));
@@ -113,15 +116,14 @@ public class CertificateParserTest extends AbstractPkgTest{
         assertTrue(hts.contains("Alternative Names:"));
         assertTrue(hts.contains("This certificate has no alternative names."));
         
-        assertTrue(mts.contains("certificate:subject=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("certificate:notafter=2021-07-01"));
-        assertTrue(mts.contains("dc:title=Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("Indexer-Content-Type=application/x-pem-file"));
-        assertTrue(mts.contains("certificate:subjectIsCertAuthority=true"));
-        assertTrue(mts.contains("certificate:issuer=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("title=Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("certificate:notbefore=2021-06-01"));
-        assertTrue(mts.contains("Content-Type=text/plain"));
+        assertEquals("1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(CertificateParser.SUBJECT));
+        assertEquals("2021-07-01", metadata.get(CertificateParser.NOTAFTER).substring(0,10));
+        assertEquals("Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(TikaCoreProperties.TITLE));
+        assertEquals("application/x-pem-file", metadata.get(IndexerDefaultParser.INDEXER_CONTENT_TYPE));
+        assertEquals("true", metadata.get(CertificateParser.ISSUBJECTAUTHORITY));
+        assertEquals("1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(CertificateParser.ISSUER));
+        assertEquals("2021-06-01", metadata.get(CertificateParser.NOTBEFORE).substring(0,10));
+        assertEquals("text/plain", metadata.get(HttpHeaders.CONTENT_TYPE));
         
     }
 
@@ -133,11 +135,11 @@ public class CertificateParserTest extends AbstractPkgTest{
         metadata.add(IndexerDefaultParser.INDEXER_CONTENT_TYPE,
                 MediaType.application("pkcs7-signature").toString());
         ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
         InputStream stream = getStream("test-files/test_serverPKCS7.p7b");
-        parser.getSupportedTypes(certificateContext);
-        parser.parse(stream, handler, metadata, certificateContext);
+        parser.getSupportedTypes(context);
+        parser.parse(stream, handler, metadata, context);
         String hts = handler.toString();
-        String mts = metadata.toString();
 
         assertTrue(hts.contains("Propriedade"));
         assertTrue(hts.contains("Valor"));
@@ -160,16 +162,14 @@ public class CertificateParserTest extends AbstractPkgTest{
         assertTrue(hts.contains("Alternative Names:"));
         assertTrue(hts.contains("This certificate has no alternative names."));
         
-        assertTrue(mts.contains("certificate:subject=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("certificate:notafter=2021-07-01"));
-        assertTrue(mts.contains("dc:title=Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("Indexer-Content-Type=application/pkcs7-signature"));
-        assertTrue(mts.contains("certificate:subjectIsCertAuthority=true"));
-        assertTrue(mts.contains("certificate:issuer=1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("title=Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR"));
-        assertTrue(mts.contains("certificate:notbefore=2021-06-01"));
-        assertTrue(mts.contains("Content-Type=text/plain"));
-        
+        assertEquals("1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(CertificateParser.SUBJECT));
+        assertEquals("2021-07-01", metadata.get(CertificateParser.NOTAFTER).substring(0,10));
+        assertEquals("Certificado:1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(TikaCoreProperties.TITLE));
+        assertEquals("application/pkcs7-signature", metadata.get(IndexerDefaultParser.INDEXER_CONTENT_TYPE));
+        assertEquals("true", metadata.get(CertificateParser.ISSUBJECTAUTHORITY));
+        assertEquals("1.2.840.113549.1.9.1=#161b6775696c6865726d65616e64726575636540676d61696c2e636f6d,CN=pf.gov.br,OU=PF,O=Polícia Federal,L=Asa Sul,ST=Brasília,C=BR", metadata.get(CertificateParser.ISSUER));
+        assertEquals("2021-06-01", metadata.get(CertificateParser.NOTBEFORE).substring(0,10));
+        assertEquals("text/plain", metadata.get(HttpHeaders.CONTENT_TYPE));
         
     }
 
