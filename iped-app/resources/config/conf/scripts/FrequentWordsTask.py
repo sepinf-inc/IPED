0934c64ab98e5b890e0529c3e7500ef22d67f6d7
Luis Nassif <lfcnassif@gmail.com>
2021-06-29 10:46:54 -0300

    move functions to be class methods, adjust vars capitalization

--- iped-app/resources/config/conf/scripts/FrequentWordsTask.py
+++ iped-app/resources/config/conf/scripts/FrequentWordsTask.py
@@ -12,6 +12,7 @@ class FrequentWordsTask:
     enabled = False
     configDir = None
     stopWordsSet = None
+    stemmer = None
     
     def isEnabled(self):
         return True
@@ -26,11 +27,37 @@ class FrequentWordsTask:
            self.stopWordsSet = set(stopwords.words('portuguese'))
         else:
            self.stopWordsSet = set(stopwords.words('english'))
+        self.stemmer = PorterStemmer()
         return
     
     def finish(self):      
-        return 
+        return
         
+    #Function to remove stopwords from text
+    def remove_stopwords(self, words, stopwords):
+       #Remove stopwords, small words and laughs as "kk"
+       words_without_stopwords = []
+       for item in words:
+          if item not in stopwords and len(item) >= 3 and 'kk' not in item:
+             words_without_stopwords.append(item)
+       return words_without_stopwords
+       
+    #Function to create Bag of Words
+    def create_bag_of_words(self, words):
+       wordfreq = {}
+       for item in words:
+          if item not in wordfreq.keys():
+             wordfreq[item] = 1
+          else:
+             wordfreq[item] += 1     
+       return wordfreq
+       
+    #Stemming function - Stemming is the process of reducing a word to its word stem that affixes to suffixes and prefixes or to the roots of words known as a lemma.
+    def stem(self, words):
+        for item in words:
+           item = self.stemmer.stem(item)
+        return words
+    
     def process(self, item):
         
         #Just Chats and Email
@@ -41,47 +68,22 @@ class FrequentWordsTask:
         text = str(item.getParsedTextCache()).lower()
         #Remove numbers
         text = re.sub(r'\d+','',text)
+        
         #Tokenization
         tokenizer = RegexpTokenizer(r'\w+')
-        Text_separed_from_words = tokenizer.tokenize(text)   
-        
-        #Function to remove stopwords from text
-        def remove_stopwords(words, stopwords):
-           #Remove stopwords, small words and laughs as "kk"
-           words_without_stopwords = []
-           for item in words:
-              if item not in stopwords and len(item) >= 3 and 'kk' not in item:
-                 words_without_stopwords.append(item)
-           return words_without_stopwords
-        
-        #Function to create Bag of Words
-        def Create_bag_of_words(words):
-           wordfreq ={}
-           for item in words:
-              if item not in wordfreq.keys():
-                 wordfreq[item] = 1
-              else:
-                 wordfreq[item] += 1     
-           return wordfreq
+        text_separed_from_words = tokenizer.tokenize(text)   
         
         #Removing imported stopwords from text
-        words_without_stopwords = remove_stopwords(Text_separed_from_words, self.stopWordsSet)
-        
-        #Stemming function - Stemming is the process of reducing a word to its word stem that affixes to suffixes and prefixes or to the roots of words known as a lemma.
-        def stemming(words):
-            stem = PorterStemmer()
-            for item in words:
-               item = stem.stem(item)
-            return words
+        words_without_stopwords = self.remove_stopwords(text_separed_from_words, self.stopWordsSet)
         
         #Stemming words in text
-        words_without_stopwords = stemming(words_without_stopwords)
+        words_without_stopwords = self.stem(words_without_stopwords)
         
         #Creating Bag of Words from stemming words
-        Bag_of_words = Create_bag_of_words(words_without_stopwords)
+        bag_of_words = self.create_bag_of_words(words_without_stopwords)
         
         #Selection of the top 10 words by frequency
-        most_freq = heapq.nlargest(10, Bag_of_words, key=Bag_of_words.get)
+        most_freq = heapq.nlargest(10, bag_of_words, key=bag_of_words.get)
         
         #Setting most frequency words in Extra Attributes in FrequentWords
         item.setExtraAttribute('FrequentWords', most_freq) 
