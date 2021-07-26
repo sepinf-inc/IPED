d1f8ba60d2e6be5605914153b45366b3a96b32af
streeg <140141961@aluno.unb.br>
2021-07-26 16:17:00 -0300

    Finishing SafariPlistParserTests.
    Used a Safari outdated version on virtualbox to create files.

--- iped-parsers/iped-parsers-impl/src/test/java/dpf/inc/sepinf/browsers/parsers/AbstractPkgTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/dpf/inc/sepinf/browsers/parsers/AbstractPkgTest.java
@@ -17,6 +17,7 @@ import org.apache.tika.parser.Parser;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import dpf.sp.gpinf.indexer.parsers.util.ItemInfo;
 import iped3.util.ExtraProperties;
 
 public abstract class AbstractPkgTest extends TestCase {
@@ -24,12 +25,22 @@ public abstract class AbstractPkgTest extends TestCase {
    protected ParseContext chromeContext;
    protected ParseContext edgeContext;
    protected ParseContext safariContext;
+   protected ParseContext safariContexthst;
+   protected ParseContext safariContextbkm;
+   protected ParseContext safariContextdwl;
    
    protected Parser autoDetectParser;
    protected EmbeddedFirefoxParser firefoxtracker;
    protected EmbeddedChromeParser chrometracker;
    protected EmbeddedEdgeParser edgetracker;
    protected EmbeddedSafariParser safaritracker;
+   protected EmbeddedSafariParserHst safaritrackerhst;
+   protected EmbeddedSafariParserBkm safaritrackerbkm;
+   protected EmbeddedSafariParserDwl safaritrackerdwl;
+   protected ItemInfo itemInfo;
+   protected ItemInfo itemInfohst;
+   protected ItemInfo itemInfobkm;
+   protected ItemInfo itemInfodwl;
    
    protected void setUp() throws Exception {
       super.setUp();
@@ -47,8 +58,25 @@ public abstract class AbstractPkgTest extends TestCase {
       edgeContext.set(Parser.class, edgetracker);
 
       safaritracker = new EmbeddedSafariParser();
+      safaritrackerhst = new EmbeddedSafariParserHst();
+      safaritrackerbkm = new EmbeddedSafariParserBkm();
+      safaritrackerdwl = new EmbeddedSafariParserDwl();
       safariContext = new ParseContext();
+      safariContexthst = new ParseContext();
+      safariContextbkm = new ParseContext();
+      safariContextdwl = new ParseContext();
+      ItemInfo itemInfo = new ItemInfo(0, getName(), null, null, "test-files/test_sample.plist", false);
+      ItemInfo itemInfohst = new ItemInfo(0, getName(), null, null, "History", false);
+      ItemInfo itemInfobkm = new ItemInfo(0, getName(), null, null, "Bookmarks", false);
+      ItemInfo itemInfodwl = new ItemInfo(0, getName(), null, null, "Downloads", false);
+      safariContext.set(ItemInfo.class, itemInfo);
       safariContext.set(Parser.class, safaritracker);
+      safariContexthst.set(ItemInfo.class, itemInfohst);
+      safariContexthst.set(Parser.class, safaritrackerhst);
+      safariContextbkm.set(ItemInfo.class, itemInfobkm);
+      safariContextbkm.set(Parser.class, safaritrackerbkm);
+      safariContextdwl.set(ItemInfo.class, itemInfodwl);
+      safariContextdwl.set(Parser.class, safaritrackerdwl);
       
    }
 
@@ -83,14 +111,43 @@ public abstract class AbstractPkgTest extends TestCase {
       }
    }
 
-
    @SuppressWarnings("serial")
-   protected static class EmbeddedSafariParser extends AbstractParser {
+   protected static class EmbeddedSafariParserDwl extends AbstractParser {
+       
+      protected List<String> downloadurl = new ArrayList<String>();
+      protected List<String> downloadlocalpath = new ArrayList<String>();
+      protected List<String> downloadtotalbytes = new ArrayList<String>();
+      protected List<String> downloadreceivedbytes = new ArrayList<String>();
+      
+      public Set<MediaType> getSupportedTypes(ParseContext context) {
+          return (new AutoDetectParser()).getSupportedTypes(context);
+      }
+
+      public void parse(InputStream stream, ContentHandler handler,
+              Metadata metadata, ParseContext context) throws IOException,
+              SAXException, TikaException {
+          
+              
+              if(metadata.get(ExtraProperties.URL)!= null)
+                  downloadurl.add(metadata.get(ExtraProperties.URL));
+
+              if(metadata.get(ExtraProperties.LOCAL_PATH)!= null)
+                  downloadlocalpath.add(metadata.get(ExtraProperties.LOCAL_PATH));
+
+              if(metadata.get(ExtraProperties.DOWNLOAD_TOTAL_BYTES)!= null)
+                  downloadtotalbytes.add(metadata.get(ExtraProperties.DOWNLOAD_TOTAL_BYTES));
+
+              if(metadata.get(ExtraProperties.DOWNLOAD_RECEIVED_BYTES)!= null)
+                  downloadreceivedbytes.add(metadata.get(ExtraProperties.DOWNLOAD_RECEIVED_BYTES));
+          
+      }
+   }
+   
+   @SuppressWarnings("serial")
+   protected static class EmbeddedSafariParserBkm extends AbstractParser {
        
       protected List<String> bookmarktitle = new ArrayList<String>();
       protected List<String> bookmarkurl = new ArrayList<String>();
-      protected List<String> bookmarkcreated = new ArrayList<String>();
-      protected List<String> bookmarkmodified = new ArrayList<String>();
       
       public Set<MediaType> getSupportedTypes(ParseContext context) {
           return (new AutoDetectParser()).getSupportedTypes(context);
@@ -105,15 +162,55 @@ public abstract class AbstractPkgTest extends TestCase {
               
               if(metadata.get(ExtraProperties.URL)!= null)
                   bookmarkurl.add(metadata.get(ExtraProperties.URL));
-              
-              if(metadata.get(TikaCoreProperties.CREATED)!= null)
-                  bookmarkcreated.add(metadata.get(TikaCoreProperties.CREATED));
-              
-              if(metadata.get(TikaCoreProperties.MODIFIED)!= null)
-                  bookmarkmodified.add(metadata.get(TikaCoreProperties.MODIFIED));
           
       }
    }
+   
+   @SuppressWarnings("serial")
+   protected static class EmbeddedSafariParserHst extends AbstractParser {
+       
+      protected List<String> historytitle = new ArrayList<String>();
+      protected List<String> historyaccessed = new ArrayList<String>();
+      protected List<String> historyvisitdate = new ArrayList<String>();
+      protected List<String> historyurl = new ArrayList<String>();
+      
+      public Set<MediaType> getSupportedTypes(ParseContext context) {
+          return (new AutoDetectParser()).getSupportedTypes(context);
+      }
+
+      public void parse(InputStream stream, ContentHandler handler,
+              Metadata metadata, ParseContext context) throws IOException,
+              SAXException, TikaException {
+          
+              if(metadata.get(TikaCoreProperties.TITLE)!= null)
+                  historytitle.add(metadata.get(TikaCoreProperties.TITLE));
+              
+              if(metadata.get(ExtraProperties.ACCESSED)!= null)
+                  historyaccessed.add(metadata.get(ExtraProperties.ACCESSED));
+              
+              if(metadata.get(ExtraProperties.VISIT_DATE)!= null)
+                  historyvisitdate.add(metadata.get(ExtraProperties.VISIT_DATE));
+              
+              if(metadata.get(ExtraProperties.URL)!= null)
+                  historyurl.add(metadata.get(ExtraProperties.URL));
+          
+      }
+   }
+   
+   @SuppressWarnings("serial")
+   protected static class EmbeddedSafariParser extends AbstractParser {
+      
+      public Set<MediaType> getSupportedTypes(ParseContext context) {
+          return (new AutoDetectParser()).getSupportedTypes(context);
+      }
+
+      public void parse(InputStream stream, ContentHandler handler,
+              Metadata metadata, ParseContext context) throws IOException,
+              SAXException, TikaException {
+
+      }
+   }
+
 
 
    @SuppressWarnings("serial")
--- iped-parsers/iped-parsers-impl/src/test/java/dpf/inc/sepinf/browsers/parsers/SafariPlistParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/dpf/inc/sepinf/browsers/parsers/SafariPlistParserTest.java
@@ -5,13 +5,10 @@ import java.io.InputStream;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
-import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.BodyContentHandler;
 import org.junit.Test;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
-
-import dpf.sp.gpinf.indexer.parsers.util.ItemInfo;
 
 public class SafariPlistParserTest extends AbstractPkgTest{
 
@@ -28,19 +25,9 @@ public class SafariPlistParserTest extends AbstractPkgTest{
                 MediaType.application("x-safari-plist").toString());
         ContentHandler handler = new BodyContentHandler();
         InputStream stream = getStream("test-files/test_sample.plist");
-        ParseContext context = new ParseContext();
-        ItemInfo itemInfo = new ItemInfo(0, getName(), null, null, "test-files/test_sample.plist", false);
-        context.set(ItemInfo.class, itemInfo);
-        parser.getSupportedTypes(context);
+        parser.getSupportedTypes(safariContext);
         parser.setExtractEntries(true);
-        try {
-        parser.parse(stream, handler, metadata, safariContext);
-        String hts = handler.toString();
-        String mts = metadata.toString();
-        System.out.println(hts + mts);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        parser.parse(stream, handler, metadata, safariContext);;
         
     }
 
@@ -54,20 +41,37 @@ public class SafariPlistParserTest extends AbstractPkgTest{
                 MediaType.application("x-safari-history").toString());
         ContentHandler handler = new BodyContentHandler();
         InputStream stream = getStream("test-files/test_history.plist");
-        ParseContext context = new ParseContext();
-        ItemInfo itemInfo = new ItemInfo(0, getName(), null, null, "test-files/test_history.plist", false);
-        context.set(ItemInfo.class, itemInfo);
-        parser.getSupportedTypes(context);
+        parser.getSupportedTypes(safariContexthst);
         parser.setExtractEntries(true);
-        try {
-        parser.parse(stream, handler, metadata, safariContext);
-        String hts = handler.toString();
-        String mts = metadata.toString();
-        System.out.println(hts + mts);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        parser.parse(stream, handler, metadata, safariContexthst);
+        assertEquals(19, safaritrackerhst.historytitle.size());
+        assertEquals(19, safaritrackerhst.historyaccessed.size());
+        assertEquals(19, safaritrackerhst.historyvisitdate.size());
+        assertEquals(19, safaritrackerhst.historyurl.size());
         
+        assertEquals("/c/ - Anime/Cute - 4chan", safaritrackerhst.historytitle.get(0));
+        assertEquals("4chan", safaritrackerhst.historytitle.get(1));
+        assertEquals("CNN International - Breaking News, US News, World News and Video", safaritrackerhst.historytitle.get(2));
+        assertEquals("Google", safaritrackerhst.historytitle.get(17));
+        assertEquals("Falhou ao abrir a página", safaritrackerhst.historytitle.get(18));
+        
+        assertEquals("2021-07-22T18:13:38Z", safaritrackerhst.historyaccessed.get(0));
+        assertEquals("2021-07-22T18:13:28Z", safaritrackerhst.historyaccessed.get(1));
+        assertEquals("2021-07-22T18:13:21Z", safaritrackerhst.historyaccessed.get(2));
+        assertEquals("2021-07-22T18:11:50Z", safaritrackerhst.historyaccessed.get(17));
+        assertEquals("2021-07-22T18:11:39Z", safaritrackerhst.historyaccessed.get(18));
+        
+        assertEquals("2021-07-22T18:13:38Z", safaritrackerhst.historyvisitdate.get(0));
+        assertEquals("2021-07-22T18:13:28Z", safaritrackerhst.historyvisitdate.get(1));
+        assertEquals("2021-07-22T18:13:21Z", safaritrackerhst.historyvisitdate.get(2));
+        assertEquals("2021-07-22T18:11:50Z", safaritrackerhst.historyvisitdate.get(17));
+        assertEquals("2021-07-22T18:11:39Z", safaritrackerhst.historyvisitdate.get(18));
+        
+        assertEquals("https://boards.4channel.org/c/", safaritrackerhst.historyurl.get(0));   
+        assertEquals("https://4chan.org/", safaritrackerhst.historyurl.get(1));   
+        assertEquals("http://edition.cnn.com/", safaritrackerhst.historyurl.get(2));   
+        assertEquals("http://www.google.com.br/", safaritrackerhst.historyurl.get(17));   
+        assertEquals("https://www.apple.com/br/", safaritrackerhst.historyurl.get(18));
     }
 
     @Test
@@ -79,19 +83,55 @@ public class SafariPlistParserTest extends AbstractPkgTest{
                 MediaType.application("x-safari-plist").toString());
         ContentHandler handler = new BodyContentHandler();
         InputStream stream = getStream("test-files/test_bookmarks.plist");
-        ParseContext context = new ParseContext();
-        ItemInfo itemInfo = new ItemInfo(0, getName(), null, null, "test-files/test_bookmarks.plist", false);
-        context.set(ItemInfo.class, itemInfo);
-        parser.getSupportedTypes(context);
         parser.setExtractEntries(true);
-        try {
-        parser.parse(stream, handler, metadata, safariContext);
-        String hts = handler.toString();
-        String mts = metadata.toString();
-        System.out.println(hts + mts);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        parser.parse(stream, handler, metadata, safariContextbkm);
+        assertEquals(30, safaritrackerbkm.bookmarktitle.size());
+        assertEquals(30, safaritrackerbkm.bookmarkurl.size());
         
+        assertEquals("Apple", safaritrackerbkm.bookmarktitle.get(0));
+        assertEquals("Yahoo!", safaritrackerbkm.bookmarktitle.get(1));
+        assertEquals("Google Maps", safaritrackerbkm.bookmarktitle.get(2));
+        assertEquals("Windows Media", safaritrackerbkm.bookmarktitle.get(28));
+        assertEquals("MSN.com", safaritrackerbkm.bookmarktitle.get(29));
+        
+        assertEquals("http://www.apple.com/br/", safaritrackerbkm.bookmarkurl.get(0));   
+        assertEquals("http://br.yahoo.com/", safaritrackerbkm.bookmarkurl.get(1));   
+        assertEquals("http://maps.google.com.br/", safaritrackerbkm.bookmarkurl.get(2));   
+        assertEquals("http://www.microsoft.com/isapi/redir.dll?prd=ie&ar=windowsmedia", safaritrackerbkm.bookmarkurl.get(28));   
+        assertEquals("http://www.microsoft.com/isapi/redir.dll?prd=ie&pver=6&ar=IStart", safaritrackerbkm.bookmarkurl.get(29));
+
+    }
+    
+    @Test
+    public void testSafariPlistParserDownloads() throws IOException, SAXException, TikaException{
+
+        SafariPlistParser parser = new SafariPlistParser();
+        Metadata metadata = new Metadata();
+        metadata.add(Metadata.CONTENT_TYPE,
+                MediaType.application("x-safari-downloads").toString());
+        ContentHandler handler = new BodyContentHandler();
+        InputStream stream = getStream("test-files/test_downloads.plist");
+        parser.setExtractEntries(true);
+        parser.parse(stream, handler, metadata, safariContextdwl);
+        assertEquals(3, safaritrackerdwl.downloadurl.size());
+        assertEquals(3, safaritrackerdwl.downloadlocalpath.size());
+        assertEquals(3, safaritrackerdwl.downloadtotalbytes.size());
+        assertEquals(3, safaritrackerdwl.downloadreceivedbytes.size());
+        
+        assertEquals("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B11C004F6-5199-AABB-BDF6-596927E6C22A%7D%26lang%3Dpt-BR%26browser%3D0%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dstable-arch_x86%26installdataindex%3Ddefaultbrowser/update2/installers/win_xp/ChromeSetup.exe", safaritrackerdwl.downloadurl.get(0));
+        assertEquals("https://get.geo.opera.com/pub/opera-winxpvista/36.0.2130.80/win/Opera_winxpvista_36.0.2130.80_Setup.exe", safaritrackerdwl.downloadurl.get(1));
+        assertEquals("https://download-installer.cdn.mozilla.net/pub/firefox/releases/52.9.0esr/win32-sha1/pt-BR/Firefox%20Setup%2052.9.0esr.exe", safaritrackerdwl.downloadurl.get(2));;
+        
+        assertEquals("C:\\Documents and Settings\\guileb\\Meus documentos\\ChromeSetup.exe", safaritrackerdwl.downloadlocalpath.get(0));
+        assertEquals("C:\\Documents and Settings\\guileb\\Meus documentos\\Opera_winxpvista_36.0.2130.80_Setup.exe", safaritrackerdwl.downloadlocalpath.get(1));
+        assertEquals("C:\\Documents and Settings\\guileb\\Meus documentos\\Firefox Setup 52.9.0esr.exe", safaritrackerdwl.downloadlocalpath.get(2));
+        
+        assertEquals("1321688", safaritrackerdwl.downloadtotalbytes.get(0));
+        assertEquals("37689480", safaritrackerdwl.downloadtotalbytes.get(1));
+        assertEquals("45531008", safaritrackerdwl.downloadtotalbytes.get(2));
+        
+        assertEquals("1321688", safaritrackerdwl.downloadreceivedbytes.get(0));   
+        assertEquals("37689480", safaritrackerdwl.downloadreceivedbytes.get(1));   
+        assertEquals("45531008", safaritrackerdwl.downloadreceivedbytes.get(2));   
     }
 }
