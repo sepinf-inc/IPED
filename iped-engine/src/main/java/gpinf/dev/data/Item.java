5abb61fc063b19110acc22124472db2703208107
Luis Nassif <lfcnassif@gmail.com>
2021-10-20 14:44:43 -0300

    fix #791: del EXT testing hashName; test if file==hashFile before delete

--- iped-engine/src/main/java/gpinf/dev/data/Item.java
+++ iped-engine/src/main/java/gpinf/dev/data/Item.java
@@ -298,7 +298,6 @@ public class Item implements ISleuthKitItem {
         if (isSubItem && (toIgnore || !addToCase || deleteFile)) {
             try {
                 if (file != null && file.exists() && isNotHashId(file.getName()) && !file.delete()) {
-                    // in some scenarios file.delete() works but Files.delete() throws ioexception
                     throw new IOException("Fail to delete file " + file.getAbsolutePath());
                 }
                 if (inputStreamFactory != null && idInDataSource != null && isNotHashId(idInDataSource)) {
@@ -311,7 +310,10 @@ public class Item implements ISleuthKitItem {
     }
 
     private boolean isNotHashId(String id) {
-        return id == null || (id.length() != 32 && id.length() != 40 && id.length() != 64);
+        int idx = id.indexOf('.');
+        if (idx != -1)
+            id = id.substring(0, idx);
+        return id.length() != 32 && id.length() != 40 && id.length() != 64;
     }
 
     /**
