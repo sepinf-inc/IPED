sepinf-inc/IPED

commit 5e1232e098866b5b5c0fd3ec87d05e402571e289

try to fix #785: better folder datasource look up and replacement

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/IndexItem.java
@@ -1123,12 +1123,10 @@ public class IndexItem extends BasicProps {
         if (evidence.isSubItem())
             return localFile;
         Path path = localFile.toPath();
-        String pathSuffix = "";
-        if (path.getNameCount() > 1)
-            pathSuffix = path.subpath(1, path.getNameCount()).toString();
-        if (localFile.toPath().endsWith(pathSuffix)) {
-            String evidenceFolderStr = localFile.getAbsolutePath().substring(0,
-                    localFile.getAbsolutePath().lastIndexOf(pathSuffix));
+        Path pathSuffix = Paths.get(evidence.getPath());
+        pathSuffix = pathSuffix.subpath(1, pathSuffix.getNameCount());
+        if (path.endsWith(pathSuffix)) {
+            String evidenceFolderStr = path.toString().substring(0, path.toString().lastIndexOf(pathSuffix.toString()));
             File evidenceFolder = new File(evidenceFolderStr);
             File mappedFolder = localEvidenceMap.get(evidenceFolder);
             if (mappedFolder == null) {
@@ -1148,7 +1146,7 @@ public class IndexItem extends BasicProps {
                 }
                 localEvidenceMap.put(evidenceFolder, mappedFolder);
             }
-            localFile = new File(mappedFolder, pathSuffix);
+            localFile = new File(mappedFolder, pathSuffix.toString());
         }
         return localFile;
     }

commit 0841683722cfaab5aaef28bf7eb6812f898ba7f9

fix #821: fix IllegalArgumentException caused by 5e1232e

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/IndexItem.java
@@ -1147,7 +1147,8 @@ public class IndexItem extends BasicProps {
             return localFile;
         Path path = localFile.toPath();
         Path pathSuffix = Paths.get(evidence.getPath());
-        pathSuffix = pathSuffix.subpath(1, pathSuffix.getNameCount());
+        if (pathSuffix.getNameCount() > 1)
+            pathSuffix = pathSuffix.subpath(1, pathSuffix.getNameCount());
         if (path.endsWith(pathSuffix)) {
             String evidenceFolderStr = path.toString().substring(0, path.toString().lastIndexOf(pathSuffix.toString()));
             File evidenceFolder = new File(evidenceFolderStr);

iped-utils/src/main/java/dpf/sp/gpinf/indexer/util/SelectImagePathWithDialog.java
@@ -54,7 +54,7 @@ public class SelectImagePathWithDialog implements Runnable {
 
         public ImageFilter(File file) {
             int extIdx = file.getName().lastIndexOf('.');
-            if (extIdx >= file.getName().length() - 5)
+            if (extIdx != -1 && extIdx >= file.getName().length() - 5)
                 ext = file.getName().substring(extIdx).toLowerCase();
         }
 