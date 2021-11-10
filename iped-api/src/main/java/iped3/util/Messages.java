sepinf-inc/IPED

commit 9f2d6334798cfe637904347f665540154b4ca53d

#18: externalize all resource bundles to 'localization' folder

iped-api/src/main/java/iped3/util/Messages.java
@@ -1,29 +1,43 @@
 package iped3.util;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Locale;
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class Messages {
+import iped3.configuration.IConfigurationDirectory;
 
-    private static final String BUNDLE_NAME = "iped-api-messages"; //$NON-NLS-1$
+public abstract class Messages {
+
+    public static final String LOCALE_SYS_PROP = "iped-locale";
+    public static final String BUNDLES_FOLDER = "localization";
+
+    private static final String BUNDLE_NAME = "iped-basicprops"; //$NON-NLS-1$
 
     private static ResourceBundle RESOURCE_BUNDLE;
 
     private Messages() {
     }
 
+    public static ResourceBundle getExternalBundle(String bundleName, Locale locale) {
+        File file = new File(System.getProperty(IConfigurationDirectory.IPED_ROOT), BUNDLES_FOLDER);
+        try {
+            URL[] urls = { file.toURI().toURL() };
+            ClassLoader loader = new URLClassLoader(urls);
+            return ResourceBundle.getBundle(bundleName, locale, loader);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            Locale locale = Locale.forLanguageTag(System.getProperty("iped-locale")); //$NON-NLS-1$
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            String localeStr = System.getProperty(LOCALE_SYS_PROP); // $NON-NLS-1$
+            Locale locale = localeStr != null ? Locale.forLanguageTag(localeStr) : Locale.getDefault();
+            RESOURCE_BUNDLE = getExternalBundle(BUNDLE_NAME, locale);
         }
-        try {
-            return RESOURCE_BUNDLE.getString(key);
-
-        } catch (MissingResourceException e) {
-            e.printStackTrace();
-            throw e;
-        }
+        return RESOURCE_BUNDLE.getString(key);
     }
 }

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/Messages.java
@@ -16,9 +16,9 @@ public class Messages {
 
     public static String get(String key) {
         if (RESOURCE_BUNDLE == null) {
-            String localeStr = System.getProperty("iped-locale"); //$NON-NLS-1$
+            String localeStr = System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP); // $NON-NLS-1$
             Locale locale = localeStr != null ? Locale.forLanguageTag(localeStr) : Locale.getDefault();
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
         }
         try {
             return RESOURCE_BUNDLE.getString(key);

iped-engine/src/main/java/dpf/sp/gpinf/indexer/Messages.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class Messages {
 
-    private static final String BUNDLE_NAME = "iped-messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "iped-engine-messages"; //$NON-NLS-1$
 
     private static Logger LOGGER = LoggerFactory.getLogger(Messages.class);
 
@@ -20,9 +20,9 @@ public class Messages {
 
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            String localeProp = System.getProperty("iped-locale"); //$NON-NLS-1$
+            String localeProp = System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP); // $NON-NLS-1$
             Locale locale = localeProp != null ? Locale.forLanguageTag(localeProp) : Locale.getDefault();
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
             String finalLocale = RESOURCE_BUNDLE.getLocale().toLanguageTag();
             if (finalLocale.equals("und")) //$NON-NLS-1$
                 finalLocale = "en"; //$NON-NLS-1$

iped-geo/src/main/java/dpf/mt/gpinf/mapas/util/Messages.java
@@ -20,9 +20,9 @@ public class Messages {
 
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            String localeProp = System.getProperty("iped-locale"); //$NON-NLS-1$
+            String localeProp = System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP); // $NON-NLS-1$
             Locale locale = localeProp != null ? Locale.forLanguageTag(localeProp) : Locale.getDefault();
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
             String finalLocale = RESOURCE_BUNDLE.getLocale().toLanguageTag();
             if (finalLocale.equals("und")) //$NON-NLS-1$
                 finalLocale = "en"; //$NON-NLS-1$

iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/util/Messages.java
@@ -15,9 +15,9 @@ public class Messages {
 
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            String str = System.getProperty("iped-locale"); //$NON-NLS-1$
+            String str = System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP); // $NON-NLS-1$
             Locale locale = str != null ? Locale.forLanguageTag(str) : Locale.getDefault();
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
         }
         try {
             return RESOURCE_BUNDLE.getString(key);

iped-utils/src/main/java/dpf/sp/gpinf/indexer/util/Messages.java
@@ -15,8 +15,8 @@ public class Messages {
 
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            Locale locale = Locale.forLanguageTag(System.getProperty("iped-locale")); //$NON-NLS-1$
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            Locale locale = Locale.forLanguageTag(System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP)); // $NON-NLS-1$
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
         }
         try {
             return RESOURCE_BUNDLE.getString(key);

iped-viewers/iped-viewers-api/src/main/java/iped3/desktop/util/Messages.java
@@ -15,8 +15,8 @@ public class Messages {
 
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            Locale locale = Locale.forLanguageTag(System.getProperty("iped-locale")); //$NON-NLS-1$
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            Locale locale = Locale.forLanguageTag(System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP)); // $NON-NLS-1$
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
         }
         try {
             return RESOURCE_BUNDLE.getString(key);

iped-viewers/iped-viewers-impl/src/main/java/dpf/sp/gpinf/indexer/ui/fileViewer/Messages.java
@@ -15,8 +15,8 @@ public class Messages {
 
     public static String getString(String key) {
         if (RESOURCE_BUNDLE == null) {
-            Locale locale = Locale.forLanguageTag(System.getProperty("iped-locale")); //$NON-NLS-1$
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+            Locale locale = Locale.forLanguageTag(System.getProperty(iped3.util.Messages.LOCALE_SYS_PROP)); // $NON-NLS-1$
+            RESOURCE_BUNDLE = iped3.util.Messages.getExternalBundle(BUNDLE_NAME, locale);
         }
         try {
             return RESOURCE_BUNDLE.getString(key);

commit 015eb71fe698ba286c6653af358a302fc5c1ac16

fix NPE when starting GUI w/ unsupported java versions caused by 9f2d633

iped-api/src/main/java/iped3/util/Messages.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLConnection;
@@ -13,8 +14,6 @@ import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.ResourceBundle.Control;
-
-import iped3.configuration.IConfigurationDirectory;
 
 public abstract class Messages {
 
@@ -31,10 +30,13 @@ public abstract class Messages {
 
     public static ResourceBundle getExternalBundle(String bundleName, Locale locale) {
         File file = null;
-        String baseFolder = System.getProperty(IConfigurationDirectory.IPED_ROOT);
-        if (baseFolder != null) {
-            file = new File(baseFolder, BUNDLES_FOLDER);
-        } else {
+        try {
+            URL url = Messages.class.getProtectionDomain().getCodeSource().getLocation();
+            file = new File(new File(url.toURI()).getParentFile().getParentFile(), BUNDLES_FOLDER);
+        } catch (URISyntaxException e1) {
+            e1.printStackTrace();
+        }
+        if (file != null && !file.exists()) {
             File baseFile = new File(System.getProperty("user.dir"));
             do {
                 baseFile = baseFile.getParentFile();