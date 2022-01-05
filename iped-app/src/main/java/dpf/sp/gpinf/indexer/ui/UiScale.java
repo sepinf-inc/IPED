8c89fa7164917152ebd8f6c3755a043832bd5215
Luis Nassif <lfcnassif@gmail.com>
2022-01-05 12:31:50 -0300

    #900: add a method to save the ui scale factor

--- iped-app/src/main/java/dpf/sp/gpinf/indexer/ui/UiScale.java
+++ iped-app/src/main/java/dpf/sp/gpinf/indexer/ui/UiScale.java
@@ -1,43 +1,58 @@
 package dpf.sp.gpinf.indexer.ui;
 
 import java.io.File;
-import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
+
+import dpf.sp.gpinf.indexer.util.UTF8Properties;
 
 public class UiScale {
+
+    public static final String AUTO = "auto";
+    private static final File file = new File(System.getProperty("user.home") + "/.iped", "UiScale.txt");
     private static final String key = "uiScale";
 
-    public static void loadUserSetting() {
+    public static String loadUserSetting() {
         try {
-            File file = new File(System.getProperty("user.home") + "/.iped", "UiScale.txt");
             if (!file.exists()) {
-                if (file.getParentFile() != null && !file.getParentFile().exists())
-                    file.getParentFile().mkdirs();
-                // Create a file with the default setting
-                List<String> l = new ArrayList<String>();
-                l.add("# Sets the user interface scaling factor.");
-                l.add("# \"auto\" to use the default automatic scaling based on the screen resolution.");
-                l.add("# Use a float point value to set a fixed scale, e.g. " + key + " = 1.5");
-                l.add(key + " = auto");
-                Files.write(file.toPath(), l, StandardOpenOption.CREATE_NEW);
+                saveUserSetting(AUTO);
             } else {
-                Properties prop = new Properties();
-                prop.load(new FileReader(file));
+                UTF8Properties prop = new UTF8Properties();
+                prop.load(file);
                 String value = prop.getProperty(key);
                 if (value != null) {
                     value = value.trim();
-                    if (!value.equalsIgnoreCase("auto")) {
+                    if (!value.equalsIgnoreCase(AUTO)) {
                         double factor = Double.parseDouble(value);
                         DecimalFormat df = new DecimalFormat("0.000");
-                        System.setProperty("sun.java2d.uiScale", df.format(factor));
+                        String val = df.format(factor);
+                        System.setProperty("sun.java2d.uiScale", val);
+                        return val;
                     }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return AUTO;
+    }
+
+    public static void saveUserSetting(String value) {
+        try {
+            Files.deleteIfExists(file.toPath());
+            if (file.getParentFile() != null && !file.getParentFile().exists())
+                file.getParentFile().mkdirs();
+            // Create a file with the default setting
+            List<String> l = new ArrayList<String>();
+            l.add("# Sets the user interface scaling factor.");
+            l.add("# \"" + AUTO + "\" to use the default automatic scaling based on the screen resolution.");
+            l.add("# Use a float point value to set a fixed scale, e.g. " + key + " = 1.5");
+            l.add(key + " = " + value);
+            Files.write(file.toPath(), l, StandardOpenOption.CREATE_NEW);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
