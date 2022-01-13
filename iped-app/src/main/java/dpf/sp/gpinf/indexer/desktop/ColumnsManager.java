5ba2bbce9ded5ca81c4b67f94796439b233fb3d1
Luis Nassif <lfcnassif@gmail.com>
2022-01-13 18:22:19 -0300

    #570: group common: metadata keys in a new common group in UI

--- iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ColumnsManager.java
+++ iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ColumnsManager.java
@@ -79,6 +79,7 @@ public class ColumnsManager implements ActionListener, Serializable, IColumnsMan
 
     public static final String[] groupNames = { Messages.getString("ColumnsManager.Basic"),
             Messages.getString("ColumnsManager.HashDB"), Messages.getString("ColumnsManager.Advanced"), //$NON-NLS-2$ //$NON-NLS-2$
+            Messages.getString("ColumnsManager.Common"), // $NON-NLS-2$
             Messages.getString("ColumnsManager.Message"), Messages.getString("ColumnsManager.Audio"), //$NON-NLS-2$
             Messages.getString("ColumnsManager.Image"), Messages.getString("ColumnsManager.Video"), //$NON-NLS-1$
             Messages.getString("ColumnsManager.PDF"), Messages.getString("ColumnsManager.Office"), //$NON-NLS-1$ //$NON-NLS-2$
@@ -529,6 +530,7 @@ public class ColumnsManager implements ActionListener, Serializable, IColumnsMan
         ArrayList<String> ufedFields = new ArrayList<String>();
         ArrayList<String> hashDbFields = new ArrayList<String>();
         ArrayList<String> emailFields = new ArrayList<String>();
+        ArrayList<String> commonFields = new ArrayList<String>();
 
         for (String f : allExtraAttrs) {
             if (f.startsWith(RegexTask.REGEX_PREFIX))
@@ -561,10 +563,13 @@ public class ColumnsManager implements ActionListener, Serializable, IColumnsMan
                 ufedFields.add(f);
             else if (f.startsWith(Message.MESSAGE_PREFIX) || ExtraProperties.EMAIL_BASIC_PROPS.contains(f))
                 emailFields.add(f);
+            else if (f.startsWith(ExtraProperties.COMMON_META_PREFIX))
+                commonFields.add(f);
         }
 
         String[][] customGroups = new String[][] { defaultFields.clone(), hashDbFields.toArray(new String[0]),
-                extraAttrs.toArray(new String[0]), emailFields.toArray(new String[0]),
+                extraAttrs.toArray(new String[0]), commonFields.toArray(new String[0]),
+                emailFields.toArray(new String[0]),
                 audioFields.toArray(new String[0]), imageFields.toArray(new String[0]),
                 videoFields.toArray(new String[0]), pdfFields.toArray(new String[0]),
                 officeFields.toArray(new String[0]), htmlFields.toArray(new String[0]),
