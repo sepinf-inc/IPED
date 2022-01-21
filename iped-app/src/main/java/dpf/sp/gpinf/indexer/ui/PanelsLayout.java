a18971ab3918cb46e4f20171756f9cde91e58b70
tc-wleite <wladimir.leite@gmail.com>
2022-01-21 16:31:41 -0300

    #933: Also save/restore main window position/state and gallery colCount.

--- iped-app/src/main/java/dpf/sp/gpinf/indexer/ui/PanelsLayout.java
+++ iped-app/src/main/java/dpf/sp/gpinf/indexer/ui/PanelsLayout.java
@@ -1,33 +1,78 @@
 package dpf.sp.gpinf.indexer.ui;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 import bibliothek.gui.dock.common.CControl;
+import dpf.sp.gpinf.indexer.desktop.App;
+import dpf.sp.gpinf.indexer.util.IOUtil;
 
 public class PanelsLayout {
-    private static final File file = new File(System.getProperty("user.home") + "/.iped", "PanelsLayout.dat");
+    private static final File dir = new File(System.getProperty("user.home") + "/.iped");
+    private static final File file = new File(dir, "PanelsLayout.dat");
+    private static final File fileExt = new File(dir, "PanelsLayoutExt.dat");
 
     public static boolean load(CControl control) {
-        if (file.exists()) {
-            try {
+        BufferedReader in = null;
+        try {
+            if (file.exists())
                 control.read(file);
-                return true;
-            } catch (Exception e) {
-                e.printStackTrace();
+
+            if (fileExt.exists()) {
+                in = new BufferedReader(new FileReader(fileExt));
+
+                App app = App.get();
+                int colCount = Integer.parseInt(in.readLine());
+                app.setGalleryColCount(colCount);
+
+                String[] s = in.readLine().split(",");
+                int x = Integer.parseInt(s[0]);
+                int y = Integer.parseInt(s[1]);
+                int w = Integer.parseInt(s[2]);
+                int h = Integer.parseInt(s[3]);
+                app.setBounds(x, y, w, h);
+
+                int state = Integer.parseInt(in.readLine());
+                app.setExtendedState(state);
             }
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtil.closeQuietly(in);
         }
         return false;
     }
 
     public static boolean save(CControl control) {
+        BufferedWriter out = null;
         try {
-            if (file.getParentFile() != null && !file.getParentFile().exists())
-                file.mkdirs();
+            if (!dir.getParentFile().exists())
+                dir.mkdirs();
 
             control.write(file);
+
+            out = new BufferedWriter(new FileWriter(fileExt));
+
+            App app = App.get();
+            out.write("" + app.getGalleryColCount());
+            out.newLine();
+
+            out.write(app.getX() + "," + app.getY() + "," + app.getWidth() + "," + app.getHeight());
+            out.newLine();
+
+            out.write("" + app.getExtendedState());
+            out.newLine();
+
             return true;
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            IOUtil.closeQuietly(out);
         }
         return false;
     }
