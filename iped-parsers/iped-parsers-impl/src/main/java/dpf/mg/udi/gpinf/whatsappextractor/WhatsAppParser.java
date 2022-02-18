sepinf-inc/IPED

commit a37c8ffaf8e4ab72dd032e473c6531964b82e5dc

#150: remove synchronization bottlenecks using new list instances

iped-parsers/iped-parsers-impl/src/main/java/dpf/mg/udi/gpinf/whatsappextractor/WhatsAppParser.java
@@ -217,62 +217,60 @@ public class WhatsAppParser extends SQLite3DBParser {
             ContentHandler handler, EmbeddedDocumentExtractor extractor, WAAccount account) throws Exception {
         int chatVirtualId = 0;
         HashMap<String, String> cache = new HashMap<>();
-        synchronized (chatList) {
-            for (Chat c : chatList) {
-                getAvatar(searcher, c.getRemote());
-                searchMediaFilesForMessagesInBatches(c.getMessages(), searcher);
-                int frag = 0;
-                int firstMsg = 0;
-                ReportGenerator reportGenerator = new ReportGenerator();
-                byte[] bytes = reportGenerator.generateNextChatHtml(c, contacts, account);
-                while (bytes != null) {
-                    Metadata chatMetadata = new Metadata();
-                    int nextMsg = reportGenerator.getNextMsgNum();
+        for (Chat c : chatList) {
+            getAvatar(searcher, c.getRemote());
+            searchMediaFilesForMessagesInBatches(c.getMessages(), searcher);
+            int frag = 0;
+            int firstMsg = 0;
+            ReportGenerator reportGenerator = new ReportGenerator();
+            byte[] bytes = reportGenerator.generateNextChatHtml(c, contacts, account);
+            while (bytes != null) {
+                Metadata chatMetadata = new Metadata();
+                int nextMsg = reportGenerator.getNextMsgNum();
 
-                    List<Message> msgSubset = c.getMessages().subList(firstMsg, nextMsg);
-                    storeLinkedHashes(msgSubset, chatMetadata, searcher);
+                List<Message> msgSubset = c.getMessages().subList(firstMsg, nextMsg);
+                storeLinkedHashes(msgSubset, chatMetadata, searcher);
 
-                    // condition to avoid duplicate locations being saved in chat & messages
-                    if (!extractMessages) {
-                        storeLocations(msgSubset, chatMetadata);
+                // condition to avoid duplicate locations being saved in chat & messages
+                if (!extractMessages) {
+                    storeLocations(msgSubset, chatMetadata);
+                }
+                firstMsg = nextMsg;
+                byte[] nextBytes = reportGenerator.generateNextChatHtml(c, contacts, account);
+
+                String chatName = c.getTitle();
+                if (frag > 0 || nextBytes != null)
+                    chatName += "_" + frag++; //$NON-NLS-1$
+
+                chatMetadata.set(TikaCoreProperties.TITLE, chatName);
+                chatMetadata.set(IndexerDefaultParser.INDEXER_CONTENT_TYPE, WHATSAPP_CHAT.toString());
+                chatMetadata.set(ExtraProperties.ITEM_VIRTUAL_ID, Integer.toString(chatVirtualId));
+                chatMetadata.set(ExtraProperties.DECODED_DATA, Boolean.TRUE.toString());
+
+                if (extractMessages && msgSubset.size() > 0) {
+                    chatMetadata.set(BasicProps.HASCHILD, Boolean.TRUE.toString());
+                }
+                if (account != null) {
+                    String local = formatContact(account, cache);
+                    chatMetadata.add(ExtraProperties.PARTICIPANTS, local);
+                }
+                if (c.isGroupChat()) {
+                    for (WAContact member : c.getGroupmembers()) {
+                        chatMetadata.add(ExtraProperties.PARTICIPANTS, formatContact(member, cache));
                     }
-                    firstMsg = nextMsg;
-                    byte[] nextBytes = reportGenerator.generateNextChatHtml(c, contacts, account);
-
-                    String chatName = c.getTitle();
-                    if (frag > 0 || nextBytes != null)
-                        chatName += "_" + frag++; //$NON-NLS-1$
-
-                    chatMetadata.set(TikaCoreProperties.TITLE, chatName);
-                    chatMetadata.set(IndexerDefaultParser.INDEXER_CONTENT_TYPE, WHATSAPP_CHAT.toString());
-                    chatMetadata.set(ExtraProperties.ITEM_VIRTUAL_ID, Integer.toString(chatVirtualId));
-                    chatMetadata.set(ExtraProperties.DECODED_DATA, Boolean.TRUE.toString());
-
-                    if (extractMessages && msgSubset.size() > 0) {
-                        chatMetadata.set(BasicProps.HASCHILD, Boolean.TRUE.toString());
+                } else {
+                    if (c.getRemote() != null) {
+                        chatMetadata.add(ExtraProperties.PARTICIPANTS, formatContact(c.getRemote(), cache));
                     }
-                    if (account != null) {
-                        String local = formatContact(account, cache);
-                        chatMetadata.add(ExtraProperties.PARTICIPANTS, local);
-                    }
-                    if (c.isGroupChat()) {
-                        for (WAContact member : c.getGroupmembers()) {
-                            chatMetadata.add(ExtraProperties.PARTICIPANTS, formatContact(member, cache));
-                        }
-                    } else {
-                        if (c.getRemote() != null) {
-                            chatMetadata.add(ExtraProperties.PARTICIPANTS, formatContact(c.getRemote(), cache));
-                        }
-                    }
+                }
 
-                    ByteArrayInputStream chatStream = new ByteArrayInputStream(bytes);
-                    extractor.parseEmbedded(chatStream, handler, chatMetadata, false);
-                    bytes = nextBytes;
+                ByteArrayInputStream chatStream = new ByteArrayInputStream(bytes);
+                extractor.parseEmbedded(chatStream, handler, chatMetadata, false);
+                bytes = nextBytes;
 
-                    if (extractMessages) {
-                        extractMessages(chatName, c, msgSubset, account, contacts, chatVirtualId++, handler, extractor,
-                                cache);
-                    }
+                if (extractMessages) {
+                    extractMessages(chatName, c, msgSubset, account, contacts, chatVirtualId++, handler, extractor,
+                            cache);
                 }
             }
         }
@@ -385,7 +383,7 @@ public class WhatsAppParser extends SQLite3DBParser {
         xhtml.endDocument();
     }
 
-    private synchronized void parseAllDBS(InputStream stream, ContentHandler handler, Metadata metadata,
+    private void parseAllDBS(InputStream stream, ContentHandler handler, Metadata metadata,
             ParseContext context, ExtractorFactory extFactory) throws IOException, SAXException, TikaException {
 
 
@@ -417,7 +415,10 @@ public class WhatsAppParser extends SQLite3DBParser {
                 }
                 for (WhatsAppContext main : mainDbFound.toArray(new WhatsAppContext[0])) {
 
-                    ChatMerge cm = new ChatMerge(main.getChalist(), DB.getName());
+                    // new instance to avoid threading issues
+                    List<Chat> mainDBChatList = new ArrayList<Chat>(main.getChalist());
+
+                    ChatMerge cm = new ChatMerge(mainDBChatList, DB.getName());
                     if (cm.isBackup(wcontext.getChalist())) {
                         wcontext.setBackup(true);
                         wcontext.setMainDBItem(main.getItem());
@@ -456,12 +457,15 @@ public class WhatsAppParser extends SQLite3DBParser {
                     }
                 });
 
+                // new instance to avoid threading issues
+                List<Chat> mainDBChatList = new ArrayList<Chat>(mainDb.getChalist());
+
                 for (WhatsAppContext other : dbs) {
                     // skip main dbs
                     if (other.isMainDB()) {
                         continue;
                     }
-                    ChatMerge cm = new ChatMerge(mainDb.getChalist(), other.getItem().getName());
+                    ChatMerge cm = new ChatMerge(mainDBChatList, other.getItem().getName());
                     if (other.getChalist() == null) {
                         other.setChalist(extractChatList(other, extFactory, metadata, context, contacts, account));
                     }
@@ -485,7 +489,7 @@ public class WhatsAppParser extends SQLite3DBParser {
                     return;
                 }
 
-                createReport(mainDb.getChalist(), searcher, contacts, handler, extractor, account);
+                createReport(mainDBChatList, searcher, contacts, handler, extractor, account);
             }
         } catch (Exception e) {
             if (e instanceof TikaException)

iped-parsers/iped-parsers-impl/src/main/java/dpf/mg/udi/gpinf/whatsappextractor/ChatMerge.java
@@ -1,5 +1,6 @@
 package dpf.mg.udi.gpinf.whatsappextractor;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -38,6 +39,12 @@ public class ChatMerge {
     public ChatMerge(List<Chat> main, String dbname) {
         this.main = main;
         this.dbname = dbname;
+        for (Chat c : main) {
+            // new instance to avoid threading issues
+            List<Message> messages = new ArrayList<Message>(c.getMessages());
+            Collections.sort(messages, cmpMessage);
+            c.setMessages(messages);
+        }
     }
 
     public boolean isBackup(List<Chat> backup) {
@@ -60,7 +67,6 @@ public class ChatMerge {
 
 
     private boolean hasCompatibleMessage(List<Message> backup, List<Message> main) {
-        Collections.sort(main, cmpMessage);
         int maxMsgsToCheck = 10;
         for (int i = 0; i < backup.size(); i += Math.max(1, backup.size() / maxMsgsToCheck)) {
             int idx = Collections.binarySearch(main, backup.get(i), cmpMessage);
@@ -95,10 +101,8 @@ public class ChatMerge {
 
     }
 
-    public int mergeMessageList(List<Message> main, List<Message> backup) {
+    private int mergeMessageList(List<Message> main, List<Message> backup) {
         int tot_rec = 0;
-
-        Collections.sort(main, cmpMessage);
         int indexmain = 0;
         for (Message m : backup) {
 

commit a0de7b11ceb810f6c2aeb7165e6a9ff06f212406

#150: fix issue caused by a37c8ff where just the last backup was merged

iped-parsers/iped-parsers-impl/src/main/java/dpf/mg/udi/gpinf/whatsappextractor/WhatsAppParser.java
@@ -458,6 +458,7 @@ public class WhatsAppParser extends SQLite3DBParser {
                         // merge backup in the main chat list
                         int numMsgRecovered = cm.mergeChatList(other.getChalist());
                         logger.info("Recovered {} messages from {}", numMsgRecovered, other.getItem().getPath()); //$NON-NLS-1$
+                        mainDb.setChalist(mainDBChatList);
                         dbChatList = mainDBChatList;
                     }
                     if (wcontext == other) {