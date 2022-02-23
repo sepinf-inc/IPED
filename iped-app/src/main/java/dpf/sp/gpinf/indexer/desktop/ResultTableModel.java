sepinf-inc/IPED

commit 3b453573b458f6240377cff1d7575e7c16a36e81

#646: use localized format for all numbers in table tab

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ResultTableModel.java
@@ -21,6 +21,7 @@ package dpf.sp.gpinf.indexer.desktop;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.Collator;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -58,6 +59,11 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
     private static final String lengthField = BasicProps.getLocalizedField(IndexItem.LENGTH);
     public static String BOOKMARK_COL = Messages.getString("ResultTableModel.bookmark"); //$NON-NLS-1$
     public static String SCORE_COL = Messages.getString("ResultTableModel.score"); //$NON-NLS-1$
+
+    private SimpleDateFormat df = new SimpleDateFormat(Messages.getString("ResultTableModel.DateFormat")); //$NON-NLS-1$
+    private SimpleDateFormat fatAccessedDf = new SimpleDateFormat(Messages.getString("ResultTableModel.FATDateFormat")); //$NON-NLS-1$
+    private DecimalFormat numberFormat = LocalizedFormat.getDecimalInstance("#,###.############"); //$NON-NLS-1$
+    private Collator collator = Collator.getInstance();
 
     public static String[] fields;
 
@@ -110,10 +116,6 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
 
         columnNames = cols.toArray(new String[0]);
     }
-
-    private SimpleDateFormat df = new SimpleDateFormat(Messages.getString("ResultTableModel.DateFormat")); //$NON-NLS-1$
-    private SimpleDateFormat fatAccessedDf = new SimpleDateFormat(Messages.getString("ResultTableModel.FATDateFormat")); //$NON-NLS-1$
-    private Collator collator = Collator.getInstance();
 
     public ResultTableModel() {
         super();
@@ -250,16 +252,16 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
             }
 
             SortedNumericDocValues sndv = App.get().appCase.getLeafReader().getSortedNumericDocValues(field);
-            if (sndv == null)
+            if (sndv == null) {
                 sndv = App.get().appCase.getLeafReader()
-                        .getSortedNumericDocValues(IndexItem.POSSIBLE_NUM_DOCVALUES_PREFIX + field); // $NON-NLS-1$
-
-            boolean mayBeNumeric = MetadataPanel.mayBeNumeric(field);
+                        .getSortedNumericDocValues(IndexItem.POSSIBLE_NUM_DOCVALUES_PREFIX + field);
+            }
+            boolean isNumeric = IndexItem.isNumeric(field);
 
             String[] values = doc.getValues(field);
             if (values.length > 1) {
                 boolean sorted = false;
-                if (mayBeNumeric && sndv != null) {
+                if (isNumeric && sndv != null) {
                     try {
                         Arrays.sort(values, new Comparator<String>() {
                             @Override
@@ -321,14 +323,15 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
                     // e.printStackTrace();
                 }
 
+            // TODO remove this because #644 gave a better solution
             if (Date.class.equals(IndexItem.getMetadataTypes().get(field))) {
                 // it was stored lowercase because query parser converts range queries to
                 // lowercase
                 value = value.toUpperCase();
             }
 
-            if (field.equals(IndexItem.LENGTH)) {
-                value = LocalizedFormat.format(Long.valueOf(value));
+            if (IndexItem.isNumeric(field)) {
+                value = numberFormat.format(Double.valueOf(value));
 
             } else if (field.equals(IndexItem.NAME)) {
                 TextFragment[] fragments = TextHighlighter.getHighlightedFrags(false, value, field, 0);

commit 426c22ec711c2e300de8ab1e73072abf3591f0e4

#646 fix NFE caused by 3b45357 w/ multivalued cols, remove unneeded code

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ResultTableModel.java
@@ -252,14 +252,12 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
                 }
             }
 
-            SortedNumericDocValues sndv = App.get().appCase.getLeafReader().getSortedNumericDocValues(field);
-
             boolean isNumeric = IndexItem.isNumeric(field);
 
             String[] values = doc.getValues(field);
             if (values.length > 1) {
                 boolean sorted = false;
-                if (isNumeric && sndv != null) {
+                if (isNumeric) {
                     try {
                         Arrays.sort(values, new Comparator<String>() {
                             @Override
@@ -284,15 +282,9 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
 
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < values.length; i++) {
-                try {
-                    // do not use scientific notation for longs
-                    Double d = Double.valueOf(values[i]);
-                    if (d.doubleValue() == d.longValue()) {
-                        values[i] = Long.toString(d.longValue());
-                    }
-                } catch (NumberFormatException e) {
+                if (isNumeric) {
+                    values[i] = numberFormat.format(Double.valueOf(values[i]));
                 }
-
                 sb.append(values[i]);
                 if (i != values.length - 1) {
                     if (i == 9) {
@@ -321,10 +313,7 @@ public class ResultTableModel extends AbstractTableModel implements SearchResult
                     // e.printStackTrace();
                 }
 
-            if (IndexItem.isNumeric(field)) {
-                value = numberFormat.format(Double.valueOf(value));
-
-            } else if (field.equals(IndexItem.NAME)) {
+            if (field.equals(IndexItem.NAME)) {
                 TextFragment[] fragments = TextHighlighter.getHighlightedFrags(false, value, field, 0);
                 if (fragments[0].getScore() > 0) {
                     StringBuilder s = new StringBuilder();