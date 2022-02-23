c0fa4fcee00a6a8d07076d02f47ced8c080c3c3c
Luis Nassif <lfcnassif@gmail.com>
2022-02-22 23:04:10 -0300

    #890: first impl to search for KNN indexed faces, limited to 1000

--- iped-engine/src/main/java/dpf/sp/gpinf/indexer/search/SimilarFacesSearch.java
+++ iped-engine/src/main/java/dpf/sp/gpinf/indexer/search/SimilarFacesSearch.java
@@ -5,31 +5,34 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.lucene.index.LeafReader;
-import org.apache.lucene.index.SortedSetDocValues;
-import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 
+import dpf.sp.gpinf.indexer.process.IndexItem;
 import iped3.IItem;
-import iped3.IItemId;
 
 public class SimilarFacesSearch {
 
     public static final String FACE_FEATURES = "face_encodings";
     public static final String FACE_LOCATIONS = "face_locations";
 
-    private static final float DEFAULT_MIN_DISTANCE = 0.5f;
+    private static final float DEFAULT_MIN_SCORE = 50;
 
-    private static float minDistSquared = DEFAULT_MIN_DISTANCE * DEFAULT_MIN_DISTANCE;
+    private static float minimumScore = DEFAULT_MIN_SCORE;
+
+    private static int MAX_KNN_RESULTS = 1000;
 
     private IPEDMultiSource ipedCase;
-    private double[] refSimilarityFeatures;
+    private float[] refSimilarityFeatures;
 
     public SimilarFacesSearch(IPEDSource ipedCase, IItem refImage) {
         this.ipedCase = ipedCase instanceof IPEDMultiSource ? (IPEDMultiSource) ipedCase
                 : new IPEDMultiSource(Collections.singletonList(ipedCase));
-        this.refSimilarityFeatures = getFirstFace(refImage);
+        this.refSimilarityFeatures = IndexItem.convDoubleToFloatArray(getFirstFace(refImage));
     }
 
     public MultiSearchResult search() throws IOException {
@@ -40,83 +43,49 @@ public class SimilarFacesSearch {
 
     public MultiSearchResult filter(MultiSearchResult result) throws IOException {
         score(result);
-        return ImageSimilarityLowScoreFilter.filter(result, squaredDistToScore(minDistSquared));
+        return ImageSimilarityLowScoreFilter.filter(result, minimumScore);
     }
 
     public static final int getMinScore() {
-        return (int) squaredDistToScore(minDistSquared);
+        return (int) minimumScore;
     }
 
     public static final void setMinScore(int minScore) {
+        minimumScore = minScore;
+    }
+    
+    public static final float scoreToSquaredDist(float minScore) {
         float dist = 1 - ((float) minScore / 100);
-        minDistSquared = dist * dist;
+        return dist * dist;
     }
 
     private static final float squaredDistToScore(float squaredDist) {
         return Math.max(0, (1 - (float) Math.sqrt(squaredDist)) * 100);
     }
 
+    private static final float convertLuceneScoreToFinalScore(float luceneScore) {
+        // inverts the formula taken from
+        // https://github.com/apache/lucene/blob/releases/lucene/9.0.0/lucene/core/src/java/org/apache/lucene/index/VectorSimilarityFunction.java
+        float squareDistance = (1f / luceneScore) - 1;
+
+        return squaredDistToScore(squareDistance);
+    }
+
     private void score(MultiSearchResult result) throws IOException {
 
         LeafReader leafReader = ipedCase.getLeafReader();
-        int numThreads = Runtime.getRuntime().availableProcessors();
-        Thread[] threads = new Thread[numThreads];
-        int len = result.getLength();
-        int itemsPerThread = (len + numThreads - 1) / numThreads;
-        for (int k = 0; k < numThreads; k++) {
-            int threadIdx = k;
-            (threads[k] = new Thread() {
-                public void run() {
-                    SortedSetDocValues similarityFeaturesValues = null;
-                    try {
-                        similarityFeaturesValues = leafReader.getSortedSetDocValues(FACE_FEATURES);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return;
-                    }
-                    int i0 = Math.min(len, itemsPerThread * threadIdx);
-                    int i1 = Math.min(len, i0 + itemsPerThread);
-                    for (int i = i0; i < i1; i++) {
-                        if (i % 1000 == 0 && this.isInterrupted()) {
-                            return;
-                        }
-                        IItemId itemId = result.getItem(i);
-                        int luceneId = ipedCase.getLuceneId(itemId);
-                        long ordinal;
-                        float score = 0;
-                        try {
-                            boolean hasVal = similarityFeaturesValues.advanceExact(luceneId);
-                            while (hasVal && (ordinal = similarityFeaturesValues.nextOrd()) != SortedSetDocValues.NO_MORE_ORDS) {
-                                BytesRef bytesRef = similarityFeaturesValues.lookupOrd(ordinal);
-                                double[] currentFeatures = convToDoubleVec(bytesRef.bytes);
-                                float squaredDist = distance(refSimilarityFeatures, currentFeatures, minDistSquared);
-                                if (squaredDist <= minDistSquared) {
-                                    score = squaredDistToScore(squaredDist);
-                                    break;
-                                }
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } finally {
-                            result.setScore(i, score);
-                        }
-                    }
-                }
-            }).start();
+        TopDocs topDocs = leafReader.searchNearestVectors(FACE_FEATURES, refSimilarityFeatures, MAX_KNN_RESULTS, null);
+        HashMap<Integer, Float> topDocsMap = new HashMap<>();
+        for (ScoreDoc doc : topDocs.scoreDocs) {
+            topDocsMap.put(doc.doc, convertLuceneScoreToFinalScore(doc.score));
         }
-        boolean canceled = false;
-        for (Thread thread : threads) {
-            if (thread != null) {
-                if (!canceled) {
-                    try {
-                        thread.join();
-                    } catch (InterruptedException e) {
-                        canceled = true;
-                    }
-                }
-                if (canceled) {
-                    thread.interrupt();
-                }
+        for (int i = 0; i < result.getLength(); i++) {
+            int luceneId = ipedCase.getLuceneId(result.getItem(i));
+            Float score = topDocsMap.get(luceneId);
+            if (score != null) {
+                result.setScore(i, score);
+            } else {
+                result.setScore(i, 0);
             }
         }
 
@@ -152,11 +121,12 @@ public class SimilarFacesSearch {
         ArrayList<String> matchLocations = new ArrayList<>();
         Object location = item.getExtraAttribute(SimilarFacesSearch.FACE_LOCATIONS);
         if (location instanceof List) {
+            float maxDistSquared = scoreToSquaredDist(minimumScore);
             double[] ref = getFirstFace(refImage);
             List<byte[]> features = (List<byte[]>) item.getExtraAttribute(SimilarFacesSearch.FACE_FEATURES);
             for (int i = 0; i < ((List) location).size(); i++) {
                 double[] face = convToDoubleVec(features.get(i));
-                if (distance(ref, face, minDistSquared) <= minDistSquared) {
+                if (distance(ref, face, maxDistSquared) <= maxDistSquared) {
                     matchLocations.add((String) ((List) location).get(i));
                 }
             }
