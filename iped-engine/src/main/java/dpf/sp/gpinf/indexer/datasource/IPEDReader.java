sepinf-inc/IPED

commit 841b95b03aa40d6c6527697cf3ca8bcfa1d171cd

change ISeekableInputStreamFactory, store minio uri in dataSourcePath

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/IndexItem.java
@@ -22,8 +22,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.lang.reflect.Constructor;
+import java.net.URI;
+import java.nio.file.FileSystemException;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -115,7 +119,7 @@ public class IndexItem extends BasicProps {
 
     private static volatile boolean guessMetaTypes = false;
 
-    private static Map<Path, SeekableInputStreamFactory> inputStreamFactories = new ConcurrentHashMap<>();
+    private static Map<String, SeekableInputStreamFactory> inputStreamFactories = new ConcurrentHashMap<>();
     private static Map<File, File> localEvidenceMap = new ConcurrentHashMap<>();
 
     private static class StringComparator implements Comparator<String> {
@@ -240,9 +244,10 @@ public class IndexItem extends BasicProps {
             doc.add(new StringField(ID_IN_SOURCE, value, Field.Store.YES));
             doc.add(new SortedDocValuesField(ID_IN_SOURCE, new BytesRef(value)));
         }
-        if (evidence.getInputStreamFactory() != null && evidence.getInputStreamFactory().getDataSourcePath() != null) {
-            Path srcPath = evidence.getInputStreamFactory().getDataSourcePath();
-            value = Util.getRelativePath(output, srcPath.toFile());
+        if (evidence.getInputStreamFactory() != null && evidence.getInputStreamFactory().getDataSourceURI() != null) {
+            URI uri = evidence.getInputStreamFactory().getDataSourceURI();
+            value = Util.getRelativePath(output, uri);
+
             doc.add(new StringField(SOURCE_PATH, value, Field.Store.YES));
             doc.add(new SortedDocValuesField(SOURCE_PATH, new BytesRef(value)));
 
@@ -765,18 +770,24 @@ public class IndexItem extends BasicProps {
                     evidence.setIdInDataSource(value.trim());
                 }
                 if (doc.get(IndexItem.SOURCE_PATH) != null) {
-                    String relPath = doc.get(IndexItem.SOURCE_PATH);
-                    Path absPath = Util.getResolvedFile(outputBase.getParent(), relPath).toPath();
-                    SeekableInputStreamFactory sisf = inputStreamFactories.get(absPath);
+                    String sourcePath = doc.get(IndexItem.SOURCE_PATH);
+                    SeekableInputStreamFactory sisf = inputStreamFactories.get(sourcePath);
                     if (sisf == null) {
                         String className = doc.get(IndexItem.SOURCE_DECODER);
                         Class<?> clazz = Class.forName(className);
-                        Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(Path.class);
-                        sisf = c.newInstance(absPath);
+                        try {
+                            Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(Path.class);
+                            Path absPath = Util.getResolvedFile(outputBase.getParent(), sourcePath).toPath();
+                            sisf = c.newInstance(absPath);
+
+                        } catch (NoSuchMethodException e) {
+                            Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(URI.class);
+                            sisf = c.newInstance(URI.create(sourcePath));
+                        }
                         if (sisf.checkIfDataSourceExists()) {
                             checkIfExistsAndAsk(sisf, outputBase);
                         }
-                        inputStreamFactories.put(absPath, sisf);
+                        inputStreamFactories.put(sourcePath, sisf);
                     }
                     evidence.setInputStreamFactory(sisf);
                 }
@@ -909,17 +920,17 @@ public class IndexItem extends BasicProps {
     }
 
     private static void checkIfExistsAndAsk(SeekableInputStreamFactory sisf, File caseModuleDir) throws IOException {
-        Path path = sisf.getDataSourcePath();
+        Path path = Paths.get(sisf.getDataSourceURI());
         if (path != null && !Files.exists(path)) {
             Path newPath = loadDataSourcePath(caseModuleDir, path);
             if (newPath != null && Files.exists(newPath)) {
-                sisf.setDataSourcePath(newPath);
+                sisf.setDataSourceURI(newPath.toUri());
                 return;
             }
             SelectImagePathWithDialog siwd = new SelectImagePathWithDialog(path.toFile());
             File newDataSource = siwd.askImagePathInGUI();
             if (newDataSource != null) {
-                sisf.setDataSourcePath(newDataSource.toPath());
+                sisf.setDataSourceURI(newDataSource.toPath().toUri());
                 saveDataSourcePath(caseModuleDir, path, newDataSource.toPath());
             }
         }

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ExportFileTask.java
@@ -27,8 +27,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -567,11 +569,11 @@ public class ExportFileTask extends AbstractTask {
         private Connection conn;
 
         public SQLiteInputStreamFactory(Path datasource) {
-            super(datasource);
+            super(datasource.toUri());
         }
 
         public SQLiteInputStreamFactory(Path datasource, Connection conn) {
-            super(datasource);
+            super(datasource.toUri());
             this.conn = conn;
         }
 
@@ -598,7 +600,7 @@ public class ExportFileTask extends AbstractTask {
             try {
                 byte[] bytes = null;
                 if (conn == null || conn.isClosed()) {
-                    conn = getSQLiteStorageCon(getDataSourcePath().toFile());
+                    conn = getSQLiteStorageCon(Paths.get(getDataSourceURI()).toFile());
                 }
                 try (PreparedStatement ps = conn.prepareStatement(SELECT_DATA)) {
                     ps.setString(1, identifier);

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/Util.java
@@ -32,8 +32,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.net.URI;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -210,6 +213,14 @@ public class Util {
         else {
             prefix = prefix.replace('\\', File.separatorChar).replace('/', File.separatorChar);
             return new File(prefix, suffix);
+        }
+    }
+
+    public static String getRelativePath(File baseFile, URI uri) {
+        try {
+            return Util.getRelativePath(baseFile, Paths.get(uri).toFile());
+        } catch (FileSystemNotFoundException e) {
+            return uri.toString();
         }
     }
 

iped-api/src/main/java/iped3/io/ISeekableInputStreamFactory.java
@@ -1,13 +1,13 @@
 package iped3.io;
 
 import java.io.IOException;
-import java.nio.file.Path;
+import java.net.URI;
 
 public interface ISeekableInputStreamFactory {
 
     public SeekableInputStream getSeekableInputStream(String identifier) throws IOException;
 
-    public Path getDataSourcePath();
+    public URI getDataSourceURI();
 
     public default void deleteItemInDataSource(String identifier) throws IOException {
         // do nothing by default

iped-engine/src/main/java/dpf/sp/gpinf/indexer/datasource/AD1DataSourceReader.java
@@ -3,6 +3,7 @@ package dpf.sp.gpinf.indexer.datasource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import dpf.sp.gpinf.indexer.util.IPEDException;
 import dpf.sp.gpinf.indexer.util.SeekableInputStreamFactory;
@@ -126,12 +127,12 @@ public class AD1DataSourceReader extends DataSourceReader {
         AD1Extractor ad1;
 
         public AD1InputStreamFactory(Path dataSource) {
-            super(dataSource);
+            super(dataSource.toUri());
         }
 
         private synchronized void init() throws IOException {
             if (ad1 == null)
-                ad1 = new AD1Extractor(dataSource.toFile());
+                ad1 = new AD1Extractor(Paths.get(dataSource).toFile());
         }
 
         @Override

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ElasticSearchIndexTask.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.net.URI;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
@@ -451,7 +454,8 @@ public class ElasticSearchIndexTask extends AbstractTask {
 
     private String getInputStreamSourcePath(IItem item) {
         if (item.getInputStreamFactory() != null) {
-            return Util.getRelativePath(output, item.getInputStreamFactory().getDataSourcePath().toFile());
+            URI uri = item.getInputStreamFactory().getDataSourceURI();
+            return Util.getRelativePath(output, uri);
         }
         return null;
     }

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/MinIOTask.java
@@ -3,6 +3,7 @@ package dpf.sp.gpinf.indexer.process.task;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
@@ -93,7 +94,7 @@ public class MinIOTask extends AbstractTask {
         loadCredentials(caseData);
 
         minioClient = MinioClient.builder().endpoint(server).credentials(accessKey, secretKey).build();
-        inputStreamFactory = new MinIOInputInputStreamFactory(null);
+        inputStreamFactory = new MinIOInputInputStreamFactory(URI.create(server));
 
         // Check if the bucket already exists.
         boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
@@ -169,10 +170,10 @@ public class MinIOTask extends AbstractTask {
             }
         }
 
-        String uri = buildUri(server, bucket, hash);
+        String identifier = buildPath(bucket, hash);
 
         if (exists) {
-            updateDataSource(item, uri);
+            updateDataSource(item, identifier);
             return;
         }
 
@@ -183,7 +184,7 @@ public class MinIOTask extends AbstractTask {
                         .stream(is, item.getLength(), -1).contentType(item.getMediaType().toString()).build());
 
             }
-            updateDataSource(item, uri);
+            updateDataSource(item, identifier);
 
         } catch (Exception e) {
             logger.error("Error when uploading object " + item.getPath(), e);
@@ -191,24 +192,12 @@ public class MinIOTask extends AbstractTask {
 
     }
 
-    private static String buildUri(String server, String bucket, String hash) {
-        return server + "/" + bucket + "/" + hash;
+    private static String buildPath(String bucket, String hash) {
+        return bucket + "/" + hash;
     }
 
-    private static String[] parseUri(String uri) {
-        ArrayList<String> parts = new ArrayList<>();
-        int end = uri.length();
-        for (int i = uri.length() - 1; i > 0; i--) {
-            if (uri.charAt(i) == '/') {
-                parts.add(0, uri.substring(i + 1, end));
-                end = i;
-            }
-            if (parts.size() == 2) {
-                parts.add(0, uri.substring(0, i));
-                break;
-            }
-        }
-        return parts.toArray(new String[3]);
+    private static String[] parsePath(String path) {
+        return path.split("/");
     }
 
     private void updateDataSource(IItem item, String id) {
@@ -227,8 +216,8 @@ public class MinIOTask extends AbstractTask {
 
         private static Map<String, MinioClient> map = new ConcurrentHashMap<>();
 
-        public MinIOInputInputStreamFactory(Path dataSource) {
-            super(Paths.get("minio-storage"));
+        public MinIOInputInputStreamFactory(URI dataSource) {
+            super(dataSource);
         }
 
         public boolean checkIfDataSourceExists() {
@@ -237,10 +226,10 @@ public class MinIOTask extends AbstractTask {
 
         @Override
         public SeekableInputStream getSeekableInputStream(String identifier) throws IOException {
-            String[] parts = parseUri(identifier);
-            String server = parts[0];
-            String bucket = parts[1];
-            String hash = parts[2];
+            String server = dataSource.toString();
+            String[] parts = parsePath(identifier);
+            String bucket = parts[0];
+            String hash = parts[1];
 
             MinioClient minioClient = map.get(server);
             if (minioClient == null) {

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/ZIPInputStreamFactory.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.nio.channels.ClosedChannelException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
@@ -22,12 +23,12 @@ public class ZIPInputStreamFactory extends SeekableInputStreamFactory implements
     private ZipFile4j zip;
 
     public ZIPInputStreamFactory(Path dataSource) {
-        super(dataSource);
+        super(dataSource.toUri());
     }
 
     private synchronized void init() throws ZipException {
         if (zip == null) {
-            zip = new ZipFile4j(this.dataSource.toFile());
+            zip = new ZipFile4j(Paths.get(this.dataSource).toFile());
         }
     }
 

commit 240e58da778c3bd2dc22809afe43e77e91d7ee35

fix #990: fix reading subitem content in multicases, broken by 841b95b

iped-engine/src/main/java/dpf/sp/gpinf/indexer/datasource/IPEDReader.java
@@ -93,7 +93,7 @@ public class IPEDReader extends DataSourceReader {
 
     private static org.slf4j.Logger LOGGER = LoggerFactory.getLogger(IPEDReader.class);
 
-    private static Map<String, SeekableInputStreamFactory> inputStreamFactories = new ConcurrentHashMap<>();
+    private static Map<Path, SeekableInputStreamFactory> inputStreamFactories = new ConcurrentHashMap<>();
 
     IPEDSource ipedCase;
     HashSet<Integer> selectedLabels;
@@ -527,7 +527,7 @@ public class IPEDReader extends DataSourceReader {
             if (!treeNode) {
                 value = doc.get(IndexItem.EXPORT);
                 if (value != null && !value.isEmpty()) {
-                    File localFile = Util.getResolvedFile(ipedCase.getModuleDir().getParent(), value);
+                    File localFile = Util.getResolvedFile(ipedCase.getModuleDir().getParent(), value).toFile();
                     if (!ipedCase.isReport()) {
                         localFile = IndexItem.checkIfEvidenceFolderExists(evidence, localFile, ipedCase.getModuleDir());
                     }
@@ -548,23 +548,23 @@ public class IPEDReader extends DataSourceReader {
                 }
                 if (doc.get(IndexItem.SOURCE_PATH) != null) {
                     String sourcePath = doc.get(IndexItem.SOURCE_PATH);
-                    SeekableInputStreamFactory sisf = inputStreamFactories.get(sourcePath);
+                    Path absPath = Util.getResolvedFile(basePath, sourcePath);
+                    SeekableInputStreamFactory sisf = inputStreamFactories.get(absPath);
                     if (sisf == null) {
                         String className = doc.get(IndexItem.SOURCE_DECODER);
                         Class<?> clazz = Class.forName(className);
                         try {
                             Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(Path.class);
-                            Path absPath = Util.getResolvedFile(basePath, sourcePath).toPath();
                             sisf = c.newInstance(absPath);
-                            if (!ipedCase.isReport() && sisf.checkIfDataSourceExists()) {
-                                IndexItem.checkIfExistsAndAsk(sisf, ipedCase.getModuleDir());
-                            }
 
                         } catch (NoSuchMethodException e) {
                             Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(URI.class);
-                            sisf = c.newInstance(URI.create(sourcePath));
+                            sisf = c.newInstance(absPath.toUri());
                         }
-                        inputStreamFactories.put(sourcePath, sisf);
+                        if (!ipedCase.isReport() && sisf.checkIfDataSourceExists()) {
+                            IndexItem.checkIfExistsAndAsk(sisf, ipedCase.getModuleDir());
+                        }
+                        inputStreamFactories.put(absPath, sisf);
                     }
                     evidence.setInputStreamFactory(sisf);
 

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/IndexItem.java
@@ -130,7 +130,7 @@ public class IndexItem extends BasicProps {
 
     private static volatile boolean collectMetaTypes = false;
 
-    private static Map<String, SeekableInputStreamFactory> inputStreamFactories = new ConcurrentHashMap<>();
+    private static Map<Path, SeekableInputStreamFactory> inputStreamFactories = new ConcurrentHashMap<>();
     private static Map<File, File> localEvidenceMap = new ConcurrentHashMap<>();
 
     private static Map<String, Class<?>> typesMap = Collections
@@ -928,7 +928,7 @@ public class IndexItem extends BasicProps {
             File outputBase = iCase.getModuleDir();
             value = doc.get(IndexItem.EXPORT);
             if (value != null && !value.isEmpty()) {
-                File localFile = Util.getResolvedFile(outputBase.getParent(), value);
+                File localFile = Util.getResolvedFile(outputBase.getParent(), value).toFile();
                 if (!iCase.isReport()) {
                     localFile = checkIfEvidenceFolderExists(evidence, localFile, outputBase);
                 }
@@ -952,26 +952,23 @@ public class IndexItem extends BasicProps {
             }
             if (doc.get(IndexItem.SOURCE_PATH) != null) {
                 String sourcePath = doc.get(IndexItem.SOURCE_PATH);
-                SeekableInputStreamFactory sisf = inputStreamFactories.get(sourcePath);
+                Path absPath = Util.getResolvedFile(outputBase.getParent(), sourcePath);
+                SeekableInputStreamFactory sisf = inputStreamFactories.get(absPath);
                 if (sisf == null) {
                     String className = doc.get(IndexItem.SOURCE_DECODER);
                     Class<?> clazz = Class.forName(className);
                     try {
                         Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(Path.class);
-                        Path absPath = Util.getResolvedFile(outputBase.getParent(), sourcePath).toPath();
                         sisf = c.newInstance(absPath);
-                        if (!iCase.isReport() && sisf.checkIfDataSourceExists()) {
-                            IndexItem.checkIfExistsAndAsk(sisf, iCase.getModuleDir());
-                        }
 
                     } catch (NoSuchMethodException e) {
                         Constructor<SeekableInputStreamFactory> c = (Constructor) clazz.getConstructor(URI.class);
-                        sisf = c.newInstance(URI.create(sourcePath));
+                        sisf = c.newInstance(absPath.toUri());
                     }
                     if (!iCase.isReport() && sisf.checkIfDataSourceExists()) {
-                        checkIfExistsAndAsk(sisf, outputBase);
+                        checkIfExistsAndAsk(sisf, iCase.getModuleDir());
                     }
-                    inputStreamFactories.put(sourcePath, sisf);
+                    inputStreamFactories.put(absPath, sisf);
                 }
                 evidence.setInputStreamFactory(sisf);
             }
@@ -1140,7 +1137,7 @@ public class IndexItem extends BasicProps {
         String path = props.getProperty(oldPath.toString());
         if (path == null)
             return null;
-        return Util.getResolvedFile(caseModuleDir.getParentFile().toPath().toString(), path).toPath();
+        return Util.getResolvedFile(caseModuleDir.getParentFile().toPath().toString(), path);
     }
 
     public static synchronized File checkIfEvidenceFolderExists(Item evidence, File localFile, File caseModuleDir) throws IOException {

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/Worker.java
@@ -145,7 +145,7 @@ public class Worker extends Thread {
     private void checkFile(IItem evidence) {
         String filePath = evidence.getFileToIndex();
         if (evidence.getFile() == null && !filePath.isEmpty()) {
-            File file = Util.getResolvedFile(baseFilePath, filePath);
+            File file = Util.getResolvedFile(baseFilePath, filePath).toFile();
             evidence.setFile(file);
             evidence.setLength(file.length());
         }

iped-engine/src/main/java/dpf/sp/gpinf/indexer/process/task/ExportFileTask.java
@@ -126,6 +126,7 @@ public class ExportFileTask extends AbstractTask {
 
     private static final String CLEAR_DATA = "DELETE FROM t1 WHERE id=?;";
 
+    // maps below are used to track different storages/connections in multicases
     private static HashMap<File, HashMap<Integer, File>> storage = new HashMap<>();
     private static HashMap<File, HashMap<Integer, Connection>> storageCon = new HashMap<>();
 

iped-engine/src/main/java/dpf/sp/gpinf/indexer/util/Util.java
@@ -235,15 +235,10 @@ public class Util {
                 || file.getAbsolutePath().toLowerCase().startsWith("/dev/"); //$NON-NLS-1$
     }
 
-    public static File getResolvedFile(String prefix, String suffix) {
-        suffix = suffix.replace('\\', File.separatorChar).replace('/', File.separatorChar);
-        File file = new File(suffix);
-        if (file.isAbsolute())
-            return file;
-        else {
-            prefix = prefix.replace('\\', File.separatorChar).replace('/', File.separatorChar);
-            return new File(prefix, suffix);
-        }
+    public static Path getResolvedFile(String prefix, String suffix) {
+        Path first = Paths.get(prefix);
+        Path other = Paths.get(suffix);
+        return first.resolve(other);
     }
 
     public static String getRelativePath(File baseFile, URI uri) {