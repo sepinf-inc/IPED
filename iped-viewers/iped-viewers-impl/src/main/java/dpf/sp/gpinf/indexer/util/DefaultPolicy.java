586cd0e80151b2ff4c8983584d49f1593f90af80
Luis Nassif <lfcnassif@gmail.com>
2022-03-08 16:47:23 -0300

    #908: better implementation for the default security Policy

--- iped-viewers/iped-viewers-impl/src/main/java/dpf/sp/gpinf/indexer/util/DefaultPolicy.java
+++ iped-viewers/iped-viewers-impl/src/main/java/dpf/sp/gpinf/indexer/util/DefaultPolicy.java
@@ -19,21 +19,33 @@ import dpf.sp.gpinf.indexer.ui.fileViewer.frames.HtmlViewer;
  */
 public class DefaultPolicy extends Policy {
 
+    private static final URI viewer = getHtmlViewerURI();
+
+    private static URI getHtmlViewerURI() {
+        try {
+            return HtmlViewer.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public boolean implies(ProtectionDomain domain, Permission perm) {
 
-        if (!(perm instanceof SocketPermission || perm instanceof URLPermission)) {
-            return true;
-        }
-
         try {
+
+            if (domain.getCodeSource() == null || domain.getCodeSource().getLocation() == null) {
+                return true;
+            }
             URI from = domain.getCodeSource().getLocation().toURI();
-            URI viewer = HtmlViewer.class.getProtectionDomain().getCodeSource().getLocation().toURI();
-            if (from.equals(viewer)) {
+
+            if (from.equals(viewer) && (perm instanceof SocketPermission || perm instanceof URLPermission)) {
                 return false;
             }
+
         } catch (URISyntaxException e) {
             e.printStackTrace();
+
         }
 
         return true;
