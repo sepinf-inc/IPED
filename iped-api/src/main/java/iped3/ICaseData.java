2433b1b3eae31dfd26d23fb67bfeea1275f5bd4b
Luis Nassif <lfcnassif@gmail.com>
2022-03-23 13:30:48 -0300

    #180: remove unused gpinf.dev.data classes

--- iped-api/src/main/java/iped3/ICaseData.java
+++ iped-api/src/main/java/iped3/ICaseData.java
@@ -3,7 +3,6 @@ package iped3;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * (INTERFACE DO IPED) Classe que define todos os dados do caso.
@@ -12,14 +11,6 @@ import java.util.List;
  * @author Nassif (GPINF/SP)
  */
 public interface ICaseData extends Serializable {
-
-    /**
-     * Adiciona um bookmark.
-     *
-     * @param bookmark
-     *            bookmark a ser adicionado
-     */
-    void addBookmark(IFileGroup bookmark);
 
     /**
      * Adiciona um arquivo de evidência.
@@ -38,34 +29,12 @@ public interface ICaseData extends Serializable {
 
     void addItemToQueue(IItem item, int queuePriority) throws InterruptedException;
 
-    /**
-     * Adiciona um grupo de arquivos classificados por data.
-     *
-     * @param timeGroup
-     *            grupo de arquivos classificados por data
-     */
-    void addTimeGroup(IFileGroup timeGroup);
-
     Integer changeToNextQueue();
 
     /**
      * @return true se o caso contém um report
      */
     boolean containsReport();
-
-    /**
-     * Obtém lista de bookmarks.
-     *
-     * @return lista não modificável de bookmarks.
-     */
-    List<IFileGroup> getBookmarks();
-
-    /**
-     * Retorna o objeto com as informações do caso.
-     *
-     * @return objeto da classe CaseInformation com informações do caso
-     */
-    ICaseInfo getCaseInformation();
 
     /**
      * Retorna um objeto armazenado no caso.
@@ -84,21 +53,6 @@ public interface ICaseData extends Serializable {
      * @return retorna o volume de dados descobertos até o momento
      */
     long getDiscoveredVolume();
-
-    /**
-     * Obtém o objeto raiz da árvore de arquivos do caso.
-     *
-     * @return objeto raiz, a partir do qual é possível navegar em todo estrutura de
-     *         diretórios do caso.
-     */
-    IPathNode getRootNode();
-
-    /**
-     * Obtém lista de grupo de arquivos por data.
-     *
-     * @return lista não modificável de grupo de arquivos por data.
-     */
-    List<IFileGroup> getTimeGroups();
 
     void incDiscoveredEvidences(int inc);
 
