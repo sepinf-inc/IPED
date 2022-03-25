84f43a127a48648209cff55aaf80fa3dc2ba67fa
Luis Nassif <lfcnassif@gmail.com>
2022-03-25 15:20:13 -0300

    #1034: aux class to turn pdf thumb computation on/off in parser per item

---
+++ iped-parsers/iped-parsers-impl/src/main/java/dpf/sp/gpinf/indexer/parsers/util/ComputeThumb.java
@@ -0,0 +1,15 @@
+package dpf.sp.gpinf.indexer.parsers.util;
+
+import java.io.Serializable;
+
+public class ComputeThumb implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public ComputeThumb() {
+    }
+
+}
