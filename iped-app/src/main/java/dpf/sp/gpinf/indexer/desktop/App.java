sepinf-inc/IPED

commit e6127f7b37c3522d4c3c066405dde06ccd685526

#180: initial commit to remove AppSearchParams usage

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/App.java
@@ -122,6 +122,7 @@ import dpf.sp.gpinf.indexer.process.task.ImageSimilarityTask;
 import dpf.sp.gpinf.indexer.process.task.ImageThumbTask;
 import dpf.sp.gpinf.indexer.search.IPEDMultiSource;
 import dpf.sp.gpinf.indexer.search.IPEDSearcher;
+import dpf.sp.gpinf.indexer.search.IPEDSource;
 import dpf.sp.gpinf.indexer.search.ItemId;
 import dpf.sp.gpinf.indexer.search.MultiSearchResult;
 import dpf.sp.gpinf.indexer.ui.PanelsLayout;
@@ -241,11 +242,7 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
     final static String FILTRO_SELECTED = Messages.getString("App.Checked"); //$NON-NLS-1$
     public final static String SEARCH_TOOL_TIP = Messages.getString("App.SearchBoxTip"); //$NON-NLS-1$
 
-    public static int MAX_LINE_SIZE = 100; // tamanho de quebra do texto para highlight
-
     static int MAX_HITS = 10000;
-
-    public static int MAX_LINES = 100000;
 
     final static int FRAG_SIZE = 100, TEXT_BREAK_SIZE = 1000000;
 
@@ -257,16 +254,12 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
 
     public boolean useVideoThumbsInGallery = false;
 
+    private IPEDSource lastSelectedSource;
+
+    private String codePath;
+
     private App() {
         this.appSearchParams = new AppSearchParams();
-        this.appSearchParams.mainFrame = (JFrame) this;
-        this.appSearchParams.HIGHLIGHT_START_TAG = "<font color=\"black\" bgcolor=\"yellow\">"; //$NON-NLS-1$
-        this.appSearchParams.HIGHLIGHT_END_TAG = "</font>"; //$NON-NLS-1$
-        this.appSearchParams.TEXT_BREAK_SIZE = TEXT_BREAK_SIZE;
-        this.appSearchParams.FRAG_SIZE = FRAG_SIZE;
-        this.appSearchParams.MAX_LINES = MAX_LINES;
-        this.appSearchParams.MAX_HITS = MAX_HITS;
-        this.appSearchParams.MAX_LINE_SIZE = MAX_LINE_SIZE;
         this.appSearchParams.highlightTerms = new HashSet<String>();
     }
 
@@ -298,14 +291,16 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
     }
 
     public void init(LogConfiguration logConfiguration, boolean isMultiCase, File casesPathFile,
-            Manager processingManager) {
+            Manager processingManager, String codePath) {
 
         this.logConfiguration = logConfiguration;
         this.isMultiCase = isMultiCase;
         this.casesPathFile = casesPathFile;
         this.processingManager = processingManager;
-        if (processingManager != null)
+        if (processingManager != null) {
             processingManager.setSearchAppOpen(true);
+        }
+        this.codePath = codePath;
 
         LOGGER = LoggerFactory.getLogger(App.class);
         LOGGER.info("Starting..."); //$NON-NLS-1$
@@ -535,9 +530,7 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         
         appGraphAnalytics = new AppGraphAnalytics();
 
-        viewerController = new ViewerController(appSearchParams);
         hitsTable = new HitsTable(new HitsTableModel(getTextViewer()));
-        appSearchParams.hitsTable = hitsTable;
         hitsScroll = new JScrollPane(hitsTable);
         hitsTable.setFillsViewportHeight(true);
         hitsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -547,9 +540,10 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         hitsTable.getTableHeader().setPreferredSize(new Dimension(0, 0));
         hitsTable.setShowGrid(false);
 
+        viewerController = new ViewerController(hitsTable, codePath);
+
         subItemTable = new HitsTable(subItemModel);
         subItemScroll = new JScrollPane(subItemTable);
-        this.appSearchParams.subItemScroll = subItemScroll;
         subItemTable.setFillsViewportHeight(true);
         subItemTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         subItemTable.getColumnModel().getColumn(0).setPreferredWidth(40);
@@ -574,7 +568,6 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
 
         parentItemTable = new HitsTable(parentItemModel);
         parentItemScroll = new JScrollPane(parentItemTable);
-        this.appSearchParams.parentItemScroll = parentItemScroll;
         parentItemTable.setFillsViewportHeight(true);
         parentItemTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         parentItemTable.getColumnModel().getColumn(0).setPreferredWidth(40);
@@ -716,14 +709,11 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         dialogBar.setBounds(0, 0, 150, 30);
         dialogBar.setUndecorated(true);
         dialogBar.getContentPane().add(progressBar);
-        appSearchParams.dialogBar = dialogBar;
 
-        
         adjustLayout(false);
         PanelsLayout.load(dockingControl);
 
         status = new JLabel(" "); //$NON-NLS-1$
-        this.appSearchParams.status = status;
 
         this.getContentPane().add(topPanel, BorderLayout.PAGE_START);
         // this.getContentPane().add(treeSplitPane, BorderLayout.CENTER);
@@ -1437,5 +1427,13 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
     @Override
     public IColumnsManager getColumnsManager() {
         return ColumnsManager.getInstance();
+    }
+
+    public void setLastSelectedSource(IPEDSource lastSelectedSource) {
+        this.lastSelectedSource = lastSelectedSource;
+    }
+
+    public IPEDSource getLastSelectedSource() {
+        return this.lastSelectedSource;
     }
 }

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ViewerController.java
@@ -46,6 +46,7 @@ import dpf.sp.gpinf.indexer.ui.fileViewer.frames.TextViewer;
 import dpf.sp.gpinf.indexer.ui.fileViewer.frames.TiffViewer;
 import dpf.sp.gpinf.indexer.ui.fileViewer.frames.TikaHtmlViewer;
 import dpf.sp.gpinf.indexer.ui.fileViewer.util.AppSearchParams;
+import dpf.sp.gpinf.indexer.ui.hitsViewer.HitsTable;
 import dpf.sp.gpinf.indexer.util.LibreOfficeFinder;
 import iped3.io.IStreamSource;
 
@@ -73,12 +74,12 @@ public class ViewerController {
     private boolean isFixed;
     private final Object lock = new Object();
 
-    public ViewerController(AppSearchParams params) {
+    public ViewerController(HitsTable hitsTable, final String codePath) {
         Window owner = App.get();
         
         // These viewers will have their own docking frame
         viewers.add(new HexViewerPlus(owner, new HexSearcherImpl()));
-        viewers.add(textViewer = new TextViewer(params));
+        viewers.add(textViewer = new TextViewer(hitsTable));
         viewers.add(new MetadataViewer() {
             @Override
             public boolean isFixed() {
@@ -113,13 +114,13 @@ public class ViewerController {
                     }
 
                     // LibreOffice viewer initialization
-                    LibreOfficeFinder loFinder = new LibreOfficeFinder(new File(params.codePath).getParentFile());
+                    LibreOfficeFinder loFinder = new LibreOfficeFinder(new File(codePath).getParentFile());
                     final String pathLO = loFinder.getLOPath();
                     if (pathLO != null) {
                         SwingUtilities.invokeAndWait(new Runnable() {
                             @Override
                             public void run() {
-                                officeViewer = new LibreOfficeViewer(params.codePath + "/../lib/nativeview", pathLO); //$NON-NLS-1$
+                                officeViewer = new LibreOfficeViewer(codePath + "/../lib/nativeview", pathLO); //$NON-NLS-1$
                                 viewersRepository.addViewer(officeViewer);
                             }
                         });

iped-app/src/main/java/dpf/sp/gpinf/indexer/ui/fileViewer/frames/TextViewer.java
@@ -1,15 +1,15 @@
 package dpf.sp.gpinf.indexer.ui.fileViewer.frames;
 
-import dpf.sp.gpinf.indexer.desktop.TextParser;
-import dpf.sp.gpinf.indexer.ui.fileViewer.util.AppSearchParams;
-import iped3.io.IStreamSource;
-
 import java.util.Set;
+
+import dpf.sp.gpinf.indexer.desktop.TextParser;
+import dpf.sp.gpinf.indexer.ui.hitsViewer.HitsTable;
+import iped3.io.IStreamSource;
 
 public class TextViewer extends ATextViewer {
 
-    public TextViewer(AppSearchParams params) {
-        super(params);
+    public TextViewer(HitsTable hitsTable) {
+        super(hitsTable);
     }
 
     @Override
@@ -18,7 +18,7 @@ public class TextViewer extends ATextViewer {
         if (content == null) {
             loadFile(content, null);
         } else {
-            textParser = new TextParser(appSearchParams, content, contentType, tmp);
+            textParser = new TextParser(content, contentType, tmp);
             textParser.execute();
         }
     }

iped-viewers/iped-viewers-impl/src/main/java/dpf/sp/gpinf/indexer/ui/fileViewer/frames/ATextViewer.java
@@ -27,13 +27,29 @@ import dpf.sp.gpinf.indexer.util.LuceneSimpleHTMLEncoder;
 import iped3.io.IStreamSource;
 import dpf.sp.gpinf.indexer.ui.fileViewer.Messages;
 import dpf.sp.gpinf.indexer.ui.fileViewer.util.AppSearchParams;
+import dpf.sp.gpinf.indexer.ui.hitsViewer.HitsTable;
 import dpf.sp.gpinf.indexer.ITextParser;
 
 public abstract class ATextViewer extends AbstractViewer implements KeyListener {
 
     public static Font font = new Font(Font.MONOSPACED, Font.PLAIN, 11);
+    
+    /**
+     * Maximum number of text break lines to track/keep on memory.
+     */
+    public static final int MAX_LINES = 100000;
+
+    /**
+     * Maximum size of text line
+     */
+    public static final int MAX_LINE_SIZE = 100;
+
+    public static final String HIGHLIGHT_START_TAG = "<font color=\"black\" bgcolor=\"yellow\">";
+
+    public static final String HIGHLIGHT_END_TAG = "</font>";
 
     public static final String TEXT_ENCODING = "UTF-32BE"; //$NON-NLS-1$
+
     public static final int CHAR_BYTE_COUNT = 4;
 
     public JTable textTable;
@@ -41,12 +57,12 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
     private JScrollPane viewerScroll;
     public ITextParser textParser;
     protected TemporaryResources tmp;
-    protected AppSearchParams appSearchParams;
+    protected HitsTable hitsTable;
     private AbstractTableModel hitsModel;
 
-    public ATextViewer(AppSearchParams params) {
+    public ATextViewer(HitsTable hitsTable) {
         super(new GridLayout());
-        appSearchParams = params;
+        this.hitsTable = hitsTable;
         textViewerModel = new TextViewerModel();
         textTable = new JTable(textViewerModel) {
             private static final long serialVersionUID = -5129153322350459095L;
@@ -144,10 +160,10 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
             if (textParser != null) {
                 try {
                     int lines = textParser.getViewRows().size() - 1;
-                    if (lines == appSearchParams.MAX_LINES) {
-                        lines = appSearchParams.MAX_LINES + (int) ((textParser.getParsedFile().size()
-                                - textParser.getViewRows().get(appSearchParams.MAX_LINES))
-                                / (CHAR_BYTE_COUNT * appSearchParams.MAX_LINE_SIZE)) + 1;
+                    if (lines == MAX_LINES) {
+                        lines = MAX_LINES + (int) ((textParser.getParsedFile().size()
+                                - textParser.getViewRows().get(MAX_LINES))
+                                / (CHAR_BYTE_COUNT * MAX_LINE_SIZE)) + 1;
                     }
                     return lines;
 
@@ -171,13 +187,13 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
         public Object getValueAt(int row, int col) {
             try {
                 long off = 0, len;
-                if (row < appSearchParams.MAX_LINES) {
+                if (row < MAX_LINES) {
                     off = textParser.getViewRows().get(row);
                     len = textParser.getViewRows().get(row + 1) - off;
                 } else {
-                    len = appSearchParams.MAX_LINE_SIZE * CHAR_BYTE_COUNT;
-                    off = textParser.getViewRows().get(appSearchParams.MAX_LINES)
-                            + (long) (row - appSearchParams.MAX_LINES) * len;
+                    len = MAX_LINE_SIZE * CHAR_BYTE_COUNT;
+                    off = textParser.getViewRows().get(MAX_LINES)
+                            + (long) (row - MAX_LINES) * len;
 
                     // Tratamento para não dividir hits destacados
                     // Desloca início da linha para final de fragmento com hit
@@ -246,8 +262,8 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
             for (Integer row : textTable.getSelectedRows()) {
                 String value = textViewerModel.getValueAt(row, 0).toString();
                 value = value.replaceAll("<html><pre>", "").replaceAll("</pre></html>", ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-                value = value.replaceAll(appSearchParams.HIGHLIGHT_START_TAG, "") //$NON-NLS-1$
-                        .replaceAll(appSearchParams.HIGHLIGHT_END_TAG, ""); //$NON-NLS-1$
+                value = value.replaceAll(HIGHLIGHT_START_TAG, "") //$NON-NLS-1$
+                        .replaceAll(HIGHLIGHT_END_TAG, ""); //$NON-NLS-1$
                 value = LuceneSimpleHTMLEncoder.htmlDecode(value);
                 copy.append(value + "\r\n"); //$NON-NLS-1$
             }
@@ -265,21 +281,21 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
     @Override
     public void scrollToNextHit(boolean forward) {
 
-        currentHit = appSearchParams.hitsTable.getSelectedRow();
+        currentHit = hitsTable.getSelectedRow();
         totalHits = textParser.getHits().size();
         if (forward) {
             if (currentHit < totalHits - 1) {
-                appSearchParams.hitsTable.setRowSelectionInterval(currentHit + 1, currentHit + 1);
+                hitsTable.setRowSelectionInterval(currentHit + 1, currentHit + 1);
             }
 
         } else {
             if (currentHit > 0) {
-                appSearchParams.hitsTable.setRowSelectionInterval(currentHit - 1, currentHit - 1);
+                hitsTable.setRowSelectionInterval(currentHit - 1, currentHit - 1);
             }
 
         }
-        appSearchParams.hitsTable.scrollRectToVisible(appSearchParams.hitsTable
-                .getCellRect(appSearchParams.hitsTable.getSelectionModel().getLeadSelectionIndex(), 0, false));
+        hitsTable.scrollRectToVisible(
+                hitsTable.getCellRect(hitsTable.getSelectionModel().getLeadSelectionIndex(), 0, false));
 
     }
 }

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/AppMain.java
@@ -200,8 +200,8 @@ public class AppMain {
                 CustomLoader.run(customArgs, jars);
 
             } else {
-                App.get().getSearchParams().codePath = libDir.getAbsolutePath();
-                App.get().init(logConfiguration, isMultiCase, casesPathFile, processingManager);
+                App.get().init(logConfiguration, isMultiCase, casesPathFile, processingManager,
+                        libDir.getAbsolutePath());
 
                 UICaseDataLoader init = new UICaseDataLoader(processingManager);
                 init.execute();

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/FileProcessor.java
@@ -132,7 +132,7 @@ public class FileProcessor extends CancelableWorker<Void, Void> implements IFile
 
         // TODO usar nova API e contornar exibição da Ajuda
         IPEDSource iCase = (IPEDSource) App.get().appCase.getAtomicSource(docId);
-        App.get().getSearchParams().lastSelectedSource = iCase;
+        App.get().setLastSelectedSource(iCase);
         IItem item = IndexItem.getItem(doc, iCase, false);
 
         long textSize = iCase.getTextSize(item.getId());

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/HitsTableListener.java
@@ -57,17 +57,17 @@ public class HitsTableListener implements ListSelectionListener {
                 int[] hit = App.get().getTextViewer().textParser.getSortedHits().get(hitOff);
                 int hitLen = hit[0];
 
-                int line_disk_size = App.MAX_LINE_SIZE * ATextViewer.CHAR_BYTE_COUNT;
+                int line_disk_size = ATextViewer.MAX_LINE_SIZE * ATextViewer.CHAR_BYTE_COUNT;
 
                 int startViewRow = hit[1];
                 long viewRowOff = App.get().getTextViewer().textParser.getViewRows().get(startViewRow);
-                if (startViewRow == App.MAX_LINES) {
+                if (startViewRow == ATextViewer.MAX_LINES) {
                     startViewRow += (int) (hitOff - viewRowOff) / line_disk_size;
                 }
 
                 int endViewRow = hit[2];
                 viewRowOff = App.get().getTextViewer().textParser.getViewRows().get(endViewRow);
-                if (endViewRow == App.MAX_LINES) {
+                if (endViewRow == ATextViewer.MAX_LINES) {
                     endViewRow += (int) (hitOff + hitLen - viewRowOff) / line_disk_size;
                 }
 
@@ -75,11 +75,11 @@ public class HitsTableListener implements ListSelectionListener {
                 do {
                     String line = App.get().getTextViewer().textViewerModel.getValueAt(viewRow, 0).toString();
                     int index1, index2, x = 0, width = 0;
-                    if ((index1 = line.indexOf(App.get().getParams().HIGHLIGHT_START_TAG)) != -1) {
+                    if ((index1 = line.indexOf(ATextViewer.HIGHLIGHT_START_TAG)) != -1) {
                         String text = line.substring(0, index1);
                         x = getWidth(text) - 100;
-                        index2 = line.indexOf(App.get().getParams().HIGHLIGHT_END_TAG);
-                        text = line.substring(index1, index2 - App.get().getParams().HIGHLIGHT_START_TAG.length());
+                        index2 = line.indexOf(ATextViewer.HIGHLIGHT_END_TAG);
+                        text = line.substring(index1, index2 - ATextViewer.HIGHLIGHT_START_TAG.length());
                         width = App.get().getFontMetrics(App.get().getTextViewer().textTable.getFont())
                                 .stringWidth(text) + 150;
                         Rectangle rect = App.get().getTextViewer().textTable.getCellRect(viewRow, 0, true);

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ResultTableListener.java
@@ -41,6 +41,7 @@ import org.slf4j.LoggerFactory;
 
 import dpf.sp.gpinf.indexer.search.IPEDSearcher;
 import dpf.sp.gpinf.indexer.search.MultiSearchResult;
+import dpf.sp.gpinf.indexer.ui.fileViewer.frames.ATextViewer;
 import iped3.IItem;
 import iped3.IItemId;
 import iped3.util.BasicProps;
@@ -372,8 +373,7 @@ public class ResultTableListener implements ListSelectionListener, MouseListener
         if (App.get().getParams().FONT_START_TAG != null)
             cell = cell.replace(App.get().getParams().FONT_START_TAG, ""); //$NON-NLS-1$
         return cell.replace("<html><nobr>", "").replace("</html>", "") //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-                .replace(App.get().getParams().HIGHLIGHT_START_TAG, "") //$NON-NLS-1$
-                .replace(App.get().getParams().HIGHLIGHT_END_TAG, ""); //$NON-NLS-1$
+                .replace(ATextViewer.HIGHLIGHT_START_TAG, "").replace(ATextViewer.HIGHLIGHT_END_TAG, "");
     }
 
 }

commit dee30f926796efd416de50d9802d0651c5f7459f

#180: fix NPE caused by commit e6127f7

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/App.java
@@ -549,7 +549,9 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         
         appGraphAnalytics = new AppGraphAnalytics();
 
-        hitsTable = new HitsTable(new HitsTableModel(getTextViewer()));
+        viewerController = new ViewerController(codePath);
+
+        hitsTable = new HitsTable(new HitsTableModel(viewerController.getTextViewer()));
         hitsScroll = new JScrollPane(hitsTable);
         hitsTable.setFillsViewportHeight(true);
         hitsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -559,7 +561,7 @@ public class App extends JFrame implements WindowListener, IMultiSearchResultPro
         hitsTable.getTableHeader().setPreferredSize(new Dimension(0, 0));
         hitsTable.setShowGrid(false);
 
-        viewerController = new ViewerController(hitsTable, codePath);
+        viewerController.setHitsTableInTextViewer(hitsTable);
 
         subItemTable = new HitsTable(subItemModel);
         subItemScroll = new JScrollPane(subItemTable);

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/ViewerController.java
@@ -73,12 +73,12 @@ public class ViewerController {
     private boolean isFixed;
     private final Object lock = new Object();
 
-    public ViewerController(HitsTable hitsTable, final String codePath) {
+    public ViewerController(final String codePath) {
         Window owner = App.get();
         
         // These viewers will have their own docking frame
         viewers.add(new HexViewerPlus(owner, new HexSearcherImpl()));
-        viewers.add(textViewer = new TextViewer(hitsTable));
+        viewers.add(textViewer = new TextViewer());
         viewers.add(new MetadataViewer() {
             @Override
             public boolean isFixed() {
@@ -153,6 +153,10 @@ public class ViewerController {
 
     public List<AbstractViewer> getViewers() {
         return Collections.unmodifiableList(viewers);
+    }
+
+    public void setHitsTableInTextViewer(HitsTable hitsTable) {
+        textViewer.setHitsTable(hitsTable);
     }
 
     public ATextViewer getTextViewer() {

iped-app/src/main/java/dpf/sp/gpinf/indexer/ui/fileViewer/frames/TextViewer.java
@@ -8,8 +8,8 @@ import iped3.io.IStreamSource;
 
 public class TextViewer extends ATextViewer {
 
-    public TextViewer(HitsTable hitsTable) {
-        super(hitsTable);
+    public TextViewer() {
+        super();
     }
 
     @Override

iped-viewers/iped-viewers-impl/src/main/java/dpf/sp/gpinf/indexer/ui/fileViewer/frames/ATextViewer.java
@@ -59,9 +59,8 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
     protected HitsTable hitsTable;
     private AbstractTableModel hitsModel;
 
-    public ATextViewer(HitsTable hitsTable) {
+    public ATextViewer() {
         super(new GridLayout());
-        this.hitsTable = hitsTable;
         textViewerModel = new TextViewerModel();
         textTable = new JTable(textViewerModel) {
             private static final long serialVersionUID = -5129153322350459095L;
@@ -90,6 +89,10 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
         textTable.getTableHeader().setPreferredSize(new Dimension(0, 0));
         textTable.addKeyListener(this);
         this.getPanel().add(viewerScroll);
+    }
+
+    public void setHitsTable(HitsTable hitsTable) {
+        this.hitsTable = hitsTable;
     }
 
     public void setHitsModel(AbstractTableModel hitsModel) {
@@ -279,7 +282,9 @@ public abstract class ATextViewer extends AbstractViewer implements KeyListener 
 
     @Override
     public void scrollToNextHit(boolean forward) {
-
+        if (hitsTable == null) {
+            return;
+        }
         currentHit = hitsTable.getSelectedRow();
         totalHits = textParser.getHits().size();
         if (forward) {