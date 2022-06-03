sepinf-inc/IPED

commit db3d10c6b6101d99fcd991fae52f5e0c50df0830

#925: fix item list to use index id order before bookmarking duplicates

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/BookmarksManager.java
@@ -506,24 +506,24 @@ public class BookmarksManager implements ActionListener, ListSelectionListener, 
     }
 
     private ArrayList<IItemId> getUniqueSelectedIds() {
-        ArrayList<IItemId> uniqueSelectedIds = new ArrayList<IItemId>();
-
+        final ArrayList<IItemId> uniqueSelectedIds = new ArrayList<IItemId>();
+        final App app = App.get();
         if (checked.isSelected()) {
             for (IPEDSource source : App.get().appCase.getAtomicSources()) {
-                for (int id = 0; id <= source.getLastId(); id++) {
-                    if (source.getBookmarks().isChecked(id)) {
-                        uniqueSelectedIds.add(new ItemId(source.getSourceId(), id));
+                for (IItemId itemId : app.ipedResult.getIterator()) {
+                    if(app.appCase.getMultiBookmarks().isChecked(itemId)) {
+                        uniqueSelectedIds.add(itemId);
                     }
                 }
             }
-
         } else if (highlighted.isSelected()) {
-            App app = App.get();
-            for (Integer row : App.get().resultsTable.getSelectedRows()) {
-                int rowModel = App.get().resultsTable.convertRowIndexToModel(row);
-                IItemId id = app.ipedResult.getItem(rowModel);
-                uniqueSelectedIds.add(id);
+            BitSet bitSet = new BitSet();
+            for (int row : app.resultsTable.getSelectedRows()) {
+                int rowModel = app.resultsTable.convertRowIndexToModel(row);
+                bitSet.set(rowModel);
             }
+            // we must add items in index order
+            bitSet.stream().forEach(rowModel -> uniqueSelectedIds.add(app.ipedResult.getItem(rowModel)));
         }
 
         return uniqueSelectedIds;

commit 6e0df33b897b0812491bc5f6b0a51584dc676ad0

fix #925: fix bookmarking checked items regression introduced by db3d10c

iped-app/src/main/java/dpf/sp/gpinf/indexer/desktop/BookmarksManager.java
@@ -509,10 +509,18 @@ public class BookmarksManager implements ActionListener, ListSelectionListener, 
         final ArrayList<IItemId> uniqueSelectedIds = new ArrayList<IItemId>();
         final App app = App.get();
         if (checked.isSelected()) {
-            for (IItemId itemId : app.ipedResult.getIterator()) {
-                if (app.appCase.getMultiBookmarks().isChecked(itemId)) {
-                    uniqueSelectedIds.add(itemId);
+            int sourceId = 0;
+            for (IPEDSource source : app.appCase.getAtomicSources()) {
+                BitSet ids = new BitSet();
+                // we must add items in index order
+                for (int luceneId = 0; luceneId < source.getAtomicReader().maxDoc(); luceneId++) {
+                    int id = source.getId(luceneId);
+                    if (source.getBookmarks().isChecked(id) && !ids.get(id)) {
+                        uniqueSelectedIds.add(new ItemId(sourceId, id));
+                        ids.set(id);
+                    }
                 }
+                sourceId++;
             }
         } else if (highlighted.isSelected()) {
             BitSet bitSet = new BitSet();