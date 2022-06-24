5adf7a10379a2397efd76ec5ef2ca86f3f0fdf6d
Luis Nassif <lfcnassif@gmail.com>
2022-06-24 18:55:39 -0300

    -#1179: change the default value to validate SSL certificates

--- iped-engine/src/main/java/iped/engine/util/SSLFix.java
+++ iped-engine/src/main/java/iped/engine/util/SSLFix.java
@@ -5,6 +5,9 @@ import javax.net.ssl.SSLContext;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
 
+/**
+ * To be used just for testing purposes not in production!
+ */
 public class SSLFix {
     public static SSLContext getUnsecureSSLContext() {
         try {
