0e404c7f881c60d6e3b526e0f4f64453e84ac13b
tc-wleite <wladimir.leite@gmail.com>
2022-08-10 18:58:40 -0300

    '#1202: Add a quality-related rendering hint to ImageViewPanel.

--- iped-viewers/iped-viewers-impl/src/main/java/iped/viewers/components/ImageViewPanel.java
+++ iped-viewers/iped-viewers-impl/src/main/java/iped/viewers/components/ImageViewPanel.java
@@ -91,6 +91,7 @@ public class ImageViewPanel extends JPanel {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
                 if (image != null) {
                     int w = (int) Math.ceil(image.getWidth() * zoomFactor);
                     int h = (int) Math.ceil(image.getHeight() * zoomFactor);
