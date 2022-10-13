2d5be4251aea0d4872f1a634713a678d714533b8
patrick.pdb <patrick.dalla@gmail.com>
2022-10-13 13:42:42 -0400

    Exposes caseData as context variable to parsers implementation.

--- iped-engine/src/main/java/iped/engine/task/ParsingTask.java
+++ iped-engine/src/main/java/iped/engine/task/ParsingTask.java
@@ -52,6 +52,7 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 import iped.configuration.Configurable;
+import iped.data.ICaseData;
 import iped.data.IItem;
 import iped.data.IItemReader;
 import iped.engine.config.CategoryToExpandConfig;
@@ -210,6 +211,7 @@ public class ParsingTask extends ThumbTask implements EmbeddedDocumentExtractor 
         // DEFINE CONTEXTO: PARSING RECURSIVO, ETC
         context = new ParseContext();
         context.set(Parser.class, this.autoParser);
+        context.set(ICaseData.class, caseData);
 
         ItemInfo itemInfo = ItemInfoFactory.getItemInfo(evidence);
         context.set(ItemInfo.class, itemInfo);
