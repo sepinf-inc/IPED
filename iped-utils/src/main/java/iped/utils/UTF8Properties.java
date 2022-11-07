f22ff5ac418ae076d3639b3b8cd7643e4e7b81af
patrick.pdb <patrick.dalla@gmail.com>
2022-11-07 11:12:26 -0300

    Make property to indicate if the properties to be loaded should have the values concatenated if the occur more than once on the file.

--- iped-utils/src/main/java/iped/utils/UTF8Properties.java
+++ iped-utils/src/main/java/iped/utils/UTF8Properties.java
@@ -21,6 +21,9 @@ import java.util.Properties;
 public class UTF8Properties extends Properties {
 
     private static final long serialVersionUID = -8198271272010610933L;
+    
+    boolean cumulative = false;
+    String cumulativeSeparator=";";
 
     public synchronized void load(File file) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8")); //$NON-NLS-1$
@@ -34,9 +37,17 @@ public class UTF8Properties extends Properties {
                 pos = str.indexOf('=', pos + 1);
             }
             if (pos > 0) {
-                String key = str.substring(0, pos).replace("\\=", "=").trim(); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-                String val = str.substring(pos + 1).replace("\\=", "=").trim(); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-                super.put(key, val);
+                String key = str.substring(0, pos).replace("\\=", "=").replace("\\:", ":").trim(); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+                String val = str.substring(pos + 1).replace("\\=", "=").replace("\\:", ":").trim(); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+                if(isCumulative()) {
+                    String valant = super.getProperty(key);
+                    if(valant!=null) {
+                        val = valant + cumulativeSeparator + val;
+                    }
+                    super.put(key, val);
+                }else {
+                    super.put(key, val);
+                }
             }
         }
         in.close();
@@ -51,5 +62,21 @@ public class UTF8Properties extends Properties {
             writer.write("\r\n"); //$NON-NLS-1$
         }
         writer.close();
+    }
+
+    public boolean isCumulative() {
+        return cumulative;
+    }
+
+    public void setCumulative(boolean cumulative) {
+        this.cumulative = cumulative;
+    }
+
+    public String getCumulativeSeparator() {
+        return cumulativeSeparator;
+    }
+
+    public void setCumulativeSeparator(String cumulativeSeparator) {
+        this.cumulativeSeparator = cumulativeSeparator;
     }
 }
