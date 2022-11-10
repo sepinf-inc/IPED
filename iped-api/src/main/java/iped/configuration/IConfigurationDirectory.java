a4be1419da0eab0374c6736df1d5e3c376684dcb
patrick.pdb <patrick.dalla@gmail.com>
2022-11-10 08:50:01 -0300

    Add a name to a configuration directory.

--- iped-api/src/main/java/iped/configuration/IConfigurationDirectory.java
+++ iped-api/src/main/java/iped/configuration/IConfigurationDirectory.java
@@ -8,14 +8,12 @@ import java.util.function.Predicate;
 public interface IConfigurationDirectory {
 
     public static final String IPED_CONF_PATH = "iped.configPath";
-
     public static final String IPED_ROOT = "iped.root";
-
     public static final String IPED_APP_ROOT = "iped.app.root";
 
     public List<Path> getResourceLookupFolders();
-
     public List<Path> lookUpResource(Predicate<Path> predicate) throws IOException;
-
     public List<Path> lookUpResource(Configurable<?> configurable) throws IOException;
+
+    public String getName();
 }
