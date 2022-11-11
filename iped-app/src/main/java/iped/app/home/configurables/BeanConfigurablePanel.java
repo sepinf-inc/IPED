002121d596dae27c59a745ba078c21b5989e66fd
patrick.pdb <patrick.dalla@gmail.com>
2022-11-11 09:58:36 -0300

    Bean adaptation for Pair objects (start).

--- iped-app/src/main/java/iped/app/home/configurables/BeanConfigurablePanel.java
+++ iped-app/src/main/java/iped/app/home/configurables/BeanConfigurablePanel.java
@@ -8,88 +8,97 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentEvent.ElementChange;
-import javax.swing.event.DocumentEvent.EventType;
-import javax.swing.text.Document;
-import javax.swing.text.Element;
-import javax.swing.text.AbstractDocument.DefaultDocumentEvent;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import iped.app.home.MainFrame;
 import iped.configuration.Configurable;
 
 public class BeanConfigurablePanel extends ConfigurablePanel{
+    private ArrayList<Component> comps;
+    private JLabel largestLabel;
+    private JLabel lastLabel;
+    private int max;
+
     protected BeanConfigurablePanel(Configurable<?> configurable, MainFrame mainFrame) {
         super(configurable, mainFrame);
     }
 
+    public void createBeanGUI(Object config) throws IntrospectionException {
+        property:for (PropertyDescriptor pd : Introspector.getBeanInfo(config.getClass()).getPropertyDescriptors()) {
+            if(pd.getName().equals("class")) {
+                continue property;
+            }
+            JLabel label = new JLabel(pd.getDisplayName()+":");
+            int width = pd.getDisplayName().length();
+            if(max<width) {
+                max=width;
+                largestLabel=label;
+            }
+            comps.add(label);
+            if(lastLabel!=null) {
+                layout.putConstraint(SpringLayout.NORTH, label, 15, SpringLayout.SOUTH, lastLabel);
+            }
+
+            Object o=null;
+            try {
+                o = pd.getReadMethod().invoke(config);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            JComponent uiField = null;
+            if(o!=null) {
+                if(o instanceof Boolean) {
+                    JCheckBox checkField = new JCheckBox();
+                    checkField.setText(o.toString());
+                    checkField.addChangeListener(e->{
+                        changed=true;
+                    });
+                    uiField = checkField;
+                }else {
+                    JTextField textField = new JTextField();
+                    textField.setText(o.toString());
+                    textField.getDocument().addDocumentListener(this);
+                    uiField = textField;
+                }
+            }
+
+            layout.putConstraint(SpringLayout.VERTICAL_CENTER, uiField, 0, SpringLayout.VERTICAL_CENTER, label);
+            comps.add(uiField);
+            lastLabel=label;
+        }
+        for (Iterator iterator = comps.iterator(); iterator.hasNext();) {
+            Component component = (Component) iterator.next();
+            if(component!=largestLabel) {
+                if(component instanceof JLabel) {
+                    layout.putConstraint(SpringLayout.EAST, component, 0, SpringLayout.EAST, largestLabel);
+                }else {
+                    layout.putConstraint(SpringLayout.WEST, component, 5,SpringLayout.EAST, largestLabel);
+                }
+            }
+            this.add(component);
+        }
+    }
+
     public void createConfigurableGUI() {
         try {
-            Component lastLabel = null;
-            ArrayList<Component> comps = new ArrayList<Component>();
-            JLabel largestLabel = null;
-            int max=0;
+            lastLabel = null;
+            comps = new ArrayList<Component>();
+            largestLabel = null;
+            max=0;
 
             Object config = configurable.getConfiguration();
             if(config!=null) {
-                property:for (PropertyDescriptor pd : Introspector.getBeanInfo(config.getClass()).getPropertyDescriptors()) {
-                    if(pd.getName().equals("class")) {
-                        continue property;
-                    }
-                    JLabel label = new JLabel(pd.getDisplayName()+":");
-                    int width = pd.getDisplayName().length();
-                    if(max<width) {
-                        max=width;
-                        largestLabel=label;
-                    }
-                    comps.add(label);
-                    if(lastLabel!=null) {
-                        layout.putConstraint(SpringLayout.NORTH, label, 15, SpringLayout.SOUTH, lastLabel);
-                    }
-
-                    Object o=null;
-                    try {
-                        o = pd.getReadMethod().invoke(config);
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    
-                    JComponent uiField = null;
-                    if(o!=null) {
-                        if(o instanceof Boolean) {
-                            JCheckBox checkField = new JCheckBox();
-                            checkField.setText(o.toString());
-                            checkField.addChangeListener(e->{
-                                changed=true;
-                            });
-                            uiField = checkField;
-                        }else {
-                            JTextField textField = new JTextField();
-                            textField.setText(o.toString());
-                            textField.getDocument().addDocumentListener(this);
-                            uiField = textField;
-                        }
-                    }
-
-                    layout.putConstraint(SpringLayout.VERTICAL_CENTER, uiField, 0, SpringLayout.VERTICAL_CENTER, label);
-                    comps.add(uiField);
-                    lastLabel=label;
-                }
-                for (Iterator iterator = comps.iterator(); iterator.hasNext();) {
-                    Component component = (Component) iterator.next();
-                    if(component!=largestLabel) {
-                        if(component instanceof JLabel) {
-                            layout.putConstraint(SpringLayout.EAST, component, 0, SpringLayout.EAST, largestLabel);
-                        }else {
-                            layout.putConstraint(SpringLayout.WEST, component, 5,SpringLayout.EAST, largestLabel);
-                        }
-                    }
-                    this.add(component);
+                if(config instanceof Pair<?, ?>) {
+                    createBeanGUI(((Pair<?, ?>)config).getLeft());
+                    createBeanGUI(((Pair<?, ?>)config).getRight());
+                }else {
+                    createBeanGUI(config);
                 }
             }else {
                 System.out.print("null");
