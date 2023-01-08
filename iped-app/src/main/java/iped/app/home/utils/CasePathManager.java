83ff81bbccdc135f39fdb0651a436944757323b9
thiago figueiredo <thiagofuer@gmail.com>
2023-01-08 15:30:51 -0400

    -First implementation of a bootstrat to start iped home -Added an iped_home.jar build to POM

--- iped-app/src/main/java/iped/app/home/utils/CasePathManager.java
+++ iped-app/src/main/java/iped/app/home/utils/CasePathManager.java
@@ -1,11 +1,15 @@
 package iped.app.home.utils;
 
 import iped.app.home.MainFrame;
+import iped.io.URLUtil;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /*
  * @created 22/11/2022
@@ -15,15 +19,9 @@ import java.nio.file.Paths;
 public class CasePathManager {
 
     private static CasePathManager casePathManager;
-    private File testPath = new File("");
+    private final File testPath = null; //new File("");
     private File casePath;
     private File libDir;
-
-    private final String CONFIG_FILE = "IPEDConfig.txt";
-    private final String LOCAL_CONFIG = "LocalConfig.txt";
-    private final String CONF_DIR = "conf";
-    private final String PROFILES_DIR = "profiles";
-    private final String CASE_PROFILE_DIR = "profile";
 
     private CasePathManager() {
         try {
@@ -45,17 +43,38 @@ public class CasePathManager {
             casePath = testPath;
             libDir = new File(casePath + "/iped/lib");
         }else {
-            libDir = detectLibDir();
-            casePath = libDir.getParentFile().getParentFile();
+            URL url = URLUtil.getURL(MainFrame.class);
+            String rootPath = new File(url.toURI()).getParent();
+            // test for report generation from case folder
+            if (rootPath.endsWith("iped" + File.separator + "lib")) { //$NON-NLS-1$ //$NON-NLS-2$
+                rootPath = new File(url.toURI()).getParentFile().getParent();
+            }
+            if (rootPath.endsWith("iped")) { //$NON-NLS-1$ //$NON-NLS-2$
+                rootPath = new File(url.toURI()).getParentFile().getParent();
+            }
+            casePath = new File(rootPath);
+            libDir = new File(casePath + "/iped/lib");
         }
 
-
+        System.out.println(new File(casePath, "iped").toPath());
         if (!new File(casePath, "iped").exists()) //$NON-NLS-1$
             casePath = null;
     }
 
+    private static List<String> getSystemProperties() {
+        List<String> props = new ArrayList<>();
+        for (Map.Entry<Object, Object> e : System.getProperties().entrySet()) {
+            String key = e.getKey().toString();
+            if (!key.equals("java.class.path") && !key.equals("sun.boot.library.path")) {
+                props.add("-D" + key + "=" + e.getValue().toString().replace("\"", "\\\""));
+            }
+        }
+        return props;
+    }
+
     private File detectLibDir() throws URISyntaxException {
-        URL url = MainFrame.class.getProtectionDomain().getCodeSource().getLocation();
+        //URL url = MainFrame.class.getProtectionDomain().getCodeSource().getLocation();
+        URL url = URLUtil.getURL(MainFrame.class);
         File jarFile = null;
         if (url.toURI().getAuthority() == null)
             jarFile = new File(url.toURI());
@@ -78,6 +97,7 @@ public class CasePathManager {
     }
 
     public File getLocalConfigFile(){
+        String LOCAL_CONFIG = "LocalConfig.txt";
         return Paths.get(casePath.getAbsolutePath(), "/iped", LOCAL_CONFIG).toFile();
     }
 
