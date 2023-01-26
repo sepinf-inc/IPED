e0ccb9cd3fffd9cdadab171a2bdc8d643ce3d06c
tc-wleite <wladimir.leite@gmail.com>
2023-01-26 09:33:35 -0300

    '#1474: Support displaying BufferedImage in QualityIcon class.

--- iped-utils/src/main/java/iped/utils/QualityIcon.java
+++ iped-utils/src/main/java/iped/utils/QualityIcon.java
@@ -4,35 +4,48 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class QualityIcon implements Icon {
     private final ImageIcon icon;
-    private int size;
+    private final BufferedImage img;
+    private int w, h;
 
     public QualityIcon(ImageIcon icon, int size) {
         this.icon = icon;
-        this.size = size;
+        this.img = null;
+        w = h = size;
+    }
+
+    public QualityIcon(BufferedImage img, int w, int h) {
+        this.icon = null;
+        this.img = img;
+        this.w = w;
+        this.h = h;
     }
 
     public void paintIcon(Component c, Graphics g, int x, int y) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2.drawImage(((ImageIcon) icon).getImage(), x, y, size, size, c);
+        if (img == null)
+            g2.drawImage(((ImageIcon) icon).getImage(), x, y, w, h, c);
+        else 
+            g2.drawImage(img, x, y, w, h, c);
     }
 
     public int getIconWidth() {
-        return size;
+        return w;
     }
 
     public int getIconHeight() {
-        return size;
+        return h;
     }
 
     public void setSize(int size) {
-        this.size = size;
+        w = h = size;
     }
 }
