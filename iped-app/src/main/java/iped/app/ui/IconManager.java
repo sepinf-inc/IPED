c0d91561d19c275726b295f16213ca01205b9b7c
patrick.pdb <patrick.dalla@gmail.com>
2023-02-15 16:08:37 -0300

    Load icons if src location is a class path (for example, when debbuging from eclipse).

--- iped-app/src/main/java/iped/app/ui/IconManager.java
+++ iped-app/src/main/java/iped/app/ui/IconManager.java
@@ -19,9 +19,11 @@
 package iped.app.ui;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.net.URL;
 import java.security.CodeSource;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -63,21 +65,35 @@ public class IconManager {
             CodeSource src = IconManager.class.getProtectionDomain().getCodeSource();
             if (src != null) {
                 URL jar = src.getLocation();
-                try (ZipInputStream zip = new ZipInputStream(jar.openStream())) {
-                    while (true) {
-                        ZipEntry e = zip.getNextEntry();
-                        if (e == null) {
-                            break;
-                        }
-                        String path = IconManager.class.getName().toString().replace(".", separator)
-                                .replace(IconManager.class.getSimpleName(), "") + iconPath + separator;
-                        String nameWithPath = e.getName();
-                        String name = nameWithPath.replace(path, "");
-                        if (nameWithPath.startsWith(path) && name.toLowerCase().endsWith(ICON_EXTENSION)) {
-                            BufferedImage img = ImageIO
-                                    .read(IconManager.class.getResource(iconPath + separator + name));
-                            map.put(name.replace(ICON_EXTENSION, "").toLowerCase(),
+                File f = new File(jar.getPath());
+                if(f.isDirectory()) {
+                    File iconRoot = new File(f, IconManager.class.getName().toString().replace(".", separator)
+                            .replace(IconManager.class.getSimpleName(), "") + iconPath + separator);
+                    File[] icons = iconRoot.listFiles();
+                    for (int i = 0; i < icons.length; i++) {
+                        if(icons[i].getName().endsWith(ICON_EXTENSION)) {
+                            BufferedImage img = ImageIO.read(icons[i]);
+                            map.put(icons[i].getName().replace(ICON_EXTENSION, "").toLowerCase(),
                                     new QualityIcon(new ImageIcon(img), size));
+                        }
+                    }
+                }else {
+                    try (ZipInputStream zip = new ZipInputStream(jar.openStream())) {
+                        while (true) {
+                            ZipEntry e = zip.getNextEntry();
+                            if (e == null) {
+                                break;
+                            }
+                            String path = IconManager.class.getName().toString().replace(".", separator)
+                                    .replace(IconManager.class.getSimpleName(), "") + iconPath + separator;
+                            String nameWithPath = e.getName();
+                            String name = nameWithPath.replace(path, "");
+                            if (nameWithPath.startsWith(path) && name.toLowerCase().endsWith(ICON_EXTENSION)) {
+                                BufferedImage img = ImageIO
+                                        .read(IconManager.class.getResource(iconPath + separator + name));
+                                map.put(name.replace(ICON_EXTENSION, "").toLowerCase(),
+                                        new QualityIcon(new ImageIcon(img), size));
+                            }
                         }
                     }
                 }
@@ -428,6 +444,7 @@ public class IconManager {
 
         icon = availableIconsMap.get("video");
         if (icon != null) {
+            mimeIconMap.put("video", icon);
             mimeIconMap.put("video/quicktime", icon);
             mimeIconMap.put("video/ts", icon);
             mimeIconMap.put("video/webm", icon);
