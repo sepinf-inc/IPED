ae61da64dcdadd3b2c4cf4d22d5af31b26181e9d
Luis Nassif <lfcnassif@gmail.com>
2023-03-01 15:59:31 -0300

    '#1556: dont repaint if multiSetting, use updateUISelection in some tabs

--- iped-app/src/main/java/iped/app/ui/ReferencedByTableModel.java
+++ iped-app/src/main/java/iped/app/ui/ReferencedByTableModel.java
@@ -100,7 +100,7 @@ public class ReferencedByTableModel extends AbstractTableModel
     public void setValueAt(Object value, int row, int col) {
         App.get().appCase.getMultiBookmarks().setChecked((Boolean) value,
                 App.get().appCase.getItemId(results.getLuceneIds()[row]));
-        BookmarksController.get().updateUI();
+        BookmarksController.get().updateUISelection();
     }
 
     @Override
