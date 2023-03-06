5ab6e2b1e682a75ff8a6eb04747c0dc286a7a640
tc-wleite <wladimir.leite@gmail.com>
2023-03-05 21:30:44 -0300

    '#1551: Remove unused imports.

--- iped-utils/src/main/java/iped/utils/IconUtil.java
+++ iped-utils/src/main/java/iped/utils/IconUtil.java
@@ -1,9 +1,5 @@
 package iped.utils;
 
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.ArrayList;
