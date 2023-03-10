71ddc158c30e5bfdc64cdc47d80ce8f9c0f611d3
patrick.pdb <patrick.dalla@gmail.com>
2023-03-10 06:24:51 -0400

    Adding dependency to POM and removing unused dependency from IFilter.

--- iped-viewers/iped-viewers-api/src/main/java/iped/viewers/api/IFilter.java
+++ iped-viewers/iped-viewers-api/src/main/java/iped/viewers/api/IFilter.java
@@ -2,8 +2,6 @@ package iped.viewers.api;
 
 import java.util.Map;
 
-import com.zaxxer.sparsebits.SparseBitSet;
-
 /**
  * Represents an individual filter to be applied in a resultset
  * @author patrick.pdb
