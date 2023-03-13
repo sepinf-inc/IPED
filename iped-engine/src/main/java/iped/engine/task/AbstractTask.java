3d83348cb536ca5c53b318146d6dbeab9e1d612a
Luis Nassif <lfcnassif@gmail.com>
2023-03-13 16:15:43 -0300

    Revert "'#1584: don't try to del text cache when re-enqueuing items"
    This reverts commit 50ce7c4792446d0c18ff053e1bbcbaa520473db9.

--- iped-engine/src/main/java/iped/engine/task/AbstractTask.java
+++ iped-engine/src/main/java/iped/engine/task/AbstractTask.java
@@ -16,7 +16,6 @@ import iped.engine.core.Statistics;
 import iped.engine.core.Worker;
 import iped.engine.core.Worker.STATE;
 import iped.engine.data.CaseData;
-import iped.engine.data.Item;
 import iped.engine.io.TimeoutException;
 import iped.parsers.util.CorruptedCarvedException;
 
@@ -255,9 +254,7 @@ public abstract class AbstractTask {
     }
 
     private void reEnqueueItem(IItem item, int queue) throws InterruptedException {
-        if (item instanceof Item) {
-            ((Item) item).dispose(false);
-        }
+        item.dispose();
         SkipCommitedTask.checkAgainLaterProcessedParents(item);
         worker.manager.getProcessingQueues().addItemToQueue(item, queue);
         if (!item.isQueueEnd()) {
