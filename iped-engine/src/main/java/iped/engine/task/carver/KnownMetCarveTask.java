3990af3d5cfbdd6654cf2415cdfa77ffa25daf76
hugohmk <hugohmkling@gmail.com>
2023-04-15 14:47:45 -0300

    Carve part.met files in KnownMetCarveTask
    Updated KnownMetCarveTask to also work with part.met files

--- iped-engine/src/main/java/iped/engine/task/carver/KnownMetCarveTask.java
+++ iped-engine/src/main/java/iped/engine/task/carver/KnownMetCarveTask.java
@@ -74,6 +74,12 @@ public class KnownMetCarveTask extends BaseCarveTask {
      */
 
     private static final MediaType eMuleMediaType = MediaType.application("x-emule"); //$NON-NLS-1$
+    
+    /**
+     * Media type dos arquivos part.met recuperados.
+     */
+
+    private static final MediaType eMulePartMetMediaType = MediaType.application("x-emule-part-met"); //$NON-NLS-1$
 
     /**
      * Passo para verificação do início do arquivo.
@@ -141,6 +147,7 @@ public class KnownMetCarveTask extends BaseCarveTask {
         // Percorre conteúdo buscando padrões plausíveis de arquivos known.met
         byte[] bb = new byte[1];
         byte[] buf = new byte[step - 1];
+        byte[] buf2 = new byte[1 << 20];
         BufferedInputStream is = null;
         long offset = 0;
         try {
@@ -182,6 +189,39 @@ public class KnownMetCarveTask extends BaseCarveTask {
                             }
                         }
                     }
+                } else if (read == -32 || read == -31 || read == -30) {
+                	is.read(buf);
+                	long date = toInt(buf, 0) * 1000L;
+                	if (date > dateMin && date < dateMax) {
+                		int pos = 20;
+                		int numParts = toSmall(buf, pos);
+                		int numTags = 2;
+                		pos += 2;
+                		pos += 16 * numParts;
+                		if (pos < 500) {
+                			numTags = toInt(buf, pos);
+                		}
+                		if (numTags >= 0 && numTags <= 1024 && numParts <= 4096 && numParts >= 0) {
+                			BufferedInputStream inParse = null;
+                			try {
+                				inParse = evidence.getBufferedInputStream();
+                				inParse.skip(offset);
+                				int bytesRead = inParse.read(buf2);
+                				if (bytesRead > 25) {
+                					KnownMetEntry entry = new KnownMetEntry();
+                					int len = KnownMetDecoder.parseEntry(entry, 1, buf2);
+                					if (len > 0) {
+                						addCarvedFile(evidence, offset, len+1, "Carved-" + offset + "-part.met", //$NON-NLS-1$ //$NON-NLS-2$
+	                        					eMulePartMetMediaType);
+                						numCarvedItems.incrementAndGet();
+                					}
+                				}
+                			} catch (Exception e) {
+                			} finally {
+                				IOUtil.closeQuietly(inParse);
+	                        }
+                        }
+                	}
                 } else {
                     long skip = 0;
                     do {
