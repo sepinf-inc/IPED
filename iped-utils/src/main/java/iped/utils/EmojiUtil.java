1465def4397906ddf32a8b6a042e7c04788e14f3
tc-wleite <wladimir.leite@gmail.com>
2023-04-20 10:45:44 -0300

    '#1655: Handle emojis that use modifier sequences (e.g. skin tone).

--- iped-utils/src/main/java/iped/utils/EmojiUtil.java
+++ iped-utils/src/main/java/iped/utils/EmojiUtil.java
@@ -19,101 +19,118 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class EmojiUtil {
-    private static final Map<Integer, String> base64ImagesPerCode = new HashMap<Integer, String>();
+    private static final Map<String, String> base64ImagesPerCode = new HashMap<String, String>();
     private static final AtomicBoolean init = new AtomicBoolean();
 
     public static byte[] replaceByImages(byte[] inBytes) {
+        synchronized (init) {
+            if (!init.get()) {
+                init();
+                init.set(true);
+            }
+        }
         BufferedReader in = null;
         try {
             in = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(inBytes), StandardCharsets.UTF_8));
             in.mark(inBytes.length);
             String s = null;
-            Set<Integer> found = new HashSet<Integer>();
+            Set<String> usedEmojis = new HashSet<String>();
+            Map<Integer, String> replacedLines = new HashMap<Integer, String>();
+            int idx = 0;
             while ((s = in.readLine()) != null) {
-                for (int i = 0; i < s.length() - 1; i++) {
+                final int len = s.length();
+                boolean found = false;
+                for (int i = 0; i < len;) {
                     int c = s.codePointAt(i);
-                    if (c > 0x2000) {
-                        found.add(c);
+                    if (c > 0x2000 && base64ImagesPerCode.containsKey(format(c))) {
+                        found = true;
+                        break;
                     }
+                    i += Character.charCount(c);
                 }
-            }
-            if (!found.isEmpty()) {
-                synchronized (init) {
-                    if (!init.get()) {
-                        init();
-                        init.set(true);
-                    }
-                }
-                found.retainAll(base64ImagesPerCode.keySet());
-                if (!found.isEmpty()) {
-                    in.reset();
-                    ByteArrayOutputStream outBytes = new ByteArrayOutputStream(inBytes.length + inBytes.length / 4);
-                    PrintWriter out = new PrintWriter(new OutputStreamWriter(outBytes, StandardCharsets.UTF_8));
-                    boolean styleFound = false;
-                    boolean bodyFound = false;
-                    while ((s = in.readLine()) != null) {
-                        if (!styleFound) {
-                            out.println(s);
-                            if (s.indexOf("<style>") >= 0) {
-                                styleFound = true;
-                                for (int c : found) {
-                                    out.print("img.e");
-                                    out.print(Integer.toHexString(c));
-                                    out.print("{width:18px;height:18px;margin-right:1px;margin-left:1px;");
-                                    out.print("content:url('data:image/png;base64,");
-                                    out.print(base64ImagesPerCode.get(c));
-                                    out.println("');}");
-                                }
-                            }
-                        } else if (!bodyFound) {
-                            out.println(s);
-                            if (s.indexOf("<body>") >= 0) {
-                                bodyFound = true;
-                            }
-                        } else {
-                            boolean hasEmoji = false;
-                            if (s.length() > 1) {
-                                for (int i = 0; i < s.length() - 1; i++) {
-                                    int c = s.codePointAt(i);
-                                    if (found.contains(c)) {
-                                        hasEmoji = true;
-                                        break;
+                if (found) {
+                    StringBuilder line = new StringBuilder();
+                    StringBuilder sb = new StringBuilder();
+                    String key = "";
+                    for (int i = 0; i < len;) {
+                        int c = s.codePointAt(i);
+                        if (c > 0x2000) {
+                            sb.delete(0, sb.length());
+                            sb.append(format(c));
+                            if (base64ImagesPerCode.containsKey(sb.toString())) {
+                                key = sb.toString();
+                                i += Character.charCount(c);
+                                int skip = i;
+                                int j = i;
+                                int cnt = 0;
+                                while (j < len && ++cnt <= 5) {
+                                    int next = s.codePointAt(j);
+                                    j += Character.charCount(next);
+                                    sb.append('_').append(format(next));
+                                    if (base64ImagesPerCode.containsKey(sb.toString())) {
+                                        key = sb.toString();
+                                        skip = j;
                                     }
                                 }
-                            }
-                            if (hasEmoji) {
-                                for (int i = 0; i < s.length(); i++) {
-                                    int c = s.codePointAt(i);
-                                    if (found.contains(c)) {
-                                        out.print("<img class=\"e");
-                                        out.print(Integer.toHexString(c));
-                                        out.print("\"/>");
-                                        if (Character.isHighSurrogate(s.charAt(i))) {
-                                            i++;
-                                        }
-                                    } else {
-                                        out.print(s.charAt(i));
-                                    }
-                                }
-                                out.println();
-                            } else {
-                                out.println(s);
+                                line.append("<img class=\"e").append(key).append("\"/>");
+                                usedEmojis.add(key);
+                                i = skip;
+                                continue;
                             }
                         }
+                        line.append(s.charAt(i));
+                        i++;
                     }
-                    out.flush();
-                    return outBytes.toByteArray();
+                    replacedLines.put(idx, line.toString());
                 }
+                idx++;
+            }
+            if (!replacedLines.isEmpty()) {
+                in.reset();
+                ByteArrayOutputStream outBytes = new ByteArrayOutputStream(inBytes.length + inBytes.length / 4);
+                PrintWriter out = new PrintWriter(new OutputStreamWriter(outBytes, StandardCharsets.UTF_8));
+                boolean styleFound = false;
+                boolean bodyFound = false;
+                idx = 0;
+                while ((s = in.readLine()) != null) {
+                    if (!styleFound) {
+                        out.println(s);
+                        if (s.indexOf("<style>") >= 0) {
+                            styleFound = true;
+                            for (String c : usedEmojis) {
+                                out.print("img.e");
+                                out.print(c);
+                                out.print("{width:18px;");
+                                out.print("height:18px;margin-right:1px;margin-left:1px;");
+                                out.print("content:url('data:image/png;base64,");
+                                out.print(base64ImagesPerCode.get(c));
+                                out.println("');}");
+                            }
+                        }
+                    } else if (!bodyFound) {
+                        out.println(s);
+                        if (s.indexOf("<body>") >= 0) {
+                            bodyFound = true;
+                        }
+                    } else {
+                        String line = replacedLines.get(idx);
+                        out.println(line == null ? s : line);
+                    }
+                    idx++;
+                }
+                out.flush();
+                return outBytes.toByteArray();
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                in.close();
-            } catch (Exception e) {
-            }
+            IOUtil.closeQuietly(in);
         }
         return inBytes;
+    }
+
+    private static final String format(int c) {
+        return String.format("%04x", c);
     }
 
     private static final void init() {
@@ -128,12 +145,15 @@ public class EmojiUtil {
                         if (e == null) {
                             break;
                         }
-                        if (e.getName().startsWith(path) && e.getName().endsWith(".png")) {
-                            byte[] bytes = EmojiUtil.class.getResourceAsStream("/" + e.getName()).readAllBytes();
-                            String base64 = Base64.getEncoder().encodeToString(bytes);
-                            int code = Integer.parseInt(e.getName().substring(path.length(), e.getName().length() - 4),
-                                    16);
-                            base64ImagesPerCode.put(code, base64);
+                        String name = e.getName();
+                        if (name.startsWith(path)) {
+                            int pos = name.indexOf(".png");
+                            if (pos > 0) {
+                                String code = name.substring(path.length(), pos);
+                                byte[] bytes = EmojiUtil.class.getResourceAsStream("/" + name).readAllBytes();
+                                String base64 = Base64.getEncoder().encodeToString(bytes);
+                                base64ImagesPerCode.put(code, base64);
+                            }
                         }
                     }
                 }
