0071de311ee5bf999e87891367f2aa96e6e312fc
patrick.pdb <patrick.dalla@gmail.com>
2023-05-22 14:15:00 -0400

    '#1341 Correcting last manual merge errors.

--- iped-app/src/main/java/iped/app/home/newcase/tabs/process/TableTaskEnabledCellRenderer.java
+++ iped-app/src/main/java/iped/app/home/newcase/tabs/process/TableTaskEnabledCellRenderer.java
@@ -2,11 +2,7 @@ package iped.app.home.newcase.tabs.process;
 
 import java.awt.Component;
 
-<<<<<<< HEAD
 import javax.swing.JCheckBox;
-=======
-import javax.swing.JComponent;
->>>>>>> branch '#23_UI_4_config_proc' of https://github.com/thiagofuer/IPED.git
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -25,19 +21,13 @@ public class TableTaskEnabledCellRenderer extends DefaultTableCellRenderer {
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
             int row, int column) {
-        Component comp = null;
         if(column==1) {
             TasksTableModel tm = (TasksTableModel) table.getModel();
             if(tm.isCellEditable(row, column)) {
-<<<<<<< HEAD
                 JCheckBox checkbox = (JCheckBox) wrapped.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
                 checkbox.setEnabled(true);
                 return checkbox;
-=======
-                comp =  wrapped.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
->>>>>>> branch '#23_UI_4_config_proc' of https://github.com/thiagofuer/IPED.git
             }else {
-<<<<<<< HEAD
                 if(showUnremovableCheckBox){
                     JCheckBox checkbox = (JCheckBox) wrapped.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
                     checkbox.setEnabled(false);
@@ -46,16 +36,9 @@ public class TableTaskEnabledCellRenderer extends DefaultTableCellRenderer {
                 }else{
                     return emptyLabel;
                 }
-=======
-                comp = emptyLabel;
->>>>>>> branch '#23_UI_4_config_proc' of https://github.com/thiagofuer/IPED.git
             }
-        } else {
-            comp = wrapped.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);;
         }
-        ((JComponent) comp).setOpaque(true);
-        comp.setBackground(TableCellRendererUtil.getBackground(table, row, isSelected));
-        return comp;
+        return wrapped.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
     }
 
 }
