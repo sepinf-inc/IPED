c94cf648687f45a328e7b534853429a408d07265
tc-wleite <wladimir.leite@gmail.com>
2023-09-06 09:34:02 -0300

    '#1866: Define some standard bookmark colors.

---
+++ iped-app/src/main/java/iped/app/ui/bookmarks/BookmarkStandardColors.java
@@ -0,0 +1,8 @@
+package iped.app.ui.bookmarks;
+
+import java.awt.Color;
+
+public class BookmarkStandardColors {
+    static final Color defaultColor = new Color(180, 180, 240);
+    static final Color noBookmarksColor = new Color(220, 220, 220);
+}
