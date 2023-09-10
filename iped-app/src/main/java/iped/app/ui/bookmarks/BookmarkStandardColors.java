0d240a13bb8cdd86db595fec3afb5915960abe3b
tc-wleite <wladimir.leite@gmail.com>
2023-09-10 15:49:24 -0300

    '#1866: Add some standard bookmark colors.

--- iped-app/src/main/java/iped/app/ui/bookmarks/BookmarkStandardColors.java
+++ iped-app/src/main/java/iped/app/ui/bookmarks/BookmarkStandardColors.java
@@ -5,4 +5,50 @@ import java.awt.Color;
 public class BookmarkStandardColors {
     static final Color defaultColor = new Color(180, 180, 240);
     static final Color noBookmarksColor = new Color(220, 220, 220);
+
+    static final int numStandardColors = 36; // Do no include last gray colors of colors array
+    static final Color[] colors = new Color[39];
+
+    static {
+        colors[0] = new Color(102, 0, 6);
+        colors[1] = new Color(124, 55, 19);
+        colors[2] = new Color(127, 106, 16);
+        colors[3] = new Color(66, 109, 54);
+        colors[4] = new Color(23, 82, 38);
+        colors[5] = new Color(27, 71, 114);
+        colors[6] = new Color(32, 47, 91);
+        colors[7] = new Color(46, 19, 75);
+        colors[8] = new Color(78, 17, 65);
+        colors[9] = new Color(207, 0, 15);
+        colors[10] = new Color(242, 121, 53);
+        colors[11] = new Color(244, 208, 63);
+        colors[12] = new Color(135, 211, 124);
+        colors[13] = new Color(38, 166, 91);
+        colors[14] = new Color(52, 152, 219);
+        colors[15] = new Color(68, 108, 179);
+        colors[16] = new Color(102, 51, 152);
+        colors[17] = new Color(131, 29, 109);
+        colors[18] = new Color(215, 80, 91);
+        colors[19] = new Color(242, 159, 111);
+        colors[20] = new Color(248, 227, 140);
+        colors[21] = new Color(171, 225, 157);
+        colors[22] = new Color(110, 193, 137);
+        colors[23] = new Color(118, 179, 231);
+        colors[24] = new Color(126, 149, 201);
+        colors[25] = new Color(145, 110, 184);
+        colors[26] = new Color(181, 120, 167);
+        colors[27] = new Color(245, 204, 207);
+        colors[28] = new Color(252, 228, 215);
+        colors[29] = new Color(253, 246, 217);
+        colors[30] = new Color(231, 246, 229);
+        colors[31] = new Color(212, 237, 222);
+        colors[32] = new Color(214, 234, 248);
+        colors[33] = new Color(218, 226, 240);
+        colors[34] = new Color(243, 239, 248);
+        colors[35] = new Color(230, 210, 226);
+        
+        colors[36] = new Color(100, 100, 100);
+        colors[37] = new Color(160, 160, 160);
+        colors[38] = new Color(220, 220, 220);
+    }
 }
