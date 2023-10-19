ad6b9d3a1a89980bc19de91d0e5506d362ecd0ea
patrick.pdb <patrick.dalla@gmail.com>
2023-10-19 06:30:08 -0400

    '#1598 removes command to refresh chart after all cache creation.

--- iped-app/src/main/java/iped/app/timelinegraph/IpedChartsPanel.java
+++ iped-app/src/main/java/iped/app/timelinegraph/IpedChartsPanel.java
@@ -610,7 +610,6 @@ public class IpedChartsPanel extends JPanel implements ResultSetViewer, TableMod
                         @Override
                         public void run() {
                             ipedTimelineDatasetManager.startBackgroundCacheCreation();
-                            self.refreshChart();
                         }
                     };
                     new Thread(r).start();
