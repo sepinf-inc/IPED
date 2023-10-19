sepinf-inc/IPED

commit 659291d9faeb2ddcc5790ff29ed584caab9c6f53

'#1598: move bitstreams serialized file to timecache folder

iped-app/src/main/java/iped/app/timelinegraph/cache/persistance/CachePersistance.java
@@ -74,11 +74,11 @@ public class CachePersistance {
         startDir = new File(App.get().casesPathFile, "iped");
         startDir = new File(startDir, "data");
 
-        bitstreamSerializeFile = new File(startDir, "bitstreamSerialize");
-        bitstreamSerialize = bitstreamSerializeFile.exists();
-
         startDir = new File(startDir, "timecache");
         startDir.mkdirs();
+
+        bitstreamSerializeFile = new File(startDir, "bitstreamSerialize");
+        bitstreamSerialize = bitstreamSerializeFile.exists();
 
         // if case cache folder is not writable, use user.home for caches
         if (!IOUtil.canWrite(startDir)) {

commit c3c9adecfe174e282e58416aa25c157df9933968

'#1598: fix FileNotFoundException caused by commit 659291d

iped-app/src/main/java/iped/app/timelinegraph/cache/persistance/CachePersistance.java
@@ -132,8 +132,12 @@ public class CachePersistance {
                         try {
                             if (tempCasesDir.listFiles() != null) {
                                 for (File f : tempCasesDir.listFiles()) {
-                                    if (!f.getName().equals(uuid)) {
-                                        try (RandomAccessFile ras = new RandomAccessFile(new File(f, "case.txt"), "r")) {
+                                    if (f.isDirectory() && !f.getName().equals(uuid)) {
+                                        File caseFile = new File(f, "case.txt");
+                                        if (!caseFile.exists()) {
+                                            continue;
+                                        }
+                                        try (RandomAccessFile ras = new RandomAccessFile(caseFile, "r")) {
                                             String casePath = ras.readUTF();
                                             if (casePath != null) {
                                                 File caseDir = new File(casePath);