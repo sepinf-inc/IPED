sepinf-inc/IPED

commit f23f7bc25072fb0309e7587e4fd73f679d074c73

'#1685 merge contact pic instead ignoring, better msg for future support

iped-engine/src/main/java/iped/engine/datasource/UfedXmlReader.java
@@ -41,12 +41,13 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.queryparser.flexible.standard.QueryParserUtil;
 import org.apache.tika.metadata.Message;
 import org.apache.tika.metadata.Property;
 import org.apache.tika.mime.MediaType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.ErrorHandler;
@@ -91,7 +92,9 @@ import iped.utils.SimpleHTMLEncoder;
 
 public class UfedXmlReader extends DataSourceReader {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(UfedXmlReader.class);
+    private static Logger LOGGER = LogManager.getLogger(UfedXmlReader.class);
+
+    private final Level CONSOLE = Level.getLevel("MSG"); //$NON-NLS-1$
 
     private static final String[] HEADER_STRINGS = { "project id", "extractionType", "sourceExtractions" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
@@ -107,6 +110,7 @@ public class UfedXmlReader extends DataSourceReader {
     public static final String UFED_ID = ExtraProperties.UFED_META_PREFIX + "id"; //$NON-NLS-1$
     public static final String UFED_MIME_PREFIX = MediaTypes.UFED_MIME_PREFIX;
     public static final String UFED_EMAIL_MIME = MediaTypes.UFED_EMAIL_MIME.toString();
+    public static final String UFED_CONTACTPHOTO_MIME = UFED_MIME_PREFIX + "contactphoto";
     public static final String MSISDN_PROP = "MSISDN";
 
     private static final String ESCAPED_UFED_ID = QueryParserUtil.escape(UFED_ID);
@@ -775,14 +779,28 @@ public class UfedXmlReader extends DataSourceReader {
                 item.getMetadata().add(ExtraProperties.UFED_META_PREFIX + parentNode.element, chars.toString().trim());
 
             } else if (qName.equals("file")) { //$NON-NLS-1$
-                String trackId = Util.getTrackID(item);
                 itemSeq.remove(itemSeq.size() - 1);
-                if (addedTrackIds.contains(trackId)) {
-                    LOGGER.error("Item with duplicated trakcId {}", trackId);
-                    caseData.incDiscoveredEvidences(-1);
-                } else {
+
+                // See https://github.com/sepinf-inc/IPED/issues/1685
+                boolean merged = false;
+                if (!itemSeq.isEmpty()) {
+                    IItem parentItem = itemSeq.get(itemSeq.size() - 1);
+                    if (UFED_CONTACTPHOTO_MIME.equals(parentItem.getMediaType().getSubtype())) {
+                        String[] split = item.getIdInDataSource().split(UFDRInputStreamFactory.UFDR_PATH_PREFIX);
+                        String exportPath = split[split.length - 1];
+                        parentItem.getMetadata().set(AVATAR_PATH_META, exportPath);
+                        caseData.incDiscoveredEvidences(-1);
+                        merged = true;
+                    }
+                }
+
+                if (!merged) {
                     setMediaResult(item);
-                    addedTrackIds.add(trackId);
+                    String trackId = Util.getTrackID(item);
+                    if (!addedTrackIds.add(trackId)) {
+                        LOGGER.log(CONSOLE, "Unexpected UFDR report.xml structure, item with duplicated track id {}: {}.\nPlease report this to project"
+                                + " developers sending the UFDR report.xml to add proper support for the new structure.", trackId, item.getPath());
+                    }
                     try {
                         Manager.getInstance().addItemToQueue(item);
                     } catch (Exception e) {

commit 37e7f14282b79ac3743f7233b3717edc1b4932bc

'#1685: fix NPE caused by commit f23f7bc

iped-engine/src/main/java/iped/engine/datasource/UfedXmlReader.java
@@ -785,7 +785,7 @@ public class UfedXmlReader extends DataSourceReader {
                 boolean merged = false;
                 if (!itemSeq.isEmpty()) {
                     IItem parentItem = itemSeq.get(itemSeq.size() - 1);
-                    if (UFED_CONTACTPHOTO_MIME.equals(parentItem.getMediaType().getSubtype())) {
+                    if (parentItem.getMediaType() != null && UFED_CONTACTPHOTO_MIME.equals(parentItem.getMediaType().getSubtype())) {
                         String[] split = item.getIdInDataSource().split(UFDRInputStreamFactory.UFDR_PATH_PREFIX);
                         String exportPath = split[split.length - 1];
                         parentItem.getMetadata().set(AVATAR_PATH_META, exportPath);