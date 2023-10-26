e41ff11113987ec2f5ccf633c76d3c78adeaf5ae
tc-wleite <wladimir.leite@gmail.com>
2023-10-26 16:35:43 -0300

    '#1522: Simpler way (no reflection) to get the number of loaded classes.

--- iped-app/src/main/java/iped/app/ui/splash/StartUpControl.java
+++ iped-app/src/main/java/iped/app/ui/splash/StartUpControl.java
@@ -1,20 +1,16 @@
 package iped.app.ui.splash;
 
 import java.io.File;
-import java.lang.reflect.Field;
-import java.util.Vector;
+import java.lang.management.ClassLoadingMXBean;
+import java.lang.management.ManagementFactory;
 
 public class StartUpControl {
     public static final String ipedChildProcessPID = "iped.childProcessPID";
 
     public static int getCurrentProcessSize() {
         try {
-            Field f = ClassLoader.class.getDeclaredField("classes");
-            f.setAccessible(true);
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            @SuppressWarnings("unchecked")
-            Vector<Class<?>> classes = (Vector<Class<?>>) f.get(classLoader);
-            return classes.size();
+            ClassLoadingMXBean cl = ManagementFactory.getClassLoadingMXBean();
+            return (int) cl.getTotalLoadedClassCount();
         } catch (Exception e) {
             e.printStackTrace();
         }
