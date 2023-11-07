bc6f4d7c5caa1a8a6edc70ec7d9aae1c448d3928
patrick.pdb <patrick.dalla@gmail.com>
2023-11-07 10:00:18 -0300

    '#1859 Sync class with old branch to adjust later.

---
+++ iped-parsers/iped-parsers-impl/src/main/java/iped/parsers/discord/cache/ChromeCacheException.java
@@ -0,0 +1,9 @@
+package iped.parsers.discord.cache;
+
+public class ChromeCacheException extends Exception {
+
+    public ChromeCacheException(String message) {
+        super(message);
+    }
+
+}
