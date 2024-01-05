5a39fe7a543e943b2b22bfec96555ab80b9dadf9
patrick.pdb <patrick.dalla@gmail.com>
2024-01-05 15:49:56 -0300

    '#2043 improves implementation to permit more additions.

--- iped-utils/src/main/java/iped/utils/tika/IpedMetadata.java
+++ iped-utils/src/main/java/iped/utils/tika/IpedMetadata.java
@@ -9,19 +9,26 @@ import org.apache.tika.metadata.writefilter.MetadataWriteFilter;
 public class IpedMetadata extends SyncMetadata {
     class IpedMetadataFilter implements MetadataWriteFilter {
         String arr[];
-        int pos = -1;
+        int pos = 0;
 
         @Override
         public void add(String field, String value, Map<String, String[]> data) {
-            if (pos == -1) {
-                // when pos == -1 the method to add an array list was never called
-                set(field, value, data);
-            } else {
-                arr[pos] = value;
-                pos++;
-                if (pos == arr.length) {
+            if (arr == null) {
+                reset();
+            }
+            arr[pos] = value;
+            pos++;
+            if (pos == arr.length) {
+                String[] values = data.get(field);
+                if (values == null) {
                     data.put(field, arr);
+                } else {
+                    String[] merge = new String[values.length + arr.length];
+                    System.arraycopy(values, 0, merge, 0, values.length);
+                    System.arraycopy(arr, 0, merge, values.length, arr.length);
+                    data.put(field, merge);
                 }
+                reset();
             }
         }
 
@@ -45,6 +52,11 @@ public class IpedMetadata extends SyncMetadata {
             arr = new String[count];
             pos = 0;
         }
+
+        public void reset() {
+            arr = new String[1];
+            pos = 0;
+        }
     }
 
     IpedMetadataFilter ipedFilter = new IpedMetadataFilter();
@@ -53,15 +65,22 @@ public class IpedMetadata extends SyncMetadata {
         setMetadataWriteFilter(ipedFilter);
     }
 
-	public void set(String arg0, ArrayList<String> list) {
+    public void set(String arg0, ArrayList<String> list) {
         String[] arr = new String[list.size()];
-		int i=0;
-		for (Iterator iterator = list.iterator(); iterator.hasNext();) {
-			arr[i++]=(String) iterator.next();
-		}
+        int i = 0;
+        for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+            arr[i++] = (String) iterator.next();
+        }
 
         ipedFilter.allocateSpace(list.size());
         super.set(arg0, arr);
-	}
+    }
+
+    public void add(String arg0, ArrayList<String> list) {
+        ipedFilter.allocateSpace(list.size());
+        for (String value : list) {
+            add(arg0, value);
+        }
+    }
 
 }
