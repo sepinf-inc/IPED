5ff79b2a2be0a1456fb183533eedb4b77f53019f
patrick.pdb <patrick.dalla@gmail.com>
2024-01-29 11:31:16 -0300

    '#43 Implements some ALeapp artifacts categorization.

--- iped-app/resources/scripts/tasks/LeappCategoryTask.js
+++ iped-app/resources/scripts/tasks/LeappCategoryTask.js
@@ -29,14 +29,44 @@ function process(e){
 	var path = e.getPath().toLowerCase().replace(/\\/g, "/");
 	
 	var m = e.getMetadata();
-	var pluginName = m.get("ALEAPP:PLUGIN");
-	
-	switch(pluginName){
+    var pluginName = m.get("ALEAPP:PLUGIN");
+    
+    switch(pluginName){
         case "accounts_ce":
+        case "accounts_de":
             e.setCategory("User Accounts");
             break;
+        case "accounts_ce_authtokens":
+            e.setCategory("Passwords");
+            break;
+        case "sim_info":
+            e.setCategory("SIM Data");
+            break;
+        case "Cello":
+            e.setCategory("GDrive File Entries");
+            break;
+        case "roles":
+            e.setCategory("AppRoles");
+            break;
+        case "frosting":
+            e.setCategory("Update information");
+            break;
+        case "FacebookMessenger":
+            if(mime.contains("contacts")){
+                e.setCategory("Contacts");
+            }
+            if(mime.contains("chats")){
+                e.setCategory("Instant Messages");
+            }
+            break;
+        case "settingsSecure":
+            var name = m.get("ALEAPP:Name");
+            var value = m.get("ALEAPP:Value");
+            if((name == "bluetooth_address")||(name == "bluetooth_name")){
+                e.setCategory("Bluetooth Devices");
+            }
+            break;
     }
-	
 	
 }
 
