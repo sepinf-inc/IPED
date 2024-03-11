d25d00bbe3c9e0234c355b70236657cfea0f352b
tc-wleite <wladimir.leite@gmail.com>
2024-03-11 09:59:18 -0300

    '#39: Use theme colors and fix cursor in ResizablePopupMenu.

--- iped-app/src/main/java/iped/app/ui/controls/ResizablePopupMenu.java
+++ iped-app/src/main/java/iped/app/ui/controls/ResizablePopupMenu.java
@@ -6,21 +6,64 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPopupMenu;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-public class ResizablePopupMenu extends JPopupMenu implements MouseListener, MouseMotionListener {
+public class ResizablePopupMenu extends JPopupMenu {
     private static final long serialVersionUID = 4807545296165600782L;
 
+    private Point mouseStart = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
+    private Dimension startSize;
+    private boolean isResizing;
+    private Color c1, c2;
+
     public ResizablePopupMenu() {
-        addMouseListener(this);
-        addMouseMotionListener(this);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                mouseStart = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
+                isResizing = false;
+                updateCursor(e);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseStart = toScreen(e);
+                startSize = getSize();
+                isResizing = isCorner(e.getPoint());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                updateCursor(e);
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                updateCursor(e);
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (isResizing) {
+                    Point p = toScreen(e);
+                    Dimension minDim = getMinimumSize();
+                    Dimension newDim = new Dimension(Math.max(minDim.width, startSize.width + p.x - mouseStart.x),
+                            Math.max(minDim.height, startSize.height + p.y - mouseStart.y));
+                    setPopupSize(newDim);
+                }
+            }
+        });
     }
 
+    @Override
     public void paintChildren(Graphics g) {
         super.paintChildren(g);
         int x = getWidth() - 2;
@@ -30,17 +73,25 @@ public class ResizablePopupMenu extends JPopupMenu implements MouseListener, Mou
         drawDot(g, x - 2, y - 6);
     }
 
-    private void drawDot(Graphics g, int x, int y) {
-        // TODO: Dark mode
-        g.setColor(Color.WHITE);
-        g.fillRect(x, y, 2, 2);
-        g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(x - 1, y - 1, 2, 2);
+    @Override
+    public void updateUI() {
+        super.updateUI();
+        c1 = UIManager.getColor("nimbusLightBackground");
+        if (c1 == null) {
+            c1 = Color.white;
+        }
+        c2 = UIManager.getColor("activeCaption");
+        if (c2 == null) {
+            c2 = Color.lightGray;
+        }
     }
 
-    private Point mouseStart = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
-    private Dimension startSize;
-    private boolean isResizing = false;
+    private void drawDot(Graphics g, int x, int y) {
+        g.setColor(c1);
+        g.fillRect(x, y, 2, 2);
+        g.setColor(c2);
+        g.fillRect(x - 1, y - 1, 2, 2);
+    }
 
     private boolean isCorner(Point point) {
         if (point == null)
@@ -49,50 +100,14 @@ public class ResizablePopupMenu extends JPopupMenu implements MouseListener, Mou
         return resizeSpot.contains(point);
     }
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        setCursor(Cursor.getPredefinedCursor(isCorner(e.getPoint()) ? Cursor.SE_RESIZE_CURSOR : Cursor.DEFAULT_CURSOR));
-    }
-
     private Point toScreen(MouseEvent e) {
         Point p = e.getPoint();
         SwingUtilities.convertPointToScreen(p, e.getComponent());
         return p;
     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        mouseStart = toScreen(e);
-        startSize = getSize();
-        isResizing = isCorner(e.getPoint());
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        mouseStart = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
-        isResizing = false;
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        if (isResizing) {
-            Point p = toScreen(e);
-            Dimension minDim = getMinimumSize();
-            Dimension newDim = new Dimension(Math.max(minDim.width, startSize.width + p.x - mouseStart.x),
-                    Math.max(minDim.height, startSize.height + p.y - mouseStart.y));
-            setPopupSize(newDim);
-        }
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
+    private void updateCursor(MouseEvent e) {
+        setCursor(Cursor.getPredefinedCursor(
+                isCorner(e.getPoint()) || isResizing ? Cursor.SE_RESIZE_CURSOR : Cursor.DEFAULT_CURSOR));
     }
 }
