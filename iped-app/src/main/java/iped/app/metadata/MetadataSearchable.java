baf907412303ffce328b88ac95b5564e66bedd8c
Luis Nassif <lfcnassif@gmail.com>
2024-03-13 18:26:46 -0300

    '#39: removes unused code and class

--- iped-app/src/main/java/iped/app/metadata/MetadataSearchable.java
+++ iped-app/src/main/java/iped/app/metadata/MetadataSearchable.java
@@ -2,7 +2,6 @@ package iped.app.metadata;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 
 import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.NumericDocValues;
@@ -26,13 +25,11 @@ public class MetadataSearchable {
     protected volatile static LeafReader reader;
 
     volatile boolean isCategory = false;
-    private String field;
 
     public MetadataSearchable() {
     }
 
     public MetadataSearchable(String field) throws IOException {
-        this.field = field;
         loadDocValues(field);
     }
 
@@ -53,57 +50,6 @@ public class MetadataSearchable {
 
     public boolean isSingleValuedField() {
         return numValues != null || docValues != null;
-    }
-
-    Iterator empty = new Iterator<String>() {
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        @Override
-        public String next() {
-            return null;
-        }
-    };
-
-    public Iterator<String> getIterator(int doc) {
-        try {
-            loadDocValues(field);
-            boolean adv = docValuesSet.advanceExact(doc);
-
-            if (adv) {
-                return empty;
-            }
-
-            return new Iterator<String>() {
-                long ord = -1;
-
-                @Override
-                public boolean hasNext() {
-                    try {
-                        ord = docValuesSet.nextOrd();
-                        return ord != SortedSetDocValues.NO_MORE_ORDS;
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    return false;
-                }
-
-                @Override
-                public String next() {
-                    try {
-                        return docValuesSet.lookupOrd(ord).utf8ToString();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException(e);
-                    }
-                }
-            };
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return empty;
     }
 
 }
