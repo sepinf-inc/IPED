49afe07aa88a907bba46040a6f1c0c98fc3ede18
hugohmk <hugohmkling@gmail.com>
2024-03-14 10:51:41 -0300

    Create removal method

--- iped-app/src/main/java/iped/app/ui/BookmarksManager.java
+++ iped-app/src/main/java/iped/app/ui/BookmarksManager.java
@@ -659,29 +659,30 @@ public class BookmarksManager implements ActionListener, ListSelectionListener, 
                 e.consume();
                 return;
             }
+            
+            int index = list.getSelectedIndex();
+            
             if (keystrokeToBookmark.containsKey(stroke)) {
-                showMessage(Messages.getString("BookmarksManager.KeyStrokeAlert3"));
+            	if (list.getModel().getElementAt(index).getKey() == stroke)
+            	{
+            		removeKeyStroke(list.getModel().getElementAt(index).getName());
+            		list.getModel().getElementAt(index).setKey(null);
+            		list.repaint();
+            	}
+            	else {
+            		showMessage(Messages.getString("BookmarksManager.KeyStrokeAlert3"));
+            	}
                 e.consume();
                 return;
             }
-            int index = list.getSelectedIndex();
+            
             list.getModel().getElementAt(index).setKey(stroke);
             list.repaint();
 
             String bookmarkStr = list.getModel().getElementAt(index).getName();
-            Iterator<KeyStroke> iterator = keystrokeToBookmark.keySet().iterator();
-            while (iterator.hasNext()) {
-                String bookmark = keystrokeToBookmark.get(iterator.next());
-                if (bookmark.equalsIgnoreCase(bookmarkStr)) {
-                    iterator.remove();
-                }
-            }
+            removeKeyStroke(bookmarkStr);
 
-            keystrokeToBookmark.put(stroke, bookmarkStr);
-            keystrokeToBookmark.put(getRemoveKey(stroke), bookmarkStr);
-
-            App.get().appCase.getMultiBookmarks().setBookmarkKeyStroke(bookmarkStr, stroke);
-            App.get().appCase.getMultiBookmarks().saveState();
+            setKeyStroke(stroke, bookmarkStr);
             e.consume();
 
         } else {
@@ -695,6 +696,26 @@ public class BookmarksManager implements ActionListener, ListSelectionListener, 
             e.consume();
         }
 
+    }
+    
+    private void setKeyStroke(KeyStroke stroke, String bookmarkStr) {
+    	keystrokeToBookmark.put(stroke, bookmarkStr);
+        keystrokeToBookmark.put(getRemoveKey(stroke), bookmarkStr);
+
+        App.get().appCase.getMultiBookmarks().setBookmarkKeyStroke(bookmarkStr, stroke);
+        App.get().appCase.getMultiBookmarks().saveState();
+    }
+    
+    private void removeKeyStroke(String bookmarkStr) {
+    	Iterator<KeyStroke> iterator = keystrokeToBookmark.keySet().iterator();
+        while (iterator.hasNext()) {
+            String bookmark = keystrokeToBookmark.get(iterator.next());
+            if (bookmark.equalsIgnoreCase(bookmarkStr)) {
+                iterator.remove();
+            }
+        }
+        App.get().appCase.getMultiBookmarks().removeBookmarkKeyStroke(bookmarkStr);
+        App.get().appCase.getMultiBookmarks().saveState();
     }
 
     // alt key remove from bookmark
