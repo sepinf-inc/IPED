5c2956c35cc504bf0b0428f9ade5af3f94e2d7f6
Luis Nassif <lfcnassif@gmail.com>
2024-03-20 11:09:14 -0300

    '#2034: fix and improve VLC ini parser test

--- iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/vlc/VLCIniParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/vlc/VLCIniParserTest.java
@@ -1,46 +1,35 @@
 package iped.parsers.vlc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.io.InputStream;
-import java.io.StringWriter;
 
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.sax.ToTextContentHandler;
-import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.ContentHandler;
 
-public class VLCIniParserTest {
+import junit.framework.TestCase;
 
-    private VLCIniParser parser;
-    private StringWriter writer;
-
-    @Before
-    public void setUp() {
-        parser = new VLCIniParser();
-        writer = new StringWriter();
-    }
+public class VLCIniParserTest extends TestCase {
 
     @Test
     public void testParse() throws Exception {
-        // Arrange
-        InputStream stream = getClass().getResourceAsStream("/test-files/test_vlcParser.ini");
-        assertNotNull("Input stream should not be null", stream);
 
-        // Act
-        ContentHandler handler = new ToTextContentHandler(writer);
+        VLCIniParser parser = new VLCIniParser();
+        ToTextContentHandler handler = new ToTextContentHandler();
         Metadata metadata = new Metadata();
         ParseContext context = new ParseContext();
-        parser.parse(stream, handler, metadata, context);
 
-        // Assert
-        String parsedContent = writer.toString().trim();
-        String[] lines = parsedContent.split("\n");
-        String actualTitle = lines[0].trim();
-        String expectedTitle = "Recent Medias:";
-        assertEquals(expectedTitle, actualTitle);
+        try (InputStream stream = getClass().getResourceAsStream("/test-files/test_vlcParser.ini");) {
+            assertNotNull("Input stream should not be null", stream);
+            parser.parse(stream, handler, metadata, context);
+            // Asserts
+            String parsedContent = handler.toString().trim();
+            String[] lines = parsedContent.lines().toArray(String[]::new);
+            assertEquals("C:\\Users\\teste\\Desktop\\Um churrasco diferenciado ｜ Shark Tank Brasil [YRMN8tm1zJM].webm [LastSkipPosition=494728ms]", lines[2].trim());
+            assertEquals("C:\\Users\\teste\\Desktop\\TESTEI INTELIGÊNCIA ARTIFICIAL DO S24 ULTRA ‹ EduKof › [x2TOevkEpsk].webm [LastSkipPosition=410790ms]", lines[3].trim());
+            assertEquals("C:\\Users\\teste\\Desktop\\App MyGoals ｜ React Native na Prática ｜ Aula 02 [Fdoq7I39xOI].mkv [LastSkipPosition=4461088ms]", lines[4].trim());
+            assertEquals("C:\\Users\\teste\\Desktop\\Cook app ｜ React Native na Prática ｜ Aula #01 [xmtDatG6SZk].mkv [LastSkipPosition=7588506ms]", lines[5].trim());
+            assertEquals("C:\\Users\\teste\\Desktop\\PILL： ＂Não existe estabilidade na área de tecnologia＂ [xA8g1mappsM].webm [LastSkipPosition=575083ms]", lines[6].trim());
+        }
     }
 }
