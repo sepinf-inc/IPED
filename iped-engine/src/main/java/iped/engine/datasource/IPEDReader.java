0a926279fd8193a3a18e2508b2d476be254fadc7
Luis Nassif <lfcnassif@gmail.com>
2024-03-22 19:47:30 -0300

    '#1632: revert wrong change

--- iped-engine/src/main/java/iped/engine/datasource/IPEDReader.java
+++ iped-engine/src/main/java/iped/engine/datasource/IPEDReader.java
@@ -250,7 +250,7 @@ public class IPEDReader extends DataSourceReader {
             return;
         }
 
-        IBookmarks reportState = new BitmapBookmarks(lastId - 1, lastId, output);
+        IBookmarks reportState = new BitmapBookmarks(totalItems, lastId, output);
 
         reportState.loadState();
 
