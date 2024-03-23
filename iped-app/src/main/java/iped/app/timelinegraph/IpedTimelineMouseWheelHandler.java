c33df08c7b2a6184d295ff86f25e911194a64eb7
Luis Nassif <lfcnassif@gmail.com>
2024-03-22 21:08:46 -0300

    '#1632: fix and adjust value to avoid multiple timechart zoom events

--- iped-app/src/main/java/iped/app/timelinegraph/IpedTimelineMouseWheelHandler.java
+++ iped-app/src/main/java/iped/app/timelinegraph/IpedTimelineMouseWheelHandler.java
@@ -65,7 +65,7 @@ public class IpedTimelineMouseWheelHandler implements MouseWheelListener, Serial
      */
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
-        if (lastWhen < e.getWhen() + 100) {// avoid calling the event more then once
+        if (lastWhen < e.getWhen() - 25) { // avoid calling the event more then once
             JFreeChart chart = this.chartPanel.getChart();
             if (chart == null) {
                 return;
