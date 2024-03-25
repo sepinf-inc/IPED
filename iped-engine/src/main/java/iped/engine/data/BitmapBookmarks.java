sepinf-inc/IPED

commit 295a5df3e6c22cb2298baa6a05cd99b663a8e38e

'#1632: remove possible bookmarks of deleted evidences left behind

iped-engine/src/main/java/iped/engine/data/BitmapBookmarks.java
@@ -459,8 +459,13 @@ public class BitmapBookmarks implements IBookmarks {
                 int len = Math.min(state.selected.length, this.selected.length);
                 System.arraycopy(state.selected, 0, this.selected, 0, len);
             }
-
             this.bookmarks = state.bookmarks;
+            for (RoaringBitmap bitmap : this.bookmarks.values()) {
+                if (bitmap.last() > lastId) {
+                    bitmap.remove((long) lastId + 1, (long) bitmap.last() + 1);
+                }
+            }
+
             this.typedWords = state.typedWords;
             this.selectedItens = state.selectedItens;
             this.bookmarkNames = state.bookmarkNames;
@@ -468,6 +473,7 @@ public class BitmapBookmarks implements IBookmarks {
             this.bookmarkKeyStrokes = state.bookmarkKeyStrokes;
             this.reportBookmarks = state.reportBookmarks;
             this.bookmarkColors = state.bookmarkColors;
+
         } catch (ClassCastException e) {
             // try to load old format
             Bookmarks state = loadOldBookmarks(file);
@@ -505,7 +511,7 @@ public class BitmapBookmarks implements IBookmarks {
         return (BitmapBookmarks) Util.readObject(file.getAbsolutePath());
     }
 
-    public static Bookmarks loadOldBookmarks(File file) throws ClassNotFoundException, IOException {
+    private static Bookmarks loadOldBookmarks(File file) throws ClassNotFoundException, IOException {
         LOGGER.info("Loading state from file " + file.getAbsolutePath()); //$NON-NLS-1$
         return (Bookmarks) Util.readObject(file.getAbsolutePath());
     }

commit fc8e929f08fa31e29780a0789d131485f03c8973

'#1632: fix NoSuchElementException with empty bookmark caused by 295a5df

iped-engine/src/main/java/iped/engine/data/BitmapBookmarks.java
@@ -465,7 +465,7 @@ public class BitmapBookmarks implements IBookmarks {
             }
             this.bookmarks = state.bookmarks;
             for (RoaringBitmap bitmap : this.bookmarks.values()) {
-                if (bitmap.last() > lastId) {
+                if (!bitmap.isEmpty() && bitmap.last() > lastId) {
                     bitmap.remove((long) lastId + 1, (long) bitmap.last() + 1);
                 }
             }