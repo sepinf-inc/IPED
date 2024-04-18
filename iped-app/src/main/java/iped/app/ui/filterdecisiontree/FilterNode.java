945ad1303469dc3723e2f9d6658b781500b7cb81
patrick.pdb <patrick.dalla@gmail.com>
2024-04-18 13:43:17 -0400

    '#39 refactors FilterNode classes in a way to keep track of its correspondent CombinedFilterTreeModel where it is created, so it can update the map of Filters with corresponding nodes set when removing or adding one of its children.

--- iped-app/src/main/java/iped/app/ui/filterdecisiontree/FilterNode.java
+++ iped-app/src/main/java/iped/app/ui/filterdecisiontree/FilterNode.java
@@ -1,11 +1,13 @@
 package iped.app.ui.filterdecisiontree;
 
+import iped.app.ui.CombinedFilterTreeModel;
 import iped.viewers.api.IFilter;
 
 public class FilterNode extends DecisionNode {
     IFilter filter;
 
-    public FilterNode(IFilter filter) {
+    public FilterNode(IFilter filter, CombinedFilterTreeModel model) {
+        super(model);
         this.filter = filter;
     }
 
