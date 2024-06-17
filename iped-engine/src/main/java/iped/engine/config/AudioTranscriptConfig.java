5bf47e4d78ac0a61775cec19a5ae1ad5a4471324
gfd2020 <59742865+gfd2020@users.noreply.github.com>
2024-06-17 15:18:51 -0300

    fix conflits

--- iped-engine/src/main/java/iped/engine/config/AudioTranscriptConfig.java
+++ iped-engine/src/main/java/iped/engine/config/AudioTranscriptConfig.java
@@ -27,18 +27,14 @@ public class AudioTranscriptConfig extends AbstractTaskPropertiesConfig {
     private static final String MAX_REQUESTS_KEY = "maxConcurrentRequests";
     private static final String MIN_WORD_SCORE = "minWordScore";
     public static final String HUGGING_FACE_MODEL = "huggingFaceModel";
+    public static final String WHISPER_MODEL = "whisperModel";
     public static final String WAV2VEC2_SERVICE = "wav2vec2Service";
+    public static final String REMOTE_SERVICE = "remoteServiceAddress";
     private static final String GOOGLE_MODEL = "googleModel";
     private static final String LANG_AUTO_VAL = "auto";
     private static final String SKIP_KNOWN_FILES = "skipKnownFiles";
     private static final String PRECISION = "precision";
-    private static final String BATCH_SIZE = "batchSize";    
-
-    private static final String REQUEUE_HEURISTICS = "clientDynamicThreadRequeueHeuristics";
-    private static final String CLIENTE_HELP = "clientTranscriptHelp";
-    private static final String IMPL_CLASS_KEY_CLIENT = "clientTranscriptHelpImplementationClass";
-    private static final String REQUEUE_RATIO = "clientSplitQueueRatio";
-    private static final String REQUEUE_DELTA_TIME = "clientRequeueDeltaTime";
+    private static final String BATCH_SIZE = "batchSize";
 
     private List<String> languages = new ArrayList<>();
     private List<String> mimesToProcess = new ArrayList<>();
@@ -51,17 +47,12 @@ public class AudioTranscriptConfig extends AbstractTaskPropertiesConfig {
     private int maxConcurrentRequests;
     private float minWordScore = 0.7f;
     private String huggingFaceModel;
-    private String wav2vec2Service;
+    private String whisperModel;
+    private String remoteService;
     private String googleModel;
     private boolean skipKnownFiles = true;
     private String precision = "int8";
-    private int batchSize = 1;    
-
-    private boolean requeueHeuristic = false;
-    private boolean clientTranscriptHelp = false;
-    private String classNameFallBack = "";
-    private int requeueRatio = 4;
-    private long requeueDeltaTime = 5000;
+    private int batchSize = 1;
 
     public String getPrecision() {
         return precision;
@@ -133,34 +124,17 @@ public class AudioTranscriptConfig extends AbstractTaskPropertiesConfig {
         return huggingFaceModel;
     }
 
-    public String getWav2vec2Service() {
-        return wav2vec2Service;
+    public String getWhisperModel() {
+        return whisperModel;
+    }
+
+    public String getRemoteService() {
+        return remoteService;
     }
 
     public String getGoogleModel() {
         return googleModel;
     }
-
-    public boolean getRequeueHeuristic() {
-        return requeueHeuristic;
-    }
-
-    public boolean getClientTranscriptHelp() {
-        return clientTranscriptHelp;
-    }    
-
-    public String getClassNameFallBack() {
-        return classNameFallBack;
-    }    
-
-    public int getRequeueRatio() {
-        return requeueRatio;
-    }
-
-    public long getRequeueDeltaTime() {
-        return requeueDeltaTime;
-    }    
-
 
     @Override
     public void processProperties(UTF8Properties properties) {
@@ -189,9 +163,17 @@ public class AudioTranscriptConfig extends AbstractTaskPropertiesConfig {
         if (huggingFaceModel != null) {
             huggingFaceModel = huggingFaceModel.trim();
         }
-        wav2vec2Service = properties.getProperty(WAV2VEC2_SERVICE);
-        if (wav2vec2Service != null) {
-            wav2vec2Service = wav2vec2Service.trim();
+        whisperModel = properties.getProperty(WHISPER_MODEL);
+        if (whisperModel != null) {
+            whisperModel = whisperModel.strip();
+        }
+
+        remoteService = properties.getProperty(REMOTE_SERVICE);
+        if (remoteService == null) {
+            remoteService = properties.getProperty(WAV2VEC2_SERVICE);
+        }
+        if (remoteService != null) {
+            remoteService = remoteService.trim();
         }
         googleModel = properties.getProperty(GOOGLE_MODEL);
         if (googleModel != null) {
@@ -210,38 +192,14 @@ public class AudioTranscriptConfig extends AbstractTaskPropertiesConfig {
         if (value != null) {
             timeoutPerSec = Integer.valueOf(value.trim());
         }
-
-        value = properties.getProperty(REQUEUE_HEURISTICS);
-        if (value != null) {
-            requeueHeuristic = Boolean.valueOf(value.trim());
-        }
-
-        value = properties.getProperty(CLIENTE_HELP);
-        if (value != null) {
-            clientTranscriptHelp = Boolean.valueOf(value.trim());
-        }
-
-        value = properties.getProperty(IMPL_CLASS_KEY_CLIENT);
-        if (value != null) {
-            classNameFallBack = value.trim();
-        }
-
-        value = properties.getProperty(REQUEUE_RATIO);
-        if (value != null) {
-            requeueRatio = Integer.valueOf(value.trim());
-        }
-
-        value = properties.getProperty(REQUEUE_DELTA_TIME);
-        if (value != null) {
-            requeueDeltaTime = Long.valueOf(value.trim());
-        }
         value = properties.getProperty(PRECISION);
         if (value != null) {
             precision = value.trim();
         }
         value = properties.getProperty(BATCH_SIZE);
         if (value != null) {
-            batchSize = Integer.parseInt(value.trim());        
+            batchSize = Integer.parseInt(value.trim());
+        }
     }
 
     /**
