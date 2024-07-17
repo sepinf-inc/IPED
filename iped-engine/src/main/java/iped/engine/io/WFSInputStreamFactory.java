1868b7d1b0c18064313f0d8ad22875bdafbbfe49
gfd2020 <59742865+gfd2020@users.noreply.github.com>
2024-07-17 16:14:59 -0300

    create a new input stream factory to handle WFS

---
+++ iped-engine/src/main/java/iped/engine/io/WFSInputStreamFactory.java
@@ -0,0 +1,34 @@
+package iped.engine.io;
+
+import iped.utils.SeekableInputStreamFactory;
+import iped.engine.task.dvr.wfs.WFSExtractor;
+import iped.utils.SeekableFileInputStream;
+import iped.io.SeekableInputStream;
+import java.io.IOException;
+
+public class WFSInputStreamFactory extends SeekableInputStreamFactory {
+
+    WFSExtractor wfs;
+    SeekableInputStream is;
+
+    public WFSInputStreamFactory(SeekableInputStream is) {
+
+        super(null);
+        this.is = is;
+    }
+
+    private synchronized void init() throws IOException {
+        if (wfs == null){
+            wfs = new WFSExtractor();
+            wfs.init(is);
+        }
+    }
+
+    @Override
+    public SeekableInputStream getSeekableInputStream(String identifier) throws IOException {
+        if (wfs == null)
+            init();
+        return wfs.getDescriptorFromIdentifier(identifier);
+    }
+
+}
