3f53dbc60330a675dc989eb62cae83d57a21ef04
gfd2020 <59742865+gfd2020@users.noreply.github.com>
2024-07-19 14:22:10 -0300

    add author info, add util methods

--- iped-engine/src/main/java/iped/engine/task/dvr/Util.java
+++ iped-engine/src/main/java/iped/engine/task/dvr/Util.java
@@ -1,12 +1,49 @@
 package iped.engine.task.dvr;
 
+import iped.io.SeekableInputStream;
+import java.io.IOException;
+
+/*
+
+ * WFSExtractor.
+ *
+ * @author guilherme.dutra
+
+*/
+
 public class Util {
+
+
+	public static long readLongFromBufLE(byte [] cbuf, int pos, int tam) {
+	
+		long r = 0L;
+		
+		for (int i = (pos-1); i > ((pos-1) - tam); i--){
+			r = (r << 8) + (cbuf[i] & 0xFF);
+		}
+		
+		return r;
+	
+	}
+
+    public static int readBytesFromAbsoluteFilePos(SeekableInputStream is,byte[] cbuf, long off, long len)  throws IOException  {
+				
+		int r = -1;
+		
+        if (is != null){
+		    is.seek(off);
+		    r = is.readNBytes(cbuf, 0, (int) len);
+        }
+	
+		return r;
+		
+	} 
 
     /**
      * Search the data byte array for the first occurrence 
      * of the byte array pattern.
      */
-    public static int indexOfKPM(byte[] data, byte[] pattern, int index) {
+    public static int indexOf(byte[] data, byte[] pattern, int index) {
         int[] failure = computeFailure(pattern);
 
         int j = 0;
