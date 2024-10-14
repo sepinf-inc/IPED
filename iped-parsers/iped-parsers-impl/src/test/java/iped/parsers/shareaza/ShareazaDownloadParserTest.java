c7487bcb6180e5a09ccdad3efa76079de9842008
marcus6n <marcusdeoliveiraneves@gmail.com>
2024-10-14 16:20:38 -0300

    Implement test case for ShareazaDownloadParser to validate parsing functionality

--- iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/shareaza/ShareazaDownloadParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/shareaza/ShareazaDownloadParserTest.java
@@ -18,4 +18,35 @@ public class ShareazaDownloadParserTest extends TestCase {
     private static InputStream getStream(String name) {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
     }
+
+
+    @Test
+    public void testShareazaDownloadParser() throws IOException, SAXException, TikaException {
+        ShareazaDownloadParser parser = new ShareazaDownloadParser();
+        Metadata metadata = new Metadata();
+        ContentHandler handler = new ToTextContentHandler();
+        ParseContext context = new ParseContext();
+        parser.getSupportedTypes(context);
+
+        try (InputStream stream = getStream("test-files/test_shareazaDownload.sd")) {
+            parser.parse(stream, handler, metadata, context);
+
+            String parsedText = handler.toString();
+            String[] downloadHashes = metadata.getValues("download-hashes");
+            String[] contentType = metadata.getValues(Metadata.CONTENT_TYPE);
+
+            assertTrue(parsedText.contains("expected-file.mp3"));
+            assertTrue(parsedText.contains("expected-image.png"));
+            assertEquals(ShareazaDownloadParser.SHAREAZA_DOWNLOAD_META, contentType[0]);
+            assertEquals(1, downloadHashes.length);
+            assertEquals("abcdef1234567890", downloadHashes[0]);
+
+            assertTrue(parsedText.contains("Komodor - Electrize.mp3"));
+            assertTrue(parsedText.contains("File Length:             5,613,696 Bytes (5.4 MB)"));
+            assertTrue(parsedText.contains("Number of Fragments: 1"));
+            assertTrue(parsedText.contains("SHA1:                    6FC0E7F66C3B8059B2C3B485710FA4B09BAA4F86"));
+            assertTrue(parsedText.contains("Source Address:                 http://189.60.225.131:6346/uri-res/N2R?urn:sha1:N7AOP5TMHOAFTMWDWSCXCD5EWCN2UT4G"));
+            assertTrue(parsedText.contains("Server Name:                    04 - Artist - Komodor - electrize.mp3"));
+        }
+    }
 }
