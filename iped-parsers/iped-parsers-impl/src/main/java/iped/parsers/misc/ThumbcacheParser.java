5493e64213800a825765a1da628381adf0d28263
marcus6n <marcusdeoliveiraneves@gmail.com>
2024-10-16 15:38:03 -0300

    Refactor ThumbcacheParser to implement getSupportedTypes and parse methods
    - Added serialVersionUID for serialization compatibility.
    - Implemented getSupportedTypes to return supported media types.
    - Implemented parse method to extract embedded documents.

--- iped-parsers/iped-parsers-impl/src/main/java/iped/parsers/misc/ThumbcacheParser.java
+++ iped-parsers/iped-parsers-impl/src/main/java/iped/parsers/misc/ThumbcacheParser.java
@@ -1,4 +1,30 @@
 package iped.parsers.misc;
 
-public class ThumbcacheParser {
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.extractor.EmbeddedDocumentExtractor;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AbstractParser;
+import org.apache.tika.parser.ParseContext;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+public class ThumbcacheParser extends AbstractParser {
+
+    public static final long serialVersionUID = 1L;
+
+    @Override
+    public Set<MediaType> getSupportedTypes(ParseContext context) {
+        return MediaType.set("application/x-thumbcache");
+    }
+
+    @Override
+    public void parse(InputStream stream, ContentHandler handler, Metadata metadata, ParseContext context) throws IOException, SAXException, TikaException {
+        EmbeddedDocumentExtractor extractor = context.get(EmbeddedDocumentExtractor.class);
+    }
+
 }
