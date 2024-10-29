f7cc003743b37875dc818bb3a043e744021f4891
patrick.pdb <patrick.dalla@gmail.com>
2024-10-29 12:16:53 -0400

    '#2351 Closes all parents of clicked object reference link to force back repaints after object reopen and scroll.

--- iped-parsers/iped-parsers-impl/src/main/resources/iped/parsers/css/uuidlink.js
+++ iped-parsers/iped-parsers-impl/src/main/resources/iped/parsers/css/uuidlink.js
@@ -1,21 +1,39 @@
 function clickUID(uid) {
     var obj = document.querySelector("details[treeuid='"+uid+"']");
     var a = obj;
+    var b=obj;
+    
     while (a) {
-        a.open = 'true';
+        if(a instanceof HTMLElement){
+            a.removeAttribute('open');
+        }
+        b = a;
         a = a.parentNode;
     }
-    obj.scrollIntoView();
-
-    if(!obj.highlight){
-        obj.highlight=true;
-        bcolor = obj.style.backgroundColor;
-        obj.style.backgroundColor="#AAAAAA";
-        setTimeout(function(){
-            obj.style.backgroundColor=bcolor;
-            obj.highlight=false;},1000);
-    }
-
+    
+    setTimeout(function(){
+        a = obj;
+        while (a) {
+            if(a instanceof HTMLElement){
+                a.open = 'true';
+            }
+            b = a;
+            a = a.parentNode;
+        }
+    
+        obj.scrollIntoView();
+        
+        if(!obj.highlight){
+            obj.highlight=true;
+            bcolor = obj.style.backgroundColor;
+            obj.style.backgroundColor="#AAAAAA";
+            setTimeout(function(){
+                obj.style.backgroundColor=bcolor;
+                obj.highlight=false;},1000);
+        }
+    
+    },1);
+    
 }
 
 window.containsNavigableTree=true;
