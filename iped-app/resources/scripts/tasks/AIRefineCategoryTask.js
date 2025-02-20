d9020ca43016a18eb7c9e1cf066df7b1f0dfce95
Hauck <joaoh14@gmail.com>
2025-02-20 16:48:02 -0300

    Adds a refinement task to label the images in categories;

---
+++ iped-app/resources/scripts/tasks/AIRefineCategoryTask.js
@@ -0,0 +1,50 @@
+﻿/*
+ * Script of Category Specialization based on item properties.
+ * Uses javascript language to allow flexibility in definitions.
+ */
+
+/* Name of processing task
+*/
+
+var categorizationThreshold=0.5;
+var RemoteImageClassifierConfig=Java.type("iped.engine.config.RemoteImageClassifierConfig")
+var Arrays=Java.type("java.util.Arrays")
+
+function getName(){
+	return "AIRefineCategoryTask";
+}
+
+
+function getConfigurables() {
+	
+	return Arrays.asList(new RemoteImageClassifierConfig());
+}
+
+function init(configurationManager) {
+	config=configurationManager.findObject(RemoteImageClassifierConfig.class)
+	categorizationThreshold=config.getCategorizationThreshold()	
+}
+
+function finish(){}
+
+/*
+ * Changes category of items based on their properties
+ *
+ */
+function process(e){
+
+	listcategories={
+		"ASI":"AI Label: Child Sexual Abuse",
+		"ASI_SUSP":"AI Label: Likely Child Sexual Abuse",
+		"Desenhos":"AI Label: Drawing",
+		"Outros":"AI Label: Other",
+		"Pessoas":"AI Label: People",
+		"Porn":"AI Label: Pornography",
+		}
+	for(cat in listcategories){
+		if (e.getExtraAttribute(cat)>0.5){
+			e.addCategory(listcategories[cat]);
+		}		
+	}
+}
+
--- iped-engine/src/main/java/iped/engine/config/RemoteImageClassifierConfig.java
+++ iped-engine/src/main/java/iped/engine/config/RemoteImageClassifierConfig.java
@@ -9,14 +9,17 @@ public class RemoteImageClassifierConfig extends AbstractTaskPropertiesConfig {
 
     private static final String URL_KEY = "url";
     private static final String BATCH_SIZE_KEY = "batch_size";
+    private static final String CATEGORIZATION_THRESHOLD = "categorization_threshold";
 
     private String url;
     private int batchSize;
+    private double categorizationThreshold = 0.5;
     @Override
     void processProperties(UTF8Properties properties) {
         // TODO Auto-generated method stub
         setUrl(properties.getProperty(URL_KEY).trim());
         setBatchSize(Integer.parseInt(properties.getProperty(BATCH_SIZE_KEY).trim()));
+        setCategorizationThreshold(Double.parseDouble(properties.getProperty(CATEGORIZATION_THRESHOLD).trim()));
 
     }
 
@@ -46,6 +49,14 @@ public class RemoteImageClassifierConfig extends AbstractTaskPropertiesConfig {
 
     public void setBatchSize(int batchSize) {
         this.batchSize = batchSize;
+    }
+
+    public double getCategorizationThreshold() {
+        return categorizationThreshold;
+    }
+
+    public void setCategorizationThreshold(double categorizationThreshold) {
+        this.categorizationThreshold = categorizationThreshold;
     }
 
 }
