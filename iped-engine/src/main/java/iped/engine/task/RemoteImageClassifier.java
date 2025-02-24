3197827bb7f50996562895b4aa27a32cf455a908
Hauck <joaoh14@gmail.com>
2025-02-24 20:40:02 -0300

    Extract each thumb of videos and get the highest score between them.

--- iped-engine/src/main/java/iped/engine/task/RemoteImageClassifier.java
+++ iped-engine/src/main/java/iped/engine/task/RemoteImageClassifier.java
@@ -1,10 +1,13 @@
 package iped.engine.task;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +15,8 @@ import java.util.TreeMap;
 import java.util.zip.CRC32;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import javax.imageio.ImageIO;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -32,15 +37,39 @@ import iped.configuration.Configurable;
 import iped.data.IItem;
 import iped.engine.config.ConfigurationManager;
 import iped.engine.config.RemoteImageClassifierConfig;
+import iped.engine.task.die.DIETask;
 import iped.engine.task.index.IndexItem;
+import iped.utils.ImageUtil;
 public class RemoteImageClassifier extends AbstractTask {
 
-    private static Logger logger = LoggerFactory.getLogger(MinIOTask.class);
+    private static Logger logger = LoggerFactory.getLogger(RemoteImageClassifier.class);
     private String url = "http://localhost:8000/zip";
     private int batch_size = 50;
     private Map<String, IItem> queue = new TreeMap<>();
     private zipfile zip = null;
     private RemoteImageClassifierConfig config;
+
+    private static class ResultItem {
+        public String name;
+        public TreeMap<String, ArrayList<Double>> classes = new TreeMap<>();
+
+        public void addClass(String classname,double val) {
+            if (!classes.containsKey(classname)) {
+                classes.put(classname, new ArrayList<>());
+            }
+            classes.get(classname).add(val);
+        }
+
+        public double getClassProb(String classname) {
+            double resp = 0;
+            for (Double val : classes.get(classname)) {
+                if (val > resp)
+                    resp = val;
+            }
+            return resp;
+
+        }
+    }
 
     private static class zipfile {
         TemporaryResources tmp;
@@ -148,6 +177,62 @@ public class RemoteImageClassifier extends AbstractTask {
 
     }
 
+    private void processResult(InputStream responseStream) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse = objectMapper.readTree(responseStream);
+        logger.debug("Server Response: {}", jsonResponse.toPrettyString());
+        JsonNode resultArray = jsonResponse.get("results");
+        Map<String,ResultItem> results=new TreeMap<>();
+
+        if (resultArray != null && resultArray.isArray()) {
+            for (JsonNode item : resultArray) {
+                String name = item.get("filename").asText();
+                ResultItem res=null;
+                if (name == null) {
+                    logger.warn("Warning: Invalid name ");
+                    continue;
+                }
+                if(name.split("_").length==2) {
+                    name=name.split("_")[1];
+                    res=results.get(name);
+                }
+                if (res == null) {
+                    res = new ResultItem();
+                    results.put(name, res);
+                }
+
+                JsonNode classes = item.get("class");
+                if (classes != null && classes.isObject()) {
+                    var iterator = classes.fields();
+                    while (iterator.hasNext()) {
+                        Map.Entry<String, JsonNode> entry = iterator.next();
+                        String key = entry.getKey();
+                        double value = entry.getValue().asDouble();
+                        res.addClass(key, value);
+                    }
+                } else {
+                    logger.warn("Warning: 'class' field is missing for filename: {}", name);
+                }
+            }
+
+            for (String name : results.keySet()) {
+                IItem evidence;
+                if (name == null || (evidence = queue.get(name)) == null) {
+                    logger.warn("Warning: No matching item found ");
+                    continue;
+                }
+                ResultItem res = results.get(name);
+                for (String classname : res.classes.keySet()) {
+                    evidence.setExtraAttribute(classname, res.getClassProb(classname));
+                }
+
+            }
+
+        } else {
+            logger.warn("Warning: 'results' array is missing in JSON response.");
+        }
+    }
+    
     private void sendZipFile(File zipFile) throws IOException {
         logger.info("Envia zip de tamanho {}", zip.size());
         try (CloseableHttpClient client = HttpClients.createDefault()) {
@@ -162,34 +247,7 @@ public class RemoteImageClassifier extends AbstractTask {
                 int statusCode = response.getStatusLine().getStatusCode();
                 if (statusCode == 200) {
                     try (InputStream responseStream = response.getEntity().getContent()) {
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        JsonNode jsonResponse = objectMapper.readTree(responseStream);
-                        logger.debug("Server Response: {}", jsonResponse.toPrettyString());
-                        JsonNode resultArray = jsonResponse.get("results");
-                        if (resultArray != null && resultArray.isArray()) {
-                            for (JsonNode item : resultArray) {
-                                String name = item.get("filename").asText();
-                                IItem evidence;
-                                if (name == null || (evidence = queue.get(name)) == null) {
-                                    logger.warn("Warning: No matching item found ");
-                                    continue;
-                                }
-
-                                JsonNode classes = item.get("class");
-                                if (classes != null && classes.isObject()) {
-                                    classes.fields().forEachRemaining(entry -> {
-                                        String key = entry.getKey();
-                                        double value = entry.getValue().asDouble();
-                                        evidence.setExtraAttribute(key, value);
-
-                                    });
-                                } else {
-                                    logger.warn("Warning: 'class' field is missing for filename: {}", name);
-                                }
-                            }
-                        } else {
-                            logger.warn("Warning: 'results' array is missing in JSON response.");
-                        }
+                        processResult(responseStream);
                     }
 
                 } else {
@@ -224,9 +282,32 @@ public class RemoteImageClassifier extends AbstractTask {
                 || evidence.getThumb().length < 10 || evidence.isQueueEnd()) {
             return;
         }
-        String name = evidence.getExtraAttribute(IndexItem.TRACK_ID).toString() + ".jpg";
-        zip.addFileToZip(name, evidence.getThumb());
-        queue.put(name, evidence);
+
+        if (DIETask.isVideoType(evidence.getMediaType()) || DIETask.isAnimationImage(evidence)) {
+
+            // For videos call the detection method for each extracted frame image
+            // (VideoThumbsTask must be enabled)
+            File viewFile = evidence.getViewFile();
+            if (viewFile != null && viewFile.exists()) {
+
+                List<BufferedImage> frames = ImageUtil.getFrames(viewFile);
+                int i = 0;
+                String name = evidence.getExtraAttribute(IndexItem.TRACK_ID).toString() + ".jpg";
+                for (BufferedImage frame : frames) {
+                    String name_i = (++i) + "_" + name;
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ImageIO.write(frame, "jpeg", baos);
+                    zip.addFileToZip(name_i, baos.toByteArray());
+                }
+                queue.put(name, evidence);
+
+            }
+        } else {
+            String name = evidence.getExtraAttribute(IndexItem.TRACK_ID).toString() + ".jpg";
+            zip.addFileToZip(name, evidence.getThumb());
+            queue.put(name, evidence);
+        }
+
     }
 
 }
