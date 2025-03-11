b78362d5d4e530d7e5383b2d97d92ceecdd36168
marcus6n <marcusdeoliveiraneves@gmail.com>
2025-03-11 17:44:47 -0300

    Add unit test for ThumbcacheParser

---
+++ iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/misc/ThumbcacheParserTest.java
@@ -0,0 +1,34 @@
+package iped.parsers.misc;
+
+import junit.framework.TestCase;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.junit.Test;
+import org.xml.sax.ContentHandler;
+
+import java.io.InputStream;
+
+public class ThumbcacheParserTest extends TestCase {
+
+
+    private static InputStream getStream(String name) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+    }
+
+    @Test
+    public void testParse() throws Exception {
+        ThumbcacheParser parser = new ThumbcacheParser();
+        Metadata metadata = new Metadata();
+        ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
+        parser.getSupportedTypes(context);
+        try (InputStream stream = getStream("test-files/test_mockDoc1.doc")) {
+            parser.parse(stream, handler, metadata, context);
+            String hts = handler.toString();
+
+
+
+        }
+    }
+}
