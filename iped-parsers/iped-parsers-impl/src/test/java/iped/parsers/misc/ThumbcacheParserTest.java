0a883abd10ffcc3699891f4c37608e1dbc9ed510
marcus6n <marcusdeoliveiraneves@gmail.com>
2025-03-12 15:15:57 -0300

    Commit message: Add assertions to ThumbcacheParserTest for detailed validation
    - Add detailed assertions to validate the parsed content of the Thumbcache database
    - Include checks for cache file format version, cache type, and entry offsets
    - Validate the number of cache entries and the Windows version
    - Add assertions for entry hash, size, identifier string, data size, and checksums
    - Ensure comprehensive testing of ThumbcacheParser output for accuracy

--- iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/misc/ThumbcacheParserTest.java
+++ iped-parsers/iped-parsers-impl/src/test/java/iped/parsers/misc/ThumbcacheParserTest.java
@@ -1,5 +1,7 @@
 package iped.parsers.misc;
 
+import iped.parsers.shareaza.ShareazaLibraryDatParser;
+import iped.properties.ExtraProperties;
 import junit.framework.TestCase;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
@@ -27,7 +29,26 @@ public class ThumbcacheParserTest extends TestCase {
             parser.parse(stream, handler, metadata, context);
             String hts = handler.toString();
 
+            assertTrue(hts.contains("Cache file format version      : 32"));
+            assertTrue(hts.contains("Cache type                    : 4"));
+            assertTrue(hts.contains("Offset to first cache entry    : 0"));
+            assertTrue(hts.contains("Offset to first available entry: 24"));
+            assertTrue(hts.contains("Number of cache entries        : 41667132"));
+            assertTrue(hts.contains("Seen on Windows version        : Windows 10 / 11"));
 
+            assertTrue(hts.contains("Entry hash                    : 0xb9dcf0e3491bfa53"));
+            assertTrue(hts.contains("Entry size                    : 5936"));
+            assertTrue(hts.contains("Identifier string             : b9dcf0e3491bfa53"));
+            assertTrue(hts.contains("Data size                     : 5844"));
+            assertTrue(hts.contains("Data checksum                 : 0x8bac679ed0019198"));
+            assertTrue(hts.contains("Header checksum               : 0xc67ba0268d038d63"));
+
+            assertTrue(hts.contains("Entry hash                    : 0x232a7b7832fef0db"));
+            assertTrue(hts.contains("Entry size                    : 17250"));
+            assertTrue(hts.contains("Identifier string             : 232a7b7832fef0db"));
+            assertTrue(hts.contains("Data size                     : 17157"));
+            assertTrue(hts.contains("Data checksum                 : 0x5f424a9955ee4e51"));
+            assertTrue(hts.contains("Header checksum               : 0x3e99d8bdd31ac6b2"));
 
         }
     }
