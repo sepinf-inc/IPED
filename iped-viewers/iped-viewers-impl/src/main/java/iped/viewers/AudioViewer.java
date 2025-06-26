d236bf62141b13a4235953c8a21fa27f7ba9cf49
kraftdenker <alberto.magno@gmail.com>
2025-06-26 09:41:35 -0300

    Sanitizing code.

--- iped-viewers/iped-viewers-impl/src/main/java/iped/viewers/AudioViewer.java
+++ iped-viewers/iped-viewers-impl/src/main/java/iped/viewers/AudioViewer.java
@@ -7,12 +7,10 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Map;
@@ -27,11 +25,9 @@ import javax.swing.JTextArea;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ag.ion.bion.officelayer.application.IOfficeApplication;
 import iped.io.IStreamSource;
 import iped.io.URLUtil;
 import iped.viewers.api.AbstractViewer;
-import uk.co.caprica.vlcj.binding.support.runtime.RuntimeUtil;
 import uk.co.caprica.vlcj.player.base.MediaApi;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
@@ -43,41 +39,12 @@ public class AudioViewer extends AbstractViewer {
 	private MediaApi player;
 	private EmbeddedMediaPlayer mediaPlayerComponent;
 	private JTextArea audioDescription = new JTextArea();
-	// private ProgressBar progressBar = new ProgressBar(0d);
 	private Button play = new Button(">");
 	private JButton playButton;
 	private JButton pauseButton;
 	private JButton rewindButton;
 	private volatile File temp;
-    public static void criarVlcjConfig(String pathVlc) {
-        String appData = System.getenv("APPDATA");
-        if (appData == null) {
-            System.err.println("Erro: variável de ambiente APPDATA não encontrada.");
-            return;
-        }
-
-        File vlcjDir = new File(appData, "vlcj");
-        if (!vlcjDir.exists()) {
-            boolean criado = vlcjDir.mkdirs();
-            if (!criado) {
-                System.err.println("Erro ao criar diretório: " + vlcjDir.getAbsolutePath());
-                return;
-            }
-        }
-
-        File configFile = new File(vlcjDir, "vlcj.config");
-        if (configFile.exists()) {
-            System.out.println("Arquivo vlcj.config já existe em: " + configFile.getAbsolutePath());
-            return;
-        }
-
-        try (FileWriter writer = new FileWriter(configFile)) {
-            writer.write("nativeDirectory=" + pathVlc + System.lineSeparator());
-            System.out.println("Arquivo vlcj.config criado com sucesso em: " + configFile.getAbsolutePath());
-        } catch (IOException e) {
-            System.err.println("Erro ao criar arquivo vlcj.config: " + e.getMessage());
-        }
-    }
+    
 	public AudioViewer()  {
 		super(new GridLayout());
         if (System.getProperty("os.name").startsWith("Windows")) {
@@ -88,29 +55,10 @@ public class AudioViewer extends AbstractViewer {
 			System.setProperty("VLC_PLUGIN_PATH",
                 new File(nativelib, "\\vlc").getAbsolutePath());
 			
-				//File pluginsvlc = new File(nativelib, "nativelibs/vlc/plugins");
-			//setEnv("VLC_PLUGIN_PATH", pluginsvlc.getAbsolutePath());
 			File vlc = new File(System.getProperty("VLC_PLUGIN_PATH"), "\\win32-x86-64");
-			criarVlcjConfig(vlc.getAbsolutePath());
+			//criarVlcjConfig(vlc.getAbsolutePath());
 			System.setProperty("jna.library.path", vlc.getAbsolutePath());
 			
-			//File libvlc = new File(nativelib, "nativelibs/vlc/libvlc.dll");
-			//System.load(libvlc.getAbsolutePath());
-
-			/*System.setProperty("java.library.path", System.getProperty("java.library.path") + ";" + vlc.getAbsolutePath());
-			Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
-        	fieldSysPath.setAccessible(true);
-        	fieldSysPath.set(null, null);*/
-			//File libvlc = new File(nativelib, "nativelibs/vlc/libvlc.dll");
-            //File libvlccore = new File(nativelib, "nativelibs/vlc/libvlccore.dll");
-            //File axvlc = new File(nativelib, "nativelibs/vlc/axvlc.dll");
-            //File npvlc = new File(nativelib, "nativelibs/vlc/npvlc.dll");
-			//File pluginsvlc = new File(nativelib, "nativelibs/vlc/plugins");
-			//System.load(pluginsvlc.getAbsolutePath());
-            //System.load(npvlc.getAbsolutePath());
-            //System.load(axvlc.getAbsolutePath());
-            //System.load(libvlccore.getAbsolutePath());
-            //System.load(libvlc.getAbsolutePath());
 			} catch (Exception e) {
 				LOGGER.error("Error loading VLC native libraries", e);
 			}
@@ -151,7 +99,6 @@ public class AudioViewer extends AbstractViewer {
 		play.setLabel(">");
 		play.requestFocus();
 		audioDescription.setText("");
-		// progressBar.setProgress(0d);
 
 		// create temp file here to not block EDT
 		if (content != null) {
@@ -165,19 +112,6 @@ public class AudioViewer extends AbstractViewer {
 				if (ok) player = mediaPlayerComponent.media();
 				else
 					LOGGER.info("Error open file");
-				/*
-				 * HeadlessMediaPlayer mediaPlayer =
-				 * mediaPlayerFactory.newHeadlessMediaPlayer();
-				 * mediaPlayer.playMedia("path/to/your/file.ogg"); mediaPlayer.
-				 */
-
-				/*
-				 * Media mediaFile = new Media(temp.toURI().toString()); player = new
-				 * MediaPlayer(mediaFile); player.stop();
-				 * player.currentTimeProperty().addListener((observable, oldValue, newValue) ->
-				 * { double progress = newValue.toMillis() /
-				 * player.getTotalDuration().toMillis(); progressBar.setProgress(progress); });
-				 */
 				iped.data.IItem item = (iped.data.IItem) content;
 
 				for (String mdName : item.getMetadata().names()) {
@@ -251,11 +185,6 @@ public class AudioViewer extends AbstractViewer {
 				mediaPlayerComponent.controls().skipTime(-14000);
 			}
 		});
-		/*skipButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				mediaPlayerComponent.mediaPlayer().controls().skipTime(4000);
-			}
-		});*/
 		audioDescription = new JTextArea(3, 1);
 		audioDescription.setLineWrap(true);
 		audioDescription.setWrapStyleWord(true);
@@ -264,7 +193,6 @@ public class AudioViewer extends AbstractViewer {
 
 		audioDescription.setEditable(false);
 		this.getPanel().add(scrollPane);
-		// mediaPlayerFactory = new MediaPlayerFactory();
 	}
 
 	@Override
